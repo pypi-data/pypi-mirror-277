4b44c8cdb382acbb4e71ab93305830c69de678c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:54 +0000

    Add package torch4keras/0.2.3/torch4keras-0.2.3.tar.gz

---
+++ packages/torch4keras/torch4keras-0.2.3.tar.gz/torch4keras-0.2.3/torch4keras/model.py
@@ -0,0 +1,25 @@
+from torch import nn
+from torch4keras.trainer import *
+
+
+class BaseModel(Trainer, nn.Module):
+    '''BaseModel, 使用继承的方式来使用
+    
+    Examples:
+    ```python
+    >>> class MyModel(BaseModel):
+    ...     def __init__(self, module):
+    ...         super().__init__()
+    ...         self.model = module
+    ...     def forward(self, inputs):
+    ...         return self.model(inputs)
+
+    >>> model = MyModel(net)  # 实例化模型对象
+    ```
+    '''
+    def __init__(self, *args, **kwargs):
+        nn.Module.__init__(self)
+        Trainer.__init__(self, *args, **kwargs)
+        
+BaseModelDP = TrainerDP  # 使用方式和nn.DataParallel一致
+BaseModelDDP = TrainerDDP  # 使用方式和DistributedDataParallel一致
