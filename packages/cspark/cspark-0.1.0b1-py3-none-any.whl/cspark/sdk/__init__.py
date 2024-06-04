eb010ab5293ee0a39498fab859ba47071b781207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 06:04:29 +0000

    Add package cspark/0.1.0b1/cspark-0.1.0b1-py3-none-any.whl

---
+++ packages/cspark/cspark-0.1.0b1-py3-none-any.whl/cspark/sdk/__init__.py
@@ -0,0 +1,6 @@
+from .auth import *
+from .client import *
+from .config import *
+from .constants import *
+from .errors import *
+from .version import *
---
+++ packages/cspark/cspark-0.1.0b1-py3-none-any.whl/cspark/sdk/utils.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from typing import Any, cast
+
+
+def is_str(value: Any | None) -> bool:
+    return isinstance(value, str)
+
+
+def is_str_empty(value: Any | None) -> bool:
+    return value is None or (isinstance(value, str) and value.strip() == '')
+
+
+def is_str_not_empty(value: Any | None) -> bool:
+    return isinstance(value, str) and value.strip() != ''
+
+
+def is_int(value: Any | None) -> bool:
+    if isinstance(value, int):
+        return True
+    try:
+        int(str(value))
+        return True
+    except ValueError:
+        return False
+
+
+def is_positive_int(value: Any | None) -> bool:
+    return is_int(value) and cast(int, value) > 0
+
+
+def mask(value: str, start: int = 0, end: int = 4, char: str = '*') -> str:
+    if not value or start < 0 or end < 0:
+        return value
+    return value[:start] + char * (len(value) - start - end) + value[-end:]
