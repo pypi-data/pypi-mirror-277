a24c2f2c77874a3cd6d59b570fe8677ad0adf078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 06:04:50 +0000

    Add package swarmauri/0.3.1/swarmauri-0.3.1-py3-none-any.whl

---
+++ packages/swarmauri/swarmauri-0.3.1-py3-none-any.whl/swarmauri/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.3.1"
+__long_desc__ = """
+# swarmaURI sdk
+
+This repository includes core interfaces, standard ABCs, and standard concrete references of the SwarmaURI Framework.
+
+
+## Core 
+- ABCs
+- Utilities
+
+## Standard
+- Concrete Classes
+
+## Community
+- Concrete Classes that utilize third party plug-ins
+
+## Experimental
+- Tools in development
+
+
+"""
