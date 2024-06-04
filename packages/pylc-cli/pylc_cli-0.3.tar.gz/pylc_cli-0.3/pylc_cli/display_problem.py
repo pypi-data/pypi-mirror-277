ef01afcb30a88a5632aafa24aecdf3dda765ec63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 03:03:41 +0000

    Add package pylc-cli/0.3/pylc_cli-0.3.tar.gz

---
+++ packages/pylc-cli/pylc_cli-0.3.tar.gz/pylc_cli-0.3/pylc_cli/display_problem.py
@@ -0,0 +1,46 @@
+import html2text
+from rich.markdown import Markdown
+from rich.padding import Padding
+from . import console, con
+
+DIFF_COLOR = {"Easy": "green", "Medium": "yellow", "Hard": "red"}
+
+
+def display_problem(id: int):
+    def replaceSup(r: str) -> str:
+        r = r.replace("<sup>", "^")
+        r = r.replace("</sup>", "")
+        return r
+
+    res = con.execute(
+        "SELECT md.title, md.difficulty, c.content "
+        "FROM "
+        "metadata AS md "
+        "JOIN "
+        "content AS c "
+        "ON "
+        "md.frontend_id = c.frontend_id "
+        "WHERE "
+        f"md.frontend_id = {id}"
+    )
+    data = res.fetchone()
+    diff = data["difficulty"]
+    title = data["title"]
+    content = data["content"]
+    color = DIFF_COLOR[diff]
+
+    res = con.execute(f"SELECT tags FROM tags WHERE frontend_id = {id}")
+    data = res.fetchall()
+    tags = ", ".join(d["tags"] for d in data)
+
+    console.print(Padding(f"[b][[{color}]{id}[/{color}]] [u]{title}[/u][/b]", (1, 2)))
+
+    html = "\n".join(map(str, content.split("\n")))
+    html = replaceSup(html)
+    h = html2text.HTML2Text()
+    h.ignore_images = True
+    h.ignore_emphasis = True
+    md = h.handle(html)
+    console.print(Padding(Markdown(markup=md), (0, 2)))
+
+    console.print(Padding(f"Topics: {tags}", (1, 2)))
