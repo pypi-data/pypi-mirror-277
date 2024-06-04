a1d77ee4515a0411086471993084c7784e302228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:03 +0000

    Add package cdps/1.0.18/cdps-1.0.18-py3-none-any.whl

---
+++ packages/cdps/cdps-1.0.18-py3-none-any.whl/cdps/constants/core_constant.py
@@ -0,0 +1,24 @@
+"""
+Core contants
+"""
+
+NAME = 'CDPS'
+PACKAGE_NAME = 'cdps'
+VERSION = '1.0.18'
+
+CLI_COMMAND = PACKAGE_NAME
+
+INFO = {
+    "version": VERSION,
+    "description": {
+        "zh_tw": "CDPS"
+    },
+    "author": [
+        "YuYu1015"
+    ],
+    "dependencies": {},
+    "resources": [
+        "AGPL-3.0"
+    ],
+    "link": "https://github.com/ExpTechTW/CDPS"
+}
