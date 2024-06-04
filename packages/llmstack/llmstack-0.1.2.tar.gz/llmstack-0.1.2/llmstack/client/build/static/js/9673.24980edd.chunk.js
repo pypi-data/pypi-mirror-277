e3cce22dda34dbc9ededc9331b2c7abf8173a85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:55 +0000

    Add package llmstack/0.1.2/llmstack-0.1.2.tar.gz

---
+++ packages/llmstack/llmstack-0.1.2.tar.gz/llmstack-0.1.2/llmstack/client/build/static/js/9673.24980edd.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpromptmanager=self.webpackChunkpromptmanager||[]).push([[9673],{40802:function(a,e,p){p(7276);var s=p(23015),n=p(45263);e.Z=(0,s.Z)((0,n.jsx)("path",{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}),"Close")}}]);
