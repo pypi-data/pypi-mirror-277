9b7439b1fa8956dad6d161fdd9d7cfad49b79d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:54 +0000

    Add package cosmic-crisp/0.1.0.dev20240603145849/cosmic_crisp-0.1.0.dev20240603145849.tar.gz

---
+++ packages/cosmic-crisp/cosmic_crisp-0.1.0.dev20240603145849.tar.gz/cosmic_crisp-0.1.0.dev20240603145849/pinecone_plugins/knowledge/models/__init__.py
@@ -0,0 +1,10 @@
+from .knowledge_model import KnowledgeModel
+from .chat import Message, ChatResultModel
+from .file_model import FileModel
+
+__all__ = [
+    'KnowledgeModel',
+    'FileModel',
+    'Message', 
+    'ChatResultModel'
+]
