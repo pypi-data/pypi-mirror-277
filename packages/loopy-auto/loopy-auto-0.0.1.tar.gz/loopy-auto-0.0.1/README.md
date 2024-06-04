4aad5efa609b982efd435d70660dcfcb33d4e3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:47 +0000

    Add package loopy-auto/0.0.1/loopy-auto-0.0.1.tar.gz

---
+++ packages/loopy-auto/loopy-auto-0.0.1.tar.gz/loopy-auto-0.0.1/README.md
@@ -0,0 +1,2 @@
+# loopy
+Loopy is an automation framework in which anybody can create a test script simply with your favorite language.
---
+++ packages/loopy-auto/loopy-auto-0.0.1.tar.gz/loopy-auto-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='loopy-auto',
+    version='0.0.1',
+    packages=find_packages(),
+    description='Loopy-auto is an automation framework in which anybody can create a test script simply with your favorite language.',
+    author='Jooho Lee',
+    author_email='ljhiyh@gmail.com',
+)
