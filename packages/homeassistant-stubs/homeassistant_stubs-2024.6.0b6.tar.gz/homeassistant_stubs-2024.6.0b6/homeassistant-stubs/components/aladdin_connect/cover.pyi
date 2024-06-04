23e67fe3bb39d408248a0663c7b9cf393e077b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:37 +0000

    Add package homeassistant-stubs/2024.6.0b6/homeassistant_stubs-2024.6.0b6.tar.gz

---
+++ packages/homeassistant-stubs/homeassistant_stubs-2024.6.0b6.tar.gz/homeassistant_stubs-2024.6.0b6/homeassistant-stubs/components/aladdin_connect/cover.pyi
@@ -0,0 +1,38 @@
+from . import api as api
+from .const import DOMAIN as DOMAIN, SUPPORTED_FEATURES as SUPPORTED_FEATURES
+from .model import GarageDoor as GarageDoor
+from _typeshed import Incomplete
+from genie_partner_sdk.client import AladdinConnectClient
+from homeassistant.components.cover import CoverDeviceClass as CoverDeviceClass, CoverEntity as CoverEntity
+from homeassistant.config_entries import ConfigEntry as ConfigEntry
+from homeassistant.core import HomeAssistant as HomeAssistant
+from homeassistant.exceptions import PlatformNotReady as PlatformNotReady
+from homeassistant.helpers.device_registry import DeviceInfo as DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback as AddEntitiesCallback
+from typing import Any
+
+SCAN_INTERVAL: Incomplete
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None: ...
+def remove_stale_devices(hass: HomeAssistant, config_entry: ConfigEntry, devices: list[GarageDoor]) -> None: ...
+
+class AladdinDevice(CoverEntity):
+    _attr_device_class: Incomplete
+    _attr_supported_features = SUPPORTED_FEATURES
+    _attr_has_entity_name: bool
+    _attr_name: Incomplete
+    _acc: Incomplete
+    _device_id: Incomplete
+    _number: Incomplete
+    _attr_device_info: Incomplete
+    _attr_unique_id: Incomplete
+    def __init__(self, acc: AladdinConnectClient, device: GarageDoor, entry: ConfigEntry) -> None: ...
+    async def async_open_cover(self, **kwargs: Any) -> None: ...
+    async def async_close_cover(self, **kwargs: Any) -> None: ...
+    async def async_update(self) -> None: ...
+    @property
+    def is_closed(self) -> bool | None: ...
+    @property
+    def is_closing(self) -> bool | None: ...
+    @property
+    def is_opening(self) -> bool | None: ...
