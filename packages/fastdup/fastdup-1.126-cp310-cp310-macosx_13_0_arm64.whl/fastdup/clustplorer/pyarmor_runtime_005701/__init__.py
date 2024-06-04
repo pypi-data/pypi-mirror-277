26472fd2b312e49a5b4aa2020ce7c529b47a080c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:49 +0000

    Add package fastdup/1.126/fastdup-1.126-cp310-cp310-macosx_13_0_arm64.whl

---
+++ packages/fastdup/fastdup-1.126-cp310-cp310-macosx_13_0_arm64.whl/fastdup/clustplorer/pyarmor_runtime_005701/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.7 (pro), 005701, 2024-06-03T14:58:10.175824
+from .pyarmor_runtime import __pyarmor__
---
+++ packages/fastdup/fastdup-1.126-cp310-cp310-macosx_13_0_arm64.whl/fastdup/vl/utils/fs_utils.py
@@ -0,0 +1,26 @@
+import logging
+from pathlib import Path
+
+
+def is_gz_file(filepath):
+    with open(filepath, 'rb') as test_f:
+        return test_f.read(2) == b'\x1f\x8b'
+
+
+def move_sub_dirs(src: Path, dst: Path):
+    """
+    Move all subdirectories of a source directory into the target directory
+    """
+    if not src.is_dir():
+        logging.warning(f'Source does not exist or is not a directory: {src}')
+        return
+
+    if not dst.is_dir():
+        logging.warning(f'Source does not exist or is not a directory: {src}')
+        return
+
+    subdir: Path
+    for partition in src.iterdir():
+        if partition.is_dir():
+            partition.rename(dst / partition.name)
+
