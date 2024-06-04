457062f481076a222800fcac6e2c82c002474cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 07:03:47 +0000

    Add package opentelemetry-instrumentation-haystack/0.21.4/opentelemetry_instrumentation_haystack-0.21.4-py3-none-any.whl

---
+++ packages/opentelemetry-instrumentation-haystack/opentelemetry_instrumentation_haystack-0.21.4-py3-none-any.whl/opentelemetry/instrumentation/haystack/utils.py
@@ -0,0 +1,48 @@
+import logging
+from opentelemetry.instrumentation.haystack.config import Config
+
+
+def set_span_attribute(span, name, value):
+    if value is not None:
+        if value != "":
+            span.set_attribute(name, value)
+    return
+
+
+def with_tracer_wrapper(func):
+    """Helper for providing tracer for wrapper functions."""
+
+    def _with_tracer(tracer, to_wrap):
+        def wrapper(wrapped, instance, args, kwargs):
+            # prevent double wrapping
+            if hasattr(wrapped, "__wrapped__"):
+                return wrapped(*args, **kwargs)
+
+            return func(tracer, to_wrap, wrapped, instance, args, kwargs)
+
+        return wrapper
+
+    return _with_tracer
+
+
+def dont_throw(func):
+    """
+    A decorator that wraps the passed in function and logs exceptions instead of throwing them.
+
+    @param func: The function to wrap
+    @return: The wrapper function
+    """
+    # Obtain a logger specific to the function's module
+    logger = logging.getLogger(func.__module__)
+
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logger.debug(
+                "OpenLLMetry failed to trace in %s, error: %s", func.__name__, str(e)
+            )
+            if Config.exception_logger:
+                Config.exception_logger(e)
+
+    return wrapper
