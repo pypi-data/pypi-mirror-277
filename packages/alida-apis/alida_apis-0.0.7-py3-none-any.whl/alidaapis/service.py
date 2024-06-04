047a93717a8274466712e801758e05d4123bf554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:23 +0000

    Add package alida-apis/0.0.7/alida_apis-0.0.7-py3-none-any.whl

---
+++ packages/alida-apis/alida_apis-0.0.7-py3-none-any.whl/alidaapis/service.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from .auth import _get_token
+from .utils import read_var
+
+
+def get(order = "id", desc="true", limit=20, page=1):
+    url = read_var("url_base") + read_var("url_services") + "?sort=" + order + "&desc=" + desc + "&limit=" + str(limit) + "&page=" + str(page)
+
+    payload = {}
+    headers = {
+        'authorization': 'Bearer ' + _get_token()
+    }
+    return json.loads(requests.request("GET", url, headers=headers, data=payload).text)
+
+def get_services_by_name(name):
+    result = []
+    services = json.loads(get().text)['collection']
+    
+    for service in services:
+        if service['name'] == name:
+            result.append(service)
+    return result
+
+def get_by_id(id):
+    url = read_var("URL_ADD_SERVICES") + "/" + str(id)
+    payload = {}
+    headers = {
+        'authorization': 'Bearer ' + _get_token()
+    }
+    return json.loads(requests.request("GET", url, headers=headers, data=payload).text)
+
+def add(service_metamodel):
+    
+    url = read_var("url_add_services")
+
+    payload = service_metamodel
+    headers = {
+        'authorization': 'Bearer ' + _get_token(),
+        'content-type': 'application/json'
+    }
+    return requests.request("POST", url, headers=headers, data=json.dumps(payload))
+
