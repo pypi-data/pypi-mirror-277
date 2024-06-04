40a270cb4cd54b60aebea15edbb47a06b5b73d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 07:03:42 +0000

    Add package dpugen/0.2.1/dpugen-0.2.1-py3-none-any.whl

---
+++ packages/dpugen/dpugen-0.2.1-py3-none-any.whl/dashgen/dash_vnet_table.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+from dpugen.confbase import ConfBase
+from dpugen.confutils import common_main
+
+
+class Vnets(ConfBase):
+
+    def __init__(self, params={}):
+        super().__init__(params)
+        self.num_yields = 0
+
+    def items(self):
+        print('  Generating %s ...' % os.path.basename(__file__), file=sys.stderr)
+        p = self.params
+
+        for eni_index, eni in enumerate(range(p.ENI_START, p.ENI_START + p.ENI_COUNT * p.ENI_STEP, p.ENI_STEP)):
+            r_vni_id = p.ENI_L2R_STEP + eni
+            self.num_yields += 1
+            yield {
+                'DASH_VNET_TABLE:vnet-%d' % r_vni_id: {
+                    'vni': r_vni_id,
+                },
+                'OP': 'SET'
+            }
+
+
+if __name__ == '__main__':
+    conf = Vnets()
+    common_main(conf)
