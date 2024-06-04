a64676d345145fa16ebca167dd2d0c7886d0781a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:14 +0000

    Add package aikicore/0.2.0a6/aikicore-0.2.0a6.tar.gz

---
+++ packages/aikicore/aikicore-0.2.0a6.tar.gz/aikicore-0.2.0a6/aikicore/data/__init__.py
@@ -0,0 +1,3 @@
+from typing import List, Dict
+
+from .feature import *
---
+++ packages/aikicore/aikicore-0.2.0a6.tar.gz/aikicore-0.2.0a6/aikicore/repositories/error_cache/__init__.py
@@ -0,0 +1,6 @@
+from ...objects import *
+
+class ErrorCache():
+
+    def get(self, error_name: str, lang: str = 'en_US', error_type: type = Error) -> Error:
+        pass
