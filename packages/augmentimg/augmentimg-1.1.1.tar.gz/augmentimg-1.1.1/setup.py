cd83ac7cd9db1d353cb21bc9499fa2c259328fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 14:04:03 +0000

    Add package augmentimg/1.1.1/augmentimg-1.1.1.tar.gz

---
+++ packages/augmentimg/augmentimg-1.1.1.tar.gz/augmentimg-1.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+ 
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    description = f.read()
+
+
+setup(
+    name="augmentimg",
+    version="1.1.1",
+    packages=find_packages(),
+    install_requires = [
+        "torch==2.2.1",
+        "albumentations==1.3.1",
+        "opencv-python==4.9.0.80",
+        "torchvision==0.17.1",
+        "PyQt5",
+        
+    ],
+    entry_points = {
+        "console_scripts": [
+            "augment-img = augmentimg.main:main",
+        ],
+    },
+    long_description=description,
+    long_description_content_type = "text/markdown",
+
+)
