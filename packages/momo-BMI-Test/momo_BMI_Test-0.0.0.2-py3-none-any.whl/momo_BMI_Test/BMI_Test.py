8e2a0def5e5299f8645dd0515e7ad0380790dc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:30 +0000

    Add package momo-BMI-Test/0.0.0.2/momo_BMI_Test-0.0.0.2-py3-none-any.whl

---
+++ packages/momo-BMI-Test/momo_BMI_Test-0.0.0.2-py3-none-any.whl/momo_BMI_Test/BMI_Test.py
@@ -0,0 +1,23 @@
+def BMI (height, weight) :
+    height_m = height * 0.01
+    height_double = height_m * height_m
+    bmi = weight / height_double
+    round_bmi = round(bmi, 1)
+    if bmi <= 18.5 :
+        print(f"MY BMI: {round_bmi}".center(40))
+        print(f"나의 BMI(신체질량지수)는{bmi} 이고, 저체중입니다.")
+    elif round_bmi >= 18.5 and round_bmi <= 22.9 :
+        print(f"MY BMI: {round_bmi}".center(40))
+        print(f"나의 BMI(신체질량지수)는{round_bmi} 이고, 정상입니다.")
+    elif round_bmi >= 23.0 and round_bmi <= 24.9:
+        print(f"MY BMI: {round_bmi}".center(40))
+        print(f"나의 BMI(신체질량지수)는{round_bmi} 이고, 과체중입니다.")
+    elif round_bmi >= 25.0 :
+        print(f"MY BMI: {round_bmi}".center(40))
+        print(f"나의 BMI(신체질량지수)는{round_bmi} 이고, 비만입니다.")
+
+
+height = int(input("신장을 입력해주세요(소수점 제외).: "))
+weight = int(input("몸무게를 입력해주세요(소수점 제외).: "))
+
+BMI(height, weight)
