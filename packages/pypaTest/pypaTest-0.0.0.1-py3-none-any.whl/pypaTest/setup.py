c09c936b0d70175680a280e274921ffdca1a6b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:24 +0000

    Add package pypaTest/0.0.0.1/pypaTest-0.0.0.1-py3-none-any.whl

---
+++ packages/pypaTest/pypaTest-0.0.0.1-py3-none-any.whl/pypaTest/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypaTest',
+    version='0.0.0.1',
+    description='First Test Package',
+    author='secludor',
+    author_email='caelumator@gmail.com',
+    url='https://github.com/Secludor/',
+    license='MIT',
+    py_modules=['Test1','Test2'],
+    python_requires = '>=3',
+    packages=['pypaTest']
+)
