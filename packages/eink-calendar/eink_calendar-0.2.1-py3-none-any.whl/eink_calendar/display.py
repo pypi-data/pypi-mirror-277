5981c40c037a83ad3b703bd1b5e27bf8845c4b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:53 +0000

    Add package eink-calendar/0.2.1/eink_calendar-0.2.1-py3-none-any.whl

---
+++ packages/eink-calendar/eink_calendar-0.2.1-py3-none-any.whl/eink_calendar/display.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from queue import Empty, Queue
+from threading import Thread
+
+from inky import Inky7Colour
+from PIL import Image
+
+
+class Display(ABC):
+    @abstractmethod
+    def set_image(self, image: Image.Image) -> None:
+        pass
+
+    @abstractmethod
+    def close(self) -> None:
+        pass
+
+
+class MockDisplay(Display):
+    def set_image(self, image: Image.Image) -> None:
+        image.show("eInk Calendar")
+
+    def close(self) -> None:
+        pass
+
+
+class EInkDisplay(Display):
+    def __init__(self) -> None:
+        self._inky = Inky7Colour()
+        self._image_queue = Queue[Image.Image]()
+        self._running = True
+        self._thread = Thread(target=self._run, daemon=True)
+        self._thread.start()
+
+    def set_image(self, image: Image.Image) -> None:
+        self._image_queue.put(image)
+
+    def close(self) -> None:
+        self._running = False
+        self._thread.join()
+
+    def _run(self) -> None:
+        while self._running:
+            try:
+                image = self._image_queue.get(timeout=1)
+                self._inky.set_image(image)
+                self._inky.show()
+            except Empty:
+                pass
