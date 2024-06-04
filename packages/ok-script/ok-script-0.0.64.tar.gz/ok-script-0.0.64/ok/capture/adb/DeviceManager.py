258cc9b503767991d90f3e6a5d856390d4951c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 05:04:10 +0000

    Add package ok-script/0.0.64/ok-script-0.0.64.tar.gz

---
+++ packages/ok-script/ok-script-0.0.64.tar.gz/ok-script-0.0.64/ok/capture/adb/DeviceManager.py
@@ -0,0 +1,320 @@
+import win32gui
+
+from ok.capture.HwndWindow import HwndWindow, find_hwnd_by_title_and_exe, enum_windows
+from ok.capture.adb.ADBCaptureMethod import ADBCaptureMethod, do_screencap
+from ok.capture.adb.WindowsCaptureFactory import update_capture_method
+from ok.capture.windows.window import get_window_bounds
+from ok.config.Config import Config
+from ok.gui.Communicate import communicate
+from ok.interaction.ADBInteraction import ADBBaseInteraction
+from ok.interaction.Win32Interaction import Win32Interaction
+from ok.logging.Logger import get_logger
+from ok.util.Handler import Handler
+
+logger = get_logger(__name__)
+
+
+class DeviceManager:
+
+    def __init__(self, config_folder, windows_capture_config, adb_capture_config, debug=False, exit_event=None):
+        self._device = None
+        self._connect_all = False
+        self._adb = None
+        self.windows_capture_config = windows_capture_config
+        self.adb_capture_config = adb_capture_config
+        self.pc_device = None
+        self.debug = debug
+        self.interaction = None
+        self.exit_event = exit_event
+        if windows_capture_config is not None:
+            self.hwnd = HwndWindow(exit_event, windows_capture_config.get('title'),
+                                   windows_capture_config.get('exe'))
+        self.config = Config({"devices": {}, "preferred": "none", "pc_full_path": None}, config_folder,
+                             "DeviceManager")
+        self.capture_method = None
+        self.handler = Handler(exit_event, 'RefreshAdb')
+        self.handler.post(self.do_refresh, 5)
+
+    def refresh(self):
+        logger.debug('calling refresh')
+        self.handler.post(self.do_refresh, remove_existing=True)
+
+    @property
+    def device_dict(self):
+        device_dict = self.config.get("devices")
+        if device_dict is None:
+            device_dict = {}
+        if self.pc_device is not None:
+            full = {'pc': self.pc_device}
+            full.update(device_dict)
+            return full
+        else:
+            return device_dict
+
+    @property
+    def adb(self):
+        if self._adb is None:
+            import adbutils
+            self._adb = adbutils.AdbClient(host="127.0.0.1")
+            logger.debug(f'connect adb')
+        return self._adb
+
+    def adb_connect(self, addr):
+        try:
+            self.adb.connect(addr, timeout=2)
+        except Exception as e:
+            logger.error("adb connect error", e)
+
+    def connect_all(self):
+        if not self._connect_all:
+            for device in self.device_dict.values():
+                if device["device"] == "adb":
+                    self.adb_connect(device['address'])
+            self._connect_all = True
+            logger.debug(f'connect_all')
+
+    def get_devices(self):
+        return list(self.device_dict.values())
+
+    def update_pc_device(self):
+        if self.windows_capture_config is not None:
+            nick = ""
+            width = 0
+            height = 0
+            hwnd, full_path = find_hwnd_by_title_and_exe(self.windows_capture_config.get('title'),
+                                                         self.windows_capture_config.get('exe'))
+            if hwnd is not None:
+                nick = win32gui.GetWindowText(hwnd)
+                _, _, _, _, width, height, _, _, _ = get_window_bounds(
+                    hwnd)
+            if not nick:
+                nick = self.windows_capture_config.get('exe')
+            self.pc_device = {"address": "", "imei": 'pc', "device": "windows",
+                              "model": "", "nick": nick, "width": width,
+                              "height": height,
+                              "hwnd": nick, "capture": "windows",
+                              "connected": hwnd is not None,
+                              "full_path": full_path or self.config.get('pc_full_path')
+                              }
+            if full_path and full_path != self.config.get('pc_full_path'):
+                self.config['pc_full_path'] = full_path
+
+            if width != 0:
+                self.pc_device["resolution"] = f"{width}x{height}"
+
+    def do_refresh(self, fast=False):
+        self.update_pc_device()
+
+        self.refresh_adb_device(fast)
+
+        if self.exit_event.is_set():
+            return
+        self.start()
+        communicate.adb_devices.emit(True)
+        logger.debug(f'refresh {self.device_dict}')
+
+    def refresh_adb_device(self, fast):
+        if self.adb_capture_config is None:
+            return
+        if not fast:
+            self.connect_all()
+        from ok.alas.emulator_windows import EmulatorManager
+        manager = EmulatorManager()
+        installed_emulators = [] if fast else manager.all_emulator_instances
+        for device in self.adb.list():
+            logger.debug(f'adb.list() {device}')
+        for emulator in installed_emulators:
+            logger.debug(f"installed_emulator: {emulator}")
+            self.adb_connect(emulator.serial)
+        device_list = self.adb.device_list()
+        device_list = sorted(device_list, key=lambda x: x.serial)
+        adb_connected = []
+        for device in device_list:
+            imei = device.shell("settings get secure android_id") or device.shell(
+                "service call iphonesubinfo 4") or device.prop.model
+            frame = None if fast else do_screencap(device)
+            width, height = 0, 0
+            if frame is not None:
+                height, width, _ = frame.shape
+            adb_device = {"address": device.serial, "imei": imei, "device": "adb", "capture": "adb", "width": width,
+                          "height": height,
+                          "model": device.prop.model, "nick": device.prop.model, "connected": True,
+                          "resolution": f"{width}x{height}"}
+            found_emulator = None
+            for emulator in installed_emulators:
+                if emulator.serial == adb_device['address']:
+                    adb_device['nick'] = emulator.name
+                    found_emulator = emulator
+            if self.device_dict.get(imei):
+                if found_emulator or not self.device_dict[imei]['connected']:
+                    old_capture = self.device_dict[imei].get("capture")
+                    old_hwnd = self.device_dict[imei].get("hwnd")
+                    if not old_hwnd and found_emulator and found_emulator.path:
+                        windows = enum_windows(found_emulator.path)
+                        logger.debug(f'try auto set hwnd window from emulator {found_emulator} {windows}')
+                        if len(windows) == 1:
+                            logger.info(f'set hwnd window from emulator {windows[0][1]} {found_emulator}')
+                            old_hwnd = windows[0][1]
+                    if found_emulator:
+                        self.update_device_value(imei, 'full_path', found_emulator.path)
+                    self.device_dict[imei] = adb_device
+                    if old_capture is not None:
+                        self.update_device_value(imei, 'capture', old_capture)
+                    if old_hwnd is not None:
+                        self.update_device_value(imei, 'hwnd', old_hwnd)
+            else:
+                self.update_device(imei, adb_device)
+            adb_connected.append(imei)
+        for imei in self.device_dict:
+            if self.device_dict[imei]['device'] == 'adb':
+                self.update_device_value(imei, 'connected', imei in adb_connected)
+        self.config.save_file()
+
+    def update_device(self, imei, device=None):
+        devices = self.config.get("devices", {})
+        devices[imei] = device
+        self.config["devices"] = devices
+
+    def update_device_value(self, imei, key, value):
+        devices = self.config.get("devices", {})
+        if imei in devices:
+            devices[imei][key] = value
+        self.config["devices"] = devices
+
+    def set_preferred_device(self, imei=None):
+        logger.debug(f"set_preferred_device {imei}")
+        if imei is None:
+            imei = self.config.get("preferred")
+        preferred = self.device_dict.get(imei)
+        if preferred is None and len(self.device_dict) > 0:
+            preferred = next(iter(self.device_dict.values()))
+            imei = preferred['imei']
+        if self.config.get("preferred") != imei:
+            self.config["preferred"] = imei
+            self.config.save_file()
+        logger.debug(f'preferred device: {preferred}')
+        self.start()
+        return preferred
+
+    def get_preferred_device(self):
+        imei = self.config.get("preferred")
+        preferred = self.device_dict.get(imei)
+        return preferred
+
+    def set_hwnd_name(self, hwnd_name):
+        preferred = self.get_preferred_device()
+        if preferred.get("hwnd") != hwnd_name:
+            preferred['hwnd'] = hwnd_name
+            self.config.save_file()
+            self.start()
+
+    def set_capture(self, capture):
+        preferred = self.get_preferred_device()
+        if preferred.get("capture") != capture:
+            preferred['capture'] = capture
+            self.config.save_file()
+        self.start()
+
+    def get_hwnd_name(self):
+        preferred = self.get_preferred_device()
+        return preferred.get('hwnd')
+
+    def ensure_hwnd(self, title, exe, frame_width=0, frame_height=0):
+        if self.hwnd is None:
+            self.hwnd = HwndWindow(self.exit_event, title, exe, frame_width, frame_height)
+        else:
+            self.hwnd.update_frame_size(frame_width, frame_height)
+            self.hwnd.update_title_and_exe(title, exe)
+
+    def use_windows_capture(self, override_config=None, require_bg=False):
+        if not override_config:
+            override_config = self.windows_capture_config
+        self.capture_method = update_capture_method(override_config, self.capture_method, self.hwnd,
+                                                    require_bg)
+        if self.capture_method is None:
+            logger.error(f'can find a usable windows capture')
+        else:
+            logger.info(f'capture method {type(self.capture_method)}')
+
+    def start(self):
+        preferred = self.get_preferred_device()
+        if preferred is None:
+            if self.device_dict:
+                self.set_preferred_device()
+            return
+
+        if preferred['device'] == 'windows':
+            self.ensure_hwnd(self.windows_capture_config.get('title'), self.windows_capture_config.get('exe'))
+            self.use_windows_capture()
+            if not isinstance(self.interaction, Win32Interaction):
+                self.interaction = Win32Interaction(self.capture_method)
+            preferred['connected'] = self.capture_method.connected()
+        else:
+            self.connect_all()
+            for adb_device in self.adb.device_list():
+                if adb_device.serial == preferred.get('address'):
+                    logger.debug(f"set device {adb_device}")
+                    self._device = adb_device
+            hwnd_name = preferred.get('hwnd')
+            width = preferred.get('width', 0)
+            height = preferred.get('height', 0)
+            if preferred.get('capture') == "windows":
+                self.ensure_hwnd(hwnd_name, preferred.get('full_path'), width, height)
+                self.use_windows_capture({'can_bit_blt': True}, require_bg=True)
+            elif not isinstance(self.capture_method, ADBCaptureMethod):
+                if self.capture_method is not None:
+                    self.capture_method.close()
+                self.capture_method = ADBCaptureMethod(self, self.exit_event, width=width,
+                                                       height=height)
+            self.interaction = ADBBaseInteraction(self, self.capture_method, width, height)
+            if self.debug and hwnd_name:
+                self.ensure_hwnd(hwnd_name, None, width, height)
+            elif self.hwnd is not None:
+                self.hwnd.stop()
+                self.hwnd = None
+
+    @property
+    def device(self):
+        return self._device
+
+    def adb_kill_server(self):
+        if self.adb is not None:
+            self.adb.server_kill()
+            logger.debug('adb kill_server')
+
+    @property
+    def width(self):
+        if self.capture_method is not None:
+            return self.capture_method.width
+        return 0
+
+    @property
+    def height(self):
+        if self.capture_method is not None:
+            return self.capture_method.height
+        return 0
+
+    def update_device_list(self):
+        pass
+
+    def shell(self, *args, **kwargs):
+        device = self.device
+        if device is not None:
+            from adbutils import AdbError
+            try:
+                return device.shell(*args, **kwargs)
+            except AdbError as e:
+                addr = self.get_preferred_device()['address']
+                self.adb.connect(addr)
+                logger.error(f"shell_wrapper error occurred: {e} ,try reconnect {addr}")
+                return device.shell(*args, **kwargs)
+        else:
+            raise Exception('Device is none')
+
+    def get_exe_path(self, device):
+        path = device.get('full_path')
+        if device.get('device') == 'windows' and self.windows_capture_config and self.windows_capture_config.get(
+                'calculate_pc_exe_path'):
+            return self.windows_capture_config.get('calculate_pc_exe_path')(path)
+        else:
+            return path
---
+++ packages/ok-script/ok-script-0.0.64.tar.gz/ok-script-0.0.64/ok/gui/MainWindow.py
@@ -0,0 +1,152 @@
+from PySide6.QtCore import QObject, Signal, Qt
+from PySide6.QtGui import QKeySequence, QShortcut
+from PySide6.QtWidgets import QMenu, QSystemTrayIcon
+from qfluentwidgets import FluentIcon, NavigationItemPosition, MSFluentWindow, InfoBar, InfoBarPosition, MessageBox
+
+import ok.gui
+from ok.capture.windows.dump import dump_threads
+from ok.gui.Communicate import communicate
+from ok.gui.about.AboutTab import AboutTab
+from ok.gui.debug.DebugTab import DebugTab
+from ok.gui.start.StartTab import StartTab
+from ok.gui.tasks.OneTimeTaskTab import OneTimeTaskTab
+from ok.gui.tasks.TriggerTaskTab import TriggerTaskTab
+from ok.logging.Logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class Communicate(QObject):
+    speak = Signal(str)
+
+
+class MainWindow(MSFluentWindow):
+    def __init__(self, config, icon, title, version, debug=False, about=None, exit_event=None):
+        super().__init__()
+        self.exit_event = exit_event
+        self.start_tab = StartTab()
+        self.second_tab = None
+        self.onetime_tab = None
+        self.trigger_tab = None
+        self.addSubInterface(self.start_tab, FluentIcon.PLAY, self.tr('Start'))
+        if len(ok.gui.executor.onetime_tasks) > 0:
+            self.onetime_tab = OneTimeTaskTab()
+            self.second_tab = self.onetime_tab
+            self.addSubInterface(self.onetime_tab, FluentIcon.BOOK_SHELF, self.tr('Tasks'))
+        if len(ok.gui.executor.trigger_tasks) > 0:
+            self.trigger_tab = TriggerTaskTab()
+            if self.second_tab is None:
+                self.second_tab = self.trigger_tab
+            self.addSubInterface(self.trigger_tab, FluentIcon.ROBOT, self.tr('Triggers'))
+        # if debug:
+        #     debug_tab = DebugTab()
+        #     self.addSubInterface(debug_tab, FluentIcon.COMMAND_PROMPT, self.tr('Debug'))
+        # ... Add other tabs similarly
+        if debug:
+            debug_tab = DebugTab(config, exit_event)
+            self.addSubInterface(debug_tab, FluentIcon.DEVELOPER_TOOLS, self.tr('Debug'),
+                                 position=NavigationItemPosition.BOTTOM)
+
+        if about:
+            about_tab = AboutTab(icon, title, version, debug, about)
+            self.addSubInterface(about_tab, FluentIcon.QUESTION, self.tr('About'),
+                                 position=NavigationItemPosition.BOTTOM)
+        # Styling the tabs and content if needed, for example:
+        self.setWindowTitle(f'{title} {version}')
+
+        communicate.executor_paused.connect(self.executor_paused)
+        communicate.tab.connect(self.navigate_tab)
+
+        self.shortcut = QShortcut(QKeySequence("Ctrl+Alt+D"), self)
+        self.shortcut.activated.connect(dump_threads)
+
+        # Create a context menu for the tray
+        menu = QMenu()
+        exit_action = menu.addAction(self.tr("Exit"))
+        exit_action.triggered.connect(ok.gui.ok.quit)
+
+        self.tray = QSystemTrayIcon(icon)
+
+        # Set the context menu and show the tray icon
+        self.tray.setContextMenu(menu)
+        self.tray.show()
+
+        communicate.capture_error.connect(self.capture_error)
+        communicate.notification.connect(self.show_notification)
+        communicate.config_validation.connect(self.config_validation)
+
+    def config_validation(self, message):
+        title = self.tr('Error')
+        InfoBar.error(
+            title=title,
+            content=message,
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.TOP,
+            duration=5000,  # won't disappear automatically
+            parent=self.window()
+        )
+        self.tray.showMessage(title, message)
+
+    def show_notification(self, message, title=None, error=False, tray=True):
+        bar = InfoBar.error if error else InfoBar.info
+        if title is None:
+            title = f"{self.tr('Error') if error else 'Info'}:"
+        bar(
+            title=title,
+            content=message,
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.TOP,
+            duration=5000,  # won't disappear automatically
+            parent=self.window()
+        )
+        if tray:
+            self.tray.showMessage(title, message)
+
+    def capture_error(self):
+        self.show_notification(self.tr('Please check whether the game window is selected correctly!'),
+                               self.tr('Capture Error'), error=True)
+
+    def navigate_tab(self, index):
+        if index == "start":
+            self.switchTo(self.start_tab)
+        elif index == "second":
+            self.switchTo(self.second_tab)
+        elif index == "onetime" and self.onetime_tab is not None:
+            self.switchTo(self.onetime_tab)
+        elif index == "trigger" and self.trigger_tab is not None:
+            self.switchTo(self.trigger_tab)
+
+    def executor_paused(self, paused):
+        if not paused:
+            InfoBar.info(
+                title="",
+                content=self.tr(
+                    "Start Success."),
+                orient=Qt.Horizontal,
+                isClosable=True,
+                position=InfoBarPosition.TOP,
+                duration=5000,  # won't disappear automatically
+                parent=self
+            )
+
+    def btn_clicked(self):
+        self.comm.speak.emit("Hello, PySide6 with parameters!")
+
+    def closeEvent(self, event):
+        # Create a message box that asks the user if they really want to close the window
+        if ok.gui.ok.exit_event.is_set():
+            event.accept()
+            return
+        title = self.tr('Exit')
+        content = self.tr(
+            "Are you sure you want to exit the app?")
+        w = MessageBox(title, content, self.window())
+        w.setContentCopyable(True)
+        if w.exec():
+            logger.info("Window closed")
+            ok.gui.ok.quit()
+            event.accept()
+        else:
+            event.ignore()
