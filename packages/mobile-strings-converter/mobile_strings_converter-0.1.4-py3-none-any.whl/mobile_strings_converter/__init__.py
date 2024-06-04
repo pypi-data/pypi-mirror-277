505bd038a294330ac6541de1511b15ea659874e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:39 +0000

    Add package mobile-strings-converter/0.1.4/mobile_strings_converter-0.1.4-py3-none-any.whl

---
+++ packages/mobile-strings-converter/mobile_strings_converter-0.1.4-py3-none-any.whl/mobile_strings_converter/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2024 mobile-strings-converter
+
+# @license: http://www.opensource.org/licenses/mit-license.php
+# @author: José Carlos López Henestrosa
+
+"""Imports for the mobile-strings-converter package."""
+from .converter import convert_strings, to_google_sheets
+
+# Constants
+__version__ = "0.1.4"
+
+__author__ = "José Carlos López Henestrosa"
+__license = "MIT"
+__author_email__ = "henestrosadev@gmail.com"
+__maintainer_email__ = "henestrosadev@gmail.com"
