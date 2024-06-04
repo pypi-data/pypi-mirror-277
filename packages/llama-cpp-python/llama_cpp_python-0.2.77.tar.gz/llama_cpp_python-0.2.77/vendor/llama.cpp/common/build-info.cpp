a9a55240b17e677b357d03b11596f765d8768ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:04:06 +0000

    Add package llama-cpp-python/0.2.77/llama_cpp_python-0.2.77.tar.gz

---
+++ packages/llama-cpp-python/llama_cpp_python-0.2.77.tar.gz/llama_cpp_python-0.2.77/vendor/llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 1;
+char const *LLAMA_COMMIT = "bde7cd3";
+char const *LLAMA_COMPILER = "cc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0";
+char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
