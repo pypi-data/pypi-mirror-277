9093ba028b03819fa7f3861ae7d9b10be6fb991e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:22 +0000

    Add package flask-context-manager/1.2.0/flask_context_manager-1.2.0-py2.py3-none-any.whl

---
+++ packages/flask-context-manager/flask_context_manager-1.2.0-py2.py3-none-any.whl/flask_context_manager/__init__.py
@@ -0,0 +1,23 @@
+from flask_context_manager.src.main.core.context_manager import ContextManager
+from flask_context_manager.src.main.core.routes import *
+from flask_context_manager.src.main.model.beans.bean import Bean
+from flask_context_manager.src.main.model.beans.component import Component
+from flask_context_manager.src.main.model.beans.configuration import Configuration
+from flask_context_manager.src.main.model.beans.controller import Controller
+from flask_context_manager.src.main.model.beans.repository import Repository
+from flask_context_manager.src.main.model.beans.service import Service
+
+__all__ = [
+    "ContextManager",
+    "Component",
+    "Configuration",
+    "Controller",
+    "Repository",
+    "Service",
+    "Bean",
+    "get_mapping",
+    "post_mapping",
+    "put_mapping",
+    "delete_mapping",
+    "rest_mapping",
+]
---
+++ packages/flask-context-manager/flask_context_manager-1.2.0-py2.py3-none-any.whl/flask_context_manager/src/main/model/beans/base_bean.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+from flask_context_manager.src.main.core.context_manager import ContextManager
+
+
+class BaseBean(ABC):
+    decorated_class = __name__
+
+    def __init__(self, cls):
+        self.child_class = cls
+        ContextManager.add_bean(cls)
+
+    def __hash__(self):
+        return hash(self.child_class)
+
+    def __eq__(self, other):
+        return getattr(self, "child_class", None) == getattr(other, "child_class", None)
