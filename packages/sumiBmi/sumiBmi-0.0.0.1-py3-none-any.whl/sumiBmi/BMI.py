185384aaafba54cc006ebe9483a72de5d1a1e05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:04:02 +0000

    Add package sumiBmi/0.0.0.1/sumiBmi-0.0.0.1-py3-none-any.whl

---
+++ packages/sumiBmi/sumiBmi-0.0.0.1-py3-none-any.whl/sumiBmi/BMI.py
@@ -0,0 +1,13 @@
+h = int(input('키를 입력하세요(cm): '))
+w = int(input('몸무게를 입력하세요(kg): '))
+
+BMI = round(w/(h/100)**2,2)
+
+if float(BMI) >= 25.0:
+    print(f'BMI는 {BMI}, 비만입니다')
+elif float(BMI) >= 23.0:
+    print(f'BMI는 {BMI}, 과체중입니다')
+elif float(BMI) >= 18.5:
+    print(f'BMI는 {BMI}, 정상입니다')
+else:
+    print(f'BMI는 {BMI}, 저체중입니다')
