f2ff913de14ff61dc44f18fb3a522f9630d9047c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 06:04:20 +0000

    Add package cognee/0.1.11/cognee-0.1.11-py3-none-any.whl

---
+++ packages/cognee/cognee-0.1.11-py3-none-any.whl/cognee/infrastructure/databases/graph/get_graph_client.py
@@ -0,0 +1,41 @@
+"""Factory function to get the appropriate graph client based on the graph type."""
+
+from cognee.shared.data_models import GraphDBType
+from .config import get_graph_config
+from .graph_db_interface import GraphDBInterface
+from .networkx.adapter import NetworkXAdapter
+
+
+async def get_graph_client(graph_type: GraphDBType, graph_file_name: str = None) -> GraphDBInterface :
+    """Factory function to get the appropriate graph client based on the graph type."""
+    config = get_graph_config()
+
+    if graph_type == GraphDBType.NEO4J:
+        try:
+            from .neo4j_driver.adapter import Neo4jAdapter
+
+            return Neo4jAdapter(
+                graph_database_url = config.graph_database_url,
+                graph_database_username = config.graph_database_username,
+                graph_database_password = config.graph_database_password
+            )
+        except:
+            pass
+
+    elif graph_type == GraphDBType.FALKORDB:
+        try:
+            from .falkordb.adapter import FalcorDBAdapter
+
+            return FalcorDBAdapter(
+                graph_database_url = config.graph_database_url,
+                graph_database_username = config.graph_database_username,
+                graph_database_password = config.graph_database_password,
+                graph_database_port = config.graph_database_port
+            )
+        except:
+            pass
+    graph_client = NetworkXAdapter(filename = config.graph_file_path)
+    if (graph_client.graph is None):
+        await graph_client.load_graph_from_file()
+
+    return graph_client
