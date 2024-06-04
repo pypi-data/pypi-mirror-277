b9db70175a1ae60b3be6e643ba6ff1e05786a466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 03:03:25 +0000

    Add package modrinth-downloader/0.3/modrinth_downloader-0.3.tar.gz

---
+++ packages/modrinth-downloader/modrinth_downloader-0.3.tar.gz/modrinth_downloader-0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modrinth_downloader',
+    version='0.3',
+    author='Alexander Brightwater',
+    description="Download projects modrinth",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+    install_requires=[
+        'requests',
+        'toml',
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'modrinth_downloader=scripts.run_downloader:main',
+        ],
+    },
+)
