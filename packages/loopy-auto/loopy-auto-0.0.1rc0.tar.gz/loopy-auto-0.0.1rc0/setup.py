75c046017747e6a428a0056c49b1d2e3ce60223d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:36 +0000

    Add package loopy-auto/0.0.1rc0/loopy-auto-0.0.1rc0.tar.gz

---
+++ packages/loopy-auto/loopy-auto-0.0.1rc0.tar.gz/loopy-auto-0.0.1rc0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='loopy-auto',
+    version='0.0.1-c',
+    packages=find_packages(),
+    description='Loopy-auto is an automation framework in which anybody can create a test script simply with your favorite language.',
+    author='Jooho Lee',
+    author_email='ljhiyh@gmail.com',
+    url='https://github.com/jooho/loopy',
+    install_requires=['automation', 'openshift'],
+    keywords=['jooho', 'loopy automation','openshift', 'opendatahub ai' 'pypi'],
+    python_requires='>=3.10',
+    package_data={},
+    zip_safe=False,
+    classifiers=[        
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
