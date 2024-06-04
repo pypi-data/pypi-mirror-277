51762e849265f41d59c9f7d7e3c7292235a037ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:50 +0000

    Add package metaflow-stubs/2.12.2/metaflow_stubs-2.12.2-py2.py3-none-any.whl

---
+++ packages/metaflow-stubs/metaflow_stubs-2.12.2-py2.py3-none-any.whl/metaflow-stubs/plugins/pypi/pypi_decorator.pyi
@@ -0,0 +1,22 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.2                                                             #
+# Generated on 2024-06-04T07:19:10.140362                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+class PyPIStepDecorator(metaflow.decorators.StepDecorator, metaclass=type):
+    def step_init(self, flow, graph, step, decos, environment, flow_datastore, logger):
+        ...
+    ...
+
+class PyPIFlowDecorator(metaflow.decorators.FlowDecorator, metaclass=type):
+    def flow_init(self, flow, graph, environment, flow_datastore, metadata, logger, echo, options):
+        ...
+    ...
+
---
+++ packages/metaflow-stubs/metaflow_stubs-2.12.2-py2.py3-none-any.whl/metaflow-stubs/plugins/resources_decorator.pyi
@@ -0,0 +1,15 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.2                                                             #
+# Generated on 2024-06-04T07:19:10.092276                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+class ResourcesDecorator(metaflow.decorators.StepDecorator, metaclass=type):
+    ...
+
