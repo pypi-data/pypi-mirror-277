adc91207c7463cf991e40af157ea443aa2b94879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:37 +0000

    Add package protein-sequence-annotation/0.2.0/protein-sequence-annotation-0.2.0.tar.gz

---
+++ packages/protein-sequence-annotation/protein-sequence-annotation-0.2.0.tar.gz/protein-sequence-annotation-0.2.0/psalm/esm_utils.py
@@ -0,0 +1,18 @@
+import torch
+import esm
+
+class esm2_model:
+    def __init__(self):
+        """Initialize the ESM model."""
+        self.model, self.alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+        self.model.eval()
+
+    def forward(self, data):
+        """Generate embeddings for the given data using the ESM model."""
+        batch_converter = self.alphabet.get_batch_converter()
+        batch_labels, batch_strs, batch_tokens = batch_converter(data)
+        with torch.no_grad():
+            results = self.model(batch_tokens, repr_layers=[33], return_contacts=True)
+        token_representations = results["representations"][33]
+        token_representations = token_representations.squeeze()
+        return token_representations
