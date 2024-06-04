ba2337b0b9e1860ce4276e4e2c9fc04ae15ec0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:28 +0000

    Add package digitalhub-ml/0.5.0b7/digitalhub_ml-0.5.0b7-py3-none-any.whl

---
+++ packages/digitalhub-ml/digitalhub_ml-0.5.0b7-py3-none-any.whl/digitalhub_ml/entities/models/metadata.py
@@ -0,0 +1,18 @@
+"""
+Model metadata module.
+"""
+from __future__ import annotations
+
+from digitalhub_core.entities._base.metadata import Metadata
+
+
+class ModelMetadata(Metadata):
+    """
+    A class representing Model metadata.
+    """
+
+
+class ModelMetadataModel(ModelMetadata):
+    """
+    A class representing Model metadata.
+    """
