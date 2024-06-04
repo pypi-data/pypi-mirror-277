639a7fd6b366f1ab8af117a7465f59af7f141aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 05:03:29 +0000

    Add package nel-calculations/0.1/nel_calculations-0.1.tar.gz

---
+++ packages/nel-calculations/nel_calculations-0.1.tar.gz/nel_calculations-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nel_calculations',
+    version='0.1',
+    description='A simple python library for calculating area of shapes',
+    author='Abdul Nelfrank',
+    packages=find_packages()
+)
