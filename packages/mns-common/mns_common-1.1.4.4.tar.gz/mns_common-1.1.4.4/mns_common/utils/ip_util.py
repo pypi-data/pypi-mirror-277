1c440365b97279253aeaea0b32de2569e8a9576d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:12 +0000

    Add package mns-common/1.1.4.4/mns_common-1.1.4.4.tar.gz

---
+++ packages/mns-common/mns_common-1.1.4.4.tar.gz/mns_common-1.1.4.4/mns_common/utils/ip_util.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+file_path = os.path.abspath(__file__)
+end = file_path.index('mns') + 14
+project_path = file_path[0:end]
+sys.path.append(project_path)
+
+import socket
+
+
+def get_host_ip():
+    """
+    查询本机ip地址
+    :return:
+    """
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
