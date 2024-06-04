662ff26eb69bf125081e5aad2729305cbbaca250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:07 +0000

    Add package odoo14-addon-energy-selfconsumption/14.0.4.0.4/odoo14-addon-energy_selfconsumption-14.0.4.0.4.tar.gz

---
+++ packages/odoo14-addon-energy-selfconsumption/odoo14-addon-energy_selfconsumption-14.0.4.0.4.tar.gz/odoo14-addon-energy_selfconsumption-14.0.4.0.4/odoo/addons/energy_selfconsumption/wizards/__init__.py
@@ -0,0 +1,5 @@
+from . import selfconsumption_import_wizard
+from . import distribution_table_import_wizard
+from . import contract_generation_wizard
+from . import define_invoicing_mode_wizard
+from . import invoicing_wizard
