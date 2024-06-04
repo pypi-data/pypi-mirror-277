7a871e532444bb6c85cf3c5f507256ffaa062982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 06:04:24 +0000

    Add package hello-world-hemanth/0.12/hello_world_hemanth-0.12-py3-none-any.whl

---
+++ packages/hello-world-hemanth/hello_world_hemanth-0.12-py3-none-any.whl/hello_world/cli.py
@@ -0,0 +1,14 @@
+import click
+from hello_world import subtract
+
+@click.command()
+@click.argument('a', type=float)
+@click.argument('b', type=float)
+def main(a, b):
+    """Simple program that subtracts two numbers."""
+    result = subtract(a, b)
+    click.echo(f"The result of {a} - {b} is {result}")
+
+if __name__ == '__main__':
+    main()
+
