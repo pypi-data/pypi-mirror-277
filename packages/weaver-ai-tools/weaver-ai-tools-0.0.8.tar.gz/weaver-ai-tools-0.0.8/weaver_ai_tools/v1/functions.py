41a7d9b246841efa23ec512ed139794df18993c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:40 +0000

    Add package weaver-ai-tools/0.0.8/weaver-ai-tools-0.0.8.tar.gz

---
+++ packages/weaver-ai-tools/weaver-ai-tools-0.0.8.tar.gz/weaver-ai-tools-0.0.8/weaver_ai_tools/v1/functions.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+from .config import Configuration
+
+internal_requests = requests.Session()
+
+internal_requests.headers.update({'Tenant': os.getenv("TENANT")})
+
+def register_service():
+    internal_requests.post("http://web:8000/api/v1/service/alive/").raise_for_status()
+
+
+class Conversation:
+    def get_messages(conversation_id):
+        return internal_requests.get(f"http://web:8000/api/v1/conversations/{conversation_id}").json()
+
+    def add_message(message):
+        print("This is being handled by the bff for now, comming soon.")
+
+    def on_initialize():
+        print("comming soon...")
