dc0daa9f36df9484ccd9cf9b9fe3fe6e09e56e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 14:03:31 +0000

    Add package brev/0.0.1a1/brev-0.0.1a1.tar.gz

---
+++ packages/brev/brev-0.0.1a1.tar.gz/brev-0.0.1a1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brev',
+    version='0.0.1-alpha1',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+)
