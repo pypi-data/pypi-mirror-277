7b2a40e2b66f51f21c248636db0b260f18c39a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 17:04:13 +0000

    Add package wallpaper-factory/0.0.16/wallpaper_factory-0.0.16-py3-none-any.whl

---
+++ packages/wallpaper-factory/wallpaper_factory-0.0.16-py3-none-any.whl/wallpaper_factory/available_palletes.py
@@ -0,0 +1,19 @@
+import json
+import os
+from PIL import ImageColor
+
+
+def convert_array(array_hex):
+    rgba_colors = []
+    for color in array_hex:
+        rgba_colors.append(ImageColor.getcolor(color, "RGBA"))
+    return rgba_colors
+
+
+available_palletes = []
+f = open(os.path.join(os.path.dirname(__file__), "palletes.json"))
+palettes = f.read()
+palettes = json.loads(palettes)
+palettes = [[scheme, colors] for scheme, colors in palettes.items()]
+for arr in palettes:
+    available_palletes.append([arr[0], convert_array(arr[1])])
