04d1e084367a01ef999c28777c7410439d63bc07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 03:03:13 +0000

    Add package balanco-library/0.2.0/balanco_library-0.2.0.tar.gz

---
+++ packages/balanco-library/balanco_library-0.2.0.tar.gz/balanco_library-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='balanco_library',
+    version='0.2.0',
+    author='Elias Isopahkala',
+    author_email='elias.isopahkala@balanco.fi',
+    description='A simple example package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/balanco-elias/balanco_library',  # Update with your repo URL
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
