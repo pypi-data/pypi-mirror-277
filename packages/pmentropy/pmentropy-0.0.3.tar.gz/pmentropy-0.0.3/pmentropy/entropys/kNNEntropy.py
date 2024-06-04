67f438b9e40ac13a3a9399754ad0c2768a4aa271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:29 +0000

    Add package pmentropy/0.0.3/pmentropy-0.0.3.tar.gz

---
+++ packages/pmentropy/pmentropy-0.0.3.tar.gz/pmentropy-0.0.3/pmentropy/entropys/kNNEntropy.py
@@ -0,0 +1,28 @@
+from pmentropy.Node import Node
+from pmentropy.kNearestLevenshtein import kNearestLevenshtein
+import math
+import numpy as np
+
+def L(j: int) -> float:
+    if j < 0:
+        return None
+    if j == 0:
+        return 0.0
+    L_j = 0.0
+    for i in range(1, j + 1):
+        L_j += 1.0 / i
+    return L_j
+
+def kNN_entropy(logs: tuple[dict[Node], dict], k: int, p: int) -> float:
+    trie, trie_infos = logs
+    end_nodes = trie_infos["end_nodes"]
+
+    distances_set = kNearestLevenshtein(k, True, end_nodes)
+    
+    acc = 0.0
+    n = len(distances_set)
+    V = pow(math.pi, p / 2.0) / math.gamma(p / 2.0 + 1)
+    for distances in distances_set:
+        d = max(distances)
+        acc += math.log(d) + math.log(V) + np.euler_gamma - L(k - 1) + math.log(n)
+    return acc * p / n
---
+++ packages/pmentropy/pmentropy-0.0.3.tar.gz/pmentropy-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+  name='pmentropy',
+  version='0.0.3',
+  author='Taverna Tanguy',
+  author_email='taverna.tanguy@gmail.com',
+  description='Compute in python entropy for process mining describe in Back, C.O., Debois, S. & Slaats, T. Entropy as a Measure of Log Variability. J Data Semant 8, 129–156 (2019). https://doi.org/10.1007/s13740-019-00105-3 (https://rdcu.be/dJMwH)',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url= 'https://github.com/TavTanguy/pmentropy',
+  packages=find_packages(),
+  readme='README.md',
+  license='GNU General Public License v3',
+  license_file='LICENSE',
+  install_requires=['numpy >= 1.26.4', 'pm4py >= 2.7.11.10', 'Levenshtein >= 0.25.1'],
+  classifiers=[
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Affero General Public License v3',
+    'Programming Language :: Python :: 3.11',
+  ])
