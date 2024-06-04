6b6eb5ce64cb9411f15a76cf5e23dfdc628c1cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:04:02 +0000

    Add package aixhello/5.12/aixhello-5.12.tar.gz

---
+++ packages/aixhello/aixhello-5.12.tar.gz/aixhello-5.12/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension(
+        "aixhello.xyello",
+        ["aixhello/xyello.pyx"],
+    )
+]
+
+cythonize_options = {
+    'compiler_directives': {
+        'language_level': 3,         
+        'emit_code_comments': False,  
+    }
+}
+
+setup(
+    name='aixhello',
+    version='5.12',  
+    description='AI HRM Package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='SecureAI',
+    author_email='package@xerocai.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',  
+        'cryptography',
+        'openai',
+    ],
+    ext_modules=cythonize(extensions, **cythonize_options),
+    packages=['aixhello'],
+)
