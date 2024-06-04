dd9695b9568aa2344aa41f5294b72607d5aa70d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:05 +0000

    Add package OmniGenome/0.0.6a0/OmniGenome-0.0.6a0-py3-none-any.whl

---
+++ packages/OmniGenome/OmniGenome-0.0.6a0-py3-none-any.whl/tutorials/benchmark/GUE/metadata.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# file: metadata.py
+# time: 22:57 26/04/2024
+# author: YANG, HENG <hy345@exeter.ac.uk> (杨恒)
+# github: https://github.com/yangheng95
+# huggingface: https://huggingface.co/yangheng
+# google scholar: https://scholar.google.com/citations?user=NPq5a_0AAAAJ&hl=en
+# Copyright (C) 2019-2024. All Rights Reserved.
+
+__name__ = "GUE"
+__source__ = "https://github.com/MAGICS-LAB/DNABERT_2"
+__author__ = ""
+__version__ = "1.0.0"
+__time__ = "22:57 26/04/2024"
+
+bench_list = [
+    "EMP.H3",
+    "EMP.H3K4me1",
+    "EMP.H3K4me2",
+    "EMP.H3K4me3",
+    "EMP.H3K9ac",
+    "EMP.H3K14ac",
+    "EMP.H3K36me3",
+    "EMP.H3K79me3",
+    "EMP.H4",
+    "EMP.H4ac",
+    "mouse.0",
+    "mouse.1",
+    "mouse.2",
+    "mouse.3",
+    "mouse.4",
+    "prom.prom_300_all",
+    "prom.prom_300_notata",
+    "prom.prom_300_tata",
+    "prom.prom_core_all",
+    "prom.prom_core_notata",
+    "prom.prom_core_tata",
+    "splice.reconstructed",
+    "tf.0",
+    "tf.1",
+    "tf.2",
+    "tf.3",
+    "tf.4",
+    "virus.covid",
+]
