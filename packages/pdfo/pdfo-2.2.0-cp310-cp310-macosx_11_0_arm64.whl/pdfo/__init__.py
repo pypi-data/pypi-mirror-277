316ccaea483b153c9941f88724b2c7116dad1206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 05:03:01 +0000

    Add package pdfo/2.2.0/pdfo-2.2.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pdfo/pdfo-2.2.0-cp310-cp310-macosx_11_0_arm64.whl/pdfo/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Management of the importable functions of pdfo."""
+from ._bobyqa import bobyqa
+from ._cobyla import cobyla
+from ._lincoa import lincoa
+from ._newuoa import newuoa
+from ._uobyqa import uobyqa
+from ._pdfo import pdfo
+from . import tests
+from .tests import test_pdfo as testpdfo
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Final release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'.
+__version__ = '2.1.0'
+
+__all__ = ['bobyqa', 'cobyla', 'lincoa', 'newuoa', 'uobyqa', 'pdfo', 'tests', 'testpdfo']
