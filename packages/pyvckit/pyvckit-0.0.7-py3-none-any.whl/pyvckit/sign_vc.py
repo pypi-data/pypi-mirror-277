b71e8aba0bf6afaa79b45b1b808a2f65cc8190a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:58 +0000

    Add package pyvckit/0.0.7/pyvckit-0.0.7-py3-none-any.whl

---
+++ packages/pyvckit/pyvckit-0.0.7-py3-none-any.whl/pyvckit/sign_vc.py
@@ -0,0 +1,47 @@
+import json
+import argparse
+from pyvckit.utils import now
+from pyvckit.did import generate_did, get_signing_key, key_read
+from pyvckit.templates import credential_tmpl, proof_tmpl
+from pyvckit.sign import sign_proof
+
+
+# source: https://github.com/mmlab-aueb/PyEd25519Signature2018/blob/master/signer.py
+
+def sign(credential, key, issuer_did):
+    document = json.loads(credential)
+    _did = issuer_did + "#" + issuer_did.split("did:key:")[1]
+    proof = json.loads(proof_tmpl)
+    proof['verificationMethod'] = _did
+    proof['created'] = now()
+
+    sign_proof(document, proof, key)
+    del proof['@context']
+    document['proof'] = proof
+    return document
+
+
+def main():
+    parser=argparse.ArgumentParser(description='Generates a new credential')
+    parser.add_argument("-k", "--key-path", required=True)
+    args=parser.parse_args()
+
+    if args.key_path:
+        key = key_read(args.key_path)
+        did = generate_did(key)
+        signing_key = get_signing_key(key)
+
+        credential = json.loads(credential_tmpl)
+        credential["issuer"] = did
+        credential["issuanceDate"] = now()
+        cred = json.dumps(credential)
+
+        vc = sign(cred, signing_key, did)
+
+        print(json.dumps(vc, separators=(',', ':')))
+
+        return
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pyvckit/pyvckit-0.0.7-py3-none-any.whl/pyvckit/sign_vp.py
@@ -0,0 +1,50 @@
+import json
+import argparse
+
+from pyvckit.utils import now
+from pyvckit.templates import presentation_tmpl, proof_tmpl
+from pyvckit.did import key_read, generate_did, get_signing_key
+from pyvckit.sign import sign_proof
+
+
+def sign_vp(signing_key, holder_did, vc):
+    presentation = json.loads(presentation_tmpl)
+    presentation["verifiableCredential"].append(json.loads(vc))
+    presentation["holder"] = holder_did
+
+    _did = holder_did + "#" + holder_did.split("did:key:")[1]
+    proof = json.loads(proof_tmpl)
+    proof['verificationMethod'] = _did
+    proof['created'] = now()
+
+    sign_proof(presentation, proof, signing_key)
+    del proof['@context']
+    presentation['proof'] = proof
+    return presentation
+
+
+def main():
+    parser=argparse.ArgumentParser(description='Generates a new credential')
+    parser.add_argument("-k", "--key-path", required=True)
+    parser.add_argument("-c", "--credential-path", required=True)
+    args=parser.parse_args()
+
+    if args.key_path and args.credential_path:
+        with open(args.credential_path, "r") as f:
+            vc = f.read()
+
+        if not vc:
+            print("You need pass a credential.")
+            return
+
+        key = key_read(args.key_path)
+        did = generate_did(key)
+        signing_key = get_signing_key(key)
+        vp = sign_vp(signing_key, did, vc)
+        print(json.dumps(vp, separators=(',', ':')))
+
+        return
+
+
+if __name__ == "__main__":
+    main()
