00f8f0ed4dd7d4c863f3e67ec8f9419c3d3d8e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 17:04:08 +0000

    Add package qatg/0.6.1/qatg-0.6.1.tar.gz

---
+++ packages/qatg/qatg-0.6.1.tar.gz/qatg-0.6.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='qatg',
+    description='An open-source quantum automatic test generator, compatible with Qiskit.',
+    url='https://github.com/NTU-LaDS-II/qATG',
+    author='Chen-Hung Wu, Chin-Yang Jen, Ching-Huan Wang, Cheng-Yun Hsieh, Jiun-Yun Li, James Chien-Mo Li',
+    author_email='lads427@gmail.com',
+    license='BSD 3-clause',
+    packages=['qatg'],
+    install_requires=['qiskit==0.39',
+					  'pylatexenc>=2.10',
+					  'scipy>=1.7.1',
+                      'ipympl>=0.9.2',
+					  'numpy>=1.20.3'
+                      ],
+
+)
