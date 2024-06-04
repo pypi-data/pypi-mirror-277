e2bc5b0b2775c9b20efea216f325b40be0518509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 03:03:34 +0000

    Add package rwkv-tokenizer/0.3.1/rwkv_tokenizer-0.3.1.tar.gz

---
+++ packages/rwkv-tokenizer/rwkv_tokenizer-0.3.1.tar.gz/rwkv_tokenizer-0.3.1/src/lib.rs
@@ -0,0 +1,86 @@
+mod trie;
+extern crate unescape;
+use pyo3::prelude::*;
+use std::fs::File;
+use std::io::{self, BufRead};
+use regex::Regex;
+use trie::Trie;
+use unescape::unescape;
+
+#[derive(Debug)]
+#[pyclass]
+pub(crate) struct Tokenizer {
+    tokens: Vec<String>,
+    trie: Trie
+}
+
+#[pymethods]
+impl Tokenizer {
+    #[new]
+    pub(crate) fn new(filename: &str) -> io::Result<Self> {
+        let mut tokenizer = Tokenizer {
+            tokens: Vec::new(),
+            trie: Trie::new()
+        };
+        let file = File::open(filename)?;
+        let reader = io::BufReader::new(file);
+
+        let re = Regex::new(r"(\d+)\s+(b?)(.+)\s+(\d+)").unwrap();
+        tokenizer.tokens.push("".to_string());
+        for line in reader.lines() {
+            let line = line?;
+            if let Some(captures) = re.captures(&line) {
+                let id = captures[1].parse::<u16>().unwrap();
+                let is_byte = captures[2].to_string();
+                let length = captures[4].parse::<usize>().unwrap();
+                let mut string: String = captures[3].to_string();
+                string = string[1..string.len()-1].parse().unwrap();
+                let sbytes: Vec<u8>;
+                if is_byte.len() == 0 {
+                    string = unescape(string.as_str()).unwrap();
+                    sbytes = string.clone().into_bytes();
+                } else {
+                    sbytes = hex_to_bytes(string.as_str()).unwrap();
+                }
+                assert_eq!(sbytes.len(), length);
+                tokenizer.trie.insert(&sbytes, id);
+                tokenizer.tokens.push(string.to_string());
+            }
+            else {
+                println!("Line with issue: {:?}", line)
+            }
+        }
+        Ok(tokenizer)
+    }
+
+    pub(crate) fn encode(&self, word: &str) -> Vec<u16> {
+        self.trie.tokenize(word)
+    }
+
+    pub(crate) fn decode(&self, vec: Vec<u16>) -> String {
+        let mut result: String = "".to_owned();
+        for index in vec.iter() {
+            result.push_str(&*self.tokens[*index as usize]);
+        }
+        return result;
+    }
+}
+
+fn hex_to_bytes(hex: &str) -> Option<Vec<u8>> {
+    let hex = hex.replace("\\x", "");
+    if hex.len() % 2 == 0 {
+        (0..hex.len())
+            .step_by(2)
+            .map(|i| hex.get(i..i + 2)
+                .and_then(|sub| u8::from_str_radix(sub, 16).ok()))
+            .collect()
+    } else {
+        None
+    }
+}
+
+#[pymodule]
+fn rwkv_tokenizer(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Tokenizer>()?;
+    Ok(())
+}
---
+++ packages/rwkv-tokenizer/rwkv_tokenizer-0.3.1.tar.gz/rwkv_tokenizer-0.3.1/src/trie.rs
@@ -0,0 +1,95 @@
+#[derive(Default, Debug)]
+struct TrieNode {
+    children: Vec<Option<TrieNode>>,
+    id: u16
+}
+
+
+impl TrieNode {
+    fn new() -> Self {
+        let mut trinode = TrieNode {
+            children: Vec::new(),
+            id: 0
+        };
+        for _ in 0..256 {
+            trinode.children.push(None);
+        }
+        trinode
+    }
+}
+
+#[derive(Debug)]
+pub struct Trie {
+    root: TrieNode,
+}
+
+
+impl Trie {
+    pub(crate) fn new() -> Self {
+        Trie {
+            root: TrieNode::new(),
+        }
+    }
+
+    pub(crate) fn insert(&mut self, word: &Vec<u8>, id: u16) {
+        let mut node = &mut self.root;
+        for ch in word {
+            if node.children[u8::from_be(*ch) as usize].is_none() {
+                node.children[u8::from_be(*ch) as usize] = Option::from(TrieNode::new());
+            }
+            match &mut node.children[u8::from_be(*ch) as usize] {
+                Some(next_node) => node = next_node,
+                None => unreachable!(),  // We've just checked that it's not None
+            }
+        }
+        node.id = id
+    }
+
+    fn search_the_longest(&self, word: &[u8]) -> (u16, u16) {
+        let mut node = &self.root;
+        let mut old_node: &TrieNode = &self.root;
+        let mut index = 0;
+        let mut old_index = 0;
+        for ch in word {
+            if let Some(next_node) = &node.children[*ch as usize] {
+                if node.id != 0 {
+                    old_node = node;
+                    old_index = index;
+                }
+                node = &next_node;
+                index += 1;
+            } else {
+                if node.id == 0 {
+                    return (old_index, old_node.id);
+                }
+                else {
+                    return (index, node.id);
+                }
+            }
+        }
+        if node.id == 0 {
+            return (old_index, old_node.id);
+        }
+        else {
+            return (index, node.id);
+        }
+    }
+
+    pub(crate) fn tokenize(&self, text: &str) -> Vec<u16> {
+        let mut vec: Vec<u16> = Vec::new();
+        let text_length = text.len();
+        let mut index: usize = 0;
+        loop {
+            let result = self.search_the_longest(&text.as_bytes()[index..]);
+            if result.0 != 0 {
+                vec.push(result.1.into());
+                index += <u16 as Into<usize>>::into(result.0);
+            } else {
+                return vec;
+            }
+            if index >= text_length {
+                return vec;
+            }
+        }
+    }
+}
