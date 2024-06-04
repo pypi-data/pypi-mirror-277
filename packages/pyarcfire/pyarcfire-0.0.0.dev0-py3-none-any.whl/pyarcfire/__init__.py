114a205468873ee167fd3f3ab0b3777f8641a10f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:31 +0000

    Add package pyarcfire/0.0.0.dev0/pyarcfire-0.0.0.dev0-py3-none-any.whl

---
+++ packages/pyarcfire/pyarcfire-0.0.0.dev0-py3-none-any.whl/pyarcfire/__init__.py
@@ -0,0 +1,22 @@
+from .arc import fit_spiral_to_image
+from .cluster import generate_clusters, GenerateClustersSettings
+from .merge_fit import merge_clusters_by_fit, MergeClustersByFitSettings
+from .orientation import generate_orientation_fields, GenerateOrientationFieldSettings
+from .similarity import generate_similarity_matrix, GenerateSimilarityMatrixSettings
+from .spiral import ClusterSpiralResult, detect_spirals_in_image, UnsharpMaskSettings
+
+
+__all__ = [
+    "ClusterSpiralResult",
+    "GenerateClustersSettings",
+    "GenerateOrientationFieldSettings",
+    "GenerateSimilarityMatrixSettings",
+    "MergeClustersByFitSettings",
+    "UnsharpMaskSettings",
+    "detect_spirals_in_image",
+    "fit_spiral_to_image",
+    "generate_clusters",
+    "generate_orientation_fields",
+    "generate_similarity_matrix",
+    "merge_clusters_by_fit",
+]
---
+++ packages/pyarcfire/pyarcfire-0.0.0.dev0-py3-none-any.whl/pyarcfire/arc/common.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+import numpy as np
+from numpy.typing import NDArray
+
+from .functions import log_spiral
+
+FloatType = TypeVar("FloatType", np.float32, np.float64)
+
+
+@dataclass
+class LogSpiralFitResult(Generic[FloatType]):
+    offset: float
+    pitch_angle: float
+    initial_radius: float
+    arc_bounds: tuple[float, float]
+    total_error: float
+    errors: NDArray[FloatType]
+    has_multiple_revolutions: bool
+
+    def calculate_cartesian_coordinates(
+        self, num_points: int
+    ) -> tuple[NDArray[np.float32], NDArray[np.float32]]:
+        start_angle = self.offset
+        end_angle = start_angle + self.arc_bounds[1]
+
+        theta = np.linspace(start_angle, end_angle, num_points, dtype=np.float32)
+        radii = log_spiral(
+            theta,
+            self.offset,
+            self.pitch_angle,
+            self.initial_radius,
+            use_modulo=not self.has_multiple_revolutions,
+        )
+        x = np.multiply(radii, np.cos(theta))
+        y = np.multiply(radii, np.sin(theta))
+        return (x, y)
