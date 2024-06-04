6d9109cd884b2a64085aebbad441a778d0f6b1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 14:04:31 +0000

    Add package pinnacle-python/0.0.2a0/pinnacle_python-0.0.2a0.tar.gz

---
+++ packages/pinnacle-python/pinnacle_python-0.0.2a0.tar.gz/pinnacle_python-0.0.2a0/pinnacle_python/endpoints.py
@@ -0,0 +1,18 @@
+endpoints = {
+    "GET": [],
+    "POST": [],
+    "PUT": [],
+    "DELETE": [],
+    "PATCH": [],
+    "OPTIONS": [],
+    "HEAD": [],
+}  # Dictionary of HTTP methods and their corresponding endpoints
+
+
+def endpoint(method: str = "POST"):
+    def endpoint_decorator(func):
+        if method not in endpoints:
+            raise ValueError(f"Unsupported HTTP method: {method}")
+        endpoints[method].append(func)
+
+    return endpoint_decorator
