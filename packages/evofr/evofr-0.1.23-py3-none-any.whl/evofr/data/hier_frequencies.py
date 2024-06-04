fa05e5d00a0fca5735991e3763d8aef4ef550431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 06:04:22 +0000

    Add package evofr/0.1.23/evofr-0.1.23-py3-none-any.whl

---
+++ packages/evofr/evofr-0.1.23-py3-none-any.whl/evofr/data/hier_frequencies.py
@@ -0,0 +1,70 @@
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+
+from .data_helpers import format_var_names, prep_dates
+from .data_spec import DataSpec
+from .variant_frequencies import VariantFrequencies
+
+
+class HierFrequencies(DataSpec):
+    def __init__(
+        self,
+        raw_seq: pd.DataFrame,
+        group: str,
+        date_to_index: Optional[dict] = None,
+        pivot: Optional[str] = None,
+    ):
+        """Construct a data specification for handling variant frequencies
+        in hierarchical models.
+
+        Parameters
+        ----------
+        raw_seq:
+            a dataframe containing sequence counts with columns 'sequences',
+            'variant', and date'.
+
+        group:
+            string defining which column to seperate data by.
+
+        date_to_index:
+            optional dictionary for mapping calender dates to nd.array indices.
+
+        pivot:
+            optional name of variant to place last.
+            Defaults to "other" if present otherwise.
+            This will usually used as a reference or pivot strain.
+
+        Returns
+        -------
+        HierFrequencies
+        """
+        # Get date to index
+        if date_to_index is None:
+            self.dates, date_to_index = prep_dates(raw_seq["date"])
+        self.date_to_index = date_to_index
+
+        # Get variant names
+        raw_var_names = list(pd.unique(raw_seq.variant))
+        raw_var_names.sort()
+        self.var_names = format_var_names(raw_var_names, pivot=pivot)
+        self.pivot = self.var_names[-1]
+
+        # Loop each group
+        grouped = raw_seq.groupby(group)
+        self.names = [name for name, _ in grouped]
+        self.groups = [
+            VariantFrequencies(group, self.date_to_index, self.var_names)
+            for _, group in grouped
+        ]
+
+        self.seq_counts = np.stack([g.seq_counts for g in self.groups], axis=-1)
+
+    def make_data_dict(self, data: Optional[dict] = None) -> dict:
+        if data is None:
+            data = dict()
+        data["seq_counts"] = np.stack([g.seq_counts for g in self.groups], axis=-1)
+        data["N"] = np.stack([g.seq_counts.sum(axis=-1) for g in self.groups], axis=-1)
+        data["var_names"] = self.var_names
+        return data
