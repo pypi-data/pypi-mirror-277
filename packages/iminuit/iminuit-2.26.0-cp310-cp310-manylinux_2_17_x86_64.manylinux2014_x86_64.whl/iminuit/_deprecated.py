273e548d54aa7b4227873200d106d49d1da472bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:33 +0000

    Add package iminuit/2.26.0/iminuit-2.26.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/iminuit/iminuit-2.26.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/iminuit/_deprecated.py
@@ -0,0 +1,41 @@
+import warnings
+from ._exceptions import VisibleDeprecationWarning
+
+
+class deprecated:
+    def __init__(self, reason):
+        self._reason = reason
+
+    def __call__(self, func):
+        def decorated_func(*args, **kwargs):
+            warnings.warn(
+                f"{func.__name__} is deprecated: {self._reason}",
+                category=VisibleDeprecationWarning,
+                stacklevel=2,
+            )
+            return func(*args, **kwargs)
+
+        decorated_func.__name__ = func.__name__
+        decorated_func.__doc__ = "deprecated: " + self._reason
+        return decorated_func
+
+
+class deprecated_parameter:
+    def __init__(self, **replacements):
+        self._replacements = replacements
+
+    def __call__(self, func):
+        def decorated_func(*args, **kwargs):
+            for new, old in self._replacements.items():
+                if old in kwargs:
+                    warnings.warn(
+                        f"keyword {old!r} is deprecated, please use {new!r}",
+                        category=VisibleDeprecationWarning,
+                        stacklevel=2,
+                    )
+                    kwargs[new] = kwargs[old]
+                    del kwargs[old]
+            return func(*args, **kwargs)
+
+        decorated_func.__name__ = func.__name__
+        return decorated_func
