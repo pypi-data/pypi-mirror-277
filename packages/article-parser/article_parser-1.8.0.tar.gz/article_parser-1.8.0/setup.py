b265bcce6f4f732b491b03826c51bbc791af554c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:26 +0000

    Add package article-parser/1.8.0/article_parser-1.8.0.tar.gz

---
+++ packages/article-parser/article_parser-1.8.0.tar.gz/article_parser-1.8.0/setup.py
@@ -0,0 +1,48 @@
+from os import path as os_path
+from setuptools import setup, find_packages
+
+this_directory = os_path.abspath(os_path.dirname(__file__))
+
+
+def read_file(filename):
+    with open(os_path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+
+setup(
+    name="article-parser",
+    version='1.8.0',
+    author="myifeng",
+    author_email="myifengs@gmail.com",
+    maintainer="myifeng",
+    maintainer_email="myifengs@gmail.com",
+    keywords="article news html parser Extract extractor body",
+    description="A parser that parses articles from any url or html",
+    long_description=read_file('README.md'),
+    long_description_content_type="text/markdown",
+    url="https://github.com/myifeng/article-parser",
+    packages=find_packages(exclude=['.github', 'test']),
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation",
+    ],
+    python_requires='>=3.8',
+    version_config={
+        "template": "{tag}",
+        "dirty_template": "{tag}",
+        "dev_template": "{tag}"
+    },
+    setup_requires=["setuptools-git-versioning"],
+    install_requires = ['beautifulsoup4==4.12.3', 'html2text==2020.1.16', 'requests==2.31.0', 'lxml==5.2.2']
+)
