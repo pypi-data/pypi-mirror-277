6463104e982e4d8e18127ef28b6c03ee809654f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:42 +0000

    Add package kaldi-native-fbank/1.19.3/kaldi_native_fbank-1.19.3-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/kaldi-native-fbank/kaldi_native_fbank-1.19.3-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/kaldi_native_fbank/__init__.py
@@ -0,0 +1,12 @@
+from _kaldi_native_fbank import (
+    FbankOptions,
+    FeatureWindowFunction,
+    FrameExtractionOptions,
+    MelBanksOptions,
+    MfccOptions,
+    OnlineFbank,
+    OnlineMfcc,
+    OnlineWhisperFbank,
+    Rfft,
+)
+__version__ = '1.19.3'
