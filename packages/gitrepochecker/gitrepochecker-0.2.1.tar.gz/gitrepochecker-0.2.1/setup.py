af6ad2386eb316a870a00c988e9d900fdc119257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 14:03:41 +0000

    Add package gitrepochecker/0.2.1/gitrepochecker-0.2.1.tar.gz

---
+++ packages/gitrepochecker/gitrepochecker-0.2.1.tar.gz/gitrepochecker-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='repochecker',
+    description='Command line tool to check all git repositories in a directory',
+    author='Oli',
+    author_email='oli@olillin.com',
+    license='MIT',
+    py_modules=['repochecker'],
+    install_requires=[
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'repochecker=repochecker.main:main',
+        ]
+    },
+)
