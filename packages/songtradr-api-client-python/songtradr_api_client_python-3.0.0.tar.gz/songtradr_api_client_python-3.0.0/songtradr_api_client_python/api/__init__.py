6ce1174793c2797e82216773f61c39e6308dbb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:22 +0000

    Add package songtradr-api-client-python/3.0.0/songtradr_api_client_python-3.0.0.tar.gz

---
+++ packages/songtradr-api-client-python/songtradr_api_client_python-3.0.0.tar.gz/songtradr_api_client_python-3.0.0/songtradr_api_client_python/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from songtradr_api_client_python.api.allowed_values_api import AllowedValuesApi
+from songtradr_api_client_python.api.recording_api import RecordingApi
+from songtradr_api_client_python.api.user_api import UserApi
+
