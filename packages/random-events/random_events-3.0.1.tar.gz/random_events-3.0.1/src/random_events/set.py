b066a98f2aba361d9f6326ec6773de04d373e6a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 03:03:13 +0000

    Add package random-events/3.0.1/random_events-3.0.1.tar.gz

---
+++ packages/random-events/random_events-3.0.1.tar.gz/random_events-3.0.1/src/random_events/set.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+import enum
+
+from sortedcontainers import SortedSet
+from typing_extensions import Self, TYPE_CHECKING, Dict, Any
+
+from .sigma_algebra import *
+
+
+class SetElement(AbstractSimpleSet, enum.Enum):
+    """
+    Base class for enums that are used as elements in a set.
+
+    Classes that inherit from this class have to define an attribute called EMPTY_SET.
+    """
+
+    @property
+    @abstractmethod
+    def EMPTY_SET(self):
+        raise NotImplementedError("The EMPTY_SET attribute has to be defined.")
+
+    @property
+    def all_elements(self):
+        return self.__class__
+
+    def intersection_with(self, other: Self) -> Self:
+        if self == other:
+            return self
+        else:
+            return self.all_elements.EMPTY_SET
+
+    def complement(self) -> SimpleSetContainer:
+        result = SortedSet()
+        for element in self.all_elements:
+            if element != self and element != self.all_elements.EMPTY_SET:
+                result.add(element)
+        return result
+
+    def is_empty(self) -> bool:
+        return self == self.EMPTY_SET
+
+    def contains(self, item: Self) -> bool:
+        return self == item
+
+    def non_empty_to_string(self) -> str:
+        return self.name
+
+    def __hash__(self):
+        return enum.Enum.__hash__(self)
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def to_json(self) -> Dict[str, Any]:
+        return {**super().to_json(), "value": self.value}
+
+    @classmethod
+    def _from_json(cls, data: Dict[str, Any]) -> Self:
+        return cls(data["value"])
+
+    def as_composite_set(self) -> AbstractCompositeSet:
+        return Set(self)
+
+
+class Set(AbstractCompositeSet):
+    simple_sets: SetElementContainer
+
+    def complement_if_empty(self) -> Self:
+        raise NotImplementedError("I don't know how to do this yet.")
+
+    def simplify(self) -> Self:
+        return self
+
+    def new_empty_set(self) -> Self:
+        return Set()
+
+    def make_disjoint(self) -> Self:
+        return self
+
+
+# Type definitions
+if TYPE_CHECKING:
+    SetElementContainer = SortedSet[SetElement]
+else:
+    SetElementContainer = SortedSet
