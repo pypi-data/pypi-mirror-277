baae82a424c10d9a1d3af3be23ccba0c1e0e2b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:45 +0000

    Add package tmp-box/0.0.3/tmp_box-0.0.3.tar.gz

---
+++ packages/tmp-box/tmp_box-0.0.3.tar.gz/tmp_box-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+	name="tmp_box",
+	version="0.0.3",
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=[
+		"typer>=0.12.3",
+		"pydantic>=2.7.2"
+	],
+	entry_points={
+		"console_scripts": [
+			"tmp-box = app.main:app"
+		]
+	}
+)
