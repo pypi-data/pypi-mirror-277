197f9bf091d44c0e91903a75e6a82dd0acaf618c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:47 +0000

    Add package awesome-object-store/3.0.0/awesome_object_store-3.0.0-py3-none-any.whl

---
+++ packages/awesome-object-store/awesome_object_store-3.0.0-py3-none-any.whl/awesome_object_store/__init__.py
@@ -0,0 +1,33 @@
+from logging import Logger
+from typing import Optional
+
+from awesome_object_store.base import BaseObjectStore
+from awesome_object_store.gcs import GoogleCloudStore
+from awesome_object_store.minio import MinioStore
+
+
+def init_object_store(
+    bucket: str,
+    host: Optional[str] = None,
+    access_key: str = None,
+    secret_key: str = None,
+    secure: bool = False,
+    region: str = None,
+    logger: Optional[Logger] = None,
+    protocol: Optional[str] = "gcs",
+) -> BaseObjectStore:
+    if protocol == "gcs":
+        return GoogleCloudStore(bucket, logger)
+    else:
+        if host is None:
+            ValueError("Host must be provided for MinioStore")
+
+        return MinioStore(
+            bucket,
+            str(host),
+            access_key,
+            secret_key,
+            secure,
+            region,
+            logger,
+        )
