77b5ec04b480044bd872f1c01069b65a2b9427db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:31 +0000

    Add package dask-databricks/0.3.2/dask_databricks-0.3.2-py3-none-any.whl

---
+++ packages/dask-databricks/dask_databricks-0.3.2-py3-none-any.whl/dask_databricks/databrickscluster.py
@@ -0,0 +1,63 @@
+import os
+import uuid
+from typing import Optional
+
+from distributed.core import rpc
+from distributed.deploy.cluster import Cluster
+from tornado.ioloop import IOLoop
+
+# Databricks Notebooks injects the `spark` session variable but we need to create it ourselves
+try:
+    from pyspark.sql import SparkSession
+
+    spark = SparkSession.getActiveSession()
+except ImportError:
+    spark = None
+
+
+class DatabricksCluster(Cluster):
+    """Connect to a Dask cluster deployed via databricks."""
+
+    def __init__(
+        self,
+        loop: Optional[IOLoop] = None,
+        asynchronous: bool = False,
+    ):
+        self.spark_local_ip = os.environ.get("SPARK_LOCAL_IP")
+        if self.spark_local_ip is None:
+            raise KeyError(
+                "Unable to find expected environment variable SPARK_LOCAL_IP. "
+                "Are you running this on a Databricks driver node?"
+            )
+        if os.environ.get("MASTER") and "local[" in os.environ.get("MASTER"):
+            raise EnvironmentError(
+                "You appear to be trying to run a multi-node Dask cluster on a "
+                "single-node databricks cluster. Maybe you want "
+                "`dask.distributed.LocalCluster().get_client()` instead"
+
+            )
+        try:
+            name = spark.conf.get("spark.databricks.clusterUsageTags.clusterId")
+        except AttributeError:
+            name = "unknown-databricks-" + uuid.uuid4().hex[:10]
+        super().__init__(name=name, loop=loop, asynchronous=asynchronous)
+
+        if not self.called_from_running_loop:
+            self._loop_runner.start()
+            self.sync(self._start)
+
+    async def _start(self):
+        self.scheduler_comm = rpc(f"{self.spark_local_ip}:8786")
+        await super()._start()
+
+    @property
+    def dashboard_link(self):
+        cluster_id = spark.conf.get("spark.databricks.clusterUsageTags.clusterId")
+        org_id = spark.conf.get("spark.databricks.clusterUsageTags.orgId")
+        workspace_url = spark.conf.get("spark.databricks.workspaceUrl")
+        return f"https://{workspace_url}/driver-proxy/o/{org_id}/{cluster_id}/8087/status"
+
+
+def get_client():
+    """Get a Dask client connected to a Databricks cluster."""
+    return DatabricksCluster().get_client()
---
+++ packages/dask-databricks/dask_databricks-0.3.2-py3-none-any.whl/dask_databricks/tests/test_databricks.py
@@ -0,0 +1,66 @@
+import os
+
+import pytest
+from dask.distributed import Client
+from distributed.deploy import Cluster, LocalCluster
+
+from dask_databricks import DatabricksCluster, get_client
+
+
+@pytest.fixture(scope="session")
+def dask_cluster():
+    """Start a LocalCluster to simulate the cluster that would be started on Databricks."""
+    return LocalCluster(scheduler_port=8786)
+
+
+@pytest.fixture
+def remove_spark_local_ip():
+    original_spark_local_ip = os.getenv("SPARK_LOCAL_IP")
+    if original_spark_local_ip:
+        del os.environ["SPARK_LOCAL_IP"]
+    yield None
+    if original_spark_local_ip:
+        os.environ["SPARK_LOCAL_IP"] = original_spark_local_ip
+
+
+@pytest.fixture
+def set_spark_local_ip():
+    original_spark_local_ip = os.getenv("SPARK_LOCAL_IP")
+    os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
+    yield None
+    if original_spark_local_ip:
+        os.environ["SPARK_LOCAL_IP"] = original_spark_local_ip
+    else:
+        del os.environ["SPARK_LOCAL_IP"]
+
+
+def test_databricks_cluster_raises_key_error_when_initialised_outside_of_databricks(remove_spark_local_ip):
+    with pytest.raises(KeyError):
+        DatabricksCluster()
+
+def test_databricks_cluster_raises_environment_error_when_master_variable_implies_single_node(
+    monkeypatch,
+    set_spark_local_ip,
+    dask_cluster,
+):
+    monkeypatch.setenv("MASTER", "local[8]")
+    with pytest.raises(EnvironmentError):
+        DatabricksCluster()
+
+def test_databricks_cluster_create(set_spark_local_ip, dask_cluster):
+    cluster = DatabricksCluster()
+    assert isinstance(cluster, Cluster)
+
+
+def test_databricks_cluster_create_client(set_spark_local_ip, dask_cluster):
+    cluster = DatabricksCluster()
+    client = Client(cluster)
+    assert isinstance(client, Client)
+    assert client.submit(sum, (10, 1)).result() == 11
+
+
+def test_get_client(set_spark_local_ip, dask_cluster):
+    client = get_client()
+    assert isinstance(client, Client)
+    assert isinstance(client.cluster, DatabricksCluster)
+    assert client.submit(sum, (10, 1)).result() == 11
