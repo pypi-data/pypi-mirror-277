a8f1f69406ef857218273f737ecb9183b10b8246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:15 +0000

    Add package rt-utils-raystation/1.0.0/rt-utils-raystation-1.0.0.tar.gz

---
+++ packages/rt-utils-raystation/rt-utils-raystation-1.0.0.tar.gz/rt-utils-raystation-1.0.0/README.md
@@ -0,0 +1,10 @@
+# Installation
+```
+pip install rt_utils
+```
+
+# Credits
+
+This work is a clone of the following repo: https://github.com/qurit/rt-utils
+For a full documentation, please check it out. 
+We just adpot this repo and introduces fixes regarding the SeriesInstanceUID to match resulting rt-struct.dcm files with corresponding MRI series. 
---
+++ packages/rt-utils-raystation/rt-utils-raystation-1.0.0.tar.gz/rt-utils-raystation-1.0.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+VERSION = "1.0.0"
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="rt-utils-raystation",
+    version=VERSION,
+    author="Lukas Heine, Fabian Hörst",
+    author_email="fabian.hoerst@uk-essen.de",
+    description="A small library for handling masks and RT-Structs, with fixed for using with RayStation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/code-lukas/rt-utils",
+    package_dir={'':"rt_utils"},
+    packages=setuptools.find_packages("rt_utils", exclude="tests"),
+    keywords=["RTStruct", "Dicom", "Pydicom"],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Typing :: Typed",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+    ],
+    python_requires=">=3.7",
+    install_requires=required,
+)
