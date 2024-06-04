598b3677ec857629385a9f2c1caa5da2be6a62b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 07:03:44 +0000

    Add package classy-sz/0.0.2/classy_sz-0.0.2.tar.gz

---
+++ packages/classy-sz/classy_sz-0.0.2.tar.gz/classy_sz-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup,find_packages
+
+
+
+
+setup(
+    name="classy_szfast",
+    version="0.0.2",
+    description="Python package for fast class_sz",
+    zip_safe=False,
+    packages=find_packages(),
+    author='Boris Bolliet et al',
+    author_email='boris.bolliet@gmail.com',
+    url='https://github.com/CLASS-SZ/class_sz',
+    download_url='https://github.com/CLASS-SZ/classy_szfast',
+    install_requires=[
+        "tensorflow==2.13.0",
+        "tensorflow-probability==0.21.0",
+        "cosmopower",
+        "mcfit"
+    ],
+    package_data={},
+)
