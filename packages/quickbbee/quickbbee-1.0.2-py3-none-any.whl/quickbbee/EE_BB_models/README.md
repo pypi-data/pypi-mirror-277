2ab56dfa9175bd4beb6846c1fe9cf9d74d4c1fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:16 +0000

    Add package quickbbee/1.0.2/quickbbee-1.0.2-py3-none-any.whl

---
+++ packages/quickbbee/quickbbee-1.0.2-py3-none-any.whl/quickbbee/EE_BB_models/README.md
@@ -0,0 +1,20 @@
+The steps taken for the creation of the EE and BB models:
+
+1. Firstly, we generated 100,000 parameters of dimension (100'000, 8) using [LatinHypercube](https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.qmc.LatinHypercube.html#scipy.stats.qmc.LatinHypercube). Then, we used these parameters as inputs into [CAMB](https://camb.readthedocs.io/en/latest/) to generate spectra using the `get_lensed_scalar_cls` function.
+   
+2. Next, we prepared the obtained data by transforming them into dictionaries with the following format: for the parameters, keys are the names of each parameter, attributes are the corresponding columns for each parameter. For the spectra, keys are the multipoles and the features are the respective attributes, consisting of the interval concerned (starting from 2 because indices 0 and 1 are zero), and the spectra. We logged the values to facilitate computation using [cosmopower](https://github.com/alessiospuriomancini/cosmopower) (logging for prediction and delogging afterwards).
+
+
+
+Here are the error graphs, for both the BB model, and the EE model:
+
+<div style="display: flex; flex-wrap: wrap;">
+  <img src="https://github.com/jusdelio/QuickBBEE/assets/43094323/194d5d47-fde0-43d9-b575-4900d7fce1eb" alt="Capture d'écran 2024-05-24 15:06:47" style="width: 24%; margin-right: 1%;">
+  <img src="https://github.com/jusdelio/QuickBBEE/assets/43094323/b429c55c-f486-4a8e-82df-ab239be30471" alt="Capture d'écran 2024-05-24 15:07:08" style="width: 24%; margin-right: 1%;">
+</div>
+<div style="display: flex; flex-wrap: wrap;">
+  <img src="https://github.com/jusdelio/QuickBBEE/assets/43094323/dda1169c-f02d-4ce7-81a4-0189f4952e2f" alt="Capture d'écran 2024-05-24 15:03:48" style="width: 24%; margin-right: 1%;">
+  <img src="https://github.com/jusdelio/QuickBBEE/assets/43094323/1b17e6f4-f88f-433c-aa7d-adf4b6601322" alt="Capture d'écran 2024-05-24 15:01:51" style="width: 24%;">
+</div>
+
+
---
+++ packages/quickbbee/quickbbee-1.0.2-py3-none-any.whl/quickbbee/bbee.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+# Author : Giulio GANCI and Emmanuel RASOLOFO
+import math as m
+import numpy as np
+from cosmopower import cosmopower_NN
+from cosmopower import cosmopower_PCAplusNN
+from numbers import Number
+import os
+dirname = os.path.dirname(os.path.abspath(__file__))
+
+
+def verify(val, interval, indice):
+    params_name = ['H0', 'Alens', 'r', 'tau', 'ns', 'As', 'ombh2', 'omch2']
+    if isinstance(val, list):
+        for i in range(len(val)):
+            if val[i] < interval[0] or val[i] > interval[1]:
+                raise ValueError(f"Invalid Interval Usage: The parameters {params_name[indice]} = {val[i]} is not in {interval}\n")
+    else:
+        if val < interval[0] or val > interval[1]:
+                raise ValueError(f"Invalid Interval Usage: The parameters {params_name[indice]} = {val} is not in {interval}\n")
+        return True
+    
+def get_drange():
+    return np.array([[65, 75],
+                           [0, 1],
+                           [0, 0.5],
+                           [0.03, 0.09],
+                           [0.94, 0.99],
+                           [m.exp(3.0)*10**-10, m.exp(3.5)*10**-10],
+                           [0.02, 0.025],
+                           [0.05, 0.30]
+                           ])
+
+class set_dparams:     
+    def __init__(self, 
+                 H0:None, 
+                 Alens:None, 
+                 r:None, 
+                 tau:None, 
+                 ns:None, 
+                 As:None, 
+                 ombh2:None, 
+                 omch2:None, 
+                 lmax=2400):
+        
+        '''CONSTRUCTOR'''
+        super(set_dparams, self).__init__()
+        
+        #verify variables
+        verify(H0, [65, 75], 0)
+        verify(Alens, [0, 1], 1) 
+        verify(r, [0, 0.5], 2) 
+        verify(tau ,[0.03, 0.09], 3) 
+        verify(ns, [0.94, 0.99], 4) 
+        verify(As, [m.exp(3.0)*10**-10, m.exp(3.5)*10**-10], 5) 
+        verify(ombh2, [0.020, 0.025], 6) 
+        verify(omch2, [0.05, 0.3], 7)
+        
+        if lmax < 0 or lmax > 2400:
+            raise ValueError("lmax should be between: 0 and 2400.")
+        
+        #set variables
+        self.lmax = lmax
+        self.H0 = H0
+        self.Alens = Alens
+        self.r = r
+        self.tau = tau
+        self.ns = ns
+        self.As = As
+        self.ombh2 = ombh2
+        self.omch2 = omch2
+        self.range_default = np.array([[65, 75],
+                           [0, 1],
+                           [0, 0.5],
+                           [0.03, 0.09],
+                           [0.94, 0.99],
+                           [m.exp(3.0)*10**-10, m.exp(3.5)*10**-10],
+                           [0.02, 0.025],
+                           [0.05, 0.30]
+                           ])
+        #model params_range specified by the user
+        self.range = None
+        
+    def check(self):
+        attribut = [self.H0, self.Alens,
+                    self.r,
+                    self.tau,
+                    self.ns,
+                    self.As,
+                    self.ombh2,
+                    self.omch2]
+        all_l = 0
+        all_num = 0
+        
+        for att in attribut:
+            if isinstance(att, list):
+                all_l += 1
+            if isinstance(att, Number):
+                all_num += 1
+        if all_l == len(attribut):
+            return True
+    
+        if all_num == len(attribut):
+            return False
+
+        else:
+            raise ValueError("All parameters are not the same type.")
+        
+    def get_spectra(self):       
+        cp_nn_EE = cosmopower_NN(restore=True,
+                        restore_filename = os.path.join(dirname,'./EE_BB_models/cp_NN_EE'),
+                        )
+        cp_nn_BB = cosmopower_NN(restore=True,
+                        restore_filename = os.path.join(dirname,'./EE_BB_models/cp_NN_BB'),
+                        )
+        
+        if self.check():
+            params = {
+                'H0' : self.H0,
+                'Alens':self.Alens,
+                'r':self.r,
+                'tau':self.tau,
+                'ns':self.ns,
+                'As':self.As,
+                'ombh2':self.ombh2,
+                'omch2':self.omch2
+            }
+        else:
+            params = {
+                'H0' : [self.H0],
+                'Alens':[self.Alens],
+                'r':[self.r],
+                'tau':[self.tau],
+                'ns':[self.ns],
+                'As':[self.As],
+                'ombh2':[self.ombh2],
+                'omch2':[self.omch2]
+            }            
+        predicted_EE = cp_nn_EE.ten_to_predictions_np(params)[:, :self.lmax]
+        predicted_BB = cp_nn_BB.ten_to_predictions_np(params)[:, :self.lmax]
+        return np.vstack((predicted_BB, predicted_EE))
+    
+    def get_params_interval(self, show=True, new_params=None):
+        if show:
+            print(f"Intervals:\nH0: {self.range_default[0]}\nAlens: {self.range_default[1]}\nr: {self.range_default[2]}\ntau: {self.range_default[3]}\nns: {self.range_default[4]}\nAs: {self.range_default[5]}\nombh2: {self.range_default[6]}\nomch2: {self.range_default[7]}\n")
+        return self.range_default
+    
+class set_cparams:
+    def __init__(self, 
+                 H0:None, 
+                 Alens:None, 
+                 r:None, 
+                 tau:None, 
+                 ns:None, 
+                 As:None, 
+                 ombh2:None, 
+                 omch2:None,
+                 filepath_model:None,
+                 params_range:None,
+                 is_PCA:bool,
+                 logspectra:bool,
+                 lmax:None
+                 ):
+        
+        '''CONSTRUCTOR'''
+        super(set_cparams, self).__init__()
+              
+        if lmax < 0:
+            raise ValueError("lmax should be positif.")
+        
+        #set variables
+        self.lmax = lmax
+        self.H0 = H0
+        self.Alens = Alens
+        self.r = r
+        self.tau = tau
+        self.ns = ns
+        self.As = As
+        self.ombh2 = ombh2
+        self.omch2 = omch2
+        self.filepath_model = filepath_model
+        self.params_range = params_range
+        self.is_PCA = is_PCA
+        self.logspectra = logspectra
+
+        #verify variables
+        verify(H0, self.params_range[0], 0)
+        verify(Alens, self.params_range[1], 1) 
+        verify(r, self.params_range[2], 2) 
+        verify(tau ,self.params_range[3], 3) 
+        verify(ns, self.params_range[4], 4) 
+        verify(As, self.params_range[5], 5) 
+        verify(ombh2, self.params_range[6], 6) 
+        verify(omch2, self.params_range[7], 7)
+
+    def check(self):
+        attribut = [self.H0, self.Alens,
+                    self.r,
+                    self.tau,
+                    self.ns,
+                    self.As,
+                    self.ombh2,
+                    self.omch2]
+        all_l = 0
+        all_num = 0
+        
+        for att in attribut:
+            if isinstance(att, list):
+                all_l += 1
+            if isinstance(att, Number):
+                all_num += 1
+        if all_l == len(attribut):
+            return True
+    
+        if all_num == len(attribut):
+            return False
+
+        else:
+            raise ValueError("All parameters are not the same type.")
+        
+        
+    def get_spectra(self):
+        if self.check():
+            params = {
+                'H0' : self.H0,
+                'Alens':self.Alens,
+                'r':self.r,
+                'tau':self.tau,
+                'ns':self.ns,
+                'As':self.As,
+                'ombh2':self.ombh2,
+                'omch2':self.omch2
+            }
+        else:
+            params = {
+                'H0' : [self.H0],
+                'Alens':[self.Alens],
+                'r':[self.r],
+                'tau':[self.tau],
+                'ns':[self.ns],
+                'As':[self.As],
+                'ombh2':[self.ombh2],
+                'omch2':[self.omch2]
+            }     
+        
+        chemin_absolu = os.path.abspath(self.filepath_model) 
+        print(f"Model used in: {chemin_absolu}\n")
+          
+
+        cp_nn = None
+        predicted = None
+        if(self.is_PCA):
+            cp_nn = cosmopower_PCAplusNN(restore=True,
+                        restore_filename=chemin_absolu,
+                        )
+            if self.logspectra:
+                predicted = cp_nn.ten_to_predictions_np(params)[:, :self.lmax]
+            else:
+                predicted = cp_nn.predictions_np(params)[:, :self.lmax]
+            return predicted
+            
+        else:
+            cp_nn = cosmopower_NN(restore=True,
+                        restore_filename=chemin_absolu,
+                        )
+            if self.logspectra:
+                predicted = cp_nn.ten_to_predictions_np(params)[:, :self.lmax]
+            else:
+                predicted = cp_nn.predictions_np(params)[:, :self.lmax]
+            return predicted
+        
+            
+        
+        
+
+    
+    def get_params_interval(self, show=True):
+        if show:
+            print(f"Intervals:\nH0: {self.params_range[0]}\nAlens: {self.params_range[1]}\nr: {self.params_range[2]}\ntau: {self.params_range[3]}\nns: {self.params_range[4]}\nAs: {self.params_range[5]}\nombh2: {self.params_range[6]}\nomch2: {self.params_range[7]}\n")
+        return self.params_range
+
