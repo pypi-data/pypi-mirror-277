213a56baa865ed4ab024d486947a643a76f1cf73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 17:04:08 +0000

    Add package django-xterm/0.2/django_xterm-0.2.tar.gz

---
+++ packages/django-xterm/django_xterm-0.2.tar.gz/django_xterm-0.2/xterm/static/xterm/js/xterm.js
@@ -0,0 +1,58 @@
+"use strict";
+
+(function () {
+  // vrt. https://github.com/Om3rr/pyxtermjs/master/pyxtermjs/index.html
+
+  let term = null;
+  let websocket = null;
+  let fit = null;
+
+  function fitToScreen(){
+    if (websocket && term && fit) {
+      fit.fit();
+      websocket.send(JSON.stringify({cols: term.cols, rows: term.rows}))
+    }
+  }
+  function debounce(func, wait_ms) {
+    let timeout
+    return function(...args) {
+      const context = this
+      clearTimeout(timeout)
+      timeout = setTimeout(function () { func.apply(context, args); }, wait_ms)
+    }
+  }
+
+  window.addEventListener(
+    "resize",
+    debounce(fitToScreen, 50),
+    {passive: true}
+  );
+
+  window.avaaXterm = function (url, asetukset) {
+    return new Promise(function (resolve, reject) {
+      if (term)
+        term.dispose();
+      if (websocket)
+        websocket.close();
+
+      term = new Terminal(asetukset);
+      fit = new FitAddon.FitAddon();
+      term.loadAddon(fit);
+      term.loadAddon(new WebLinksAddon.WebLinksAddon());
+      term.loadAddon(new SearchAddon.SearchAddon());
+      term.open(document.getElementById('xterm'));
+      fit.fit()
+      term.onKey(function(e) {
+        websocket.send(new Blob([e.key]));
+      });
+
+      websocket = new WebSocket(url);
+      websocket.onopen = function () {
+        resolve(websocket);
+      };
+      websocket.onmessage = function (e) {
+        term.write(e.data)
+      };
+    });
+  };
+})();
