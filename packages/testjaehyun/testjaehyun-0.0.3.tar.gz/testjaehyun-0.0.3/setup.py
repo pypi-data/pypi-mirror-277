ac19bc10ff1d70cbe939d72a83d31ced202aa7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:30 +0000

    Add package testjaehyun/0.0.3/testjaehyun-0.0.3.tar.gz

---
+++ packages/testjaehyun/testjaehyun-0.0.3.tar.gz/testjaehyun-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testjaehyun",  ## 소문자 영단어
+    version="0.0.3",  ##
+    author="Jaehyun Yim",  ## ex) Sunkyeong Lee
+    author_email="jhyim@cerestechs.com",  ##
+    description="test package distribution",  ##
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
