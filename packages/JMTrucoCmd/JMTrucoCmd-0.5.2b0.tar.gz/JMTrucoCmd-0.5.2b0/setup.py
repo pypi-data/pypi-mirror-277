ba569b7be17697373cd62b2094a9912b09b34dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:28 +0000

    Add package JMTrucoCmd/0.5.2b0/JMTrucoCmd-0.5.2b0.tar.gz

---
+++ packages/JMTrucoCmd/JMTrucoCmd-0.5.2b0.tar.gz/JMTrucoCmd-0.5.2b0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+readme = open("./README.md","r")
+
+setup(
+    name='JMTrucoCmd',
+    packages=['JMTrucoCmd'], # Mismo nombre que en la estructura de carpetas de arriba
+    version='0.5.2-b',
+    license='MIT', # La licencia que tenga tu paquete
+    description='Esta es la descripcion del truco',
+    long_description=readme.read(),
+    long_description_content_type='text/markdown',
+    author='Joaquin Morais',
+    author_email='joaquinmoraislussary@gmail.com',
+    url='https://github.com/JoaquinMorais/JMTrucoCmd', # Usa la URL del repositorio de GitHub
+    download_url='https://github.com/JoaquinMorais/JMTrucoCmd/tarball/v0.1', # Te lo explico a continuación
+    keywords=['testing','logging','example'], # Palabras que definan tu paquete
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6"
+)
