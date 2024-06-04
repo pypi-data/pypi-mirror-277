8602c13ece77254260daa5fbe0739d67e4cb5e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:11 +0000

    Add package iaed/1.0.1/iaed-1.0.1-py3-none-any.whl

---
+++ packages/iaed/iaed-1.0.1-py3-none-any.whl/iaed/__init__.py
@@ -0,0 +1,7 @@
+# iaed package
+# Author: Pedro Reis dos Santos (reis.santos@tecnico.ulisboa.pt)
+# @(c) IST, 2024
+
+__version__ = '1.0'
+__all__ = ['avl', 'btree', 'dbl', 'graph', # visible modules within the package
+        'hashdbl', 'hashext', 'hashlin', 'sorting', 'search']
