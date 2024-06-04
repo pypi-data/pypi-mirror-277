dc25c69bc4ccc3ba92733e92b3c7c190b750dcb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:44 +0000

    Add package GoogleCloudPlatformAPI/2.3.1/GoogleCloudPlatformAPI-2.3.1-py3-none-any.whl

---
+++ packages/GoogleCloudPlatformAPI/GoogleCloudPlatformAPI-2.3.1-py3-none-any.whl/GoogleCloudPlatformAPI/__init__.py
@@ -0,0 +1,15 @@
+
+from .AdManager import Audience, Network, Report, TargetingPreset, Traffic, Forecast
+from .Analytics import Analytics
+from .BigQuery import BigQuery
+from .CloudStorage import CloudStorage
+
+__all__ = ["Audience",
+           "Network",
+           "Report",
+           "TargetingPreset",
+           "Traffic",
+           "Forecast",
+           "Analytics",
+           "BigQuery",
+           "CloudStorage"]
