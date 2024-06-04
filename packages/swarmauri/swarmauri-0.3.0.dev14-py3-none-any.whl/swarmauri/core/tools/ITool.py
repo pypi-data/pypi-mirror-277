4ad3a0550331ad7a19a8f8f34bdec2851b30b57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 03:03:13 +0000

    Add package swarmauri/0.3.0.dev14/swarmauri-0.3.0.dev14-py3-none-any.whl

---
+++ packages/swarmauri/swarmauri-0.3.0.dev14-py3-none-any.whl/swarmauri/core/tools/ITool.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+
+class ITool(ABC):
+    
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """
+        Abstract property for getting the name of the parameter.
+        """
+        pass
+
+    @name.setter
+    @abstractmethod
+    def name(self, value: str):
+        """
+        Abstract setter for setting the name of the parameter.
+        """
+        pass
+    
+    @property
+    @abstractmethod
+    def description(self):
+        pass
+
+    @description.setter
+    @abstractmethod
+    def description(self, value) -> None:
+        pass
+    
+    @property
+    @abstractmethod
+    def parameters(self):
+        pass
+
+    @parameters.setter
+    @abstractmethod
+    def parameters(self, value)  -> None:
+        pass
+    
+    @property
+    def function(self):
+        pass
+
+    
+    @abstractmethod
+    def as_dict(self):
+        pass
+
+    @abstractmethod
+    def to_json(obj):
+        pass
+        
+    @abstractmethod
+    def call(self, *args, **kwargs):
+        pass
+    
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        pass
+
---
+++ packages/swarmauri/swarmauri-0.3.0.dev14-py3-none-any.whl/swarmauri/standard/documents/base/__init__.py
@@ -0,0 +1,2 @@
+from .DocumentBase import DocumentBase
+from .EmbeddedBase import EmbeddedBase
