92783685cd79a2fc129f59f922877137aa4c3d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 06:04:21 +0000

    Add package rsclimatelab/0.1.0/rsclimatelab-0.1.0.tar.gz

---
+++ packages/rsclimatelab/rsclimatelab-0.1.0.tar.gz/rsclimatelab-0.1.0/README.md
@@ -0,0 +1,22 @@
+![](/docs/source/_static/climate-lab-logo.png)
+
+# RS Climate Lab
+
+A project focused on climate data and tooling for **Rio Grande do Sul** in Brazil.
+
+## 💁 Contributing
+
+We are extremely open to contributions, whether it be in the form of a new feature, improved infrastructure, or better documentation. Please open an issue to discuss what you want to contribute first so we can coordinate with others that might be working on the same feature.
+
+## 📮 Contact
+
+For questions or comments regarding the usage of this repository, please use the [discussion section](https://github.com/perone/rs-climate-lab/discussions) on Github. For bug reports and feature requests, please open an [issue](https://github.com/perone/rs-climate-lab/issues) on GitHub.
+
+## 🌟 Contributors
+
+[![rs-climate-lab contributors](https://contrib.rocks/image?repo=perone/rs-climate-lab)](https://github.com/perone/rs-climate-lab/graphs/contributors)
+
+### Logo credits
+
+Logo image provided for free by `vecteezy.com`.
+
---
+++ packages/rsclimatelab/rsclimatelab-0.1.0.tar.gz/rsclimatelab-0.1.0/setup.py
@@ -0,0 +1,54 @@
+from typing import List
+
+import setuptools
+
+development_requires: List[str] = [
+    "pytest>=8.2.1",
+    "mypy>=1.10.0",
+    "flake8>=7.0.0",
+    "pytest-cov>=5.0.0",
+    "Sphinx>=7.3.7",
+    "furo>=2024.5.6",
+    "isort>=5.13.2",
+    "twine>=5.1.0",
+    "sphinx-autobuild>=2024.4.16",
+]
+
+setuptools.setup(
+    name="rsclimatelab",
+    version="0.1.0",
+    author="Christian S. Perone",
+    author_email="christian.perone@gmail.com",
+    description="A project focused on climate data and tooling for "
+                "Rio Grande do Sul in Brazil.",
+    url="https://github.com/perone/rs-climate-lab",
+    install_requires=[
+        "rich>=13.7.0",
+        "dask[complete]>=2024.5.2",
+        "xarray>=2024.5.0",
+        "rioxarray>=0.15.5",
+        "click>=8.1.7",
+        "cdsapi>=0.7.0",
+        "pyyaml>=6.0.1",
+        "pydantic>=2.7.2",
+    ],
+    extras_require={
+        'dev': development_requires,
+    },
+    project_urls={
+        "Bug Tracker": "https://github.com/perone/rs-climate-lab/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.10",
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'rscl = rsclimatelab.cli:cli',
+        ]
+    }
+)
