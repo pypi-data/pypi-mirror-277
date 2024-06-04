c707005d159614786527bf708b57ad7b8ec0e00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:30 +0000

    Add package anystore/0.1.4/anystore-0.1.4.tar.gz

---
+++ packages/anystore/anystore-0.1.4.tar.gz/anystore-0.1.4/anystore/settings.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from anystore.serialize import Mode
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="anystore_")
+
+    uri: str | None = ".anystore"
+    yaml_uri: str | None = None
+    json_uri: str | None = None
+    serialization_mode: Mode | None = "auto"
+    raise_on_nonexist: bool = True
+
+
+class SqlSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="anystore_sql_")
+
+    table: str | None = "anystore"
+    pool_size: int | None = 5
