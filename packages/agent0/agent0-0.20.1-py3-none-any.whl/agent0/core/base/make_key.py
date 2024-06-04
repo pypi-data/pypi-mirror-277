28756f8cb23fd51a6c93c93151adbf34cdb6c96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 05:03:06 +0000

    Add package agent0/0.20.1/agent0-0.20.1-py3-none-any.whl

---
+++ packages/agent0/agent0-0.20.1-py3-none-any.whl/agent0/core/base/make_key.py
@@ -0,0 +1,28 @@
+"""Make a private key using web3 eth utils"""
+
+import os
+
+from eth_account import Account
+from eth_utils.conversions import to_bytes
+from eth_utils.crypto import keccak
+from eth_utils.curried import text_if_str
+
+
+def make_private_key(extra_entropy: str = "SOME STRING") -> str:
+    """Make a private key.
+
+    Arguments
+    ---------
+    extra_entropy: str, optional
+        Any string used to add entropy to the keccak hash.
+        Defaults to "SOME STRING".
+
+    Returns
+    -------
+    str
+        The private key.
+    """
+    extra_key_bytes = text_if_str(to_bytes, extra_entropy)
+    key_bytes = keccak(os.urandom(32) + extra_key_bytes)
+    key = Account()._parse_private_key(key_bytes)  # pylint: disable=protected-access
+    return str(key)
