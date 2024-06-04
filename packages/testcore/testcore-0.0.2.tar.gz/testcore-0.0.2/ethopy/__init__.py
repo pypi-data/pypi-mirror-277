27162d17448cd2bb89f4a18199a3b25bc32a4927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 03:03:50 +0000

    Add package testcore/0.0.2/testcore-0.0.2.tar.gz

---
+++ packages/testcore/testcore-0.0.2.tar.gz/testcore-0.0.2/ethopy/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for EthoPy."""
+
+__author__ = """Alex Evangelou"""
+__email__ = "admin@example.com"
+__version__ = "0.0.2"
---
+++ packages/testcore/testcore-0.0.2.tar.gz/testcore-0.0.2/tests/test_ethopy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `ethopy` package."""
+
+
+import unittest
+
+from ethopy import ethopy
+
+
+class TestEthopy(unittest.TestCase):
+    """Tests for `ethopy` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
