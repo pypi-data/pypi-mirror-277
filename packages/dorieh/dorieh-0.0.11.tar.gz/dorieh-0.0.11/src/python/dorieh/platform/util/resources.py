07a00b50b8d84f296600d9620d126d082a705ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 14:04:26 +0000

    Add package dorieh/0.0.11/dorieh-0.0.11.tar.gz

---
+++ packages/dorieh/dorieh-0.0.11.tar.gz/dorieh-0.0.11/src/python/dorieh/platform/util/resources.py
@@ -0,0 +1,54 @@
+#  Copyright (c) 2021. Harvard University
+#
+#  Developed by Research Software Engineering,
+#  Faculty of Arts and Sciences, Research Computing (FAS RC)
+#  Author: Michael A Bouzinier
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+import glob
+import os
+import sys
+from pathlib import Path
+from typing import Dict
+
+
+def get_resource_dir() -> str:
+    root = Path(__file__).parents[4]
+    return os.path.join(root, "resources")
+
+
+def name2path(name: str) -> str:
+    return os.path.join(*(name.split('.')))
+
+
+def get_resources(name: str) -> Dict[str, str]:
+    rel_path = name2path(name)
+    dirs = [get_resource_dir()] + [
+        os.path.join(d, "resources")
+        for d in sys.path
+    ]
+    for d in dirs:
+        rpath = os.path.join(d, rel_path + "*")
+        resources = glob.glob(rpath, recursive=False)
+        if resources:
+            return {
+                '.'.join(os.path.basename(r).split('.')[1:]): r
+                for r in resources
+            }
+    return {}
+
+
+if __name__ == '__main__':
+    print(get_resources("public.us_iso"))
