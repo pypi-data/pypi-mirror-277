b71fe8b9913630cc318fd5a37d673a69a60c4d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 17:04:00 +0000

    Add package MolScore/1.6.1/MolScore-1.6.1-py3-none-any.whl

---
+++ packages/MolScore/MolScore-1.6.1-py3-none-any.whl/molscore/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "1.6"
+__all__ = ["MolScore", "MolScoreBenchmark", "MockGenerator"]
+
+# Import lib resources depending on python version
+import sys
+
+if sys.version_info[1] < 9:
+    import importlib_resources as resources
+else:
+    from importlib import resources
+
+from molscore.manager import MolScore, MolScoreBenchmark
+from molscore.utils import MockGenerator
