da64c30e2f5ec75add6250ed3179dcc0ae69f4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:29 +0000

    Add package soda-generator/1100/soda_generator-1100.tar.gz

---
+++ packages/soda-generator/soda_generator-1100.tar.gz/soda_generator-1100/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+	name="soda_generator",
+	version="1100",
+	author="Mira Terekhova",
+	author_email="miraterekhova@mail.ru",
+	description="This is package designed for auto-generation of contracts and tests in the soda syntax",
+	packages=find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.7',
+)
