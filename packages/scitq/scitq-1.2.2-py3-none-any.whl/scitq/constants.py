8db2113c61665e4fc9b4c86febd18993ff326c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 17:04:01 +0000

    Add package scitq/1.2.2/scitq-1.2.2-py3-none-any.whl

---
+++ packages/scitq/scitq-1.2.2-py3-none-any.whl/scitq/constants.py
@@ -0,0 +1,11 @@
+SIGNAL_CLEAN=-1
+SIGNAL_RESTART=-2
+SIGNAL_RESET_RESOURCES=-3
+
+DEFAULT_SERVER_CONF='/etc/scitq.conf'
+DEFAULT_WORKER_CONF='/etc/scitq-worker.conf'
+
+HTTP_ERROR_CODE_NOT_FOUND = 404
+
+TASK_STATUS = ['paused','waiting','pending','assigned','accepted','running','failed','succeeded']
+EXECUTION_STATUS = ['pending','accepted','running','failed','succeeded']
