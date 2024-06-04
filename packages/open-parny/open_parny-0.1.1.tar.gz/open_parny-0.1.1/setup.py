0086696a0ce5f6fa2cf9944bf44393505e4d8c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 06:04:22 +0000

    Add package open-parny/0.1.1/open_parny-0.1.1.tar.gz

---
+++ packages/open-parny/open_parny-0.1.1.tar.gz/open_parny-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="open_parny",
+    version="0.1.1",
+    packages=find_packages(),
+    description="A Python package to send alerts to Parny.io webhooks.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Hasan Aday",
+    author_email="hasan@parny.io",
+    url="https://github.com/haday23/open-parny.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+    ],
+    python_requires='>=3.6',
+)
