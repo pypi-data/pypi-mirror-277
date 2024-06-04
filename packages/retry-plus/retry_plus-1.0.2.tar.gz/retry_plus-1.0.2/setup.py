8fb0d45de51572b0e1357db54860258e6b2a7e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 15:03:36 +0000

    Add package retry-plus/1.0.2/retry_plus-1.0.2.tar.gz

---
+++ packages/retry-plus/retry_plus-1.0.2.tar.gz/retry_plus-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='retry_plus',
+    version='1.0.2',
+    description='A generic retry package for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Talaat Magdy',
+    author_email='talaatmagdy75@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
+        'pytest-asyncio',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
