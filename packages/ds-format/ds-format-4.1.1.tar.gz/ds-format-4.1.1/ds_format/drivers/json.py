e4f7dd583eac8ecf6fba1d5fe01d2fbe87290d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:36 +0000

    Add package ds-format/4.1.1/ds-format-4.1.1.tar.gz

---
+++ packages/ds-format/ds-format-4.1.1.tar.gz/ds-format-4.1.1/ds_format/drivers/json.py
@@ -0,0 +1,28 @@
+import json
+import numpy as np
+import ds_format as ds
+
+READ_EXT = ['json']
+WRITE_EXT = ['json']
+
+def read(filename, variables=None, sel=None, full=False, jd=False):
+	with open(filename) as f:
+		d = json.load(f)
+		for var in ds.vars(d):
+			data = ds.var(d, var)
+			if isinstance(data, list):
+				ds.var(d, var, np.array(data))
+	return d
+
+def write(filename, d):
+	d2 = ds.copy(d)
+	for var in ds.vars(d2):
+		var_e = ds.escape(var)
+		data = ds.var(d2, var)
+		if isinstance(data, np.ndarray):
+			d2[var_e] = data.tolist()
+	with open(filename, 'w') as f:
+		json.dump(d2, f)
+
+from_json = read
+to_json = write
