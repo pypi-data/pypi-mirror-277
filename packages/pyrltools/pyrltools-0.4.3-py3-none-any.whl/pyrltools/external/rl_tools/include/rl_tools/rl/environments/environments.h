c8558017eddaa574a38fa14534d6c1c5e3783f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 16:03:45 +0000

    Add package pyrltools/0.4.3/pyrltools-0.4.3-py3-none-any.whl

---
+++ packages/pyrltools/pyrltools-0.4.3-py3-none-any.whl/pyrltools/external/rl_tools/include/rl_tools/rl/environments/environments.h
@@ -0,0 +1,15 @@
+#include "../../version.h"
+#if (defined(RL_TOOLS_DISABLE_INCLUDE_GUARDS) || !defined(RL_TOOLS_RL_ENVIRONMENTS_ENVIRONMENTS_H)) && (RL_TOOLS_USE_THIS_VERSION == 1)
+#pragma once
+#define RL_TOOLS_RL_ENVIRONMENTS_ENVIRONMENTS_H
+
+RL_TOOLS_NAMESPACE_WRAPPER_START
+namespace rl_tools::rl::environments{
+    struct Environment{
+
+    };
+    struct DummyUI{};
+}
+
+
+#endif
