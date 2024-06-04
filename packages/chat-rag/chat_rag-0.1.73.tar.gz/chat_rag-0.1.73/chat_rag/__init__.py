68420408a220bdcd44a38e5f3cca63a3497ddfda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 17:04:23 +0000

    Add package chat-rag/0.1.73/chat_rag-0.1.73.tar.gz

---
+++ packages/chat-rag/chat_rag-0.1.73.tar.gz/chat_rag-0.1.73/chat_rag/__init__.py
@@ -0,0 +1,2 @@
+from chat_rag.rag import RAG
+from chat_rag.async_rag import AsyncRAG
---
+++ packages/chat-rag/chat_rag-0.1.73.tar.gz/chat_rag-0.1.73/chat_rag/llms/__init__.py
@@ -0,0 +1,7 @@
+from chat_rag.llms.base_llm import CONTEXT_PREFIX, RAGLLM
+from chat_rag.llms.claude_client import AsyncClaudeChatModel, ClaudeChatModel
+from chat_rag.llms.ggml_llm import GGMLModel
+from chat_rag.llms.hf_llm import HFModel
+from chat_rag.llms.mistral_client import AsyncMistralChatModel, MistralChatModel
+from chat_rag.llms.openai_client import AsyncOpenAIChatModel, OpenAIChatModel
+from chat_rag.llms.vllm_client import AsyncVLLMModel, VLLMModel
