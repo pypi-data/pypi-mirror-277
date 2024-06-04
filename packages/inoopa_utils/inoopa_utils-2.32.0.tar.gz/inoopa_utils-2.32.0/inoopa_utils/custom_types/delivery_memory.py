25072d43bcc8265307ab3b65defc224f9836f0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 05:03:04 +0000

    Add package inoopa_utils/2.32.0/inoopa_utils-2.32.0.tar.gz

---
+++ packages/inoopa_utils/inoopa_utils-2.32.0.tar.gz/inoopa_utils-2.32.0/inoopa_utils/custom_types/delivery_memory.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from dataclasses import dataclass, field
+
+
+# ? Note:
+# We have to have this dataclass in dataclass to be able to map the company collection
+# with the delivery_memory collection. Otherwise, it's a mess to query the collections together.
+# I know it seems overkill, but it's the only way I could get it to work simply in the delivery_manger.
+
+
+@dataclass(slots=True)
+class BestEmail:
+    email: str | None
+
+@dataclass(slots=True)
+class BestPhone:
+    phone: str | None
+
+@dataclass(slots=True)
+class BestWebsite:
+    website: str | None
+
+
+@dataclass(slots=True)
+class DeliveryMemory:
+    """The data schema for the delivery_memory collection."""
+    _id: str # delivery_id
+    company_id: str
+    delivery_date = datetime
+    # Optionals
+    best_phone: BestPhone = field(default=BestPhone(None))
+    best_email: BestEmail = field(default=BestEmail(None))
+    best_website: BestWebsite = field(default=BestWebsite(None))
+    decision_maker_ids: list | None = field(default_factory=list)
