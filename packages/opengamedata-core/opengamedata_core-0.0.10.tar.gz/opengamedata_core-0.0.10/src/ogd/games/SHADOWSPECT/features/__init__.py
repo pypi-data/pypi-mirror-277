6f7e684566174039dafb054fc9999beba953e580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 04:03:33 +0000

    Add package opengamedata-core/0.0.10/opengamedata_core-0.0.10.tar.gz

---
+++ packages/opengamedata-core/opengamedata_core-0.0.10.tar.gz/opengamedata_core-0.0.10/src/ogd/games/SHADOWSPECT/features/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "MoveShapeCount",
+    "SessionID",
+    "FunnelByUser",
+    "LevelsOfDifficulty",
+    "SequenceBetweenPuzzles",
+    "SequenceWithinPuzzles"
+]
+
+from . import MoveShapeCount
+from . import SessionID
+from . import FunnelByUser
+from . import LevelsOfDifficulty
+from . import SequenceBetweenPuzzles
+from . import SequenceWithinPuzzles
