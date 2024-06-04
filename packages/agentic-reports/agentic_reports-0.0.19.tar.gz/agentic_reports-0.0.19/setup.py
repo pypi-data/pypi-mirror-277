89658364e84beaea0348325099a69ef9f98f2831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 05:03:39 +0000

    Add package agentic-reports/0.0.19/agentic_reports-0.0.19.tar.gz

---
+++ packages/agentic-reports/agentic_reports-0.0.19.tar.gz/agentic_reports-0.0.19/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+def read_long_description():
+    this_directory = Path(__file__).parent
+    long_description = (this_directory / "README.md").read_text()
+    return long_description
+
+setup(
+    name='agentic_reports',
+    version='0.0.19',
+    packages=find_packages(where='project-root'),
+    package_dir={'': 'project-root'},
+    py_modules=['start'],  # Include the start module
+    install_requires=[
+        'twine',
+        'setuptools',
+        'wheel',
+        'flake8',
+        'black',
+        'pytest',
+        'pip-upgrader',
+        'fastapi',
+        'uvicorn',
+        'pandas',
+        'pydantic',
+        'exa_py',
+        'pydantic-settings',
+        'litellm'
+    ],
+    author='rUv',
+    author_email='null@ruv.net',
+    description='Agent Reporting using Exa.ai API.',
+    long_description=read_long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ruvnet/agentic-reports',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    entry_points={
+        'console_scripts': [
+            'agentic-reports=start:main',
+        ],
+    },
+)
