be6e6ad711a9406e8e69fa371607e34438ff9f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-04 17:04:22 +0000

    Add package firerpa/0.5/firerpa-0.5.tar.gz

---
+++ packages/firerpa/firerpa-0.5.tar.gz/firerpa-0.5/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Copyright 2024 firerpa (lamda.devel@gmail.com). All rights reserved.
+import setuptools
+setuptools.setup(
+    name            = "firerpa",
+    version         = "0.5",
+    description     = "Android automation control platform",
+    url             = "https://device-farm.com",
+    author          = "firerpa",
+    python_requires = ">=3.6,<=3.12",
+    zip_safe        = False,
+)
