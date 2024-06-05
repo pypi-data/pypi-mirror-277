81168be57f18bab10d5e6c39fcea1f1bf335a506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:55 +0000

    Add package fake-bpy-module-2.92/20240604/fake_bpy_module_2.92-20240604-py3-none-any.whl

---
+++ packages/fake-bpy-module-2.92/fake_bpy_module_2.92-20240604-py3-none-any.whl/bpy/ops/asset/__init__.pyi
@@ -0,0 +1,62 @@
+import typing
+import collections.abc
+import bpy.types
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
+
+def clear(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Delete all asset metadata and turn the selected asset data-blocks back into normal data-blocks
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def mark(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Enable easier reuse of selected data-blocks through the Asset Browser, with the help of customizable metadata (like previews, descriptions and tags)
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def tag_add(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Add a new keyword tag to the active asset
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def tag_remove(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Remove an existing keyword tag from the active asset
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
---
+++ packages/fake-bpy-module-2.92/fake_bpy_module_2.92-20240604-py3-none-any.whl/bpy/ops/cachefile/__init__.pyi
@@ -0,0 +1,113 @@
+import typing
+import collections.abc
+import bpy.types
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
+
+def open(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    filepath: str | typing.Any = "",
+    hide_props_region: bool | typing.Any | None = True,
+    check_existing: bool | typing.Any | None = True,
+    filter_blender: bool | typing.Any | None = False,
+    filter_backup: bool | typing.Any | None = False,
+    filter_image: bool | typing.Any | None = False,
+    filter_movie: bool | typing.Any | None = False,
+    filter_python: bool | typing.Any | None = False,
+    filter_font: bool | typing.Any | None = False,
+    filter_sound: bool | typing.Any | None = False,
+    filter_text: bool | typing.Any | None = False,
+    filter_archive: bool | typing.Any | None = False,
+    filter_btx: bool | typing.Any | None = False,
+    filter_collada: bool | typing.Any | None = False,
+    filter_alembic: bool | typing.Any | None = True,
+    filter_usd: bool | typing.Any | None = False,
+    filter_volume: bool | typing.Any | None = False,
+    filter_folder: bool | typing.Any | None = True,
+    filter_blenlib: bool | typing.Any | None = False,
+    filemode: typing.Any | None = 8,
+    display_type: str | None = "DEFAULT",
+    sort_method: str | None = "",
+):
+    """Load a cache file
+
+        :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+        :type execution_context: int | str | None
+        :type undo: bool | None
+        :param filepath: File Path, Path to file
+        :type filepath: str | typing.Any
+        :param hide_props_region: Hide Operator Properties, Collapse the region displaying the operator settings
+        :type hide_props_region: bool | typing.Any | None
+        :param check_existing: Check Existing, Check and warn on overwriting existing files
+        :type check_existing: bool | typing.Any | None
+        :param filter_blender: Filter .blend files
+        :type filter_blender: bool | typing.Any | None
+        :param filter_backup: Filter .blend files
+        :type filter_backup: bool | typing.Any | None
+        :param filter_image: Filter image files
+        :type filter_image: bool | typing.Any | None
+        :param filter_movie: Filter movie files
+        :type filter_movie: bool | typing.Any | None
+        :param filter_python: Filter python files
+        :type filter_python: bool | typing.Any | None
+        :param filter_font: Filter font files
+        :type filter_font: bool | typing.Any | None
+        :param filter_sound: Filter sound files
+        :type filter_sound: bool | typing.Any | None
+        :param filter_text: Filter text files
+        :type filter_text: bool | typing.Any | None
+        :param filter_archive: Filter archive files
+        :type filter_archive: bool | typing.Any | None
+        :param filter_btx: Filter btx files
+        :type filter_btx: bool | typing.Any | None
+        :param filter_collada: Filter COLLADA files
+        :type filter_collada: bool | typing.Any | None
+        :param filter_alembic: Filter Alembic files
+        :type filter_alembic: bool | typing.Any | None
+        :param filter_usd: Filter USD files
+        :type filter_usd: bool | typing.Any | None
+        :param filter_volume: Filter OpenVDB volume files
+        :type filter_volume: bool | typing.Any | None
+        :param filter_folder: Filter folders
+        :type filter_folder: bool | typing.Any | None
+        :param filter_blenlib: Filter Blender IDs
+        :type filter_blenlib: bool | typing.Any | None
+        :param filemode: File Browser Mode, The setting for the file browser mode to load a .blend file, a library or a special file
+        :type filemode: typing.Any | None
+        :param display_type: Display Type
+
+    DEFAULT
+    Default, Automatically determine display type for files.
+
+    LIST_VERTICAL
+    Short List, Display files as short list.
+
+    LIST_HORIZONTAL
+    Long List, Display files as a detailed list.
+
+    THUMBNAIL
+    Thumbnails, Display files as thumbnails.
+        :type display_type: str | None
+        :param sort_method: File sorting mode
+        :type sort_method: str | None
+    """
+
+    ...
+
+def reload(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Update objects paths list with new data from the archive
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
