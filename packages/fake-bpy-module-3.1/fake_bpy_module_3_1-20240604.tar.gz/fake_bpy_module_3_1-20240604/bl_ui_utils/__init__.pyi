eaf72785df2adbc64121a43d72b322eb9d37f1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:41 +0000

    Add package fake-bpy-module-3.1/20240604/fake_bpy_module_3_1-20240604.tar.gz

---
+++ packages/fake-bpy-module-3.1/fake_bpy_module_3_1-20240604.tar.gz/fake_bpy_module_3_1-20240604/bl_ui_utils/__init__.pyi
@@ -0,0 +1,6 @@
+import typing
+import collections.abc
+from . import bug_report_url
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
---
+++ packages/fake-bpy-module-3.1/fake_bpy_module_3_1-20240604.tar.gz/fake_bpy_module_3_1-20240604/bpy/ops/geometry/__init__.pyi
@@ -0,0 +1,152 @@
+import typing
+import collections.abc
+import bpy.types
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
+
+def attribute_add(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    name: str | typing.Any = "Attribute",
+    domain: str | None = "POINT",
+    data_type: str | None = "FLOAT",
+):
+    """Add attribute to geometry
+
+        :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+        :type execution_context: int | str | None
+        :type undo: bool | None
+        :param name: Name, Name of new attribute
+        :type name: str | typing.Any
+        :param domain: Domain, Type of element that attribute is stored on
+
+    POINT
+    Point -- Attribute on point.
+
+    EDGE
+    Edge -- Attribute on mesh edge.
+
+    FACE
+    Face -- Attribute on mesh faces.
+
+    CORNER
+    Face Corner -- Attribute on mesh face corner.
+
+    CURVE
+    Spline -- Attribute on spline.
+
+    INSTANCE
+    Instance -- Attribute on instance.
+        :type domain: str | None
+        :param data_type: Data Type, Type of data stored in attribute
+
+    FLOAT
+    Float -- Floating-point value.
+
+    INT
+    Integer -- 32-bit integer.
+
+    FLOAT_VECTOR
+    Vector -- 3D vector with floating-point values.
+
+    FLOAT_COLOR
+    Color -- RGBA color with floating-point values.
+
+    BYTE_COLOR
+    Byte Color -- RGBA color with 8-bit values.
+
+    STRING
+    String -- Text string.
+
+    BOOLEAN
+    Boolean -- True or false.
+
+    FLOAT2
+    2D Vector -- 2D vector with floating-point values.
+        :type data_type: str | None
+    """
+
+    ...
+
+def attribute_convert(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    mode: str | None = "GENERIC",
+    domain: str | None = "POINT",
+    data_type: str | None = "FLOAT",
+):
+    """Change how the attribute is stored
+
+        :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+        :type execution_context: int | str | None
+        :type undo: bool | None
+        :param mode: Mode
+        :type mode: str | None
+        :param domain: Domain, Which geometry element to move the attribute to
+
+    POINT
+    Point -- Attribute on point.
+
+    EDGE
+    Edge -- Attribute on mesh edge.
+
+    FACE
+    Face -- Attribute on mesh faces.
+
+    CORNER
+    Face Corner -- Attribute on mesh face corner.
+
+    CURVE
+    Spline -- Attribute on spline.
+
+    INSTANCE
+    Instance -- Attribute on instance.
+        :type domain: str | None
+        :param data_type: Data Type
+
+    FLOAT
+    Float -- Floating-point value.
+
+    INT
+    Integer -- 32-bit integer.
+
+    FLOAT_VECTOR
+    Vector -- 3D vector with floating-point values.
+
+    FLOAT_COLOR
+    Color -- RGBA color with floating-point values.
+
+    BYTE_COLOR
+    Byte Color -- RGBA color with 8-bit values.
+
+    STRING
+    String -- Text string.
+
+    BOOLEAN
+    Boolean -- True or false.
+
+    FLOAT2
+    2D Vector -- 2D vector with floating-point values.
+        :type data_type: str | None
+    """
+
+    ...
+
+def attribute_remove(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Remove attribute from geometry
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
