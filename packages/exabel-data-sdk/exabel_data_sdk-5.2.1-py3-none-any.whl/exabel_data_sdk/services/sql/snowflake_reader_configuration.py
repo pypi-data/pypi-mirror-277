eb744dc81f0703f38425b3f636f059b5648ebcbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 04:03:36 +0000

    Add package exabel-data-sdk/5.2.1/exabel_data_sdk-5.2.1-py3-none-any.whl

---
+++ packages/exabel-data-sdk/exabel_data_sdk-5.2.1-py3-none-any.whl/exabel_data_sdk/services/sql/snowflake_reader_configuration.py
@@ -0,0 +1,56 @@
+import argparse
+from dataclasses import dataclass
+from typing import NewType, Optional
+
+from exabel_data_sdk.services.sql.sql_reader_configuration import (
+    ConnectionString,
+    SqlReaderConfiguration,
+)
+from exabel_data_sdk.util.handle_missing_imports import handle_missing_imports
+
+with handle_missing_imports():
+    from snowflake.sqlalchemy import URL
+
+
+Account = NewType("Account", str)
+Username = NewType("Username", str)
+Password = NewType("Password", str)
+Warehouse = NewType("Warehouse", str)
+Database = NewType("Database", str)
+Schema = NewType("Schema", str)
+Role = NewType("Role", str)
+LoginTimeout = NewType("LoginTimeout", int)
+
+
+@dataclass
+class SnowflakeReaderConfiguration(SqlReaderConfiguration):
+    """SQL configuration for Snowflake."""
+
+    account: Account
+    user: Username
+    password: Password
+    warehouse: Optional[Warehouse] = None
+    database: Optional[Database] = None
+    schema: Optional[Schema] = None
+    role: Optional[Role] = None
+    login_timeout: LoginTimeout = LoginTimeout(15)
+
+    def __post_init__(self) -> None:
+        if self.schema and not self.database:
+            raise ValueError("Cannot specify schema without specifying database")
+
+    @classmethod
+    def from_args(cls, args: argparse.Namespace) -> "SnowflakeReaderConfiguration":
+        return cls(
+            account=args.account,
+            user=args.username,
+            password=args.password,
+            warehouse=args.warehouse,
+            database=args.database,
+            schema=args.schema,
+            role=args.role,
+        )
+
+    def get_connection_string(self) -> ConnectionString:
+        """Return the connection string."""
+        return URL(**self.get_connection_args())
