9dd1e222a8a5df1cd616b17bbf825c585ea42812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 17:04:07 +0000

    Add package fathom-global-client-sdk/0.11.2/fathom_global_client_sdk-0.11.2-py3-none-any.whl

---
+++ packages/fathom-global-client-sdk/fathom_global_client_sdk-0.11.2-py3-none-any.whl/fathom/sdk/common.py
@@ -0,0 +1,27 @@
+import os
+from typing import Union
+
+
+class FathomException(Exception):
+    """A client exception"""
+
+
+class TaskNotCompleteException(FathomException):
+    """Tried to download the result from a task that was not complete"""
+
+
+def write_tiff_data_to_file(geotiff_bytes, layer_id, output_dir, pattern, tiff_num):
+    format_vars = {
+        "sep": os.sep,
+        "layer_id": layer_id,
+        "tiff_num": tiff_num,
+    }
+    base = pattern.format(**format_vars)
+    filename = os.path.join(output_dir, base)
+    tiff_dir = os.path.dirname(filename)
+    os.makedirs(tiff_dir, exist_ok=True)
+    with open(filename, "wb") as shapefile:
+        shapefile.write(geotiff_bytes)
+
+
+PathOrString = Union[os.PathLike, str]
