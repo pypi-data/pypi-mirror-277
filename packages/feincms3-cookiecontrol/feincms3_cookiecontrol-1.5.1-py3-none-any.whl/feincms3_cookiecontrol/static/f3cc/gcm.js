494391a8ebea743dc60150ff4250679cf757f4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:58 +0000

    Add package feincms3-cookiecontrol/1.5.1/feincms3_cookiecontrol-1.5.1-py3-none-any.whl

---
+++ packages/feincms3-cookiecontrol/feincms3_cookiecontrol-1.5.1-py3-none-any.whl/feincms3_cookiecontrol/static/f3cc/gcm.js
@@ -0,0 +1,1 @@
+(()=>{window.dataLayer=window.dataLayer||[];function d(){dataLayer.push(arguments)}function n(a,e){d("consent",a,{ad_user_data:e,ad_personalization:e,ad_storage:e,analytics_storage:e})}n("default","denied");window.addEventListener("f3cc_consent_granted",()=>{n("update","granted")});window.addEventListener("f3cc_consent_denied",()=>{n("update","denied")});})();
