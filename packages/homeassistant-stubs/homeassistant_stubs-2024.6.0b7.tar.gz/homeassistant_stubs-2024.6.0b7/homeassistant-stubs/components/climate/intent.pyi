577de5bb3de76737c036ab200b080ed3f3ccb107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:04:00 +0000

    Add package homeassistant-stubs/2024.6.0b7/homeassistant_stubs-2024.6.0b7.tar.gz

---
+++ packages/homeassistant-stubs/homeassistant_stubs-2024.6.0b7.tar.gz/homeassistant_stubs-2024.6.0b7/homeassistant-stubs/components/climate/intent.pyi
@@ -0,0 +1,16 @@
+from . import ClimateEntity as ClimateEntity, DOMAIN as DOMAIN
+from _typeshed import Incomplete
+from homeassistant.core import HomeAssistant as HomeAssistant, State as State
+from homeassistant.helpers import intent as intent
+from homeassistant.helpers.entity_component import EntityComponent as EntityComponent
+
+INTENT_GET_TEMPERATURE: str
+
+async def async_setup_intents(hass: HomeAssistant) -> None: ...
+
+class GetTemperatureIntent(intent.IntentHandler):
+    intent_type = INTENT_GET_TEMPERATURE
+    description: str
+    slot_schema: Incomplete
+    platforms: Incomplete
+    async def async_handle(self, intent_obj: intent.Intent) -> intent.IntentResponse: ...
