721a18b02556a736564ece5c367e55c889286569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:03:50 +0000

    Add package napari-ctc-io/0.2.0/napari_ctc_io-0.2.0-py3-none-any.whl

---
+++ packages/napari-ctc-io/napari_ctc_io-0.2.0-py3-none-any.whl/napari_ctc_io/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+from ._reader import napari_get_reader
+from ._writer import write_multiple
+
+__all__ = ("napari_get_reader", "write_multiple")
