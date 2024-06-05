543c3694844e54440bfac6b1431e53f61b6a45d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 04:04:48 +0000

    Add package orderings/1.5.0/orderings-1.5.0-py3-none-any.whl

---
+++ packages/orderings/orderings-1.5.0-py3-none-any.whl/orderings/__init__.py
@@ -0,0 +1,51 @@
+"""Ordering enumeration and protocols."""
+
+__description__ = "Ordering enumeration and protocols."
+__url__ = "https://github.com/nekitdev/orderings"
+
+__title__ = "orderings"
+__author__ = "nekitdev"
+__license__ = "MIT"
+__version__ = "1.5.0"
+
+from orderings.core import Compare, Ordering, PartialCompare, is_compare
+from orderings.typing import (
+    Greater,
+    GreaterOrEqual,
+    LenientOrdered,
+    Less,
+    LessOrEqual,
+    Ordered,
+    PartialGreater,
+    PartialGreaterOrEqual,
+    PartialLenientOrdered,
+    PartialLess,
+    PartialLessOrEqual,
+    PartialOrdered,
+    PartialStrictOrdered,
+    StrictOrdered,
+)
+
+__all__ = (
+    # core
+    "Ordering",
+    "PartialCompare",
+    "Compare",
+    "is_compare",
+    # partial
+    "PartialLess",
+    "PartialGreater",
+    "PartialStrictOrdered",
+    "PartialLessOrEqual",
+    "PartialGreaterOrEqual",
+    "PartialLenientOrdered",
+    "PartialOrdered",
+    # total
+    "Less",
+    "Greater",
+    "StrictOrdered",
+    "LessOrEqual",
+    "GreaterOrEqual",
+    "LenientOrdered",
+    "Ordered",
+)
