7fdb95e75e9e42ce513b71fd01b004cb00097c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:04:31 +0000

    Add package beancount-multitool/0.6.1/beancount_multitool-0.6.1.tar.gz

---
+++ packages/beancount-multitool/beancount_multitool-0.6.1.tar.gz/beancount_multitool-0.6.1/src/beancount_multitool/__init__.py
@@ -0,0 +1,16 @@
+from .__version__ import __version__  # noqa
+
+# from .as_transaction import as_transaction
+# from .read_config import read_config
+from .JABank import JABank
+from .RakutenBank import RakutenBank
+from .RakutenCard import RakutenCard
+from .ShinseiBank import ShinseiBank
+from .SumishinNetBank import SumishinNetBank
+
+__INSTITUTIONS__ = []
+__INSTITUTIONS__.append(JABank.NAME)
+__INSTITUTIONS__.append(RakutenBank.NAME)
+__INSTITUTIONS__.append(RakutenCard.NAME)
+__INSTITUTIONS__.append(ShinseiBank.NAME)
+__INSTITUTIONS__.append(SumishinNetBank.NAME)
