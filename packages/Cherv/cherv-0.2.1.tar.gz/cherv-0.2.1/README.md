2a4ef9f83b4eac69314eb11f29a950592faab235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:04:00 +0000

    Add package Cherv/0.2.1/cherv-0.2.1.tar.gz

---
+++ packages/Cherv/cherv-0.2.1.tar.gz/cherv-0.2.1/README.md
@@ -0,0 +1,14 @@
+# cherv-say-python
+## Description
+An analogue of CowSay, only instead of a cow there is a worm.
+## Installation and Usage
+Installation:
+```bash
+pip install cherv
+```
+Usage:
+```bash
+python -m cherv [your message]
+```
+The message can be entered in any length:
+![Снимок экрана 2024-06-05 043610](https://github.com/AyaalTech/wormsay-python/assets/80465121/627b7ae8-16bb-4eee-8156-49c31ad2726d)
---
+++ packages/Cherv/cherv-0.2.1.tar.gz/cherv-0.2.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Cherv',
+    version='0.2.1',
+    packages=find_packages(),
+    description='An analogue of CowSay, only instead of a cow there is a worm.',
+    long_description=\
+    """
+    An analogue of CowSay, only instead of a cow there is a worm.
+    Usage:
+    `python -m cherv [your message]`
+    The message can be entered in any length.
+    """,
+    author='Павел, Евгений',
+    maintainer='Павел',
+    maintainer_email='nadokto8@gmail.com',
+    author_email='nadokto8@gmail.com',
+    url='https://github.com/AyaalTech/wormsay-python.git',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
+    keywords='cherv cowsay python library',
+    install_requires=[],
+    python_requires='>=3.7'
+)
