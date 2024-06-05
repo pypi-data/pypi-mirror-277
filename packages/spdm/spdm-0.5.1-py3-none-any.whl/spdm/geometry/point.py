2caa5d7b5fdef6559177c76a9050a11ec965a12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:05:21 +0000

    Add package spdm/0.5.1/spdm-0.5.1-py3-none-any.whl

---
+++ packages/spdm/spdm-0.5.1-py3-none-any.whl/spdm/geometry/point.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import typing
+import numpy as np
+from spdm.utils.logger import logger
+
+from ..utils.typing import ArrayType, NumericType
+from ..core.geo_object import GeoObject,BBox
+
+
+class Point(GeoObject):
+    """Point
+    点，零维几何体
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        if len(args) == 1:
+            if isinstance(args[0], (list, tuple)):
+                args = args[0]
+            elif isinstance(args[0], dict):
+                args = [*args[0].values()]
+        super().__init__(*args, rank=0, ndims=len(args), **kwargs)
+        self._coord = np.array(args)
+
+    def __iter__(self) -> typing.Generator[float, None, None]:
+        yield from self._coord
+
+    @property
+    def r(self) -> float:
+        """just a walkaround for spherical coordinate system"""
+        return self._coord[0]
+
+    @property
+    def z(self) -> float:
+        """just a walkaround for spherical coordinate system"""
+        return self._coord[1]
+
+    @property
+    def x(self) -> float:
+        return self._coord[0]
+
+    @property
+    def y(self) -> float:
+        return self._coord[1]
+
+    @property
+    def measure(self) -> float:
+        return 0
+
+    @property
+    def points(self):
+        return self._coord
---
+++ packages/spdm/spdm-0.5.1-py3-none-any.whl/spdm/mesh/mesh_uniform.py
@@ -0,0 +1,30 @@
+import typing
+import numpy as np
+from ..core.mesh import Mesh
+from .mesh_structured import StructuredMesh
+from ..utils.typing import ArrayType
+
+
+@Mesh.register("uniform")
+class UniformMesh(StructuredMesh):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        p_min, p_max = self.geometry.bbox
+        self._dx = (np.asarray(p_max, dtype=float)-np.asarray(p_min, dtype=float))/np.asarray(self.shape, dtype=float)
+        self._orgin = np.asarray(p_min, dtype=float)
+
+    @property
+    def origin(self) -> typing.Tuple[float]: return self._dx
+
+    @property
+    def dx(self) -> typing.Tuple[float]: return self._dx
+
+    def vertices(self, *args) -> ArrayType:
+        if len(args) == 1:
+            uvw = np.asarray(uvw, dtype=float)
+        else:
+            uvw = np.stack(list(args))
+
+        return np.stack([(uvw[i]*self.dx[i]+self.origin[i]) for i in range(self.rank)])
+
+
