f45172d30735af9052c0ada5e278a693871a27f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:05:03 +0000

    Add package plone.formwidget.masterselect/3.0.0/plone.formwidget.masterselect-3.0.0.tar.gz

---
+++ packages/plone.formwidget.masterselect/plone.formwidget.masterselect-3.0.0.tar.gz/plone.formwidget.masterselect-3.0.0/CONTRIBUTORS.rst
@@ -0,0 +1,18 @@
+Author
+------
+
+- Jason Mehring: nrgaway@gmail.com
+
+Orginal Author of Archetypes Widget
+-----------------------------------
+
+- Alec Mitchell: apm13@columbia.edu
+
+Contributors
+------------
+
+- Dorneles Tremea: deo@plonesolutions.com
+- Cédric Messiant, Ecreall: cedricmessiant@ecreall.com
+- Vincent Fretin, Ecreall: vincentfretin@ecreall.com
+- Daniel Widerin, daniel@widerin.net
+- Wesley Barroso Lopes, wesleybl@gmail.com
---
+++ packages/plone.formwidget.masterselect/plone.formwidget.masterselect-3.0.0.tar.gz/plone.formwidget.masterselect-3.0.0/src/plone/formwidget/masterselect/__init__.py
@@ -0,0 +1,55 @@
+from plone.formwidget.masterselect.interfaces import IMasterSelectBoolField
+from plone.formwidget.masterselect.interfaces import IMasterSelectField
+from plone.formwidget.masterselect.interfaces import IMasterSelectRadioField
+from plone.formwidget.masterselect.widget import \
+    MasterSelectBoolFieldWidget  # noqa: F401
+from plone.formwidget.masterselect.widget import \
+    MasterSelectFieldWidget  # noqa: F401
+from plone.formwidget.masterselect.widget import \
+    MasterSelectRadioFieldWidget  # noqa: F401
+from plone.formwidget.masterselect.widget import \
+    MasterSelectWidget  # noqa: F401
+from zope.i18nmessageid import MessageFactory
+from zope.interface import implementer
+from zope.schema import Bool
+from zope.schema import Choice
+from zope.schema.interfaces import IChoice
+
+
+_ = MessageFactory("plone.formwidget.masterselect")
+
+
+@implementer(IMasterSelectField, IChoice)
+class MasterSelectField(Choice):
+    """MasterSelectField that provides additional properties for widget
+    (extends schema.Choice)
+    """
+    slave_fields = ()
+
+    def __init__(self, slave_fields=(), **kw):
+        self.slave_fields = slave_fields
+        super(MasterSelectField, self).__init__(**kw)
+
+
+@implementer(IMasterSelectBoolField)
+class MasterSelectBoolField(Bool):
+    """MasterSelectBoolField that provides addtional properties for widget
+    (extends schema.Bool)
+    """
+    slave_fields = ()
+
+    def __init__(self, slave_fields=(), **kw):
+        self.slave_fields = slave_fields
+        super(MasterSelectBoolField, self).__init__(**kw)
+
+
+@implementer(IMasterSelectRadioField)
+class MasterSelectRadioField(Choice):
+    """MasterSelectRadioField that provides additional properties for widget
+    (extends schema.Choice)
+    """
+    slave_fields = ()
+
+    def __init__(self, slave_fields=(), **kw):
+        self.slave_fields = slave_fields
+        super(MasterSelectRadioField, self).__init__(**kw)
