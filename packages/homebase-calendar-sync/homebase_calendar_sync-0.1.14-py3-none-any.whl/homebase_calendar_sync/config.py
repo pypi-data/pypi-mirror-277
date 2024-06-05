b40b1c3619b245d09174df0dddfa46f575ceb0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 06:04:43 +0000

    Add package homebase-calendar-sync/0.1.14/homebase_calendar_sync-0.1.14-py3-none-any.whl

---
+++ packages/homebase-calendar-sync/homebase_calendar_sync-0.1.14-py3-none-any.whl/homebase_calendar_sync/config.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+ARGS = None
+
+DOTENV_BASE_DIR = Path.cwd()
+load_dotenv(Path(DOTENV_BASE_DIR, ".env"))
+
+HOMEBASE_USERNAME = os.environ["CC_HOMEBASE_USERNAME"]
+HOMEBASE_PASSWORD = os.environ["CC_HOMEBASE_PASSWORD"]
+EMPLOYEE_FIRSTNAME = os.environ["CC_HOMEBASE_EMPLOYEE_FIRSTNAME"]
+EMPLOYEE_LASTNAME = os.environ["CC_HOMEBASE_EMPLOYEE_LASTNAME"]
+START_DATE = os.environ["CC_HOMEBASE_START_DATE"]
+END_DATE = os.environ["CC_HOMEBASE_END_DATE"]
+LOOKAHEAD = os.environ["CC_HOMEBASE_LOOKAHEAD"]
+LOOKAHEAD = LOOKAHEAD.lower() in ["true", "1", "t", "y", "yes"]
+LOOKAHEAD_DAYS = os.environ["CC_HOMEBASE_DAYS_LOOKAHEAD"]
+LOOKAHEAD_DAYS = int(LOOKAHEAD_DAYS)
+
+META = None
+META_SETTINGS_FILE = ".homebase_calendar_sync"
+META_DATA_FILE = ".homebase_calendar_sync_meta"
+GOOGLE = None
+API_SCOPES: list[str] = ["https://www.googleapis.com/auth/calendar"]
+
+DB_NAME = "events.db"
+DB = None
+DB_CURSOR = None
