d625fca4c98e92e03f8e9a22f8d670a5e9a6e2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:52 +0000

    Add package PyDataforge/1.0.3/PyDataforge-1.0.3.tar.gz

---
+++ packages/PyDataforge/PyDataforge-1.0.3.tar.gz/PyDataforge-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='PyDataforge',
+    version='1.0.3',
+    description='A package for removing sensitive information from data',
+    author='Guipeng Wei',
+    author_email='18316734738@163.com',
+    url='',
+    license='MIT License',
+    packages=['PyDataforge'],
+    platforms=["all"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
