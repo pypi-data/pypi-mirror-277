1af492b408f6ae1f650bc28988df0b6dc9261995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:04:42 +0000

    Add package bipsea/0.6.0/bipsea-0.6.0.tar.gz

---
+++ packages/bipsea/bipsea-0.6.0.tar.gz/bipsea-0.6.0/src/bipsea/bipsea.py
@@ -0,0 +1,302 @@
+"""CLI"""
+
+import logging
+import re
+import select
+import sys
+
+import click
+
+from .bip32 import to_master_key
+from .bip32types import parse_ext_key
+from .bip39 import (
+    LANGUAGES,
+    N_WORDS_ALLOWED,
+    entropy_to_words,
+    normalize_list,
+    normalize_str,
+    to_master_seed,
+    verify_seed_words,
+)
+from .bip85 import (
+    APPLICATIONS,
+    DRNG,
+    INDEX_TO_LANGUAGE,
+    PURPOSE_CODES,
+    RANGES,
+    apply_85,
+    derive,
+    to_entropy,
+)
+from .util import (
+    LOGGER,
+    MIN_REL_ENTROPY,
+    __app_name__,
+    __version__,
+    relative_entropy,
+    to_hex_string,
+)
+
+ISO_TO_LANGUAGE = {v["code"]: k for k, v in LANGUAGES.items()}
+
+
+SEED_FROM_VALUES = [
+    "any",
+    "rand",
+] + list(ISO_TO_LANGUAGE.keys())
+
+
+SEED_TO_VALUES = [
+    "tprv",
+    "xprv",
+] + list(ISO_TO_LANGUAGE.keys())
+
+
+ENTROPY_TO_VALUES = list(ISO_TO_LANGUAGE.keys())
+
+N_WORDS_ALLOWED_STR = [str(n) for n in N_WORDS_ALLOWED]
+
+TIMEOUT = 0.1
+
+
+logger = logging.getLogger(LOGGER)
+
+
+@click.group()
+@click.version_option(version=__version__, prog_name=__app_name__)
+def cli():
+    pass
+
+
+@click.command(name="seed", help="Generate a BIP-32 extended private key.")
+@click.option(
+    "-f",
+    "--from",
+    "from_",
+    type=click.Choice(SEED_FROM_VALUES),
+    help="Input format.",
+    default="rand",
+)
+@click.option(
+    "-t",
+    "--to",
+    type=click.Choice(SEED_TO_VALUES),
+    default="xprv",
+    help="Output format.",
+)
+@click.option("-u", "--input", help="Text as specified by --from")
+@click.option(
+    "-n",
+    "--number",
+    default="24",
+    type=click.Choice(N_WORDS_ALLOWED_STR),
+    help="Number of words.",
+)
+@click.option("-p", "--passphrase", default="")
+@click.option(
+    "--pretty/--not-pretty",
+    is_flag=True,
+    default=False,
+    help="Number and newline between words.",
+)
+def bip39_cmd(from_, to, input, number, passphrase, pretty):
+    input = input.strip() if input else input
+    number = int(number)
+    if (from_ == "rand" and input) or (from_ != "rand" and not input):
+        raise click.BadOptionUsage(
+            option_name="--from",
+            message="`--from words` requires `--input STRING`, `--from rand` forbids `--input`",
+        )
+    language = ISO_TO_LANGUAGE.get(from_)
+    if language or from_ == "any":
+        if to == "words":
+            raise click.BadOptionUsage(
+                option_name="--to",
+                message="`--to words` incompatible with `--from LANGUAGE`",
+            )
+        words = normalize_list(re.split(r"\s+", input), lower=True)
+        if language and not verify_seed_words(words, language):
+            raise click.BadParameter(
+                f"Unexpected {language} words from `--input` ({' '.join(words)}) or bad checksum.",
+                param_hint="--input",
+            )
+        if from_ == "any":
+            implied = relative_entropy(normalize_str(input, lower=True))
+            if implied < MIN_REL_ENTROPY:
+                click.secho(
+                    (
+                        f"Warning: Relative entropy of input seems low ({implied:.2f})."
+                        " Consider more complex --input."
+                    ),
+                    fg="yellow",
+                    err=True,
+                )
+    else:  # from_ == rand
+        entropy = None
+        words = entropy_to_words(number, entropy, ISO_TO_LANGUAGE.get(to, "english"))
+
+    if to in ISO_TO_LANGUAGE.keys():
+        if pretty:
+            output = "\n".join(f"{i + 1}) {w}" for i, w in enumerate(words))
+        else:
+            output = " ".join(words)
+
+        click.echo(output)
+    else:  # to == xprv | tprv
+        if pretty:
+            raise click.BadOptionUsage(
+                option_name="pretty",
+                message="`--pretty` has no effect with `--to xprv`",
+            )
+        # TODO: we do the entropy measure for not-strict against the string
+        # but that's not what we pass in here. here we pass in split on \s+
+        # for compatibility with foreign languages but is it really what we should
+        # do for the general case of arbitrary secrets?
+        # if so then not space is not that significant... :/
+        # * probably right thing to do is give the higher of the space or string
+        # score
+        seed = to_master_seed(words, passphrase)
+        prv = to_master_key(seed, mainnet=to == "xprv", private=True)
+
+        click.echo(prv)
+
+
+cli.add_command(bip39_cmd)
+
+
+@click.command(name="entropy", help="Derive secrets according to BIP-85")
+@click.option(
+    "-a",
+    "--application",
+    default="words",
+    required=True,
+    type=click.Choice(APPLICATIONS.keys()),
+)
+@click.option(
+    "-n",
+    "--number",
+    type=int,
+    help="length of output (bytes, chars, or words)",
+)
+@click.option(
+    "-i",
+    "--index",
+    type=click.IntRange(0, 2**31 - 1),
+    default=0,
+    help="child index",
+)
+@click.option(
+    "-s",
+    "--special",
+    default=10,
+    type=int,
+    help="additional int (e.g. for 'dice' sides)",
+)
+@click.option(
+    "-u",
+    "--input",
+    help="alternative to a unix input pipe",
+)
+@click.option(
+    "-t",
+    "--to",
+    type=click.Choice(ENTROPY_TO_VALUES),
+    help="output language",
+)
+def bip85_cmd(application, number, index, special, input, to):
+    if not input:
+        stdin, _, _ = select.select([sys.stdin], [], [], TIMEOUT)
+        if stdin:
+            lines = sys.stdin.readlines()
+            if lines:
+                # get just the last line because there might be a warning above
+                prv = lines[-1].strip()
+            else:
+                no_prv()
+        else:
+            no_prv()
+    else:
+        prv = input
+    if number is not None:
+        number = int(number)
+        if application in ("wif", "xprv"):
+            raise click.BadOptionUsage(
+                option_name="--number",
+                message="`--number` has no effect when `--application wif|xprv`",
+            )
+        elif number < 1:
+            raise click.BadOptionUsage(
+                option_name="--number",
+                message="must be a positive integer",
+            )
+    else:
+        number = 24
+
+    if not prv[:4] in ("tprv", "xprv"):
+        no_prv()
+
+    master = parse_ext_key(prv)
+
+    path = f"m/{PURPOSE_CODES['BIP-85']}"
+    app_code = APPLICATIONS[application]
+    path += f"/{app_code}"
+
+    if to:
+        if application != "words":
+            raise click.BadOptionUsage(
+                option_name="--to",
+                message="--to requires `--application words`",
+            )
+    else:
+        to = "eng"
+
+    if application == "words":
+        language = ISO_TO_LANGUAGE[to]
+        code_85 = next(i for i, l in INDEX_TO_LANGUAGE.items() if l == language)
+        path += f"/{code_85}/{number}'/{index}'"
+    elif application in ("wif", "xprv"):
+        path += f"/{index}'"
+    elif application in ("base64", "base85", "hex"):
+        check_range(number, application)
+        path += f"/{number}'/{index}'"
+    elif application == "drng":
+        path += f"/0'/{index}'"
+    elif application == "dice":
+        check_range(number, application)
+        path += f"/{special}'/{number}'/{index}'"
+    else:
+        raise click.BadOptionUsage(
+            option_name="--application",
+            message=f"unrecognized {application}",
+        )
+
+    derived = derive(master, path)
+    if application == "drng":
+        drng = DRNG(to_entropy(derived.data[1:]))
+        output = to_hex_string(drng.read(number))
+    else:
+        output = apply_85(derived, path)["application"]
+    click.echo(output)
+
+
+cli.add_command(bip85_cmd)
+
+
+def check_range(number: int, application: str):
+    (min, max) = RANGES[application]
+    if not (min <= number <= max):
+        raise click.BadOptionUsage(
+            option_name="--number",
+            message=f"out of range; try [{min}, {max}] for {application}",
+        )
+
+
+def no_prv():
+    raise click.BadOptionUsage(
+        option_name="--input",
+        message="Missing xprv or tprv from pipe or --input. Try `bipsea seed | bipsea entropy`",
+    )
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/bipsea/bipsea-0.6.0.tar.gz/bipsea-0.6.0/tests/test_cli.py
@@ -0,0 +1,223 @@
+import logging
+import random
+
+import pytest
+from click.testing import CliRunner
+from data.bip39_vectors import VECTORS
+from data.bip85_vectors import BIP_39, HEX, PWD_BASE85, WIF
+
+from bipsea.bip39 import LANGUAGES, verify_seed_words
+from bipsea.bipsea import ISO_TO_LANGUAGE, N_WORDS_ALLOWED, cli
+from bipsea.util import ASCII_INPUTS, LOGGER
+
+logger = logging.getLogger(LOGGER)
+
+
+MNEMONIC_12 = {
+    "words": "punch man spread gap size struggle clean crouch cloth swear erode fan",
+    "xprv": (
+        "xprv9s21ZrQH143K417dJYmPr6Qmy2t61xrKtDCCL3Cec4NMFFFRZTF"
+        "2jSbtqSXpuCz8UqgsuyrPC5wngx3dk5Gt8zQnbnHVAsMyb7bWtHZ95Jk"
+    ),
+}
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+@pytest.mark.parametrize("language, vectors", VECTORS.items(), ids=VECTORS.keys())
+def test_seed_command_to_actual_seed(runner, language, vectors):
+    for vector in vectors[:1]:  # for speed since test_bip39 already covers all
+        _, mnemonic, _, xprv = vector
+        for upper in (True, False):
+            mnemonic = mnemonic.upper() if upper else mnemonic
+            result = runner.invoke(
+                cli,
+                [
+                    "seed",
+                    "-t",
+                    "xprv",
+                    "-f",
+                    LANGUAGES[language]["code"],
+                    "--input",
+                    mnemonic,
+                    "-p",
+                    "TREZOR",
+                ],
+            )
+            assert result.exit_code == 0
+            last = result.output.strip().split("\n")[-1]
+            assert last == xprv
+
+
+@pytest.mark.parametrize("language, vectors", VECTORS.items(), ids=VECTORS.keys())
+def test_seed_option_sensitivity(runner, language, vectors):
+    """prove that meaningful passphrase mnemonic changes change the xprv
+    (but white space after the mnemonic doesn't)"""
+    for vector in vectors[:1]:  # one vector per language for speed
+        _, mnemonic, _, xprv = vector
+        lang_code = LANGUAGES[language]["code"]
+        base_cmd = ["seed", "-t", "xprv", "-f", lang_code]
+        change_passphrase = runner.invoke(
+            cli, base_cmd + ["-u", mnemonic, "-p", "TREZoR"]
+        )
+        assert change_passphrase.exit_code == 0
+        result_xprv = change_passphrase.output.strip().split("\n")[-1]
+        assert result_xprv != xprv
+
+        for suffix in ("", ".", " \t\n "):
+            cmd = base_cmd + ["-u", mnemonic + suffix, "-p", "TREZOR"]
+            result = runner.invoke(cli, cmd)
+            if suffix == ".":
+                assert result.exit_code != 0
+                assert "Unexpected" in result.output
+            else:
+                assert result.exit_code == 0
+                result_xprv = result.output.strip().split("\n")[-1]
+                assert result_xprv == xprv
+
+
+@pytest.mark.parametrize("code", [v["code"] for v in LANGUAGES.values()])
+@pytest.mark.parametrize("n", N_WORDS_ALLOWED, ids=lambda n: f"{n}-words")
+def test_seed_command_from_rand(runner, n, code):
+    for style in ("--not-pretty", "--pretty"):
+        cmd = ["seed", "-t", code, "-n", str(n), "-f", "rand"]
+        cmd.append(style)
+        result = runner.invoke(cli, cmd)
+        output = result.output.strip()
+        split_on = "\n" if style == "--pretty" else " "
+        words = output.split(split_on)
+        assert len(words) == int(n)
+        assert result.exit_code == 0
+        if style != "--pretty":
+            assert verify_seed_words(words, ISO_TO_LANGUAGE[code])
+
+
+def test_seed_command_from_custom_words(runner):
+    lengths = {"short": 5, "enough": 42}
+    base = ["seed", "-t", "xprv"]
+    for k, v in lengths.items():
+        cmd = base + ["-f", "any", "-u", gen_custom_seed_words(v, 0)]
+        result = runner.invoke(cli, cmd)
+        assert result.exit_code == 0
+        if k == "short":
+            assert "Warning" in result.output
+        else:
+            assert "Warning" not in result.output
+
+
+def gen_custom_seed_words(length: int, seed: int):
+    """non bip-39 seeds (--from "any")"""
+    random.seed(seed)
+    custom = "".join(
+        random.choice("".join(sorted(list(ASCII_INPUTS)))) for _ in range(length)
+    )
+
+    return custom
+
+
+def test_seed_from_and_to_words(runner):
+    result = runner.invoke(cli, ["seed", "--from", "eng", "--to", "eng"])
+    assert result.exit_code != 0
+    assert "--input" in result.output
+    assert "--from rand" in result.output
+
+
+@pytest.mark.parametrize("n", [-1, 11, 13, 0, 25])
+def test_seed_bad_n(runner, n):
+    result = runner.invoke(cli, ["seed", "--from", "eng", "-n", n])
+    assert result.exit_code != 0
+    assert "--number" in result.output
+
+
+@pytest.mark.parametrize("from_", ["baz", "zho", "free"])
+def test_seed_bad_from(runner, from_):
+    result = runner.invoke(cli, ["seed", "--from", "baz"])
+    assert result.exit_code != 0
+    assert "not one of" in result.output
+
+
+def test_seed_bad_to(runner):
+    result = runner.invoke(cli, ["seed", "--to", "blah"])
+    assert result.exit_code != 0
+    assert "not one of" in result.output
+
+
+def test_bipsea_integration(runner):
+    result_seed = runner.invoke(
+        cli,
+        ["seed", "-f", "eng", "-u", MNEMONIC_12["words"], "-n", "12", "-t", "xprv"],
+    )
+    xprv = result_seed.output.strip()
+    assert xprv == MNEMONIC_12["xprv"]
+    assert result_seed.exit_code == 0
+    result_entropy = runner.invoke(
+        cli, ["entropy", "-a", "base64", "-n", "20", "--input", xprv]
+    )
+    assert result_entropy.exit_code == 0
+    pwd64 = result_entropy.output.strip()
+    assert pwd64 == "72zJIS7JhyR5r5NjkuE/"
+    assert len(pwd64) == 20
+
+
+@pytest.mark.parametrize("vector", PWD_BASE85, ids=["PWD_BASE85"])
+def test_entropy_n(runner, vector):
+    xprv = vector["master"]
+    for app in ("base64", "base85", "hex", "drng"):
+        for n in (20, 50, 64):
+            result = runner.invoke(
+                cli, ["entropy", "-a", app, "-n", n, "--input", xprv]
+            )
+            assert result.exit_code == 0
+            answer = result.output.strip()
+            length = len(answer)
+            if app in ("hex", "drng"):
+                length = length // 2
+            assert length == n
+
+
+@pytest.mark.parametrize("vector", PWD_BASE85, ids=["PWD_BASE85"])
+def test_entropy_n_out_of_range(runner, vector):
+    xprv = vector["master"]
+    for app in ("base64", "base85", "hex", "drng"):
+        for n in (-1, 0, 1025):
+            if n == 1025 and app == "drng":
+                break
+            result = runner.invoke(
+                cli, ["entropy", "-a", app, "-n", n, "--input", xprv]
+            )
+            assert result.exit_code != 0
+            assert "Error" in result.output
+
+
+@pytest.mark.parametrize(
+    "vector",
+    BIP_39,
+    ids=[f"BIP_39-{v['mnemonic_length']}-words" for v in BIP_39],
+)
+def test_entropy_bip39(runner, vector):
+    xprv = vector["master"]
+    n_words = vector["mnemonic_length"]
+    result = runner.invoke(
+        cli, ["entropy", "-a", "words", "--input", xprv, "-n", n_words]
+    )
+    assert result.exit_code == 0
+    assert result.output.strip() == vector["derived_mnemonic"]
+
+
+@pytest.mark.parametrize("vector", HEX, ids=["HEX"])
+def test_entropy_hex(runner, vector):
+    xprv = vector["master"]
+    result = runner.invoke(cli, ["entropy", "-a", "hex", "--input", xprv, "-n", 64])
+    assert result.exit_code == 0
+    assert result.output.strip() == vector["derived_entropy"]
+
+
+@pytest.mark.parametrize("vector", WIF, ids=["WIF"])
+def test_entropy_wif(runner, vector):
+    xprv = vector["master"]
+    result = runner.invoke(cli, ["entropy", "-a", "wif", "--input", xprv])
+    assert result.exit_code == 0
+    assert result.output.strip() == vector["derived_wif"]
