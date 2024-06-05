9133ec339972cadcc0fb34b5356abaef53fe3934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:04:57 +0000

    Add package best-face-identification/1.0.0/best_face_identification-1.0.0.tar.gz

---
+++ packages/best-face-identification/best_face_identification-1.0.0.tar.gz/best_face_identification-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+def pre_install():
+    f = open("readme.md", "r")
+    text = f.read()
+    return text
+
+
+def requires():
+    f = open("requirements.txt", "r")
+    text = f.read()
+    text = text.strip("\n").split("\n")
+    return text
+
+setup(
+    name= "best_face_identification",
+    version= "1.0.0",
+    author="javad nematollahi",
+    description="This app is a face identifier. If your face is existed in face bank, you recieved True, otherwise False.",
+    long_description=pre_install(),
+    long_description_content_type='text/markdown',
+    install_requires=requires(),
+    entry_points={
+        'console_scripts': [
+            'fi = face_identification:main',
+        ],
+    },
+    author_email="javadnematollahi92@gmail.com",
+    packages=['face_identification']
+)
+
+
