8548e94d5890a9dcb5bebf9a3ca39be2c5d6b68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:03:47 +0000

    Add package PieRakNet/1.0.7.3/pieraknet-1.0.7.3.tar.gz

---
+++ packages/PieRakNet/pieraknet-1.0.7.3.tar.gz/pieraknet-1.0.7.3/pieraknet/buffer.py
@@ -0,0 +1,181 @@
+#
+#
+# //--------\\    [----------]   ||--------]   ||\      /||    ||----------]
+# ||        ||         ||        ||            ||\\    //||    ||
+# ||        //         ||        ||======|     || \\  // ||    ||
+# ||-------//          ||        ||            ||  \\//  ||    ||
+# ||                   ||        ||            ||   —–   ||    ||
+# ||              [----------]   ||--------]   ||        ||    ||----------]
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# @author PieMC Team
+# @link http://www.PieMC-Dev.github.io/
+#
+#
+#
+
+import struct
+from io import BytesIO
+
+class UnsupportedIPVersion(Exception):
+    pass
+
+class EOSError(Exception):
+    pass
+
+class BuffError(Exception):
+    pass
+
+class Buffer(BytesIO):
+    def feos(self):
+        return len(self.getvalue()[self.tell():]) == 0
+
+    def read_packet_id(self):  # Read Packet ID
+        return self.read_byte()
+
+    def write_packet_id(self, data):
+        self.write_byte(data)
+
+    def read_byte(self):
+        return struct.unpack('B', self.read(1))[0]
+
+    def write_byte(self, data):
+        self.write(struct.pack('B', data))
+        
+    def read_bits(self, num_bits):
+        byte_data = self.read((num_bits + 7) // 8)
+        bits = []
+        for byte in byte_data:
+            for i in range(7, -1, -1):
+                bits.append((byte >> i) & 1)
+        return bits[:num_bits]
+    
+    def write_bits(self, bit_array):
+        num_bits = len(bit_array)
+        num_bytes = (num_bits + 7) // 8
+        byte_data = bytearray(num_bytes)
+        for i, bit in enumerate(bit_array):
+            byte_index = i // 8
+            bit_index = 7 - (i % 8)
+            if bit:
+                byte_data[byte_index] |= (1 << bit_index)
+        self.write(byte_data)
+        
+    def read_ubyte(self):
+        return struct.unpack('<B', self.read(1))[0]
+
+    def write_ubyte(self, data):
+        self.write(struct.pack('<B', data))
+
+    def read_short(self):
+        shrt = self.read(2)
+        return struct.unpack('>h', shrt)[0]
+
+    def write_short(self, data):
+        self.write(struct.pack('>h', data))
+
+    def read_unsigned_short(self):
+        ushrt = self.read(2)
+        return struct.unpack('>H', ushrt)[0]
+
+    def write_unsigned_short(self, data):
+        self.write(struct.pack('>H', data))
+
+    def read_magic(self):
+        return self.read(16)
+
+    def write_magic(self, data=b'00ffff00fefefefefdfdfdfd12345678'):
+        if not isinstance(data, bytes):
+            data = data.encode('utf-8')
+        self.write(data)
+
+    def read_long(self):
+        return struct.unpack('>q', self.read(8))[0]
+
+    def write_long(self, data):
+        self.write(struct.pack('>q', data))
+
+    def read_ulong(self):
+        return struct.unpack('>Q', self.read(8))[0]
+
+    def write_ulong(self, data):
+        self.write(struct.pack('>Q', data))
+
+    def read_int(self):
+        dat = self.read(4)
+        return struct.unpack(">i", dat)[0]
+
+    def write_int(self, data):
+        self.write(struct.pack('>i', data))
+
+    def read_uint(self):
+        return struct.unpack(">I", self.read(4))[0]
+
+    def write_uint(self, data):
+        self.write(struct.pack('>I', data))
+
+    def read_bool(self):
+        return struct.unpack('?', self.read(1))[0]
+
+    def write_bool(self, data):
+        self.write(struct.pack('?', data))
+
+    def read_uint24le(self):
+        uint24le = self.read(3) + b'\x00'
+        return struct.unpack("<I", uint24le)[0]
+
+    def write_uint24le(self, data):
+        self.write(struct.pack("<I", data)[:3])
+
+    def read_string(self):
+        length = self.read_short()
+        string = self.read(length).decode('ascii')
+        return string
+
+    def write_string(self, data):
+        if not isinstance(data, bytes):
+            data = data.encode('ascii')
+        self.write_short(len(data))
+        self.write(data)
+
+    def read_address(self):
+        ipv = self.read_byte()
+        if ipv == 4:
+            hostname_parts = []
+            for part in range(4):
+                hostname_parts.append(str(~self.read_byte() & 0xff))
+            hostname = ".".join(hostname_parts)
+            port = self.read_unsigned_short()
+            return hostname, port
+        else:
+            raise UnsupportedIPVersion('IP version is not 4')
+
+    def write_address(self, address: tuple):
+        self.write_byte(4)
+        hostname_parts: list = address[0].split('.')
+        for part in hostname_parts:
+            self.write_byte(~int(part) & 0xff)
+        self.write_unsigned_short(address[1])
+
+    def read_var_int(self):
+        value: int = 0
+        for i in range(0, 35, 7):
+            number = self.read_ubyte()
+            value |= ((number & 0x7f) << i)
+            if (number & 0x80) == 0:
+                return value
+        raise BuffError("VarInt is too big")
+
+    def write_var_int(self, value: int) -> None:
+        value &= 0xffffffff
+        for i in range(0, 5):
+            to_write: int = value & 0x7f
+            value >>= 7
+            if value != 0:
+                self.write_ubyte(to_write | 0x80)
+            else:
+                self.write_ubyte(to_write)
+                break
---
+++ packages/PieRakNet/pieraknet-1.0.7.3.tar.gz/pieraknet-1.0.7.3/pieraknet/handlers/open_connection_request_2.py
@@ -0,0 +1,38 @@
+from pieraknet.packets.open_connection_request_2 import OpenConnectionRequest2
+from pieraknet.packets.open_connection_reply_2 import OpenConnectionReply2
+from pieraknet.connection import Connection
+
+class OpenConnectionRequest2Handler:
+    @staticmethod
+    def handle(packet: OpenConnectionRequest2, server, address: tuple):
+        packet.decode()
+
+        server.logger.debug("New Packet:")
+        server.logger.debug(f"- Packet ID: {packet.PACKET_ID}")
+        server.logger.debug(f"- Packet Body: {packet.getvalue()[1:]}")
+        server.logger.debug(f"- Packet Name: Open Connection Request 2")
+        server.logger.debug(f"- MAGIC: {packet.magic}")
+        server.logger.debug(f"- Server Address: {packet.server_address}")
+        server.logger.debug(f"- MTU Size: {packet.mtu_size}")
+        server.logger.debug(f"- Client GUID: {packet.client_guid}")
+
+        new_packet = OpenConnectionReply2()
+        new_packet.magic = packet.magic  # TODO: server.magic
+        new_packet.server_guid = server.guid
+        new_packet.client_address = address
+        new_packet.mtu_size = packet.mtu_size
+        new_packet.encode()
+
+        server.send(new_packet.getvalue(), address)
+
+        server.logger.debug("Sent Packet:")
+        server.logger.debug(f"- Packet ID: {new_packet.PACKET_ID}")
+        server.logger.debug(f"- Packet Body: {new_packet.getvalue()[1:]}")
+        server.logger.debug(f"- Packet Name: Open Connection Reply 2")
+        server.logger.debug(f"- MAGIC: {new_packet.magic}")
+        server.logger.debug(f"- Server GUID: {new_packet.server_guid}")
+        server.logger.debug(f"- Client Address: {new_packet.client_address}")
+        server.logger.debug(f"- MTU Size: {new_packet.mtu_size}")
+
+        connection = Connection(address, server, packet.mtu_size, packet.client_guid)
+        server.add_connection(connection)
