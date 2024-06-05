e881e561a96846b769c5afbfe41af85fdc8a6dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:58 +0000

    Add package andrology-statistics/0.1.0/andrology-statistics-0.1.0.tar.gz

---
+++ packages/andrology-statistics/andrology-statistics-0.1.0.tar.gz/andrology-statistics-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os import path
+work_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(work_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='andrology-statistics',
+    version='0.1.0',
+    description='Stats for andrology data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(), auto_discover_packages=True,
+    install_requires=[],
+)
