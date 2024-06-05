e04f2183e8a1df515e6fae24b1fffc93a459564b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:03:59 +0000

    Add package snowman-py/0.0.5/snowman_py-0.0.5.tar.gz

---
+++ packages/snowman-py/snowman_py-0.0.5.tar.gz/snowman_py-0.0.5/crates/snowman_connector/src/query.rs
@@ -0,0 +1,5 @@
+mod get_schema_infomations;
+mod get_schemas;
+
+pub use get_schema_infomations::{get_schema_infomations, SchemaInfomation};
+pub use get_schemas::{get_schemas, DatabaseSchema};
---
+++ packages/snowman-py/snowman_py-0.0.5.tar.gz/snowman_py-0.0.5/crates/snowman_connector/src/query/get_schemas.rs
@@ -0,0 +1,29 @@
+use crate::Connection;
+
+#[derive(Debug, Clone)]
+pub struct DatabaseSchema {
+    pub database_name: String,
+    pub schema_name: String,
+}
+
+pub async fn get_schemas(connection: &Connection) -> Result<Vec<DatabaseSchema>, crate::Error> {
+    let rows = connection
+        .execute(
+            "
+            SELECT
+                catalog_name as database_name,
+                schema_name
+            FROM
+                information_schema.schemata
+            ",
+        )
+        .await?;
+    let mut db_schemas = vec![];
+    for row in rows {
+        db_schemas.push(DatabaseSchema {
+            database_name: row.get("database_name").unwrap(),
+            schema_name: row.get("schema_name").unwrap(),
+        });
+    }
+    Ok(db_schemas)
+}
