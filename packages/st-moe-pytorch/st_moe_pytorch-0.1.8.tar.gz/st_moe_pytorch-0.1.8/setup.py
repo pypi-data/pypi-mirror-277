2ef6d73c688067c776933607c6d18a20dc34ba3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 14:04:29 +0000

    Add package st-moe-pytorch/0.1.8/st_moe_pytorch-0.1.8.tar.gz

---
+++ packages/st-moe-pytorch/st_moe_pytorch-0.1.8.tar.gz/st_moe_pytorch-0.1.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'st-moe-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.1.8',
+  license='MIT',
+  description = 'ST - Mixture of Experts - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/st-moe-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'mixture of experts'
+  ],
+  install_requires=[
+    'beartype',
+    'CoLT5-attention>=0.10.15',
+    'einops>=0.6',
+    'torch>=2.0',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
