62eccad3b791a93399203cd4f60a77ad16402409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:03:53 +0000

    Add package deepeval/0.21.49/deepeval-0.21.49.tar.gz

---
+++ packages/deepeval/deepeval-0.21.49.tar.gz/deepeval-0.21.49/deepeval/models/__init__.py
@@ -0,0 +1,19 @@
+from deepeval.models.base_model import (
+    DeepEvalBaseModel,
+    DeepEvalBaseLLM,
+    DeepEvalBaseEmbeddingModel,
+)
+from deepeval.models.gpt_model import GPTModel
+
+# TODO: uncomment out once fixed
+# from deepeval.models.summac_model import SummaCModels
+
+# TODO: uncomment out once fixed
+# from deepeval.models.detoxify_model import DetoxifyModel
+# from deepeval.models.unbias_model import UnBiasedModel
+
+# TODO: restructure or delete (if model logic not needed)
+# from deepeval.models.answer_relevancy_model import (
+#     AnswerRelevancyModel,
+#     CrossEncoderAnswerRelevancyModel,
+# )
