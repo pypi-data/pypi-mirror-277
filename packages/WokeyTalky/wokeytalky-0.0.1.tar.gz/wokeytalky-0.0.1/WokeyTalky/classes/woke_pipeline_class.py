62ec93b5835183c9239d9a6e0606a37f8bfc181e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 14:04:38 +0000

    Add package WokeyTalky/0.0.1/wokeytalky-0.0.1.tar.gz

---
+++ packages/WokeyTalky/wokeytalky-0.0.1.tar.gz/wokeytalky-0.0.1/WokeyTalky/classes/woke_pipeline_class.py
@@ -0,0 +1,10 @@
+# package_name/my_class.py
+
+class WokePipeline:
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self.param2 = param2
+    
+    def some_method(self):
+        # Method implementation goes here
+        pass
