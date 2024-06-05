4559f1308221a522271ed0f3e6a625e2afdc6014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 06:04:46 +0000

    Add package quantconnect-stubs/16469/quantconnect-stubs-16469.tar.gz

---
+++ packages/quantconnect-stubs/quantconnect-stubs-16469.tar.gz/quantconnect-stubs-16469/QuantConnect/Data/Custom/AlphaStreams/__init__.pyi
@@ -0,0 +1,9 @@
+from typing import overload
+import QuantConnect.Data.Custom.AlphaStreams
+import System
+
+
+class PlaceHolder(System.Object):
+    """'QuantConnect.Data.Custom.Alphas' namespace is obsolete"""
+
+
