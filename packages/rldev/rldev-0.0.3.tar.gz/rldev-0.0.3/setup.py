05810371490ef502a6e8aabb10728613dd63db04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:05:04 +0000

    Add package rldev/0.0.3/rldev-0.0.3.tar.gz

---
+++ packages/rldev/rldev-0.0.3.tar.gz/rldev-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rldev',
+    packages=find_packages(),
+    version='0.0.3',
+    author='Zhang Dongkun',
+    author_email='zhangdongkun98@gmail.com',
+    url='https://github.com/zhangdongkun98/rl-dev',
+    description='Kit for RL.',
+    install_requires=[
+    ],
+
+    include_package_data=True
+)
