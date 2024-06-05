5f3003fa4c85ba3ec8861515d6e185bb28555d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 14:04:30 +0000

    Add package combinedpackmsnk/0.3.3/combinedpackmsnk-0.3.3.tar.gz

---
+++ packages/combinedpackmsnk/combinedpackmsnk-0.3.3.tar.gz/combinedpackmsnk-0.3.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='combinedpackmsnk',
+    version='0.3.3',  # Increment the version number
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'plotly',
+        'scikit-learn',
+        'statsmodels',
+        'xgboost',
+        'ydata-profiling',
+        'nbformat'
+    ],
+    entry_points={
+        'console_scripts': [],
+    },
+    author='mukund14',
+    author_email='mukundan.sankar14@gmail.com',
+    description='A comprehensive package for data analysis, data visualization, data preprocessing, and machine learning',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mukund14/combinedpackmsnk',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
