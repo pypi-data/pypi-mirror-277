0395d0ec855dd3a97e430d13d97199fcf0b9e3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:40 +0000

    Add package My-Core133/1.2/My_Core133-1.2.tar.gz

---
+++ packages/My-Core133/My_Core133-1.2.tar.gz/My_Core133-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='My_Core133',
+      version='1.2',
+      description='My_Core133 in OOP course',
+      packages=['My_Core133'],
+      author_email='dim.chechulov@gmail.com',
+      zip_safe=False
+      )
