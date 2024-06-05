b0345ad4a8439888217c7e73a75e11a00900c24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:05:09 +0000

    Add package superannotate-schemas/1.0.49rc1/superannotate_schemas-1.0.49rc1.tar.gz

---
+++ packages/superannotate-schemas/superannotate_schemas-1.0.49rc1.tar.gz/superannotate_schemas-1.0.49rc1/setup.py
@@ -0,0 +1,44 @@
+import os
+import re
+from pathlib import Path
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+def get_version():
+    init = open(os.path.join(this_directory, 'src', 'superannotate_schemas', '__init__.py')).read()
+    match = re.search(r'^__version__ = [\'"]([^\'"]+)[\'"]', init, re.M)
+    if not match:
+        raise RuntimeError('Unable to find version string.')
+    return match.group(1)
+
+
+requirements = []
+
+with open("requirements.txt") as f:
+    requirements.extend(f.read().splitlines())
+
+setup(
+    name='superannotate_schemas',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=get_version(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=requirements,
+    description='SuperAnnotate JSON Schemas',
+    author='Vaghinak Basentsyan',
+    author_email='vaghinak@superannotate.con',
+    url='https://www.superannotate.com/',
+    license='MIT',
+    description_file="README.md",
+    entry_points={
+        'console_scripts': ['superannotate_schemas = superannotate_schemas.bin.app:main']
+    },
+    include_package_data=True,
+    package_data={
+        'superannotate_schemas': ['schemas/*.json'],
+    },
+)
