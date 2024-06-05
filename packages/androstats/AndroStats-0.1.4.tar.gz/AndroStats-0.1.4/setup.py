5119be1a92b8d0497ac4e3367b801a985ad8f370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:59 +0000

    Add package androstats/0.1.4/AndroStats-0.1.4.tar.gz

---
+++ packages/androstats/AndroStats-0.1.4.tar.gz/AndroStats-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from os import path
+
+work_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(work_dir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="AndroStats",
+    version="0.1.4",
+    description="Stats for andrology data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    auto_discover_packages=True,
+    install_requires=[],
+)
