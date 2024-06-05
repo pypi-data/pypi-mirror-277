dfc13746e307f8e0eaf840af201a9831c294a40d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:45 +0000

    Add package django-customvueadmin/0.2.21/django_customvueadmin-0.2.21.tar.gz

---
+++ packages/django-customvueadmin/django_customvueadmin-0.2.21.tar.gz/django_customvueadmin-0.2.21/custom_admin/api/actions/delete_action.py
@@ -0,0 +1,24 @@
+from django.db.models.deletion import ProtectedError
+from django.utils.translation import gettext as _
+
+from custom_admin.api.action_functions import admin_action
+
+
+@admin_action(
+    short_description=_("Удалить"),
+    icon='mdi-delete-forever',
+    base_color='#ff3333',
+    variant='outlined',
+    confirmation_text=_('Вы уверены, что хотите удалить выбранные записи?'),
+)
+def delete_action(view, request, queryset, *args, **kwargs):
+    try:
+        queryset.delete()
+    except ProtectedError as e:
+        return _(
+            'Невозможно удалить некоторые экземпляры модели, '
+            'поскольку на них ссылаются через защищенные внешние ключи: %(objects)s') % {
+                   'objects': ", ".join([str(obj) for obj in e.protected_objects])
+               }, 400
+
+    return _('Записи успешно удалены!'), 200
