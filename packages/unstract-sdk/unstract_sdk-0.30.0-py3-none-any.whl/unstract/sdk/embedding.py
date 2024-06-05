5e9690b5803d7074b9ae0794bb1eff7bcf47644c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 17:03:53 +0000

    Add package unstract-sdk/0.30.0/unstract_sdk-0.30.0-py3-none-any.whl

---
+++ packages/unstract-sdk/unstract_sdk-0.30.0-py3-none-any.whl/unstract/sdk/embedding.py
@@ -0,0 +1,88 @@
+from typing import Any
+
+from llama_index.core.base.embeddings.base import Embedding
+from llama_index.core.embeddings import BaseEmbedding
+from typing_extensions import deprecated
+from unstract.adapters.constants import Common
+from unstract.adapters.embedding import adapters
+
+from unstract.sdk.adapters import ToolAdapter
+from unstract.sdk.constants import LogLevel, ToolEnv
+from unstract.sdk.exceptions import EmbeddingError, SdkError
+from unstract.sdk.tool.base import BaseTool
+from unstract.sdk.utils.callback_manager import CallbackManager
+
+
+class Embedding:
+    _TEST_SNIPPET = "Hello, I am Unstract"
+    MAX_TOKENS = 1024 * 16
+    embedding_adapters = adapters
+
+    def __init__(
+        self,
+        tool: BaseTool,
+        adapter_instance_id: str,
+        usage_kwargs: dict[Any, Any] = {},
+    ):
+        self._tool = tool
+        self._adapter_instance_id = adapter_instance_id
+        self._embedding_instance: BaseEmbedding = self._get_embedding()
+        self._length: int = self._get_embedding_length()
+
+        self._usage_kwargs = usage_kwargs.copy()
+        self._usage_kwargs["adapter_instance_id"] = adapter_instance_id
+        platform_api_key = self._tool.get_env_or_die(ToolEnv.PLATFORM_API_KEY)
+        CallbackManager.set_callback_manager(
+            platform_api_key=platform_api_key,
+            model=self._embedding_instance,
+            kwargs=self._usage_kwargs,
+        )
+
+    def _get_embedding(self) -> BaseEmbedding:
+        """Gets an instance of LlamaIndex's embedding object.
+
+        Args:
+            adapter_instance_id (str): UUID of the embedding adapter
+
+        Returns:
+            BaseEmbedding: Embedding instance
+        """
+        try:
+            embedding_config_data = ToolAdapter.get_adapter_config(
+                self._tool, self._adapter_instance_id
+            )
+            embedding_adapter_id = embedding_config_data.get(Common.ADAPTER_ID)
+            if embedding_adapter_id not in self.embedding_adapters:
+                raise SdkError(
+                    f"Embedding adapter not supported : " f"{embedding_adapter_id}"
+                )
+
+            embedding_adapter = self.embedding_adapters[embedding_adapter_id][
+                Common.METADATA
+            ][Common.ADAPTER]
+            embedding_metadata = embedding_config_data.get(Common.ADAPTER_METADATA)
+            embedding_adapter_class = embedding_adapter(embedding_metadata)
+            return embedding_adapter_class.get_embedding_instance()
+        except Exception as e:
+            self._tool.stream_log(
+                log=f"Error getting embedding: {e}", level=LogLevel.ERROR
+            )
+            raise EmbeddingError(f"Error getting embedding instance: {e}") from e
+
+    def get_query_embedding(self, query: str) -> Embedding:
+        return self._embedding_instance.get_query_embedding(query)
+
+    def _get_embedding_length(self) -> int:
+        embedding_list = self._embedding_instance._get_text_embedding(
+            self._TEST_SNIPPET
+        )
+        embedding_dimension = len(embedding_list)
+        return embedding_dimension
+
+    @deprecated("Use the new class Embedding")
+    def get_embedding_length(self, embedding: BaseEmbedding) -> int:
+        return self._get_embedding_length(embedding)
+
+
+# Legacy
+ToolEmbedding = Embedding
---
+++ packages/unstract-sdk/unstract_sdk-0.30.0-py3-none-any.whl/unstract/sdk/ocr.py
@@ -0,0 +1,54 @@
+from abc import ABCMeta
+from typing import Optional
+
+from typing_extensions import deprecated
+from unstract.adapters.constants import Common
+from unstract.adapters.ocr import adapters
+from unstract.adapters.ocr.ocr_adapter import OCRAdapter
+
+from unstract.sdk.adapters import ToolAdapter
+from unstract.sdk.constants import LogLevel
+from unstract.sdk.tool.base import BaseTool
+
+
+class OCR(metaclass=ABCMeta):
+    def __init__(
+        self,
+        tool: BaseTool,
+        adapter_instance_id: str,
+    ):
+        self.tool = tool
+        self.ocr_adapters = adapters
+        self.adapter_instance_id = adapter_instance_id
+        self.ocr_instance: OCRAdapter = self._get_ocr()
+
+    def _get_ocr(self) -> Optional[OCRAdapter]:
+        try:
+            ocr_config = ToolAdapter.get_adapter_config(
+                self.tool, self.adapter_instance_id
+            )
+            ocr_adapter_id = ocr_config.get(Common.ADAPTER_ID)
+            if ocr_adapter_id in self.ocr_adapters:
+                ocr_adapter = self.ocr_adapters[ocr_adapter_id][Common.METADATA][
+                    Common.ADAPTER
+                ]
+                ocr_metadata = ocr_config.get(Common.ADAPTER_METADATA)
+                self.ocr_instance = ocr_adapter(ocr_metadata)
+
+                return self.ocr_instance
+
+        except Exception as e:
+            self.tool.stream_log(
+                log=f"Unable to get OCR adapter {self.adapter_instance_id}: {e}",
+                level=LogLevel.ERROR,
+            )
+            return None
+
+    def process(
+        self, input_file_path: str, output_file_path: Optional[str] = None
+    ) -> str:
+        return self.ocr_instance.process(input_file_path, output_file_path)
+
+    @deprecated("Use the class instance")
+    def get_x2text(self, adapter_instance_id: str) -> OCRAdapter:
+        return self.ocr_instance
