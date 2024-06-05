bec3464ecf0074c7dc65b0b91ce4940aea1f99e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:04:30 +0000

    Add package pylove/1.0.3/pylove-1.0.3.tar.gz

---
+++ packages/pylove/pylove-1.0.3.tar.gz/pylove-1.0.3/version.py
@@ -0,0 +1,18 @@
+"This file holds consent values about the version and what has changed"
+VERSION = "1.0.3"
+
+CHANGE_LOG = """
+Version 1.0.3
+
+
+Version 1.0.2
+- Uploaded to pypi and updated relevant details
+
+Version 1.0.1
+- Fixed error for time values not clamped
+- Added decode_response() to help readability
+
+Version 1.0.0
+- Initial release
+- pylove/lan GameModeWrapper creation
+"""
