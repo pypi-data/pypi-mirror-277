83dfa98d77f2cb0242e5531757f4898933beedc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 04:03:37 +0000

    Add package sunny-order-splitter/0.1.1/sunny_order_splitter-0.1.1.tar.gz

---
+++ packages/sunny-order-splitter/sunny_order_splitter-0.1.1.tar.gz/sunny_order_splitter-0.1.1/README.md
@@ -0,0 +1,2 @@
+# sunny-order-splitter-python-sdk
+
---
+++ packages/sunny-order-splitter/sunny_order_splitter-0.1.1.tar.gz/sunny_order_splitter-0.1.1/tests/test_core.py
@@ -0,0 +1,33 @@
+# tests/test_core.py
+
+import unittest
+from sunny_order_splitter.core import SunnyOrderSplitter
+
+class TestSunnyOrderSplitter(unittest.TestCase):
+    def test_split_order(self):
+        # Mock API keys for testing
+        api_key = "Q9TdT7Tl9KaoFBWaxJ6Rlt8N4fEurKYeBsYfUhXgTRSFMZWzbWa14kybz5vLebCk"
+        secret_key = "9UJ7Vx3SZRF6mvmmLGksR08UMChTiWZG3fyA4JdsyK4pWKsnuqXQwk7N3q8mBw43"
+        symbol = 'NOTUSDT'
+        bos = SunnyOrderSplitter(symbol, api_key, secret_key)
+        
+        # Call split_order method with test parameters
+        """
+        params = {
+            "side": "BUY",
+            "orderAmount": 20,
+            "totalAmount": 85,
+            "interval": 5
+        }
+        """
+        params = {
+            "side": "SELL",
+            "orderAmount": 1000,
+            "totalAmount": 4100,
+            "interval": 5
+        }
+        
+        bos.split_order(params)  # Use 1 second interval for testing
+
+if __name__ == "__main__":
+    unittest.main()
