047826478910515c0d41a939625e96eac9b1e124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 06:04:40 +0000

    Add package Cherv/0.3/Cherv-0.3-py3-none-any.whl

---
+++ packages/Cherv/Cherv-0.3-py3-none-any.whl/cherv/__main__.py
@@ -0,0 +1,19 @@
+import sys
+from cherv.cherv import cherv_say
+
+def main():
+    if (len(sys.argv) >= 2):
+        if (sys.argv[1] == "--help"):
+            print("Использование: python -m cherv [сообщение]")
+            return
+        if (sys.argv[1] == "--пасхалко"):
+            cherv_say("УХ ЧТО-ТО ЖАРКО СТАЛО ЗАПУСКАЕМ ВЕНТИЛЯТОРЫ 卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐")
+            return
+        
+        cherv_say(" ".join(sys.argv[1:]))
+
+    if len(sys.argv) < 2:
+        cherv_say()
+
+if __name__ == "__main__":
+    main()
