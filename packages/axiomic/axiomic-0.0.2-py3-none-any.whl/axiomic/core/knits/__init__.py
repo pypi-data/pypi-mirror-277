8ece50c3b22e797094d2ac7dc8eac58ef62621f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:04:38 +0000

    Add package axiomic/0.0.2/axiomic-0.0.2-py3-none-any.whl

---
+++ packages/axiomic/axiomic-0.0.2-py3-none-any.whl/axiomic/core/knits/__init__.py
@@ -0,0 +1,13 @@
+
+
+# import axiomic.builtins.knits.jsonschema_gate as jsonschema_gate
+import axiomic.core.knits.pattern as pattern
+import axiomic.core.knits.chat as chat
+import axiomic.core.knits.json_pattern as json_pattern
+import axiomic.core.knits.switch as switch
+
+# JsonSchemaGate = jsonschema_gate.JsonSchemaGate
+Pattern = pattern.Pattern
+Chat = chat.Chat
+JsonPattern = json_pattern.JsonPattern
+Switch = switch.Switch
