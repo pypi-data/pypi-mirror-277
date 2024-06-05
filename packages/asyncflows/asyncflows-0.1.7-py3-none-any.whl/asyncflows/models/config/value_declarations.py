679e984f9681b9b1ed13243caf6e4fe4a3d11e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:03:55 +0000

    Add package asyncflows/0.1.7/asyncflows-0.1.7-py3-none-any.whl

---
+++ packages/asyncflows/asyncflows-0.1.7-py3-none-any.whl/asyncflows/models/config/value_declarations.py
@@ -0,0 +1,188 @@
+import ast
+import os
+from typing import Any, Union
+
+import pydantic
+import simpleeval
+from pydantic import Field
+from typing_extensions import Self
+
+from asyncflows.models.config.common import (
+    StrictModel,
+    extract_root_var,
+    render_var,
+    extract_vars_from_template,
+    render_template,
+)
+from asyncflows.models.primitives import (
+    ContextVarPath,
+    LambdaString,
+    TemplateString,
+    ContextVarName,
+    HintLiteral,
+)
+from asyncflows.utils.config_utils import get_names_from_ast, verify_ast
+from asyncflows.utils.type_utils import get_var_string
+
+
+###
+### Variables
+###
+
+
+class Declaration(StrictModel):
+    stream: bool = False
+
+    def get_dependencies(self) -> set[ContextVarName]:
+        raise NotImplementedError
+
+    async def render(self, context: dict[str, Any]) -> Any:
+        raise NotImplementedError
+
+    @classmethod
+    def from_hint_literal(cls, hint_literal: HintLiteral, strict: bool) -> type[Self]:
+        return cls
+
+
+class TextDeclaration(Declaration):
+    """
+    A template declaration is a string that can be rendered within a context.
+    """
+
+    text: TemplateString
+
+    def get_dependencies(self) -> set[ContextVarName]:
+        return extract_vars_from_template(self.text)
+
+    async def render(self, context: dict[str, Any]) -> Any:
+        rendered = await render_template(self.text, context)
+        if not rendered:
+            return ""
+        return str(rendered)
+
+
+class VarDeclaration(Declaration):
+    """
+    A variable declaration is a string that references a variable (or path to nested variable) in the context.
+    """
+
+    var: ContextVarPath
+
+    def get_dependencies(self) -> set[ContextVarName]:
+        id_ = extract_root_var(self.var)
+        return {id_}
+
+    async def render(self, context: dict[str, Any]) -> Any:
+        return await render_var(self.var, context)
+
+    @classmethod
+    def from_hint_literal(cls, hint_literal: HintLiteral, strict: bool) -> type[Self]:
+        varstr = get_var_string(hint_literal, strict)
+        field_infos = cls.model_fields.copy()
+        fields: dict[str, tuple] = {
+            var: (info.annotation, info) for var, info in field_infos.items()
+        }
+        fields["var"] = (hint_literal, ...)
+        return pydantic.create_model(  # type: ignore
+            f"{cls.__name__}_{varstr}",
+            __base__=cls,
+            __module__=__name__,
+            **fields,  # type: ignore
+        )
+
+
+class LinkDeclaration(Declaration):
+    """
+    An link declaration is a string that references another action's output
+    """
+
+    link: ContextVarPath
+
+    def get_dependencies(self) -> set[ContextVarName]:
+        id_ = extract_root_var(self.link)
+        return {id_}
+
+    async def render(self, context: dict[str, Any]) -> Any:
+        return await render_var(self.link, context)
+
+    @classmethod
+    def from_hint_literal(cls, hint_literal: HintLiteral, strict: bool) -> type[Self]:
+        varstr = get_var_string(hint_literal, strict)
+        field_infos = cls.model_fields.copy()
+        fields: dict[str, tuple] = {
+            var: (info.annotation, info) for var, info in field_infos.items()
+        }
+        fields["link"] = (hint_literal, ...)
+        return pydantic.create_model(  # type: ignore
+            f"{cls.__name__}_{varstr}",
+            __base__=cls,
+            __module__=__name__,
+            **fields,  # type: ignore
+        )
+
+
+class EnvDeclaration(Declaration):
+    """
+    An env declaration is a string that references an environment variable.
+    """
+
+    env: str
+
+    def get_dependencies(self) -> set[ContextVarName]:
+        return set()
+
+    async def render(self, context: dict[str, Any]) -> Any:
+        if self.env not in os.environ:
+            raise ValueError(f"Environment variable not found: {self.env}")
+        return os.environ[self.env]
+
+
+# class ConstDeclaration(Variable):
+#     """
+#     A constant declaration is a string that is interpreted as a constant value.
+#     """
+#     const: Any
+#
+#     def render(self, context: ContextDict):
+#         return self.const
+
+
+class LambdaDeclaration(Declaration):
+    """
+    A lambda declaration is a python expression that can be evaluated within a context.
+    """
+
+    lambda_: LambdaString = Field(alias="lambda")
+
+    def get_dependencies(self) -> set[ContextVarName]:
+        parsed_code = ast.parse(self.lambda_, mode="eval")
+        return get_names_from_ast(parsed_code)
+
+    async def render(self, context: dict[str, Any]) -> Any:
+        verify_ast(ast.parse(self.lambda_))
+
+        simpleeval.MAX_COMPREHENSION_LENGTH = 9999999999999
+
+        evaluator = simpleeval.EvalWithCompoundTypes(
+            names=context,
+            functions={
+                "range": range,
+            },
+        )
+        return evaluator.eval(self.lambda_)
+
+
+ValueDeclaration = Union[
+    TextDeclaration,
+    VarDeclaration,
+    LinkDeclaration,
+    EnvDeclaration,
+    # ConstDeclaration,
+    LambdaDeclaration,
+    # ParamDeclaration,
+]
+
+EvaluableDeclaration = Union[
+    VarDeclaration,
+    LambdaDeclaration,
+]
---
+++ packages/asyncflows/asyncflows-0.1.7-py3-none-any.whl/asyncflows/scripts/generate_config_schema.py
@@ -0,0 +1,151 @@
+import argparse
+import json
+import os
+import traceback
+
+from pydantic import ValidationError, TypeAdapter
+
+from asyncflows import BaseModel
+from asyncflows.actions import get_actions_dict
+from asyncflows.models.config.flow import (
+    Loop,
+    build_hinted_action_config,
+)
+from asyncflows.utils.loader_utils import load_config_file
+from asyncflows.utils.type_utils import get_path_literal
+
+_cache = {}
+
+
+def _build_action_specs(
+    config_filename: str,
+) -> list[dict[str, BaseModel]] | None:
+    key = config_filename
+    if key in _cache:
+        return _cache[key]
+
+    try:
+        action_config = load_config_file(config_filename)
+    except ValidationError:
+        print("Failed to load action config")
+        traceback.print_exc()
+        return None
+
+    action_specs = []
+    actions = get_actions_dict()
+    for action_id, action_invocation in action_config.flow.items():
+        if isinstance(action_invocation, Loop):
+            # TODO build for-loop specs
+            continue
+        action_name = action_invocation.action
+        action_specs.append(
+            {
+                action_id: actions[action_name]._get_outputs_type(),
+            }
+        )
+
+    _cache[key] = action_specs
+    return action_specs
+
+
+def _build_links(
+    config_filename: str,
+):
+    action_specs = _build_action_specs(
+        config_filename=config_filename,
+    )
+    if action_specs is None:
+        action_specs = []
+
+    return action_specs
+
+
+def _build_asyncflows_schema(
+    action_names: list[str],
+    strict: bool,
+    config_filename: str | None = None,
+    link_hint_literal_name: str = "__LinkHintLiteral",
+):
+    if config_filename:
+        links = _build_links(
+            config_filename=config_filename,
+        )
+        link_hint_literal = get_path_literal(links, strict)
+    else:
+        links = None
+        link_hint_literal = None
+
+    HintedActionConfig = build_hinted_action_config(
+        action_names=action_names,
+        links=link_hint_literal,
+        vars_=None,
+        strict=strict,
+    )
+    workflow_schema = HintedActionConfig.model_json_schema()
+
+    if link_hint_literal is not None:
+        definitions = workflow_schema["$defs"]
+        if link_hint_literal_name in definitions:
+            raise ValueError(
+                f"Link hint literal name `{link_hint_literal_name}` already exists in definitions"
+            )
+        definitions[link_hint_literal_name] = TypeAdapter(
+            link_hint_literal
+        ).json_schema()
+
+    return workflow_schema
+
+
+def _build_and_save_asyncflows_schema(
+    action_names: list[str],
+    output_file: str,
+    strict: bool,
+    config_filename: str | None = None,
+):
+    workflow_schema = _build_asyncflows_schema(
+        action_names=action_names,
+        strict=strict,
+        config_filename=config_filename,
+    )
+    with open(os.path.join("schemas", output_file), "w") as f:
+        json.dump(workflow_schema, f, indent=2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--flow",
+        default="",
+        help="Path to flow for populating link fields with",
+    )
+
+    args = parser.parse_args()
+
+    action_names = list(get_actions_dict().keys())
+
+    if args.flow:
+        schema = _build_asyncflows_schema(
+            action_names=action_names,
+            config_filename=args.flow,
+            strict=True,
+        )
+        # straight to stdout we go
+        print(json.dumps(schema, indent=2))
+    else:
+        # TODO assert tests not imported before this line
+        import asyncflows.tests.resources.actions  # noqa
+
+        testing_action_names = list(get_actions_dict().keys())
+
+        # build default action and test action schemas
+        _build_and_save_asyncflows_schema(
+            action_names=action_names,
+            output_file="asyncflows_schema.json",
+            strict=False,
+        )
+
+        _build_and_save_asyncflows_schema(
+            action_names=testing_action_names,
+            output_file="testing_asyncflows_schema.json",
+            strict=False,
+        )
