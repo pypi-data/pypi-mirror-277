08aa43fb6025af6150d9418602a1d98c9f04721e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 14:04:27 +0000

    Add package QuackOSM/0.8.2/quackosm-0.8.2.tar.gz

---
+++ packages/QuackOSM/quackosm-0.8.2.tar.gz/quackosm-0.8.2/quackosm/__init__.py
@@ -0,0 +1,25 @@
+"""
+QuackOSM.
+
+QuackOSM is a Python library used for reading pbf (ProtoBuffer) files with OpenStreetMap data using
+DuckDB spatial extension without GDAL.
+"""
+
+from quackosm.functions import (
+    convert_geometry_to_geodataframe,
+    convert_geometry_to_parquet,
+    convert_pbf_to_geodataframe,
+    convert_pbf_to_parquet,
+)
+from quackosm.pbf_file_reader import PbfFileReader
+
+__app_name__ = "QuackOSM"
+__version__ = "0.8.2"
+
+__all__ = [
+    "PbfFileReader",
+    "convert_pbf_to_parquet",
+    "convert_geometry_to_parquet",
+    "convert_pbf_to_geodataframe",
+    "convert_geometry_to_geodataframe",
+]
