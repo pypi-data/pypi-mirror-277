a9417d34749f37308b2a31deec278f66d906fc93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:04:00 +0000

    Add package casablanca-Stock/0.2.9/casablanca_Stock-0.2.9.tar.gz

---
+++ packages/casablanca-Stock/casablanca_Stock-0.2.9.tar.gz/casablanca_Stock-0.2.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='casablanca_Stock',
+    version='0.2.9',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'pandas',
+        'selenium',
+        'streamlit',
+        'groq'
+    ],
+)
