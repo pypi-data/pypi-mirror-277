6c4d58d9f869c6315f2c7968b79eb4540b0d8b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:04:18 +0000

    Add package fnai/0.3.10/fnai-0.3.10.tar.gz

---
+++ packages/fnai/fnai-0.3.10.tar.gz/fnai-0.3.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fnai',
+    version='0.3.10',  # 更新版本号
+    description='A Python library for AI-related functions',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/fnai',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'mediapipe',
+        'deepface',
+        "tensorflow==2.13.1; platform_system!='Windows'",
+        "tensorflow-cpu==2.13.1; platform_system=='Windows'"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
