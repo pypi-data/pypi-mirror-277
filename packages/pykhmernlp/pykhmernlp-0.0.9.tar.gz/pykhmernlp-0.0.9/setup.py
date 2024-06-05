34ee5908ecced8941fe28cc4ba44fb2054b432e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 15:04:32 +0000

    Add package pykhmernlp/0.0.9/pykhmernlp-0.0.9.tar.gz

---
+++ packages/pykhmernlp/pykhmernlp-0.0.9.tar.gz/pykhmernlp-0.0.9/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(*paths):
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+    
+requirements = [
+    "tha",
+    "regex",
+    "khmercut",
+    "khmerpronounce",
+]
+
+setup(
+    name='pykhmernlp',
+    version='0.0.9',
+    python_requires=">=3.7",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={
+        'pykhmernlp': [
+            'corpus/icu_words.txt',
+            'corpus/english_words.txt',
+            'corpus/khmer_dictionary.tsv',
+            'corpus/english_dictionary.tsv',
+            'address/address_data/phum.tsv',
+            'address/address_data/khum.tsv',
+            'address/address_data/srok.tsv',
+            'address/address_data/province.txt',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/MetythornPenn/pykhmernlp.git',
+    license='Apache Software License 2.0',
+    author = 'Metythorn Penn',
+    author_email = 'metythorn@gmail.com',
+    keywords='pykhmernlp',
+    description='Collection of Khmer language toolkits',
+    install_requires=requirements,
+    long_description=(read('README.md')),
+    long_description_content_type='text/markdown',
+	classifiers= [
+		'Natural Language :: English',
+		'License :: OSI Approved :: Apache Software License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+	],
+)
