533065a8e0ac20464f4295ba36d805384bf8e200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:03:49 +0000

    Add package dataproc-jupyter-plugin/0.1.79/dataproc_jupyter_plugin-0.1.79-py3-none-any.whl

---
+++ packages/dataproc-jupyter-plugin/dataproc_jupyter_plugin-0.1.79-py3-none-any.whl/dataproc_jupyter_plugin/commons/constants.py
@@ -0,0 +1,31 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ENVIRONMENT_API = "https://composer.googleapis.com/v1"
+TAGS = "dataproc_jupyter_plugin"
+GCS = "gs://"
+CONTENT_TYPE = "application/json"
+PACKAGE_NAME = "dataproc_jupyter_plugin"
+COMPOSER_SERVICE_NAME = "composer"
+DATAPROC_SERVICE_NAME = "dataproc"
+BIGQUERY_SERVICE_NAME = "bigquery"
+DATACATALOG_SERVICE_NAME = "datacatalog"
+CLOUDRESOURCEMANAGER_SERVICE_NAME = "cloudresourcemanager"
+STORAGE_SERVICE_NAME = "storage"
+COMPUTE_SERVICE_NAME = "compute"
+METASTORE_SERVICE_NAME = "metastore"
+CLOUDKMS_SERVICE_NAME = "cloudkms"
+COMPUTE_SERVICE_DEFAULT_URL = "https://compute.googleapis.com/compute/v1"
+STORAGE_SERVICE_DEFAULT_URL = "https://storage.googleapis.com/storage/v1/"
+WRAPPER_PAPPERMILL_FILE = "wrapper_papermill.py"
