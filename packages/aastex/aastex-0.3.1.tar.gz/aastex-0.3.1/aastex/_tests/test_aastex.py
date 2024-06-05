e8bc2de492058516ab4286553eefb55f85f0fe7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:04:56 +0000

    Add package aastex/0.3.1/aastex-0.3.1.tar.gz

---
+++ packages/aastex/aastex-0.3.1.tar.gz/aastex-0.3.1/aastex/_tests/test_aastex.py
@@ -0,0 +1,325 @@
+import pathlib
+
+import pytest
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.units as u
+import aastex
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Title("my fancy paper"),
+    ],
+)
+class TestTitle:
+    def test_name(self, a: aastex.Title):
+        assert isinstance(a.name, str)
+
+    def test_dumps(self, a: aastex.Title):
+        assert isinstance(a.dumps(), str)
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Affiliation("Fancy University"),
+    ],
+)
+class TestAffiliation:
+    def test_name(self, a: aastex.Affiliation):
+        assert isinstance(a.name, str)
+
+    def test_dumps(self, a: aastex.Affiliation):
+        assert isinstance(a.dumps(), str)
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Author(
+            name="Jane Doe",
+            affiliation=aastex.Affiliation("Fancy University"),
+            orcid="0000-0000-0000-0000",
+            email="jane.doe@tmp.com",
+        ),
+    ],
+)
+class TestAuthor:
+    def test_name(self, a: aastex.Author):
+        assert isinstance(a.name, str)
+
+    def test_affiliation(self, a: aastex.Author):
+        assert isinstance(a.affiliation, aastex.Affiliation)
+
+    def test_orcid(self, a: aastex.Author):
+        result = a.orcid
+        if result is not None:
+            assert isinstance(result, str)
+        assert result in a.dumps()
+
+    def test_email(self, a: aastex.Author):
+        result = a.email
+        if result is not None:
+            assert isinstance(result, str)
+
+    def test_dumps(self, a: aastex.Author):
+        assert isinstance(a.dumps(), str)
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Acronym(
+            acronym="NASA",
+            name_full="National Aeronautical and Space Administration",
+            name_short=name_short,
+            plural=plural,
+            short=short,
+        )
+        for short in [False, True]
+        for plural in [False, True]
+        for name_short in [None, "Naysah"]
+    ],
+)
+class TestAcronym:
+    def test_acronym(self, a: aastex.Acronym):
+        assert isinstance(a.acronym, str)
+
+    def test_name_full(self, a: aastex.Acronym):
+        assert isinstance(a.name_full, str)
+
+    def test_name_short(self, a: aastex.Acronym):
+        result = a.name_short
+        if result is not None:
+            assert isinstance(result, str)
+
+    def test_plural(self, a: aastex.Acronym):
+        result = a.plural
+        assert isinstance(result, bool)
+
+    def test_short(self, a: aastex.Acronym):
+        result = a.short
+        assert isinstance(result, bool)
+
+    def test_dumps(self, a: aastex.Title):
+        assert isinstance(a.dumps(), str)
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Variable("foo", 2),
+        aastex.Variable("bar", 3 * u.AA),
+    ],
+)
+class TestVariable:
+
+    def test_name(self, a: aastex.Variable):
+        assert isinstance(a.name, str)
+
+    def test_value(self, a: aastex.Variable):
+        assert isinstance(a.value, (int, float, u.Quantity))
+
+    def test_dumps(self, a: aastex.Variable):
+        assert isinstance(a.dumps(), str)
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Abstract(),
+    ],
+)
+class TestAbstract:
+    pass
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Section("Introduction"),
+    ],
+)
+class TestSection:
+    def test__format__(self, a: aastex.Section):
+        result = f"{a}"
+        assert r"\ref" in result
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Subsection("Foo"),
+    ],
+)
+class TestSubsection:
+    pass
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Subsubsection("Foo"),
+    ],
+)
+class TestSubsubsection:
+    pass
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Figure(aastex.Label(aastex.Marker("fig", "data"))),
+        aastex.Figure("fig:data"),
+        aastex.Figure("data"),
+    ],
+)
+class TestFigure:
+    def test__format__(self, a: aastex.Section):
+        result = f"{a}"
+        assert r"\ref" in result
+
+    def test_add_fig(self, a: aastex.Figure):
+        fig, ax = plt.subplots()
+        ax.plot(np.random.normal(size=11))
+        a.add_fig(fig, width=None)
+
+        assert r"\includegraphics" in a.dumps()
+
+    def test_add_caption(self, a: aastex.Figure):
+        a.add_caption("foo")
+        assert r"\caption" in a.dumps()
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.FigureStar("fig:figurestar"),
+    ],
+)
+class TestFigureStar:
+    pass
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Fig(
+            file=pathlib.Path("foo.pdf"),
+            width=r"\textwidth",
+            caption="test caption",
+        ),
+    ],
+)
+class TestFig:
+    pass
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.LeftFig(
+            file=pathlib.Path("foo.pdf"),
+            width=r"\textwidth",
+            caption="test caption",
+        ),
+    ],
+)
+class TestLeftFig:
+    pass
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.RightFig(
+            file=pathlib.Path("foo.pdf"),
+            width=r"\textwidth",
+            caption="test caption",
+        ),
+    ],
+)
+class TestRightFig:
+    pass
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Gridline(
+            figures=[
+                aastex.LeftFig(
+                    file=pathlib.Path("foo.pdf"),
+                    width=r"0.5\textwidth",
+                    caption="test caption",
+                ),
+                aastex.LeftFig(
+                    file=pathlib.Path("bar.pdf"),
+                    width=r"0.5\textwidth",
+                    caption="test caption",
+                ),
+            ],
+        ),
+    ],
+)
+class TestGridline:
+    pass
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Document(),
+    ],
+)
+class TestDocument:
+    @pytest.mark.parametrize(
+        argnames="value",
+        argvalues=[
+            0 * u.K,
+            1e-5 * u.m,
+            [1, 2, 3] * u.s,
+        ],
+    )
+    @pytest.mark.parametrize(
+        argnames="scientific_notation",
+        argvalues=[
+            None,
+            False,
+            True,
+        ],
+    )
+    @pytest.mark.parametrize(
+        argnames="digits_after_decimal",
+        argvalues=[
+            4,
+        ],
+    )
+    def test_set_variable_quantity(
+        self,
+        a: aastex.Document,
+        value: u.Quantity,
+        scientific_notation: None | bool,
+        digits_after_decimal: int,
+    ):
+        name = "testVariable"
+        a.set_variable_quantity(
+            name=name,
+            value=value,
+            scientific_notation=scientific_notation,
+            digits_after_decimal=digits_after_decimal,
+        )
+
+        assert name in a.dumps()
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        aastex.Bibliography("sources"),
+    ],
+)
+class TestBibliography:
+    pass
---
+++ packages/aastex/aastex-0.3.1.tar.gz/aastex-0.3.1/docs/index.rst
@@ -0,0 +1,143 @@
+Introduction
+============
+
+This Python library extends `PyLaTeX <https://github.com/JelteF/PyLaTeX>`_
+to support the `AASTeX LaTeX package <https://journals.aas.org/aastex-package-for-manuscript-preparation/>`_.
+
+Please see the `PyLaTeX documentation <https://jeltef.github.io/PyLaTeX/current/>`_
+for more information on how to use this package.
+
+|
+
+API Reference
+=============
+
+.. autosummary::
+    :toctree: _autosummary
+    :template: module_custom.rst
+    :recursive:
+
+    aastex
+
+|
+
+Example
+=======
+
+Here is a simple example showing some of the basic features of :mod:`aastex`.
+
+.. jupyter-execute::
+
+    import pathlib
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import astropy.units as u
+    import astropy.constants
+    import aastex
+
+    # Modify matplotlib defaults to use Latex backend
+    # with correct font family and size
+    plt.rcParams['text.usetex'] = True
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.size'] = 9
+    plt.rcParams['lines.linewidth'] = 1
+
+    # Define an object representing the Latex document
+    doc = aastex.Document()
+
+    # Define the title of the article
+    title = aastex.Title("An Interesting Article")
+    doc.append(title)
+
+    # Define the author of the paper and their affiliated organization
+    msu = aastex.Affiliation(
+        'Montana State University, Department of Physics, '
+        'P.O. Box 173840, Bozeman, MT 59717, USA'
+    )
+    author = aastex.Author('Roy T. Smart', msu)
+    doc.append(author)
+
+    # Define an acronym to be used in the body of the document
+    nasa = aastex.Acronym("NASA", "National Aeronautics and Space Administration")
+    doc.preamble.append(nasa)
+
+    # Define the abstract of the article
+    abstract = aastex.Abstract()
+    abstract.packages.append(aastex.Package("lipsum"))
+    abstract.append("Some text summarizing the article. ")
+    abstract.append(r"\lipsum[1-1]")
+    doc.append(abstract)
+
+    # Define the speed of light as a variable that can be used in the document
+    doc.set_variable_quantity(
+        name="speedOfLight",
+        value=astropy.constants.c.to(u.km / u.s),
+        scientific_notation=True,
+        digits_after_decimal=4,
+    )
+
+    # Define a column-width figure with random data
+    fig, ax = plt.subplots(
+        figsize=(aastex.column_width_inches, 2),
+        constrained_layout=True,
+    )
+    x = np.linspace(-6, 6, num=101)[..., np.newaxis]
+    y = np.sinc(x) + np.random.normal(scale=0.1, size=(101, 11))
+    ax.plot(*np.broadcast_arrays(x, y))
+    figure = aastex.Figure("data")
+    figure.add_fig(fig, width=None)
+    plt.close(fig)
+    figure.add_caption(aastex.NoEscape(
+        r"Here is a figure caption. \lipsum[5-5]"
+    ))
+
+    # Define the introduction of the article
+    intro = aastex.Section("Introduction")
+    intro.packages.append(aastex.Package("lipsum"))
+    intro.append(
+        rf"Here is a citation \citep{{knuth:1984}}. "
+        rf"The speed of light is \speedOfLight. "
+        rf"Here is a reference to Section {intro}. "
+        rf"Here is a reference to Figure {figure}. "
+        rf"Here is an acronym: \NASA. "
+        rf"Here is the acronym again: \NASA. "
+        rf"\lipsum[2-2]"
+    )
+    intro.append(figure)
+    intro.append(r"\lipsum[3-5]")
+    doc.append(intro)
+
+    # Add the bibliography from sources.bib
+    doc.append(aastex.Bibliography("sources"))
+
+    # Compile the document into a PDF
+    path_pdf = pathlib.Path("an_interesting_article.pdf")
+    doc.generate_pdf(filepath=path_pdf.with_suffix(""))
+
+Which outputs the following PDF:
+
+.. jupyter-execute::
+    :hide-code:
+
+    import os
+    import IPython
+
+    try:
+        path_build = pathlib.Path(os.environ["READTHEDOCS_OUTPUT"]) / "html"
+        path_pdf_new = path_pdf.rename(path_build / path_pdf.name)
+
+        url = f"https://aastex.readthedocs.io/en/latest/{path_pdf.name}"
+
+    except KeyError:
+        url = path_pdf.resolve()
+
+    IPython.display.IFrame(url, width=900, height=400)
+
+|
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
