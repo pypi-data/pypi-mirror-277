ea045031998c0823840f8725473395e0a59cc4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:04:30 +0000

    Add package vinted-api-python/0.0.2/vinted_api_python-0.0.2.tar.gz

---
+++ packages/vinted-api-python/vinted_api_python-0.0.2.tar.gz/vinted_api_python-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A vinted api wrapper in python.'
+LONG_DESCRIPTION = 'A package that allows you to interact with the vinted api.'
+
+# Setting up
+setup(
+    name="vinted_api_python",
+    version=VERSION,
+    author="Archithebald",
+    author_email="<archithebald@proton.me>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests', 'selenium'],
+    keywords=['python', 'vinted', 'pypi', 'api', 'wrapper'],
+)
---
+++ packages/vinted-api-python/vinted_api_python-0.0.2.tar.gz/vinted_api_python-0.0.2/src/app/tests/brands/test_get_all_brands.py
@@ -0,0 +1,5 @@
+from src.app import Vinted
+
+app = Vinted.Vinted()
+
+print(app.get_all_brands())
