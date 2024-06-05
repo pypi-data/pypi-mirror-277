3a50327500ca94fe699f60898d473b5ae3f02c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:39 +0000

    Add package NsparkleLog/1.0.7/NsparkleLog-1.0.7.tar.gz

---
+++ packages/NsparkleLog/NsparkleLog-1.0.7.tar.gz/NsparkleLog-1.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from NsparkleLog import __version__ , __author__
+
+setup(
+    name='NsparkleLog',
+    version=__version__,
+    packages=find_packages(),
+    author=__author__,
+    author_email='3072252442@qq.com',
+    description='A useful logging library for Python',
+    long_description=open('Readme.md','r',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/KOKOMI12345/NewSparkleLogging',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
