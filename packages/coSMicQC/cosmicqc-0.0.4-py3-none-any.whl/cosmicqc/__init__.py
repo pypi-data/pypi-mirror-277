899bb828029af624539ee2f3dee343cbad0fbcc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:04:44 +0000

    Add package coSMicQC/0.0.4/cosmicqc-0.0.4-py3-none-any.whl

---
+++ packages/coSMicQC/cosmicqc-0.0.4-py3-none-any.whl/cosmicqc/__init__.py
@@ -0,0 +1,9 @@
+"""
+Initialization for cosmicqc package
+"""
+
+from .analyze import find_outliers
+
+# note: version placeholder is updated during build
+# by poetry-dynamic-versioning.
+__version__ = "0.0.4"
