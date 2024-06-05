f9a337c8f9f0f0b5d774972213ddbdac2bf8a8a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:36 +0000

    Add package fake-bpy-module-3.5/20240604/fake_bpy_module_3_5-20240604.tar.gz

---
+++ packages/fake-bpy-module-3.5/fake_bpy_module_3_5-20240604.tar.gz/fake_bpy_module_3_5-20240604/bpy_extras/node_utils/__init__.pyi
@@ -0,0 +1,8 @@
+import typing
+import collections.abc
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
+
+def find_node_input(node, name): ...
+def find_node_input(node, name): ...
