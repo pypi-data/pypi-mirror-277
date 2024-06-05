5d4695dd6f4cbbab7916cceeb22ef695faa699ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:04:39 +0000

    Add package sentry-kafka-schemas/0.1.89/sentry-kafka-schemas-0.1.89.tar.gz

---
+++ packages/sentry-kafka-schemas/sentry-kafka-schemas-0.1.89.tar.gz/sentry-kafka-schemas-0.1.89/python/sentry_kafka_schemas/schema_types/outcomes_billing_v1.py
@@ -0,0 +1,20 @@
+from typing import TypedDict, Union
+from typing_extensions import Required
+
+
+class Outcome(TypedDict, total=False):
+    """ outcome. """
+
+    timestamp: Required[str]
+    """ Required property """
+
+    org_id: Union[int, None]
+    project_id: Union[int, None]
+    key_id: Union[int, None]
+    outcome: Required[int]
+    """ Required property """
+
+    reason: Union[str, None]
+    event_id: Union[str, None]
+    category: Union[int, None]
+    quantity: Union[int, None]
