54b5ab79f62f193d0752630f4589a3d6b3934056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:04:23 +0000

    Add package openpecha/0.11.12/openpecha-0.11.12-py3-none-any.whl

---
+++ packages/openpecha/openpecha-0.11.12-py3-none-any.whl/openpecha/formatters/__init__.py
@@ -0,0 +1,7 @@
+# from .empty import EmptyEbook
+from .formatter import BaseFormatter
+from .hfml import HFMLFormatter
+from .ocr import GoogleVisionFormatter
+from .pedurma import PedurmaFormatter
+from .tsadra import TsadraFormatter
+from .otranscribe import OTranscribeFormatter
