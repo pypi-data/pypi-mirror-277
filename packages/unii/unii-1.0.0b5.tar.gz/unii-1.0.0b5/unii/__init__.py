b69952d0f51ad14f176c1e1fd67c8a54faa8730a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 17:03:56 +0000

    Add package unii/1.0.0b5/unii-1.0.0b5.tar.gz

---
+++ packages/unii/unii-1.0.0b5.tar.gz/unii-1.0.0b5/unii/__init__.py
@@ -0,0 +1,18 @@
+"""
+Implementation of the Alphatronics UNii library
+"""
+
+try:
+    from ._version import __version__
+except ModuleNotFoundError:
+    pass
+from .sia_code import SIACode
+from .unii import DEFAULT_PORT, UNii, UNiiCommand, UNiiData, UNiiFeature, UNiiLocal
+from .unii_command_data import (
+    UNiiInputState,
+    UNiiInputStatusRecord,
+    UNiiSection,
+    UNiiSectionArmedState,
+    UNiiSectionStatusRecord,
+    UNiiSensorType,
+)
