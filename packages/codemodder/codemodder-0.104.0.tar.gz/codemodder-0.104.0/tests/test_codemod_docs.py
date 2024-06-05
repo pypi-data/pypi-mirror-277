a65907e0f272dfb87c46f185ca6726dc94cf6779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:04:10 +0000

    Add package codemodder/0.104.0/codemodder-0.104.0.tar.gz

---
+++ packages/codemodder/codemodder-0.104.0.tar.gz/codemodder-0.104.0/tests/test_codemod_docs.py
@@ -0,0 +1,24 @@
+import pytest
+
+from codemodder.codemods.api import BaseCodemod
+from codemodder.registry import load_registered_codemods
+from codemodder.scripts.generate_docs import ALL_CODEMODS_METADATA
+
+
+def pytest_generate_tests(metafunc):
+    registry = load_registered_codemods()
+    if "codemod" in metafunc.fixturenames:
+        metafunc.parametrize("codemod", registry.codemods)
+
+
+def test_load_codemod_docs_info(codemod: BaseCodemod):
+    if codemod.name in ["order-imports"]:
+        pytest.xfail(reason=f"{codemod.name} has no description")
+
+    assert codemod.description
+    assert codemod.review_guidance in (
+        "Merge After Review",
+        "Merge After Cursory Review",
+        "Merge Without Review",
+    )
+    assert codemod.name in ALL_CODEMODS_METADATA
