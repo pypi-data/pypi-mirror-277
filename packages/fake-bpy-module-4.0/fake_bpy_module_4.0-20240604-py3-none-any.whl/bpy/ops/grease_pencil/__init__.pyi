ecca5116f52a7aca77d32a86152f1ca70192341b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:37 +0000

    Add package fake-bpy-module-4.0/20240604/fake_bpy_module_4.0-20240604-py3-none-any.whl

---
+++ packages/fake-bpy-module-4.0/fake_bpy_module_4.0-20240604-py3-none-any.whl/bpy/ops/grease_pencil/__init__.pyi
@@ -0,0 +1,407 @@
+import typing
+import collections.abc
+import bpy.types
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
+
+def brush_stroke(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    stroke: bpy.types.bpy_prop_collection[bpy.types.OperatorStrokeElement]
+    | None = None,
+    mode: str | None = "NORMAL",
+):
+    """Draw a new stroke in the active Grease Pencil object
+
+        :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+        :type execution_context: int | str | None
+        :type undo: bool | None
+        :param stroke: Stroke
+        :type stroke: bpy.types.bpy_prop_collection[bpy.types.OperatorStrokeElement] | None
+        :param mode: Stroke Mode, Action taken when a paint stroke is made
+
+    NORMAL
+    Regular -- Apply brush normally.
+
+    INVERT
+    Invert -- Invert action of brush for duration of stroke.
+
+    SMOOTH
+    Smooth -- Switch brush to smooth mode for duration of stroke.
+        :type mode: str | None
+    """
+
+    ...
+
+def delete_frame(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    type: str | None = "ACTIVE_FRAME",
+):
+    """Delete Grease Pencil Frame(s)
+
+        :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+        :type execution_context: int | str | None
+        :type undo: bool | None
+        :param type: Type, Method used for deleting Grease Pencil frames
+
+    ACTIVE_FRAME
+    Active Frame -- Deletes current frame in the active layer.
+
+    ALL_FRAMES
+    All Active Frames -- Delete active frames for all layers.
+        :type type: str | None
+    """
+
+    ...
+
+def dissolve(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    type: str | None = "POINTS",
+):
+    """Delete selected points without splitting strokes
+
+        :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+        :type execution_context: int | str | None
+        :type undo: bool | None
+        :param type: Type, Method used for dissolving stroke points
+
+    POINTS
+    Dissolve -- Dissolve selected points.
+
+    BETWEEN
+    Dissolve Between -- Dissolve points between selected points.
+
+    UNSELECT
+    Dissolve Unselect -- Dissolve all unselected points.
+        :type type: str | None
+    """
+
+    ...
+
+def draw_mode_toggle(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Enter/Exit draw mode for grease pencil
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def insert_blank_frame(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    all_layers: bool | typing.Any | None = False,
+    duration: typing.Any | None = 1,
+):
+    """Insert a blank frame on the current scene frame
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param all_layers: All Layers, Insert a blank frame in all editable layers
+    :type all_layers: bool | typing.Any | None
+    :param duration: Duration
+    :type duration: typing.Any | None
+    """
+
+    ...
+
+def layer_add(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    new_layer_name: str | typing.Any = "",
+):
+    """Add a new Grease Pencil layer in the active object
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param new_layer_name: Name, Name of the new layer
+    :type new_layer_name: str | typing.Any
+    """
+
+    ...
+
+def layer_group_add(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    new_layer_group_name: str | typing.Any = "",
+):
+    """Add a new Grease Pencil layer group in the active object
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param new_layer_group_name: Name, Name of the new layer group
+    :type new_layer_group_name: str | typing.Any
+    """
+
+    ...
+
+def layer_remove(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Remove the active Grease Pencil layer
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def layer_reorder(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    target_layer_name: str | typing.Any = "GP_Layer",
+    location: str | None = "ABOVE",
+):
+    """Reorder the active Grease Pencil layer
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param target_layer_name: Target Name, Name of the target layer
+    :type target_layer_name: str | typing.Any
+    :param location: Location
+    :type location: str | None
+    """
+
+    ...
+
+def select_all(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    action: str | None = "TOGGLE",
+):
+    """(De)select all visible strokes
+
+        :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+        :type execution_context: int | str | None
+        :type undo: bool | None
+        :param action: Action, Selection action to execute
+
+    TOGGLE
+    Toggle -- Toggle selection for all elements.
+
+    SELECT
+    Select -- Select all elements.
+
+    DESELECT
+    Deselect -- Deselect all elements.
+
+    INVERT
+    Invert -- Invert selection of all elements.
+        :type action: str | None
+    """
+
+    ...
+
+def select_alternate(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    deselect_ends: bool | typing.Any | None = False,
+):
+    """Select alternated points in strokes with already selected points
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param deselect_ends: Deselect Ends, (De)select the first and last point of each stroke
+    :type deselect_ends: bool | typing.Any | None
+    """
+
+    ...
+
+def select_ends(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    amount_start: typing.Any | None = 0,
+    amount_end: typing.Any | None = 1,
+):
+    """Select end points of strokes
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param amount_start: Amount Start, Number of points to select from the start
+    :type amount_start: typing.Any | None
+    :param amount_end: Amount End, Number of points to select from the end
+    :type amount_end: typing.Any | None
+    """
+
+    ...
+
+def select_less(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Shrink the selection by one point
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def select_linked(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Select all points in curves with any point selection
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def select_more(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Grow the selection by one point
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def select_random(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    ratio: typing.Any | None = 0.5,
+    seed: typing.Any | None = 0,
+    action: str | None = "SELECT",
+):
+    """Selects random points from the current strokes selection
+
+        :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+        :type execution_context: int | str | None
+        :type undo: bool | None
+        :param ratio: Ratio, Portion of items to select randomly
+        :type ratio: typing.Any | None
+        :param seed: Random Seed, Seed for the random number generator
+        :type seed: typing.Any | None
+        :param action: Action, Selection action to execute
+
+    SELECT
+    Select -- Select all elements.
+
+    DESELECT
+    Deselect -- Deselect all elements.
+        :type action: str | None
+    """
+
+    ...
+
+def set_selection_mode(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    mode: str | None = "POINT",
+):
+    """Change the selection mode for Grease Pencil strokes
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param mode: Mode
+    :type mode: str | None
+    """
+
+    ...
+
+def stroke_simplify(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    factor: typing.Any | None = 0.001,
+):
+    """Simplify selected strokes
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param factor: Factor
+    :type factor: typing.Any | None
+    """
+
+    ...
+
+def stroke_smooth(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    iterations: typing.Any | None = 10,
+    factor: typing.Any | None = 1.0,
+    smooth_ends: bool | typing.Any | None = False,
+    keep_shape: bool | typing.Any | None = False,
+    smooth_position: bool | typing.Any | None = True,
+    smooth_radius: bool | typing.Any | None = True,
+    smooth_opacity: bool | typing.Any | None = False,
+):
+    """Smooth selected strokes
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param iterations: Iterations
+    :type iterations: typing.Any | None
+    :param factor: Factor
+    :type factor: typing.Any | None
+    :param smooth_ends: Smooth Endpoints
+    :type smooth_ends: bool | typing.Any | None
+    :param keep_shape: Keep Shape
+    :type keep_shape: bool | typing.Any | None
+    :param smooth_position: Position
+    :type smooth_position: bool | typing.Any | None
+    :param smooth_radius: Radius
+    :type smooth_radius: bool | typing.Any | None
+    :param smooth_opacity: Opacity
+    :type smooth_opacity: bool | typing.Any | None
+    """
+
+    ...
