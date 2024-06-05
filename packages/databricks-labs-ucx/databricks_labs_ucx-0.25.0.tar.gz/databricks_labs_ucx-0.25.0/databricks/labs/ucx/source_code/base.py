218f727ec95ea3650eec1dac5332ec1a773053f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 05:04:36 +0000

    Add package databricks-labs-ucx/0.25.0/databricks_labs_ucx-0.25.0.tar.gz

---
+++ packages/databricks-labs-ucx/databricks_labs_ucx-0.25.0.tar.gz/databricks_labs_ucx-0.25.0/databricks/labs/ucx/source_code/base.py
@@ -0,0 +1,126 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from collections.abc import Iterable
+from dataclasses import dataclass
+from pathlib import Path
+
+
+# Code mapping between LSP, PyLint, and our own diagnostics:
+# | LSP                       | PyLint     | Our            |
+# |---------------------------|------------|----------------|
+# | Severity.ERROR            | Error      | Failure()      |
+# | Severity.WARN             | Warning    | Advisory()     |
+# | DiagnosticTag.DEPRECATED  | Warning    | Deprecation()  |
+# | Severity.INFO             | Info       | Advice()       |
+# | Severity.HINT             | Convention | Convention()   |
+# | DiagnosticTag.UNNECESSARY | Refactor   | Convention()   |
+
+
+@dataclass
+class Advice:
+    code: str
+    message: str
+    start_line: int
+    start_col: int
+    end_line: int
+    end_col: int
+
+    def replace(
+        self,
+        code: str | None = None,
+        message: str | None = None,
+        start_line: int | None = None,
+        start_col: int | None = None,
+        end_line: int | None = None,
+        end_col: int | None = None,
+    ) -> 'Advice':
+        return self.__class__(
+            code=code if code is not None else self.code,
+            message=message if message is not None else self.message,
+            start_line=start_line if start_line is not None else self.start_line,
+            start_col=start_col if start_col is not None else self.start_col,
+            end_line=end_line if end_line is not None else self.end_line,
+            end_col=end_col if end_col is not None else self.end_col,
+        )
+
+    def as_advisory(self) -> 'Advisory':
+        return Advisory(**self.__dict__)
+
+    def as_failure(self) -> 'Failure':
+        return Failure(**self.__dict__)
+
+    def as_deprecation(self) -> 'Deprecation':
+        return Deprecation(**self.__dict__)
+
+    def as_convention(self) -> 'Convention':
+        return Convention(**self.__dict__)
+
+    def for_path(self, path: Path) -> LocatedAdvice:
+        return LocatedAdvice(self, path)
+
+
+@dataclass
+class LocatedAdvice:
+    advice: Advice
+    path: Path
+
+
+class Advisory(Advice):
+    """A warning that does not prevent the code from running."""
+
+
+class Failure(Advisory):
+    """An error that prevents the code from running."""
+
+
+class Deprecation(Advisory):
+    """An advisory that suggests to replace the code with a newer version."""
+
+
+class Convention(Advice):
+    """A suggestion for a better way to write the code."""
+
+
+class Linter:
+    @abstractmethod
+    def lint(self, code: str) -> Iterable[Advice]: ...
+
+
+class Fixer:
+    @abstractmethod
+    def name(self) -> str: ...
+
+    @abstractmethod
+    def apply(self, code: str) -> str: ...
+
+
+# The default schema to use when the schema is not specified in a table reference
+# See: https://spark.apache.org/docs/3.0.0-preview/sql-ref-syntax-qry-select-usedb.html
+DEFAULT_CATALOG = 'hive_metastore'
+DEFAULT_SCHEMA = 'default'
+
+
+@dataclass
+class CurrentSessionState:
+    """
+    A data class that represents the current state of a session.
+
+    This class can be used to track various aspects of a session, such as the current schema.
+
+    Attributes:
+        catalog (str): The current schema of the session. If not provided, it defaults to 'DEFAULT_CATALOG'.
+        schema (str): The current schema of the session. If not provided, it defaults to 'DEFAULT_SCHEMA'.
+    """
+
+    schema: str = DEFAULT_SCHEMA
+    catalog: str = DEFAULT_CATALOG
+
+
+class SequentialLinter(Linter):
+    def __init__(self, linters: list[Linter]):
+        self._linters = linters
+
+    def lint(self, code: str) -> Iterable[Advice]:
+        for linter in self._linters:
+            yield from linter.lint(code)
---
+++ packages/databricks-labs-ucx/databricks_labs_ucx-0.25.0.tar.gz/databricks_labs_ucx-0.25.0/databricks/labs/ucx/source_code/python_libraries.py
@@ -0,0 +1,117 @@
+# pylint: disable=import-outside-toplevel
+from __future__ import annotations
+
+import logging
+import tempfile
+import zipfile
+from collections.abc import Callable
+from functools import cached_property
+from pathlib import Path
+
+from databricks.labs.blueprint.entrypoint import is_in_debug
+
+from databricks.labs.ucx.framework.utils import run_command
+from databricks.labs.ucx.source_code.graph import (
+    LibraryResolver,
+    DependencyProblem,
+)
+from databricks.labs.ucx.source_code.path_lookup import PathLookup
+from databricks.labs.ucx.source_code.known import Whitelist
+
+logger = logging.getLogger(__name__)
+
+
+class PythonLibraryResolver(LibraryResolver):
+    # TODO: https://github.com/databrickslabs/ucx/issues/1640
+
+    def __init__(self, whitelist: Whitelist, runner: Callable[[str], tuple[int, str, str]] = run_command) -> None:
+        self._whitelist = whitelist
+        self._runner = runner
+
+    def register_library(self, path_lookup: PathLookup, library: Path) -> list[DependencyProblem]:
+        """We delegate to pip to install the library and augment the path look-up to resolve the library at import.
+        This gives us the flexibility to install any library that is not in the whitelist, and we don't have to
+        bother about parsing cross-version dependencies in our code."""
+        compatibility = self._whitelist.distribution_compatibility(library.name)
+        if compatibility.known:
+            return compatibility.problems
+        return self._install_library(path_lookup, library)
+
+    @cached_property
+    def _temporary_virtual_environment(self):
+        # TODO: for `databricks labs ucx lint-local-code`, detect if we already have a virtual environment
+        # and use that one. See Databricks CLI code for the labs command to see how to detect the virtual
+        # environment. If we don't have a virtual environment, create a temporary one.
+        # simulate notebook-scoped virtual environment
+        lib_install_folder = tempfile.mkdtemp(prefix='ucx-')
+        return Path(lib_install_folder)
+
+    def _install_library(self, path_lookup: PathLookup, library: Path) -> list[DependencyProblem]:
+        """Pip install library and augment path look-up to resolve the library at import"""
+        path_lookup.append_path(self._temporary_virtual_environment)
+
+        # Resolve relative pip installs from notebooks: %pip install ../../foo.whl
+        maybe_library = path_lookup.resolve(library)
+        if maybe_library is not None:
+            library = maybe_library
+
+        if library.suffix == ".egg":
+            return self._install_egg(library)
+        return self._install_pip(library)
+
+    def _install_pip(self, library: Path) -> list[DependencyProblem]:
+        return_code, stdout, stderr = self._runner(f"pip install {library} -t {self._temporary_virtual_environment}")
+        logger.debug(f"pip output:\n{stdout}\n{stderr}")
+        if return_code != 0:
+            problem = DependencyProblem("library-install-failed", f"Failed to install {library}: {stderr}")
+            return [problem]
+        return []
+
+    def _install_egg(self, library: Path) -> list[DependencyProblem]:
+        """Install egss using easy_install.
+
+        Sources:
+            See easy_install in setuptools.command.easy_install
+        """
+        verbosity = "--verbose" if is_in_debug() else "--quiet"
+        easy_install_arguments = [
+            "easy_install",
+            verbosity,
+            "--always-unzip",
+            "--install-dir",
+            self._temporary_virtual_environment.as_posix(),
+            library.as_posix(),
+        ]
+        setup = self._get_setup()
+        if callable(setup):
+            try:
+                setup(script_args=easy_install_arguments)
+                return []
+            except (SystemExit, ImportError, ValueError) as e:
+                logger.warning(f"Failed to install {library} with (setuptools|distutils).setup, unzipping instead: {e}")
+        library_folder = self._temporary_virtual_environment / library.name
+        library_folder.mkdir(parents=True, exist_ok=True)
+        try:
+            # Not a "real" install, though, the best effort to still lint eggs without dependencies
+            with zipfile.ZipFile(library, "r") as zip_ref:
+                zip_ref.extractall(library_folder)
+        except zipfile.BadZipfile as e:
+            problem = DependencyProblem("library-install-failed", f"Failed to install {library}: {e}")
+            return [problem]
+        return []
+
+    @staticmethod
+    def _get_setup() -> Callable | None:
+        try:
+            # Mypy can't analyze setuptools due to missing type hints
+            from setuptools import setup  # type: ignore
+        except ImportError:
+            try:
+                from distutils.core import setup  # pylint: disable=deprecated-module
+            except ImportError:
+                logger.warning("Could not import setup.")
+                setup = None
+        return setup
+
+    def __str__(self) -> str:
+        return f"PipResolver(whitelist={self._whitelist})"
