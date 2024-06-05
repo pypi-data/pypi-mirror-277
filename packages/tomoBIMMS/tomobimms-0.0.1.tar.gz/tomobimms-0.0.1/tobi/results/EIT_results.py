c6c4971377cc4edec2ec60bb2c6fcb963ecb786a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 04:04:58 +0000

    Add package tomoBIMMS/0.0.1/tomobimms-0.0.1.tar.gz

---
+++ packages/tomoBIMMS/tomobimms-0.0.1.tar.gz/tomobimms-0.0.1/tobi/results/EIT_results.py
@@ -0,0 +1,7 @@
+from bimms.results.Results import BIMMS_results
+
+
+class EIT_results(BIMMS_results):
+    def __init__(self, config=None, raw_data=None, ID=0):
+        super().__init__(config=config, raw_data=None, ID=0)
+
