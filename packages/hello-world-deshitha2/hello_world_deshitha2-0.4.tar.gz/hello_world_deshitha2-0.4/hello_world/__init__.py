8ad000afd38bf7744e1be261be9cffd874a94b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 14:04:28 +0000

    Add package hello-world-deshitha2/0.4/hello_world_deshitha2-0.4.tar.gz

---
+++ packages/hello-world-deshitha2/hello_world_deshitha2-0.4.tar.gz/hello_world_deshitha2-0.4/hello_world/__init__.py
@@ -0,0 +1,10 @@
+def hello():
+    return "Hello, World!"
+
+def greet_user(user_input):
+    if user_input.lower() == "hi":
+        return hello()
+    if user_input.lower() == "how are you?":
+        return "i am fine. how about you?"
+    return "Goodbye"
+
