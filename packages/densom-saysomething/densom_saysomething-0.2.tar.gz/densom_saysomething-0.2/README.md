b0f12ff97cb968d5fa6805148d275df05908adc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 15:03:59 +0000

    Add package densom-saysomething/0.2/densom_saysomething-0.2.tar.gz

---
+++ packages/densom-saysomething/densom_saysomething-0.2.tar.gz/densom_saysomething-0.2/README.md
@@ -0,0 +1,25 @@
+# Say Something
+
+## Table of Contents
+
+- [About](#about)
+
+- [Usage](#usage)
+
+## About <a name = "about"></a>
+
+A simple text to speech impelementation.
+
+### Installing
+
+```
+pip install densom-saysomething
+```
+
+## Usage <a name = "usage"></a>
+
+```python
+from saysomething import say
+
+say("Hello world")
+```
---
+++ packages/densom-saysomething/densom_saysomething-0.2.tar.gz/densom_saysomething-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="densom-saysomething",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+    author="Dennis Somerville",
+    # author_email="your.email@example.com",
+    description="A simple text to speech implementation.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/densom/saysomething",
+)
