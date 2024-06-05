99500f7ed94ff09f4484872b12daaa1c0c6e6e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 15:04:21 +0000

    Add package moeralib/0.15.3/moeralib-0.15.3.tar.gz

---
+++ packages/moeralib/moeralib-0.15.3.tar.gz/moeralib-0.15.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='moeralib',
+    version='0.15.3',
+    install_requires=[
+        'requests>=2.31.0',
+        'camel-converter',
+        'jsonschema>=4.19.0',
+    ],
+)
