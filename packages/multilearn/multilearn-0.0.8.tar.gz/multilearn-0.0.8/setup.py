d44c82253b131a84112d840f0597e6c518917f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 07:04:45 +0000

    Add package multilearn/0.0.8/multilearn-0.0.8.tar.gz

---
+++ packages/multilearn/multilearn-0.0.8.tar.gz/multilearn-0.0.8/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+# Package information
+name = 'multilearn'
+version = '0.0.8'  # Need to increment every time to push to PyPI
+description = 'Multi-task learning with Pytorch.'
+url = 'https://github.com/leschultz/multilearn'
+author = 'Lane E. Schultz'
+author_email = 'laneenriqueschultz@gmail.com'
+python_requires = '>=3.10'
+classifiers = ['Programming Language :: Python :: 3',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent',
+               ]
+packages = setuptools.find_packages(where='src')
+install_requires = [
+                    'torch',
+                    'scikit-learn',
+                    'lightning',
+                    'pandas',
+                    'matplotlib',
+                    'dill',
+                    'pytest',
+                    ]
+
+long_description = open('README.md').read()
+
+# Passing variables to setup
+setuptools.setup(
+                 name=name,
+                 version=version,
+                 description=description,
+                 url=url,
+                 author=author,
+                 author_email=author_email,
+                 packages=packages,
+                 package_dir={'': 'src'},
+                 package_data={'multilearn': ['data/*']},
+                 python_requires=python_requires,
+                 classifiers=classifiers,
+                 install_requires=install_requires,
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 )
