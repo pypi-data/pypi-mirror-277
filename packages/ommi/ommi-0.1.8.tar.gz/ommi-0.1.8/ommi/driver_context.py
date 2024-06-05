6e2aad25f47de5fa0803656e1efbd86b22a6a335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 14:04:11 +0000

    Add package ommi/0.1.8/ommi-0.1.8.tar.gz

---
+++ packages/ommi/ommi-0.1.8.tar.gz/ommi-0.1.8/ommi/driver_context.py
@@ -0,0 +1,25 @@
+from contextvars import ContextVar
+from typing import TypeVar, Generic
+
+
+T = TypeVar("T", bound="drivers.DatabaseDriver")
+
+
+active_driver = ContextVar("active_driver")
+
+
+class UseDriver(Generic[T]):
+    def __init__(self, driver: T):
+        self.driver = driver
+        self._previous_context_token = None
+
+    def __enter__(self) -> T:
+        self._previous_context_token = active_driver.set(self.driver)
+        return self.driver
+
+    def __exit__(self, *_):
+        active_driver.reset(self._previous_context_token)
+
+
+def use_driver(driver: T) -> UseDriver[T]:
+    return UseDriver(driver)
