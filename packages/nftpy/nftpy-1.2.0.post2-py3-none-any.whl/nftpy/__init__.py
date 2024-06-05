4a2c8cc880471f260f51a5f09941ceaab1dafdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 07:04:41 +0000

    Add package nftpy/1.2.0.post2/nftpy-1.2.0.post2-py3-none-any.whl

---
+++ packages/nftpy/nftpy-1.2.0.post2-py3-none-any.whl/nftpy/__init__.py
@@ -0,0 +1,8 @@
+from .EVM import NFT, NFTWallet, ABI, Chains, Chain
+from .OpenSea import OpenSea, OpenSeaChain, OpenSeaWallet, OpenSeaCollection
+from .Rarible import Rarible, RaribleChain, RaribleCollection, RaribleWallet
+__all__ = ["EVM", "OpenSea"]
+
+__version__ = '1.2.0.post2'
+__author__ = 'Coulter C. Stutz'
+__email__ = 'coulterstutz@gmail.com'
