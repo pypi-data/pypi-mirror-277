a4489bd81fd604a2422bbe27c52e5560e9f0b2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 16:04:36 +0000

    Add package relari/0.2.0a2/relari-0.2.0a2.tar.gz

---
+++ packages/relari/relari-0.2.0a2.tar.gz/relari-0.2.0a2/relari/clients/__init__.py
@@ -0,0 +1,4 @@
+from relari.clients.datasets import DatasetClient
+from relari.clients.evaluations import EvaluationsClient
+from relari.clients.projects import ProjectsClient
+from relari.clients.metrics import MetricsClient
