c365215fc546632956ad724179e5f7d7e7207921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:38 +0000

    Add package securesystemslib/1.1.0/securesystemslib-1.1.0.tar.gz

---
+++ packages/securesystemslib/securesystemslib-1.1.0.tar.gz/securesystemslib-1.1.0/tests/__init__.py
@@ -0,0 +1,12 @@
+"""Securesystemslib tests.
+
+NOTE: This file is only considered when running tests via aggregate_tests.py, or
+with the '-m' flag, when invoked individually.
+
+"""
+
+# Increase gpg subprocess timeout -- Windows CI fails frequently with default 10s.
+# pylint: disable=protected-access
+import securesystemslib._gpg.constants
+
+securesystemslib._gpg.constants.GPG_TIMEOUT = 120
