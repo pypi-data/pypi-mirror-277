021be0d5e183971f57f9d301b2dd7a58e528a170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 17:04:00 +0000

    Add package addon-system/1.2.5/addon_system-1.2.5.tar.gz

---
+++ packages/addon-system/addon_system-1.2.5.tar.gz/addon_system-1.2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="addon-system",
+    version="1.2.5",
+    packages=[
+        "addon_system",
+        "addon_system.addon",
+        "addon_system.system",
+        "addon_system.libraries",
+    ],
+    scripts=["addon.py"],
+    url="https://github.com/kuyugama/addon-system",
+    license="GNU GPVv3",
+    author="KuyuGama",
+    author_email="mail.kuyugama@gmail.com",
+    description="The AddonSystem library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=["Programming Language :: Python :: 3"],
+    python_requires=">=3.9",
+)
