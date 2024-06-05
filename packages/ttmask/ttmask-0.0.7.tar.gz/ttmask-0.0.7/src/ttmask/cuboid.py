4c09c4a5b4e3514d39fe0b68b5c48e580412df3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 04:04:52 +0000

    Add package ttmask/0.0.7/ttmask-0.0.7.tar.gz

---
+++ packages/ttmask/ttmask-0.0.7.tar.gz/ttmask-0.0.7/src/ttmask/cuboid.py
@@ -0,0 +1,52 @@
+import numpy as np
+import einops
+import typer
+from typing import Tuple
+from ._cli import cli
+from typing_extensions import Annotated
+from scipy.ndimage import distance_transform_edt
+import mrcfile
+
+
+@cli.command(name='cuboid')
+def cuboid(
+    sidelength: int = typer.Option(...),
+    cuboid_sidelengths: Annotated[Tuple[float, float, float], typer.Option()] = (None, None, None),
+    soft_edge_width: float = typer.Option(0),
+    mrc_voxel_size: float = typer.Option(...),
+    output: str = typer.Option("cuboid.mrc"),
+):
+    c = sidelength // 2
+    center = np.array([c, c, c])
+    mask = np.zeros(shape=(sidelength, sidelength, sidelength), dtype=np.float32)
+
+    # 3d positions of all voxels
+    positions = np.indices([sidelength, sidelength, sidelength])
+    positions = einops.rearrange(positions, 'zyx d h w -> d h w zyx')
+
+    # calculate the distance between the center and every pixel position
+    print(center.shape)
+    print(positions.shape)
+
+    print('calculating distance')
+    difference = np.abs(positions - center)  # (100, 100, 100, 3)
+    # z = difference[:, :, :, 0]
+    # y = difference[:, :, :, 1]
+    # x = difference[:, :, :, 2]
+    # idx_z = z < cuboid_sidelengths[0] / 2
+    # idx_y = y < cuboid_sidelengths[1] / 2
+    # idx_x = x < cuboid_sidelengths[2] / 2
+
+    # mask[np.logical_not(idx)] = 1 #if you wanted to do opposite for whatever reason
+
+    idx = np.all(difference < (np.array(cuboid_sidelengths) / 2), axis=-1)
+
+    mask[idx] = 1
+
+    distance_from_edge = distance_transform_edt(mask == 0)
+    boundary_pixels = (distance_from_edge <= soft_edge_width) & (distance_from_edge != 0)
+    normalised_distance_from_edge = (distance_from_edge[boundary_pixels] / soft_edge_width) * np.pi
+
+    mask[boundary_pixels] = (0.5 * np.cos(normalised_distance_from_edge) + 0.5)
+
+    mrcfile.write(output, mask, voxel_size= mrc_voxel_size, overwrite=True)
