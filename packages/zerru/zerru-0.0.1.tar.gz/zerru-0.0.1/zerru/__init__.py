79c87035de39ae597f3108b90bb73d297166218e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 03:03:44 +0000

    Add package zerru/0.0.1/zerru-0.0.1.tar.gz

---
+++ packages/zerru/zerru-0.0.1.tar.gz/zerru-0.0.1/zerru/__init__.py
@@ -0,0 +1,13 @@
+"""
+Zerru — это модуль zer на русском! Для сокращения и автоматизации кода.
+
+Версия модуля: 0.0.1 alpha.
+Издатель: ZerProg studio.
+"""
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os as oss
+import datetime
+import sys
+from .zerru import *
