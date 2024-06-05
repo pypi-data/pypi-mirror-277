7cb204657ccfa6dae87c5592a26fb3e08952fdc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-05 04:04:03 +0000

    Add package pylogram/0.6.0/pylogram-0.6.0.tar.gz

---
+++ packages/pylogram/pylogram-0.6.0.tar.gz/pylogram-0.6.0/pylogram/methods/premium/__init__.py
@@ -0,0 +1,17 @@
+from .apply_boost import ApplyBoost
+from .get_boosts_list import GetBoostsList
+from .get_boosts_status import GetBoostsStatus
+from .get_my_boosts import GetMyBoosts
+from .get_premium_promo import GetPremiumPromo
+from .get_user_boosts import GetUserBoosts
+
+
+class Premium(
+    ApplyBoost,
+    GetBoostsList,
+    GetBoostsStatus,
+    GetMyBoosts,
+    GetPremiumPromo,
+    GetUserBoosts,
+):
+    pass
