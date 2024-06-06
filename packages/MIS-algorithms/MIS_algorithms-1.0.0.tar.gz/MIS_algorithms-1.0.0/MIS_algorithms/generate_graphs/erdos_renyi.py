82fab421b1c7590ec65a96263e963930e087382f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 21:04:12 +0000

    Add package MIS-algorithms/1.0.0/MIS_algorithms-1.0.0.tar.gz

---
+++ packages/MIS-algorithms/MIS_algorithms-1.0.0.tar.gz/MIS_algorithms-1.0.0/MIS_algorithms/generate_graphs/erdos_renyi.py
@@ -0,0 +1,35 @@
+import random
+import networkx as nx
+
+def erdos_renyi(size: int, p: float, connected: bool = False) -> nx.Graph:
+    """
+    Generate an Erdős-Rényi random graph.
+
+    Parameters:
+    - size (int): The number of nodes in the graph.
+    - p (float): The probability of an edge between any two nodes.
+    - connected (bool): Whether the graph should be connected.
+
+    Returns:
+    - nx.Graph: The generated Erdős-Rényi graph.
+    """
+    while True:
+        G = nx.Graph()
+
+        # Create a list of all possible edges
+        edge_arr = [(i, j) for i in G.nodes() for j in G.nodes() if i < j]
+
+        # Add edges with probability p
+        for edge in edge_arr:
+            if random.random() < p:
+                G.add_edge(*edge)
+
+        if not connected:
+            # Stop the process when the graph does not have to be connected
+            break
+
+        if nx.is_connected(G):
+            # Break whenever the graph is connected
+            break
+
+    return G
