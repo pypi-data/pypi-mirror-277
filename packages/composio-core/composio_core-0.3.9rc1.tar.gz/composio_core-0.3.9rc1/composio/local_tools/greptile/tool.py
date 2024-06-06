a6342673ac0b1aaca2935dab9712ef407d07e068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 20:05:54 +0000

    Add package composio-core/0.3.9rc1/composio_core-0.3.9rc1.tar.gz

---
+++ packages/composio-core/composio_core-0.3.9rc1.tar.gz/composio_core-0.3.9rc1/composio/local_tools/greptile/tool.py
@@ -0,0 +1,14 @@
+import typing as t
+from composio.core.local import Tool, Action
+from .actions import CodeQuery
+
+class Greptile(Tool):
+    """
+    Code understanding tool. Index Code and answer questions about it.
+    """
+
+    def actions(self) -> list[t.Type[Action]]:
+        return [CodeQuery]
+
+    def triggers(self) -> list:
+        return []
