d8ec952ed61baacebcf795a198f89c1689d2d506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 16:05:53 +0000

    Add package formant/1.171.102rc0/formant-1.171.102rc0-py2.py3-none-any.whl

---
+++ packages/formant/formant-1.171.102rc0-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/commands/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import CommandResponseStreamType, ScopeFilter, CommandRequestListResponse, ScopeFilterTypesItem, CommandProgress, CommandParameter, CommandTemplate, CommandStreamType, CommandResponse, CommandQuery, CommandListResponse, FileInfo, CommandTemplateTags, CommandDeliveryEventStreamType, Command, CommandTemplateParameterMeta, CommandDeliveryEventType, CommandParameterMeta, CommandRequest, CommandDeliveryEventMetadata, CommandTags, CommandResponseTags, CommandDeliveryEventTags, CommandTemplateListResponse, CommandDeliveryEvent
+from formant.sdk.cloud.v2.formant_admin_api_client.api.command import command_controller_post, command_controller_query
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
