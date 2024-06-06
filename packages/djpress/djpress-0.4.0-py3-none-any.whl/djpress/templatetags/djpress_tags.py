fba3f4d36f5d335c2711d6c837781bed8b1c9b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 14:05:20 +0000

    Add package djpress/0.4.0/djpress-0.4.0-py3-none-any.whl

---
+++ packages/djpress/djpress-0.4.0-py3-none-any.whl/djpress/templatetags/djpress_tags.py
@@ -0,0 +1,464 @@
+"""Template tags for djpress."""
+
+from django import template
+from django.contrib.auth.models import User
+from django.db import models
+from django.template import Context
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+from djpress.conf import settings
+from djpress.models import Category, Post
+from djpress.templatetags.helpers import (
+    categories_html,
+    category_link,
+    post_read_more_link,
+)
+from djpress.utils import get_author_display_name
+
+register = template.Library()
+
+
+@register.simple_tag
+def blog_title() -> str:
+    """Return the blog title.
+
+    Returns:
+        str: The blog title.
+    """
+    return settings.BLOG_TITLE
+
+
+@register.simple_tag
+def blog_title_link(link_class: str = "") -> str:
+    """Return the blog title.
+
+    Args:
+        link_class: The CSS class(es) for the link.
+
+    Returns:
+        str: The blog title.
+    """
+    link_class_html = f' class="{link_class}"' if link_class else ""
+
+    ouptut = (
+        f'<a href="{reverse("djpress:index")}"{link_class_html}>'
+        f'{settings.BLOG_TITLE}</a>'
+    )
+
+    return mark_safe(ouptut)
+
+
+@register.simple_tag
+def get_categories() -> models.QuerySet[Category] | None:
+    """Return all categories as a queryset.
+
+    Returns:
+        models.QuerySet[Category]: All categories.
+    """
+    return Category.objects.get_categories()
+
+
+@register.simple_tag
+def blog_categories(
+    outer: str = "ul",
+    outer_class: str = "",
+    link_class: str = "",
+) -> str:
+    """Return the categories of the blog.
+
+    Args:
+        outer: The outer HTML tag for the categories.
+        outer_class: The CSS class(es) for the outer tag.
+        link_class: The CSS class(es) for the link.
+
+    Returns:
+        str: The categories of the blog.
+    """
+    categories = Category.objects.all()
+    if not categories:
+        return ""
+
+    return mark_safe(categories_html(categories, outer, outer_class, link_class))
+
+
+@register.simple_tag(takes_context=True)
+def have_posts(context: Context) -> list[Post | None]:
+    """Return the posts in the context.
+
+    Args:
+        context: The context.
+
+    Returns:
+        list[Post]: The posts in the context.
+    """
+    post: Post | None = context.get("_post")
+    posts: models.QuerySet[Post] | None = context.get("_posts")
+
+    if post:
+        return [post]
+    if posts:
+        return list(posts)
+
+    return []
+
+
+@register.simple_tag(takes_context=True)
+def post_title(context: Context) -> str:
+    """Return the title of a post.
+
+    Args:
+        context: The context.
+
+    Returns:
+        str: The title of the post.
+    """
+    post: Post | None = context.get("post")
+    if not post:
+        return ""
+
+    return post.title
+
+
+@register.simple_tag(takes_context=True)
+def post_title_link(context: Context, link_class: str = "") -> str:
+    """Return the title link for a post.
+
+    Args:
+        context: The context.
+        link_class: The CSS class(es) for the link.
+
+    Returns:
+        str: The title link for the post.
+    """
+    post: Post | None = context.get("post")
+    if not post:
+        return ""
+
+    _post: Post | None = context.get("_post")
+    if _post:
+        return post_title(context)
+
+    post_url = reverse("djpress:post_detail", args=[post.permalink])
+
+    link_class_html = f' class="{link_class}"' if link_class else ""
+
+    output = (
+        f'<a href="{post_url}" title="{post.title}"{link_class_html}>'
+        f"{post.title}</a>"
+    )
+
+    return mark_safe(output)
+
+
+@register.simple_tag(takes_context=True)
+def post_author(context: Context) -> str:
+    """Return the author display name.
+
+    Tries to display the first name and last name if available, otherwise falls back to
+    the username.
+
+    Args:
+        context: The context.
+
+    Returns:
+        str: The author display name.
+    """
+    post: Post | None = context.get("post")
+    if not post:
+        return ""
+
+    author = post.author
+    author_display_name = get_author_display_name(author)
+
+    return mark_safe(f'<span rel="author">{author_display_name}</span>')
+
+
+@register.simple_tag(takes_context=True)
+def post_author_link(context: Context, link_class: str = "") -> str:
+    """Return the author link for a post.
+
+    Args:
+        context: The context.
+        link_class: The CSS class(es) for the link.
+
+    Returns:
+        str: The author link.
+    """
+    post: Post | None = context.get("post")
+    if not post:
+        return ""
+
+    author = post.author
+    author_display_name = get_author_display_name(author)
+
+    if not settings.AUTHOR_PATH_ENABLED:
+        return f'<span rel="author">{author_display_name}</span>'
+
+    author_url = reverse("djpress:author_posts", args=[author])
+
+    link_class_html = f' class="{link_class}"' if link_class else ""
+
+    output = (
+        f'<a href="{author_url}" title="View all posts by '
+        f'{ author_display_name }"{link_class_html}>'
+        f'<span rel="author">{ author_display_name }</span></a>'
+    )
+
+    return mark_safe(output)
+
+
+@register.simple_tag
+def post_category_link(category: Category, link_class: str = "") -> str:
+    """Return the category links for a post.
+
+    Args:
+        category: The category of the post.
+        link_class: The CSS class(es) for the link.
+    """
+    if not settings.CATEGORY_PATH_ENABLED:
+        return category.name
+
+    return mark_safe(category_link(category, link_class))
+
+
+@register.simple_tag(takes_context=True)
+def post_date(context: Context) -> str:
+    """Return the date of a post.
+
+    Args:
+        context: The context.
+
+    Returns:
+        str: The date of the post.
+    """
+    post: Post | None = context.get("post")
+    if not post:
+        return ""
+
+    post_date = post.date
+    return mark_safe(post_date.strftime("%b %-d, %Y"))
+
+
+@register.simple_tag(takes_context=True)
+def post_date_link(context: Context, link_class: str = "") -> str:
+    """Return the date link for a post.
+
+    Args:
+        context: The context.
+        link_class: The CSS class(es) for the link.
+
+    Returns:
+        str: The date link for the post.
+    """
+    post: Post | None = context.get("post")
+    if not post:
+        return ""
+    post_date = post.date
+
+    if not settings.DATE_ARCHIVES_ENABLED:
+        return mark_safe(post_date.strftime("%b %-d, %Y"))
+
+    post_year = post_date.strftime("%Y")
+    post_month = post_date.strftime("%m")
+    post_month_name = post_date.strftime("%b")
+    post_day = post_date.strftime("%d")
+    post_day_name = post_date.strftime("%-d")
+    post_time = post_date.strftime("%-I:%M %p")
+
+    year_url = reverse(
+        "djpress:archives_posts",
+        args=[post_year],
+    )
+    month_url = reverse(
+        "djpress:archives_posts",
+        args=[post_year, post_month],
+    )
+    day_url = reverse(
+        "djpress:archives_posts",
+        args=[
+            post_year,
+            post_month,
+            post_day,
+        ],
+    )
+
+    link_class_html = f' class="{link_class}"' if link_class else ""
+
+    output = (
+        f'<a href="{month_url}" title="View all posts in {post_month_name} {post_year}"'
+        f"{link_class_html}>{post_month_name}</a> "
+        f'<a href="{day_url}" title="View all posts on {post_day_name} '
+        f'{post_month_name} {post_year}"{link_class_html}>{post_day_name}</a>, '
+        f'<a href="{year_url}" title="View all posts in {post_year}"{link_class_html}>'
+        f"{post_year}</a>, "
+        f"{post_time}."
+    )
+
+    return mark_safe(output)
+
+
+@register.simple_tag(takes_context=True)
+def post_content(
+    context: Context,
+    read_more_link_class: str = "",
+    read_more_text: str = "",
+) -> str:
+    """Return the content of a post.
+
+    If there's no post in the context, then we return an empty string. If there are
+    multiple posts in the context, then we return the truncated content of the post with
+    the read more link. Otherwise, we return the full content of the post.
+
+    Args:
+        context: The context.
+        read_more_link_class: The CSS class(es) for the read more link.
+        read_more_text: The text for the read more link.
+
+    Returns:
+        str: The content of the post.
+    """
+    content: str = ""
+
+    # Check if there's a post or _posts in the context.
+    post: Post | None = context.get("post")
+    _posts: models.QuerySet[Post] | None = context.get("_posts")
+
+    # If there's no post, then we return an empty string.
+    if not post:
+        return content
+
+    # If there are _posts, then we return the truncated content of the post.
+    if _posts:
+        content = mark_safe(post.truncated_content_markdown)
+        if post.is_truncated:
+            content += post_read_more_link(post, read_more_link_class, read_more_text)
+        return mark_safe(content)
+
+    # If there
+    return mark_safe(post.content_markdown)
+
+
+@register.simple_tag(takes_context=True)
+def category_name(
+    context: Context,
+    outer: str = "",
+    outer_class: str = "",
+    pre_text: str = "",
+    post_text: str = "",
+) -> str:
+    """Return the name of a category.
+
+    Expects there to be an `category` in the context set to a Category object. If
+    there's no category in the context or category is not a Category object, then retun
+    an empty string.
+
+
+    Args:
+        context: The context.
+        outer: The outer HTML tag for the category.
+        outer_class: The CSS class(es) for the outer tag.
+        pre_text: The text to prepend to the category name.
+        post_text: The text to append to the category name.
+
+    Returns:
+        str: The name of the category formatted with the outer tag and class if
+        provided.
+    """
+    category: Category | None = context.get("category")
+
+    if not category or not isinstance(category, Category):
+        return ""
+
+    allowed_outer_tags = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "div", "span"]
+    outer_class = f' class="{outer_class}"' if outer_class else ""
+
+    output = category.name
+
+    if pre_text:
+        output = f"{pre_text}{output}"
+
+    if post_text:
+        output = f"{output}{post_text}"
+
+    if outer in allowed_outer_tags:
+        return mark_safe(f"<{outer}{outer_class}>{output}</{outer}>")
+
+    return output
+
+
+@register.simple_tag(takes_context=True)
+def author_name(
+    context: Context,
+    outer: str = "",
+    outer_class: str = "",
+    pre_text: str = "",
+    post_text: str = "",
+) -> str:
+    """Return the name of a author.
+
+    Expects there to be an `author` in the context set to a user object. If there's no
+    author in the context or author is not a User object, then retun an empty string.
+
+    Args:
+        context: The context.
+        outer: The outer HTML tag for the author.
+        outer_class: The CSS class(es) for the outer tag.
+        pre_text: The text to prepend to the author name.
+        post_text: The text to append to the author name.
+
+    Returns:
+        str: The name of the author formatted with the outer tag and class if
+        provided.
+    """
+    author: User | None = context.get("author")
+
+    if not author or not isinstance(author, User):
+        return ""
+
+    allowed_outer_tags = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "div", "span"]
+    outer_class = f' class="{outer_class}"' if outer_class else ""
+
+    output = get_author_display_name(author)
+
+    if pre_text:
+        output = f"{pre_text}{output}"
+
+    if post_text:
+        output = f"{output}{post_text}"
+
+    if outer in allowed_outer_tags:
+        return mark_safe(f"<{outer}{outer_class}>{output}</{outer}>")
+
+    return output
+
+
+@register.simple_tag(takes_context=True)
+def post_categories_link(
+    context: Context,
+    outer: str = "ul",
+    outer_class: str = "",
+    link_class: str = "",
+) -> str:
+    """Return the categories of a post.
+
+    Each category is a link to the category page.
+
+    Args:
+        context: The context.
+        outer: The outer HTML tag for the categories.
+        outer_class: The CSS class(es) for the outer tag.
+        link_class: The CSS class(es) for the link.
+
+    Returns:
+        str: The categories of the post.
+    """
+    post: Post | None = context.get("post")
+    if not post:
+        return ""
+
+    categories = post.categories.all()
+    if not categories:
+        return ""
+
+    return mark_safe(categories_html(categories, outer, outer_class, link_class))
---
+++ packages/djpress/djpress-0.4.0-py3-none-any.whl/djpress/views.py
@@ -0,0 +1,170 @@
+"""djpress views file."""
+
+import logging
+
+from django.contrib.auth.models import User
+from django.http import Http404, HttpRequest, HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render
+from django.utils.timezone import datetime
+
+from djpress.models import Category, Post
+
+logger = logging.getLogger(__name__)
+
+
+def index(
+    request: HttpRequest,
+) -> HttpResponse:
+    """View for the index page."""
+    posts = Post.post_objects.get_recent_published_posts()
+
+    return render(
+        request,
+        "djpress/index.html",
+        {"_posts": posts},
+    )
+
+
+def archives_posts(
+    request: HttpRequest,
+    year: str,
+    month: str = "",
+    day: str = "",
+) -> HttpResponse:
+    """View for the date-based archives pages.
+
+    Args:
+        request (HttpRequest): The request object.
+        year (str): The year.
+        month (str): The month.
+        day (str): The day.
+    """
+    try:
+        validate_date(year, month, day)
+
+    except ValueError:
+        msg = "Invalid date"
+        return HttpResponseBadRequest(msg)
+
+    posts = Post.post_objects._get_published_posts()  # noqa: SLF001
+
+    # Django converts strings to integers when they are passed to the filter
+    if day:
+        logger.debug(f"{year}/{month}/{day}")
+        posts = posts.filter(
+            date__year=year,
+            date__month=month,
+            date__day=day,
+        )
+
+    elif month:
+        logger.debug(f"{year}/{month}")
+        posts = posts.filter(date__year=year, date__month=month)
+
+    elif year:
+        logger.debug(f"{year}")
+        posts = posts.filter(date__year=year)
+
+    return render(
+        request,
+        "djpress/index.html",
+        {"_posts": posts},
+    )
+
+
+def validate_date(year: str, month: str, day: str) -> None:
+    """Test the date values.
+
+    Convert the date values to integers and test if they are valid dates.
+
+    The regex that gets the date values checks for the following:
+    - year: four digits
+    - month: two digits
+    - day: two digits
+
+    Args:
+        year (str): The year.
+        month (str | None): The month.
+        day (str | None): The day.
+
+    Raises:
+        ValueError: If the date is invalid.
+
+    Returns:
+        None
+    """
+    int_year: int = int(year)
+    int_month: int | None = int(month) if month else None
+    int_day: int | None = int(day) if day else None
+
+    if int_month == 0 or int_day == 0:
+        msg = "Invalid date"
+        raise ValueError(msg)
+
+    try:
+        if int_month and int_day:
+            datetime(int_year, int_month, int_day)
+
+        elif int_month:
+            datetime(int_year, int_month, 1)
+
+        else:
+            datetime(int_year, 1, 1)
+
+    except ValueError as exc:
+        msg = "Invalid date"
+        raise ValueError(msg) from exc
+
+
+def post_detail(request: HttpRequest, path: str) -> HttpResponse:
+    """View for a single post.
+
+    Args:
+        request (HttpRequest): The request object.
+        path (str): The path to the post.
+    """
+    try:
+        post = Post.post_objects.get_published_post_by_path(path)
+    except ValueError as exc:
+        msg = "Post not found"
+        raise Http404(msg) from exc
+
+    return render(
+        request,
+        "djpress/index.html",
+        {"_post": post},
+    )
+
+
+def category_posts(request: HttpRequest, slug: str) -> HttpResponse:
+    """View for posts by category."""
+    try:
+        category: Category = Category.objects.get_category_by_slug(slug=slug)
+    except ValueError as exc:
+        msg = "Category not found"
+        raise Http404(msg) from exc
+
+    posts = Post.post_objects.get_published_posts_by_category(category)
+
+    return render(
+        request,
+        "djpress/index.html",
+        {"_posts": posts, "category": category},
+    )
+
+
+def author_posts(request: HttpRequest, author: str) -> HttpResponse:
+    """View for posts by author."""
+    try:
+        user: User = User.objects.get(username=author)
+    except User.DoesNotExist as exc:
+        msg = "Author not found"
+        raise Http404(msg) from exc
+
+    posts = Post.post_objects.get_published_posts_by_author(user)
+
+    return render(
+        request,
+        "djpress/index.html",
+        {"_posts": posts, "author": user},
+    )
