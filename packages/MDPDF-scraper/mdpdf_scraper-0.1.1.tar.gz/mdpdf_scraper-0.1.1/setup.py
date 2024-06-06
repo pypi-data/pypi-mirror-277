fdee81d010af8f7829aa34aee9f5c134a067dd1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 16:05:00 +0000

    Add package MDPDF-scraper/0.1.1/mdpdf_scraper-0.1.1.tar.gz

---
+++ packages/MDPDF-scraper/mdpdf_scraper-0.1.1.tar.gz/mdpdf_scraper-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MDPDF-scraper",
+    version="0.1.1",
+    author="Hashan Wickramasinghe",
+    author_email="hashanwickramasinghe@gmail.com",
+    description="A tool to scrape websites and generate PDFs from sitemap URLs.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hashangit/MDPDF-scraper",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "fpdf2",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'mdpdf-scraper=mdpdfscraper:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['DejaVuSans.ttf'],
+    },
+)
