c74bbba3675f9039f9ee38037b0aaa0521816827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 14:04:39 +0000

    Add package nwbinspector/0.4.37/nwbinspector-0.4.37-py3-none-any.whl

---
+++ packages/nwbinspector/nwbinspector-0.4.37-py3-none-any.whl/nwbinspector/__init__.py
@@ -0,0 +1,24 @@
+from .version import __version__
+from .register_checks import available_checks, Importance
+from .nwbinspector import (
+    InspectorOutputJSONEncoder,
+    inspect_all,
+    inspect_nwbfile,
+    inspect_nwbfile_object,
+    run_checks,
+    load_config,
+)
+from .nwbinspector import inspect_nwb  # TODO: remove after 7/1/2023
+from .checks.ecephys import *
+from .checks.general import *
+from .checks.image_series import *
+from .checks.images import *
+from .checks.nwb_containers import *
+from .checks.nwbfile_metadata import *
+from .checks.ogen import *
+from .checks.ophys import *
+from .checks.tables import *
+from .checks.time_series import *
+from .checks.icephys import *
+
+default_check_registry = {check.__name__: check for check in available_checks}
