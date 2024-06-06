ef069a7ab505c6a218df80b1ce3802208d984a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 16:05:03 +0000

    Add package bergmann/1.0.0/bergmann-1.0.0.tar.gz

---
+++ packages/bergmann/bergmann-1.0.0.tar.gz/bergmann-1.0.0/bergmann/di.py
@@ -0,0 +1,35 @@
+from collections.abc import Callable
+from functools import cached_property
+
+from textual.validation import ValidationResult
+
+from bergmann import failures_presenter
+from bergmann.files_helper import FilesHelper
+from bergmann.interactor import Interactor
+from bergmann.passwords_model import PasswordsModel
+
+__all__ = ["di"]
+
+
+class DI:
+    @cached_property
+    def passwords_interactor(self) -> PasswordsModel:
+        return PasswordsModel()
+
+    @cached_property
+    def failures_presenter(self) -> Callable[[ValidationResult], str]:
+        return failures_presenter.present
+
+    @cached_property
+    def files_helper(self) -> FilesHelper:
+        return FilesHelper()
+
+    @cached_property
+    def interactor(self) -> Interactor:
+        return Interactor(
+            files_helper=self.files_helper,
+            passwords_model=self.passwords_interactor,
+        )
+
+
+di = DI()
