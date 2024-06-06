6aa39d8eedeeab4cdbf1dae428e314bf71b01603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 18:04:35 +0000

    Add package bioDAG/0.0.1/biodag-0.0.1-py3-none-any.whl

---
+++ packages/bioDAG/biodag-0.0.1-py3-none-any.whl/bioDAG/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: Shi4712
+@description: 
+@version: 1.0.0
+@file: __init__.py.py
+@time: 2024/6/5 16:04
+"""
+
+if __name__ == '__main__':
+    pass
