5fcbc321ff7a5f9a040d61b0fdbc168e38c7c326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 16:05:14 +0000

    Add package eQuimage/1.5.1/equimage-1.5.1.tar.gz

---
+++ packages/eQuimage/equimage-1.5.1.tar.gz/equimage-1.5.1/src/eQuimage/gui/tools/clahe.py
@@ -0,0 +1,69 @@
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+# Author: Yann-Michel Niquet (contact@ymniquet.fr).
+# Version: 1.5.1 / 2024.06.05
+# GUI updated.
+
+"""Contrast Limited Adaptive Histogram Equalization (CLAHE) tool."""
+
+from ..gtk.customwidgets import HBox, VBox, RadioButtons, SpinButton, HScale
+from ..toolmanager import BaseToolWindow
+from ...imageprocessing import imageprocessing
+from skimage.exposure import equalize_adapthist
+
+class CLAHETool(BaseToolWindow):
+  """Contrast Limited Adaptive Histogram Equalization (CLAHE) tool class."""
+
+  _action_ = "Running Contrast Limited Adaptive Histogram Equalization (CLAHE)..."
+
+  _onthefly_ = False # This transformation can not be applied on the fly.
+
+  def open(self, image):
+    """Open tool window for image 'image'."""
+    if not super().open(image, "Contrast Limited Adaptive Histogram Equalization (CLAHE)"): return False
+    wbox = VBox()
+    self.window.add(wbox)
+    self.widgets.channelbuttons = RadioButtons(("V", "HSV value"), ("L", "Luma"))
+    wbox.pack(self.widgets.channelbuttons.hbox(prepend = "Channel:"))
+    self.widgets.sizebutton = SpinButton(15., 1., 100., 1., digits = 0)
+    wbox.pack(self.widgets.sizebutton.hbox(prepend = "Kernel size:", append = "% image width and height"))
+    self.widgets.clipscale = HScale(.5, 0., 1., 0.01, digits = 2, marks = [0., 1.], length = 320, expand = False)
+    wbox.pack(self.widgets.clipscale.hbox(prepend = "Clip limit:"))
+    wbox.pack(self.tool_control_buttons())
+    self.start(identity = False)
+    return True
+
+  def get_params(self):
+    """Return tool parameters."""
+    return self.widgets.channelbuttons.get_selected(), self.widgets.sizebutton.get_value(), \
+           self.widgets.clipscale.get_value(), imageprocessing.get_rgb_luma()
+
+  def set_params(self, params):
+    """Set tool parameters 'params'."""
+    channel, size, clip, rgbluma = params
+    self.widgets.channelbuttons.set_selected(channel)
+    self.widgets.sizebutton.set_value(size)
+    self.widgets.clipscale.set_value(clip)
+
+  def run(self, params):
+    """Run tool for parameters 'params'."""
+    channel, size, clip, rgbluma = params
+    if size <= 0. or clip <= 0.: return params, False
+    width, height = self.reference.size()
+    kwidth = max(int(round(size*width/100.)), 3)
+    kheight = max(int(round(size*height/100.)), 3)
+    if channel == "V":
+      self.image.set_image(equalize_adapthist(self.reference.rgbf_view(), kernel_size = (kheight, kwidth), clip_limit = clip), channels = -1)
+    else:
+      ref = self.reference.luma()
+      img = equalize_adapthist(ref, kernel_size = (kheight, kwidth), clip_limit = clip)
+      self.image.copy_image_from(self.reference)
+      self.image.scale_pixels(ref, img)
+    return params, True
+
+  def operation(self, params):
+    """Return tool operation string for parameters 'params'."""
+    channel, size, clip, rgbluma = params
+    if channel == "L": channel = f"L({rgbluma[0]:.2f}, {rgbluma[1]:.2f}, {rgbluma[2]:.2f})"
+    return f"CLAHE(channel = {channel}, size = {size:.0f}%, clip = {clip:.2f})"
---
+++ packages/eQuimage/equimage-1.5.1.tar.gz/equimage-1.5.1/src/eQuimage/gui/tools/nlmeans.py
@@ -0,0 +1,57 @@
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+# Author: Yann-Michel Niquet (contact@ymniquet.fr).
+# Version: 1.5.1 / 2024.06.05
+# GUI updated.
+
+"""Non-local means filter tool."""
+
+from ..gtk.customwidgets import HBox, VBox, HScaleSpinButton
+from ..toolmanager import BaseToolWindow
+from skimage.restoration import denoise_nl_means
+
+class NonLocalMeansFilterTool(BaseToolWindow):
+  """Non-local means filter tool class."""
+
+  _action_ = "Applying non-local means filter..."
+
+  _onthefly_ = False # This transformation can not be applied on the fly.
+
+  def open(self, image):
+    """Open tool window for image 'image'."""
+    if not super().open(image, "Non-local means filter"): return False
+    wbox = VBox()
+    self.window.add(wbox)
+    self.widgets.psizescale = HScaleSpinButton(7., 1., 25., 1., digits = 0, length = 320, expand = False)
+    wbox.pack(self.widgets.psizescale.layout2("Patch size (pixels):"))
+    self.widgets.pdistscale = HScaleSpinButton(11., 1., 50., 1., digits = 0, length = 320, expand = False)
+    wbox.pack(self.widgets.pdistscale.layout2("Patch distance (pixels):"))
+    self.widgets.cutoffscale = HScaleSpinButton(.1, 0., .2, .001, digits = 3, length = 320, expand = False)
+    wbox.pack(self.widgets.cutoffscale.layout2("Cut-off (gray levels):"))
+    wbox.pack(self.tool_control_buttons())
+    self.start(identity = False)
+    return True
+
+  def get_params(self):
+    """Return tool parameters."""
+    return int(round(self.widgets.psizescale.get_value())), int(round(self.widgets.pdistscale.get_value())), self.widgets.cutoffscale.get_value()
+
+  def set_params(self, params):
+    """Set tool parameters 'params'."""
+    psize, pdist, cutoff = params
+    self.widgets.psizescale.set_value(psize)
+    self.widgets.pdistscale.set_value(pdist)
+    self.widgets.cutoffscale.set_value(cutoff)
+
+  def run(self, params):
+    """Run tool for parameters 'params'."""
+    psize, pdist, cutoff = params
+    if psize <= 0 or pdist <= 0 or cutoff <= 0.: return params, False
+    self.image.rgb = denoise_nl_means(self.reference.rgb, channel_axis = 0, patch_size = psize, patch_distance = pdist, h = cutoff)
+    return params, True
+
+  def operation(self, params):
+    """Return tool operation string for parameters 'params'."""
+    psize, pdist, cutoff = params
+    return f"NonLocalMeansFilter(patch size = {psize} pixels, patch distance = {pdist} pixels, cutoff = {cutoff:.3f})"
