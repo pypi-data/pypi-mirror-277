1f27f01a5886990c1bddf9c91e3f5407a8252350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:05:10 +0000

    Add package bthome-ble/3.9.1/bthome_ble-3.9.1.tar.gz

---
+++ packages/bthome-ble/bthome_ble-3.9.1.tar.gz/bthome_ble-3.9.1/src/bthome_ble/__init__.py
@@ -0,0 +1,31 @@
+"""Parser for BLE advertisements in BTHome format."""
+from __future__ import annotations
+
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import BTHomeBluetoothDeviceData
+
+__version__ = "3.9.1"
+
+__all__ = [
+    "BinarySensorDeviceClass",
+    "BTHomeBluetoothDeviceData",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorDescription",
+    "SensorDeviceClass",
+    "SensorDeviceInfo",
+    "SensorUpdate",
+    "SensorValue",
+    "Units",
+]
