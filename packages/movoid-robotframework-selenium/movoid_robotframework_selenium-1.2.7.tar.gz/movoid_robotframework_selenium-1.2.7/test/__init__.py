69825350c555dadb888af3aab7d32e62dc3a5435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 20:06:04 +0000

    Add package movoid-robotframework-selenium/1.2.7/movoid_robotframework_selenium-1.2.7.tar.gz

---
+++ packages/movoid-robotframework-selenium/movoid_robotframework_selenium-1.2.7.tar.gz/movoid_robotframework_selenium-1.2.7/test/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Python Version: 3.8.10
+Creator:        sunyifan0649
+Create Date:    2024/4/17
+Description:    
+"""
+from movoid_package import Stub
+
+Stub(r'E:\github\movoid_robotframework_selenium\RobotFrameworkSelenium\action\action.py')
