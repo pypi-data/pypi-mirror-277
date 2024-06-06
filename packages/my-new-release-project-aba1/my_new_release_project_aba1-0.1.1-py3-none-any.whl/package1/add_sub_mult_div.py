702776175b686c66847dd86d75619ac3cb9fbae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:45 +0000

    Add package my-new-release-project-aba1/0.1.1/my_new_release_project_aba1-0.1.1-py3-none-any.whl

---
+++ packages/my-new-release-project-aba1/my_new_release_project_aba1-0.1.1-py3-none-any.whl/package1/add_sub_mult_div.py
@@ -0,0 +1,31 @@
+import math
+
+def add(a, b):
+    return round(a + b, 2)
+
+def subtraction(a, b):
+    return round(a - b, 2)
+
+def multiple(a, b):
+    return round(a * b, 2)
+
+def div(a, b):
+    return round(a // b, 2)
+
+def logariphm_two(a):
+    return round(math.log(a, 2), 2)
+
+def logariphm_natural(a):
+    return round(math.log(a), 2)
+
+def sqrt(a):
+    return round(math.sqrt(a), 2)
+
+def sqrt3(a):
+    return round(a ** (1 / 3), 2)
+
+def sqrtN(a, n):
+    return round(a ** (1 / n), 2)
+
+
+
