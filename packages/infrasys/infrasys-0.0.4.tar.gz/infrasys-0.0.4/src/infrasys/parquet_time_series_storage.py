af2c1e50147219d9c1a0e813b6fe3649716d3d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 18:04:19 +0000

    Add package infrasys/0.0.4/infrasys-0.0.4.tar.gz

---
+++ packages/infrasys/infrasys-0.0.4.tar.gz/infrasys-0.0.4/src/infrasys/parquet_time_series_storage.py
@@ -0,0 +1,25 @@
+"""Parquet time series storage"""
+
+from datetime import datetime
+from uuid import UUID
+
+from infrasys.time_series_models import TimeSeriesData, TimeSeriesMetadata
+from infrasys.time_series_storage_base import TimeSeriesStorageBase
+
+
+class ParquetTimeSeriesStorage(TimeSeriesStorageBase):
+    """Stores time series in Parquet files."""
+
+    def add_time_series(self, metadata: TimeSeriesMetadata, time_series: TimeSeriesData) -> None:
+        ...
+
+    def get_time_series(
+        self,
+        metadata: TimeSeriesMetadata,
+        start_time: datetime | None = None,
+        length: int | None = None,
+    ) -> TimeSeriesData:
+        raise NotImplementedError("ParquetTimeSeriesStorage.get_time_series")
+
+    def remove_time_series(self, uuid: UUID) -> None:
+        ...
