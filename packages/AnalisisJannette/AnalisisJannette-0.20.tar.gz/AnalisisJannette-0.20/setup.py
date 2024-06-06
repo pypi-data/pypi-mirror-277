64c71dab504e78d8243b07c41337789e826ade4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 17:05:53 +0000

    Add package AnalisisJannette/0.20/AnalisisJannette-0.20.tar.gz

---
+++ packages/AnalisisJannette/AnalisisJannette-0.20.tar.gz/AnalisisJannette-0.20/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AnalisisJannette',
+    version='0.20',
+    packages=find_packages(include=['analisisJannette', 'analisisJannette.*']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.0',
+        'sweetify',
+        'django-bootstrap5',
+        'pandas',
+        'unicodedata2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'manage = analisisJannette.manage:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+    ],
+)
