3b07455cca90c8595ed2274c59d659f1cf0a8689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 17:04:45 +0000

    Add package scisuit/1.3.3/scisuit-1.3.3-cp310-cp310-win_amd64.whl

---
+++ packages/scisuit/scisuit-1.3.3-cp310-cp310-win_amd64.whl/scisuit/plot/__init__.py
@@ -0,0 +1,13 @@
+from ._chartfuncs import layout, subplot, figure, legend, show, title, \
+	 				xlabel, ylabel, xlim, ylim, set_xticks, set_yticks, set_xposition, set_yposition, \
+					xscale, yscale
+
+from ._charts import canvas, boxplot, hist, psychrometry, scatter 
+
+from ._quiverchart import quiver, dirfield
+from ._qqcharts import qqnorm, qqplot
+from ._bubblechart import bubble
+from ._barcharts import bar, barh
+from ._moody import moody
+
+from ._chartelems import Marker
