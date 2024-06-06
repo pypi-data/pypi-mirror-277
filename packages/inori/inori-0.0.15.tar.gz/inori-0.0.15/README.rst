cea9616171aa38a6a2db1416a641994879d9b154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 14:05:01 +0000

    Add package inori/0.0.15/inori-0.0.15.tar.gz

---
+++ packages/inori/inori-0.0.15.tar.gz/inori-0.0.15/README.rst
@@ -0,0 +1,32 @@
+Inori - The Universal API Client Constructor
+============================================
+
+.. |pypi| image:: https://img.shields.io/pypi/v/inori.svg
+    :target: https://pypi.org/project/inori
+    :alt: PyPI
+
+.. |pypi_version| image:: https://img.shields.io/pypi/pyversions/inori.svg
+    :alt: PyPI - Python Version
+    :target: https://github.com/jsfehler/inori
+
+.. |license| image:: https://img.shields.io/github/license/jsfehler/inori.svg
+    :alt: GitHub
+    :target: https://github.com/jsfehler/inori/blob/master/LICENSE
+
+.. |pyup| image:: https://pyup.io/repos/github/jsfehler/inori/shield.svg
+    :alt: Updates
+    :target: https://pyup.io/repos/github/jsfehler/inori
+
+.. |CI| image:: https://github.com/jsfehler/inori/workflows/CI/badge.svg
+    :target: https://github.com/jsfehler/inori/actions/workflows/run_tests.yml
+    :alt: Build status
+
+.. |coverage| image:: https://codecov.io/gh/jsfehler/inori/branch/master/graph/badge.svg?token=C1vfu8YgWn
+    :target: https://codecov.io/gh/jsfehler/inori
+
+|pypi| |pypi_version| |license| |pyup| |CI| |coverage|
+
+Documentation
+-------------
+
+https://inori.readthedocs.io/en/latest/
---
+++ packages/inori/inori-0.0.15.tar.gz/inori-0.0.15/setup.py
@@ -0,0 +1,50 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(filename: str):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    with open(path, 'r') as f:
+        return f.read()
+
+
+def get_version_data() -> dict:
+    """Read the project's version file as text."""
+    data = {}
+
+    path = os.path.join(os.path.dirname(__file__), 'inori', 'version.py')
+
+    with open(path) as fp:
+        exec(fp.read(), data)
+
+    return data
+
+
+version_data = get_version_data()
+
+
+setup(
+    name="inori",
+    version=version_data['__version__'],
+    description="The Universal API Client Constructor.",
+    long_description=read('README.rst'),
+    author="Joshua Fehler",
+    license="GPLv3",
+    url="https://github.com/jsfehler/inori",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.26.0',
+        'shibari>=0.0.2',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+)
