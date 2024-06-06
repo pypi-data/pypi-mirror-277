d343993bb240ea2ac27aa1ee86fe01efb895ebcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 17:05:29 +0000

    Add package AnalisisJannette/0.18/analisisJannette-0.18.tar.gz

---
+++ packages/AnalisisJannette/analisisJannette-0.18.tar.gz/analisisJannette-0.18/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='analisisJannette',
+    version='0.18',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.0',
+        'sweetify',
+        'django-bootstrap5',
+        'pandas',
+        'unicodedata2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'manage = analisisJannette.manage:main',  # Asegúrate de que el nombre del módulo sea correcto
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+    ],
+)
