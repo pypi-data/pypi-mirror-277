b7da3dfa15e11e033351fd51a558eff6d553c314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 19:04:19 +0000

    Add package whenfin/0.1.1/whenfin-0.1.1.tar.gz

---
+++ packages/whenfin/whenfin-0.1.1.tar.gz/whenfin-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+  name='whenfin',
+  version='0.1.1',
+  author='Luca Dovichi',
+  author_email='lucadovichi@uchicago.edu',
+  description='Connects a Python program to Whenfin services',
+  packages=find_packages(),
+  classifiers=[
+  'Programming Language :: Python :: 3',
+  'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.6',
+)
