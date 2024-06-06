aace500f6dd0c57ebb70d4494e0f1c7fe6bee8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 20:06:09 +0000

    Add package AmferModel/1.0.3/AmferModel-1.0.3.tar.gz

---
+++ packages/AmferModel/AmferModel-1.0.3.tar.gz/AmferModel-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='AmferModel',
+    version='1.0.3',
+    author='aertsimon90',
+    author_email='simon.scap090@gmail.com',
+    description='A Python module for artificial intelligence infrastructure, including chatbots, image-drawing bots, and text-to-speech conversion.',
+    py_modules=['AmferModel'],
+    install_requires=[
+        'Pillow'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
