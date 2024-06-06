b065660d9ab558f035dd09033055638900b3ad5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 14:05:47 +0000

    Add package sae-lens/3.2.3/sae_lens-3.2.3-py3-none-any.whl

---
+++ packages/sae-lens/sae_lens-3.2.3-py3-none-any.whl/sae_lens/__init__.py
@@ -0,0 +1,32 @@
+__version__ = "3.2.3"
+
+
+from .analysis.hooked_sae_transformer import HookedSAETransformer
+from .cache_activations_runner import CacheActivationsRunner
+from .config import (
+    CacheActivationsRunnerConfig,
+    LanguageModelSAERunnerConfig,
+    PretokenizeRunnerConfig,
+)
+from .evals import run_evals
+from .pretokenize_runner import pretokenize_runner
+from .sae import SAE, SAEConfig
+from .sae_training_runner import SAETrainingRunner
+from .training.activations_store import ActivationsStore
+from .training.training_sae import TrainingSAE, TrainingSAEConfig
+
+__all__ = [
+    "SAE",
+    "SAEConfig",
+    "TrainingSAE",
+    "TrainingSAEConfig",
+    "HookedSAETransformer",
+    "ActivationsStore",
+    "LanguageModelSAERunnerConfig",
+    "SAETrainingRunner",
+    "CacheActivationsRunnerConfig",
+    "CacheActivationsRunner",
+    "PretokenizeRunnerConfig",
+    "pretokenize_runner",
+    "run_evals",
+]
