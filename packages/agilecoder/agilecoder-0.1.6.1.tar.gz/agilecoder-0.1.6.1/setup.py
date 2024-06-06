4eff7564c5a13d1e7103b1c278503a23598892e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:26 +0000

    Add package agilecoder/0.1.6.1/agilecoder-0.1.6.1.tar.gz

---
+++ packages/agilecoder/agilecoder-0.1.6.1.tar.gz/agilecoder-0.1.6.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+setup(name='agilecoder',
+version='0.1.6.1',
+description='AgileCoder',
+url='https://github.com/FSoft-AI4Code/AgileCoder',
+author='FSoft-AI4Code',
+author_email='support.aic@fpt.com',
+license='Apache-2.0',
+python_requires=">=3.8",
+include_package_data=True,
+package_data={"agilecoder": ["CompanyConfig/*/*.json"]},
+entry_points={
+        'console_scripts': ['agilecoder=agilecoder:main'],
+},
+install_requires=[
+        "openai==0.28.1",
+        "tiktoken",
+        "markdown",
+        "colorama",
+        "nltk",
+        "flask",
+        "tenacity",
+        "python-dotenv",
+        "codebleu",
+        "google-auth",
+        "google-auth-oauthlib",
+        "google-auth-httplib2",
+        "anthropic"
+      ],
+packages=find_packages(),
+zip_safe=False)
