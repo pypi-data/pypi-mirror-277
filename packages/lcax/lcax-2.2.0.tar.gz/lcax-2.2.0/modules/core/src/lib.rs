4988e419f27eb2bc17ceb0505828c237ce1b6469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:44 +0000

    Add package lcax/2.2.0/lcax-2.2.0.tar.gz

---
+++ packages/lcax/lcax-2.2.0.tar.gz/lcax-2.2.0/modules/core/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod country;
+pub mod dates;
+pub mod utils;
---
+++ packages/lcax/lcax-2.2.0.tar.gz/lcax-2.2.0/modules/models/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod assembly;
+pub mod epd;
+pub mod life_cycle_base;
+pub mod product;
+pub mod project;
+pub mod shared;
+pub mod techflow;
