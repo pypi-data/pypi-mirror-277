bcf41ad829f82f1150487fc693cdde96934d344e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 16:05:13 +0000

    Add package newscatcherapi-python-sdk/6.0.7/newscatcherapi_python_sdk-6.0.7.tar.gz

---
+++ packages/newscatcherapi-python-sdk/newscatcherapi_python_sdk-6.0.7.tar.gz/newscatcherapi_python_sdk-6.0.7/newscatcherapi_client/apis/tags/latest_headlines_api.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from newscatcherapi_client.apis.tags.latest_headlines_api_generated import LatestHeadlinesApiGenerated
+
+class LatestHeadlinesApi(LatestHeadlinesApiGenerated):
+    pass
