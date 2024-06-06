505147024299b5e7c1eba854539fb1c6162fc2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:26 +0000

    Add package happyrobot/0.2.8/happyrobot-0.2.8-py3-none-any.whl

---
+++ packages/happyrobot/happyrobot-0.2.8-py3-none-any.whl/happyrobot/models/call.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class CallMetadata(BaseModel):
+    call_sid: Optional[str] = None
+    ffrom: str = Field(alias="from")
+    to: str
+
+
+class Call(BaseModel):
+    id: str
+    organization_id: str
+    metadata: CallMetadata
+    created: str
+    extraction: Optional[dict] = None
