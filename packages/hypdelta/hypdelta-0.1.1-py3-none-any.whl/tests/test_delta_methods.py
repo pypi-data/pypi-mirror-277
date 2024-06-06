170c28fc2dd5c81f378c0db9bf1eca6ef4e06f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:25 +0000

    Add package hypdelta/0.1.1/hypdelta-0.1.1-py3-none-any.whl

---
+++ packages/hypdelta/hypdelta-0.1.1-py3-none-any.whl/tests/test_delta_methods.py
@@ -0,0 +1,31 @@
+import pytest
+
+from tests.utils import *
+
+from src.hyppy import hypdelta
+from sklearn.metrics import pairwise_distances
+from scipy.spatial.distance import pdist
+
+
+@pytest.mark.parametrize(
+    "points",
+    [
+        generate_synthetic_points(100, 100),
+        generate_synthetic_points(200, 200),
+        generate_synthetic_points(500, 500),
+    ],
+)
+def test_CCL_true_delta(points):
+
+    dist_matrix = pairwise_distances(points)
+
+    delta_CCL = hypdelta(
+        dist_matrix,
+        device="cpu",
+        strategy="CCL",
+        l=0.2,
+    )
+
+    delta_naive = hypdelta(dist_matrix, device="cpu", strategy="naive")
+
+    assert delta_CCL == pytest.approx(delta_naive, 0.01)
