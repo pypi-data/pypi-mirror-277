4dc7d94873be8b20e6193fa4cc31617e48ac394a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 21:04:13 +0000

    Add package my_js_parser/0.1/my_js_parser-0.1.tar.gz

---
+++ packages/my_js_parser/my_js_parser-0.1.tar.gz/my_js_parser-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_js_parser',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 列出你的库的依赖项
+        "lxml"
+    ],
+    py_modules=['my_js_parser.parser'],
+    # 也可以包含其他的元数据，如作者、描述、许可证等
+)
