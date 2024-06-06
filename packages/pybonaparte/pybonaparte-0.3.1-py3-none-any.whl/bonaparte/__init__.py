24706a06c98e4d2a5f4d6d62187a02c1cc899527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 18:04:13 +0000

    Add package pybonaparte/0.3.1/pybonaparte-0.3.1-py3-none-any.whl

---
+++ packages/pybonaparte/pybonaparte-0.3.1-py3-none-any.whl/bonaparte/__init__.py
@@ -0,0 +1,9 @@
+"""The Bonaparte Library."""
+
+from __future__ import annotations
+
+__version__ = "0.3.1"
+
+from .fireplace import Fireplace, FireplaceFeatures, FireplaceState
+
+__all__ = ["Fireplace", "FireplaceFeatures", "FireplaceState"]
