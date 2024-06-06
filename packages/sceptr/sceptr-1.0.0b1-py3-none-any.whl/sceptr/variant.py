ad888518d3e4b1e9b3f7d9654860b5520f6c1d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 16:04:57 +0000

    Add package sceptr/1.0.0b1/sceptr-1.0.0b1-py3-none-any.whl

---
+++ packages/sceptr/sceptr-1.0.0b1-py3-none-any.whl/sceptr/variant.py
@@ -0,0 +1,69 @@
+from sceptr._model_saves import load_variant
+
+
+def default():
+    return load_variant("SCEPTR")
+
+
+def mlm_only():
+    return load_variant("SCEPTR_MLM_only")
+
+
+def left_aligned():
+    return load_variant("SCEPTR_left_aligned")
+
+
+def left_aligned_mlm_only():
+    return load_variant("SCEPTR_left_aligned_MLM_only")
+
+
+def cdr3_only():
+    return load_variant("SCEPTR_CDR3_only")
+
+
+def cdr3_only_mlm_only():
+    return load_variant("SCEPTR_CDR3_only_MLM_only")
+
+
+def large():
+    return load_variant("SCEPTR_large")
+
+
+def small():
+    return load_variant("SCEPTR_small")
+
+
+def tiny():
+    return load_variant("SCEPTR_tiny")
+
+
+def blosum():
+    return load_variant("SCEPTR_BLOSUM")
+
+
+def average_pooling():
+    return load_variant("SCEPTR_average_pooling")
+
+
+def shuffled_data():
+    return load_variant("SCEPTR_shuffled_data")
+
+
+def synthetic_data():
+    return load_variant("SCEPTR_synthetic_data")
+
+
+def dropout_noise_only():
+    return load_variant("SCEPTR_dropout_noise_only")
+
+
+def finetuned():
+    return load_variant("SCEPTR_finetuned")
+
+
+def a_sceptr():
+    return load_variant("A_SCEPTR")
+
+
+def b_sceptr():
+    return load_variant("B_SCEPTR")
