acfabf0a7874de9b5f9930610913dad47b091cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 14:05:17 +0000

    Add package pybritive/1.8.0rc1/pybritive-1.8.0rc1-py3-none-any.whl

---
+++ packages/pybritive/pybritive-1.8.0rc1-py3-none-any.whl/pybritive/commands/ls.py
@@ -0,0 +1,49 @@
+import click
+from ..helpers.build_britive import build_britive
+from ..options.britive_options import britive_options
+
+
+@click.group()
+def ls():
+    """List resources available for currently authenticated identity."""
+    pass
+
+
+@ls.command()
+@build_britive
+@britive_options(names='format,tenant,token,silent,passphrase,federation_provider')
+def applications(ctx, output_format, tenant, token, silent, passphrase, federation_provider):
+    """List applications for the currently authenticated identity."""
+    ctx.obj.britive.list_applications()
+
+
+@ls.command()
+@build_britive
+@britive_options(names='format,tenant,token,silent,passphrase,federation_provider')
+def environments(ctx, output_format, tenant, token, silent, passphrase, federation_provider):
+    """List environments for the currently authenticated identity."""
+    ctx.obj.britive.list_environments()
+
+
+@ls.command()
+@build_britive
+@britive_options(names='checked_out,output_format,tenant,token,silent,passphrase,federation_provider')
+def profiles(ctx, checked_out, output_format, tenant, token, silent, passphrase, federation_provider):
+    """List profiles for the currently authenticated identity."""
+    ctx.obj.britive.list_profiles(checked_out=checked_out)
+
+
+@ls.command()
+@build_britive
+@britive_options(names='format,tenant,token,silent,passphrase,federation_provider')
+def secrets(ctx, output_format, tenant, token, silent, passphrase, federation_provider):
+    """List secrets for the currently authenticated identity."""
+    ctx.obj.britive.list_secrets()
+
+
+@ls.command()
+@build_britive
+@britive_options(names='format,tenant,token,silent,passphrase,federation_provider')
+def approvals(ctx, output_format, tenant, token, silent, passphrase, federation_provider):
+    """List approvals for the currently authenticated identity."""
+    ctx.obj.britive.list_approvals()
