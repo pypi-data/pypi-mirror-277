c22c0a08a2cf6c926c051a67be22400b6fb25244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 19:04:32 +0000

    Add package nonebot-plugin-osubot/6.4.7/nonebot_plugin_osubot-6.4.7-py3-none-any.whl

---
+++ packages/nonebot-plugin-osubot/nonebot_plugin_osubot-6.4.7-py3-none-any.whl/nonebot_plugin_osubot/schema/__init__.py
@@ -0,0 +1,9 @@
+from .score import Score, BeatmapUserScore, NewScore
+from .user import User, Badge
+from .beatmap import (
+    Beatmap,
+    Beatmapset,
+    SeasonalBackgrounds,
+)
+from .sayo_beatmap import SayoBeatmap
+from .alphaosu import RecommendData
