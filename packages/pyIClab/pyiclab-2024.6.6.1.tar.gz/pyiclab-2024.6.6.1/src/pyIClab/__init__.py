62f6686b9886cdaafdd1e60a107ef5ab82bcf87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 19:04:33 +0000

    Add package pyIClab/2024.6.6.1/pyiclab-2024.6.6.1.tar.gz

---
+++ packages/pyIClab/pyiclab-2024.6.6.1.tar.gz/pyiclab-2024.6.6.1/src/pyIClab/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+##### Compatibility #####
+
+import pyIClab._compat
+
+##### Built-in import #####
+
+##### External import #####
+
+
+##### Local import #####
+
+from pyIClab.interface import *
+from pyIClab.ions import *
+from pyIClab.ionchromatograph import *
+from pyIClab.assemblies import *
+from pyIClab.engines import *
+
+# --------------------------------------------------------------------------------
+
+
+# In[2]:
+
+
+__author__ = 'Kenny Zhang'
+__version__ = '2024.6.6.1'
+__license__ = '''
+    GNU GENERAL PUBLIC LICENSE
+    Version 3, 29 June 2007
+    '''
+__repository__ = 'https://github.com/xiaoxiaozhu123/pyIClab'
+
