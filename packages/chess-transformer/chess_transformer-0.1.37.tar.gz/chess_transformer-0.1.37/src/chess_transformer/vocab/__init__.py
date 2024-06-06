05a65838c15de4edfa68f6baa8bc0cce9edfb03d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:47 +0000

    Add package chess-transformer/0.1.37/chess_transformer-0.1.37.tar.gz

---
+++ packages/chess-transformer/chess_transformer-0.1.37.tar.gz/chess_transformer-0.1.37/src/chess_transformer/vocab/__init__.py
@@ -0,0 +1,7 @@
+from .vocab import legal, legal_styled, remove_symbols, SpecialToken, MASK, PAD, \
+  Vocabulary, invert
+
+__all__ = [
+  'SpecialToken', 'MASK', 'PAD', 'Vocabulary',
+  'legal', 'remove_symbols', 'legal_styled', 'invert',
+]
