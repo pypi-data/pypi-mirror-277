6389566743d572b5f594cfdc28dcb5bad92d16d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 20:05:52 +0000

    Add package app-store-scraper-mod/0.3.9/app_store_scraper_mod-0.3.9-py3-none-any.whl

---
+++ packages/app-store-scraper-mod/app_store_scraper_mod-0.3.9-py3-none-any.whl/app_store_scraper/images_info.py
@@ -0,0 +1,28 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_app_store_details(app_id):
+
+    url = f"https://apps.apple.com/app/id{app_id}"
+    response = requests.get(url)
+    html_content = response.content
+
+    # Parse the HTML content
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    # TODO: if name changes, we'll have to handle the code
+    script_tag = soup.find(
+        "script",
+        attrs={"name": "schema:software-application", "type": "application/ld+json"},
+    )
+    app_details_string = script_tag.string
+    app_details = json.loads(app_details_string)
+
+    description = app_details["description"]
+    screenshots = app_details["screenshot"]
+
+    return {
+        "description": description,
+        "screenshots": screenshots,
+    }
