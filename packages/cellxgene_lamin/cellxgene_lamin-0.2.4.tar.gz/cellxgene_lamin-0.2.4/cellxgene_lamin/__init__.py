33af4b815e89ead826788d17d6a8d561ade31fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 16:04:48 +0000

    Add package cellxgene_lamin/0.2.4/cellxgene_lamin-0.2.4.tar.gz

---
+++ packages/cellxgene_lamin/cellxgene_lamin-0.2.4.tar.gz/cellxgene_lamin-0.2.4/cellxgene_lamin/__init__.py
@@ -0,0 +1,22 @@
+"""Manage cellxgene metadata using LaminDB.
+
+Import the package::
+
+   import cellxgene_lamin
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   Annotate
+   CellxGeneFields
+   datasets
+"""
+
+__version__ = "0.2.4"  # denote a pre-release for 0.1.0 with 0.1rc1
+
+from . import datasets
+from ._annotate import Annotate
+from ._cxg_rest import get_collections_from_cxg, get_datasets_from_cxg
+from ._fields import CellxGeneFields
