f79eadeb28363da33d827e3c15ddd5f2216e6b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:45 +0000

    Add package chess-utils/0.1.17/chess_utils-0.1.17-py3-none-any.whl

---
+++ packages/chess-utils/chess_utils-0.1.17-py3-none-any.whl/chess_utils/__init__.pyi
@@ -0,0 +1,15 @@
+from .boards import unchecked_san, fen_after, CapturablePiece, captured_piece
+from .fens import position_idx
+from .pgns import read_pgns, read_sans, read_game, follow_game, \
+PGNHeaders, STRHeaders, export_headers, export_moves, export_pgn
+from .notations import sans2ucis, ucis2sans, sans2fens, ucis2fens, moves2fens
+from . import random
+
+__all__ = [
+  'unchecked_san', 'fen_after', 'CapturablePiece', 'captured_piece',
+  'position_idx',
+  'read_pgns', 'sans2ucis', 'ucis2sans', 'read_game', 'follow_game',
+  'sans2fens', 'ucis2fens', 'moves2fens',
+  'random', 'read_sans',
+  'PGNHeaders', 'STRHeaders', 'export_headers', 'export_moves', 'export_pgn',
+]
