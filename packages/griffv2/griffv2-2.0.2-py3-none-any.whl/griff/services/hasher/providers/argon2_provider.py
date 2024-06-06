1cad54d372418904a87384fb28744f5b16be6e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 14:05:27 +0000

    Add package griffv2/2.0.2/griffv2-2.0.2-py3-none-any.whl

---
+++ packages/griffv2/griffv2-2.0.2-py3-none-any.whl/griff/services/hasher/providers/argon2_provider.py
@@ -0,0 +1,23 @@
+from argon2 import PasswordHasher
+from argon2.exceptions import VerificationError, InvalidHashError
+from griff.services.hasher.providers.hasher_provider import (
+    HasherProvider,
+    UnHashedStr,
+    HashedStr,
+)
+from injector import singleton
+
+
+@singleton
+class Argon2Provider(HasherProvider):
+    def __init__(self):
+        self.ph = PasswordHasher()
+
+    def hash(self, to_hash: UnHashedStr) -> HashedStr:
+        return self.ph.hash(to_hash)
+
+    def verify(self, candidate: UnHashedStr, reference: HashedStr) -> bool:
+        try:
+            return self.ph.verify(reference, candidate)
+        except (VerificationError, InvalidHashError):
+            return False
