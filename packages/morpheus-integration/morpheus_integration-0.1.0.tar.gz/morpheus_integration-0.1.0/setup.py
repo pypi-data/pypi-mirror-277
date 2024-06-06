4432c725a6d353f42d81815b5c2579d5ea1b4314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 17:04:35 +0000

    Add package morpheus-integration/0.1.0/morpheus_integration-0.1.0.tar.gz

---
+++ packages/morpheus-integration/morpheus_integration-0.1.0.tar.gz/morpheus_integration-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages # type: ignore
+
+setup(
+    name='morpheus-integration',
+    version='0.1.0',
+    author='Bruno Moretti',
+    url='https://github.com/StafSis/Morpheus-Python-Integration',
+    install_requires=[
+        'requests; python_version<"3.12"',
+    ],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'}
+)
