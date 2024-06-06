44a88c1606b3905a8adf9df89003da39e1ed2fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 19:04:31 +0000

    Add package py-watersmart/0.1.1/py_watersmart-0.1.1-py3-none-any.whl

---
+++ packages/py-watersmart/py_watersmart-0.1.1-py3-none-any.whl/client/client.py
@@ -0,0 +1,43 @@
+"""Download Watersmart meter data"""
+
+import asyncio
+import argparse
+import logging
+from watersmart import WatersmartClient
+
+
+PARSER = argparse.ArgumentParser(description=__doc__)
+
+PARSER.add_argument(
+    "--log-level",
+    default=logging.INFO,
+    type=lambda x: getattr(logging, x.upper()),
+    help="Configure the logging level.",
+)
+PARSER.add_argument("--url", required=True)
+PARSER.add_argument("--email", required=True)
+PARSER.add_argument("--password", required=True)
+
+
+async def main():
+    args = PARSER.parse_args()
+    log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=args.log_level, format=log_format)
+    wc = WatersmartClient(args.url, args.email, args.password)
+    data = await wc.usage()
+    for datapoint in sorted(data, key=lambda x: x["read_datetime"]):
+        parts = [
+            f"{datapoint['local_datetime']}",
+            f"usage: {datapoint['gallons']:8}gal",
+            f"leak: {datapoint['leak_gallons']:8}gal",
+            f"flags: {datapoint['flags']}",
+        ]
+        print(" | ".join(parts))
+
+
+def start():
+    asyncio.run(main())
+
+
+if __name__ == "__main__":
+    start()
