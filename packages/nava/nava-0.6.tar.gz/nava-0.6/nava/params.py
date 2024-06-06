46fa3e067b73e4a188b4dc5ae3644342999b65bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 18:04:28 +0000

    Add package nava/0.6/nava-0.6.tar.gz

---
+++ packages/nava/nava-0.6.tar.gz/nava-0.6/nava/params.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Nava parameters."""
+
+NAVA_VERSION = "0.6"
+
+OVERVIEW = """
+A Python library for playing sound everywhere natively and securely.
+
+"""
+
+SOUND_FILE_PLAY_ERROR = "Sound can not play due to some issues."
+SOUND_FILE_EXIST_ERROR = "Given sound file doesn't exist."
+SOUND_FILE_PATH_TYPE_ERROR = "Sound file's path should be a string."
+SOUND_ID_EXIST_ERROR = "Given sound id doesn't exist."
+LOOP_ASYNC_ERROR = "`loop` can not be set True when `async_mode` is False."
+
+_play_threads_map = dict()
+_play_threads_counter = 0
