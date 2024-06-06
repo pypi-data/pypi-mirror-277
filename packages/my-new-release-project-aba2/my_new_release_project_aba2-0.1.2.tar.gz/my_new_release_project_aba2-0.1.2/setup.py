a2ea62d46ce0ad06806faf6271629e08a504cbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:55 +0000

    Add package my-new-release-project-aba2/0.1.2/my_new_release_project_aba2-0.1.2.tar.gz

---
+++ packages/my-new-release-project-aba2/my_new_release_project_aba2-0.1.2.tar.gz/my_new_release_project_aba2-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+long_description = open('README.md').read()
+
+setup(
+   name='my-new-release-project-aba2',
+   description='good job',
+   long_description_content_type="text/markdown",
+   long_description=long_description,
+   author='kirill',
+   author_email='KARum2004@yandex.ru',
+   packages=['package1'],
+   install_requires=[]
+)
