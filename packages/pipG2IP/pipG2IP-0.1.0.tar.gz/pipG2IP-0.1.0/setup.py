5ebad9a129795aef6a0c86b32c9ed58b5db4d247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:25 +0000

    Add package pipG2IP/0.1.0/pipG2IP-0.1.0.tar.gz

---
+++ packages/pipG2IP/pipG2IP-0.1.0.tar.gz/pipG2IP-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='pipG2IP',
+version='0.1.0',
+author='Dhia',
+author_email='meddhiabesbes@gmail.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
