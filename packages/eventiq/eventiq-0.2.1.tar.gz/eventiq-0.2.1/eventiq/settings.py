2d06cc3a07783166e90b600542667071014c8674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 21:04:11 +0000

    Add package eventiq/0.2.1/eventiq-0.2.1.tar.gz

---
+++ packages/eventiq/eventiq-0.2.1.tar.gz/eventiq-0.2.1/eventiq/settings.py
@@ -0,0 +1,28 @@
+from typing import Any, Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from .encoder import Encoder
+from .imports import ImportedType
+from .middleware import Middleware
+
+
+class BrokerSettings(BaseSettings):
+    default_consumer_timeout: int = 300
+    description: Optional[str] = None
+    middlewares: Optional[list[Middleware]] = None
+    encoder: Optional[ImportedType[Encoder]] = None
+
+    model_config = SettingsConfigDict(env_prefix="BROKER_")
+
+
+class UrlBrokerSettings(BrokerSettings):
+    url: str
+    connection_options: dict[str, Any] = {}
+
+
+class ServiceSettings(BaseSettings):
+    name: str
+    title: Optional[str] = None
+    version: str = "0.1.0"
+    description: str = ""
