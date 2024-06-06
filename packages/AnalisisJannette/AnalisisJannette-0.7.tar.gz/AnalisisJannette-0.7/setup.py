0e10787a5e1138e7909b8d29bd2bdc3212f1903e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 16:06:07 +0000

    Add package AnalisisJannette/0.7/AnalisisJannette-0.7.tar.gz

---
+++ packages/AnalisisJannette/AnalisisJannette-0.7.tar.gz/AnalisisJannette-0.7/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AnalisisJannette',
+    version='0.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.0',
+        'sweetify',
+        'django-bootstrap5',
+        'pandas',
+        'unicodedata2',
+        # Otros paquetes requeridos
+    ],
+    entry_points={
+        'console_scripts': [
+            'manage = my_django_app.manage:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+    ],
+)
+from setuptools import setup, find_packages
+
+setup(
+    name='AnalisisJannette',
+    version='0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.0',
+        'sweetify',
+        'django-bootstrap5',
+        'pandas',
+        'unicodedata2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'manage = analisisJannett.manage:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+    ],
+)
