f8fb9bf9b7ebb5efc7ae434ca30679b521765a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 15:04:34 +0000

    Add package SFlock2/0.3.61/SFlock2-0.3.61.tar.gz

---
+++ packages/SFlock2/SFlock2-0.3.61.tar.gz/sflock2-0.3.61/setup.py
@@ -0,0 +1,70 @@
+# Copyright (C) 2015-2018 Jurriaan Bremer.
+# Copyright (C) 2018-2019 Hatching B.V.
+# This file is part of SFlock - http://www.sflock.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+import sys
+
+from setuptools import setup, find_packages
+from distutils.util import convert_path
+
+ver_module_ns = {}
+ver_module = convert_path("sflock/__version__.py")
+with open(ver_module) as fh:
+    exec(fh.read(), ver_module_ns)
+assert "__version__" in ver_module_ns
+version = ver_module_ns["__version__"]
+
+install_requires=[
+    "click",
+    "cryptography>=2.1",
+    "olefile>=0.43",
+    # "peepdf>=0.4.1",
+    "pefile",
+]
+
+if sys.platform == "win32":
+    install_requires.append("python-magic-bin>=0.4.14")
+elif sys.platform.startswith("linux"):
+    install_requires.append("python-magic>=0.4.13")
+
+setup(
+    name="SFlock2",
+    version=version,
+    author="Hatching B.V.",
+    author_email="jbr@hatching.io",
+    packages=[
+        "sflock",
+        "sflock.unpack",
+        "sflock.data",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sflock = sflock.main:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Security",
+    ],
+    keywords=("sflock unarchive"),
+    url="https://github.com/doomedraven/sflock/",
+    license="GPLv3",
+    description="Sample staging and detonation utility",
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        "dev": ["mock", "pytest"],
+        "shellcode": ["unicorn>=2.0.0", "yara-python>=4.1.0"],
+    }
+)
---
+++ packages/SFlock2/SFlock2-0.3.61.tar.gz/sflock2-0.3.61/tests/test_main.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2016-2018 Jurriaan Bremer.
+# This file is part of SFlock - http://www.sflock.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+import pytest
+
+from sflock.exception import IncorrectUsageException
+from sflock.main import supported, unpack
+from sflock.unpack import AceFile, CabFile, RarFile, Zip7File
+
+
+def test_supported():
+    assert supported()
+
+
+def test_count_supported():
+    count = 19
+
+    if AceFile(None).supported():
+        count += 1
+
+    if CabFile(None).supported():
+        count += 1
+
+    if RarFile(None).supported():
+        count += 1
+
+    if Zip7File(None).supported():
+        count += 7
+
+    assert count == len(supported())
+
+
+def test_unpack_py3():
+
+    with pytest.raises(IncorrectUsageException):
+        unpack(filepath="filepath")
+
+    with pytest.raises(IncorrectUsageException):
+        unpack(contents="contents")
+
+    # with pytest.raises(IncorrectUsageException):
+    #    unpack(password="password")
+
+    with pytest.raises(IncorrectUsageException):
+        unpack(filename="filename")
+
+    # It works, but no children are extracted from this Python file.
+    assert not unpack(__file__.encode()).children
