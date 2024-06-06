0a9cc35ab05730a0be6fa8cb7ef20fd9ea1fa022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 20:05:59 +0000

    Add package easylogger4dev-alpha/0.2.5/easylogger4dev_alpha-0.2.5.tar.gz

---
+++ packages/easylogger4dev-alpha/easylogger4dev_alpha-0.2.5.tar.gz/easylogger4dev_alpha-0.2.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easylogger4dev_alpha',
+    version='0.2.5',
+    packages=find_packages(),
+    install_requires=['pandas', 'openpyxl'],
+    python_requires='>=3.10',
+    author='DLMR-CODE',
+    author_email='yuzhihao82@gmail.com',
+    description='A description of your package',
+    license='MIT',
+    keywords='log, dev, easy, alpha'
+)
