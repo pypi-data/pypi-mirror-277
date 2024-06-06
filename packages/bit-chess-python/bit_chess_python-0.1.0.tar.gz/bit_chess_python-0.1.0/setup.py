8c0392c655b04dd0bf078a8a5dd8518ca8a489ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 18:04:28 +0000

    Add package bit-chess-python/0.1.0/bit_chess_python-0.1.0.tar.gz

---
+++ packages/bit-chess-python/bit_chess_python-0.1.0.tar.gz/bit_chess_python-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bit-chess-python',
+    version='0.1.0',
+    author='Andrew Yin',
+    author_email='andrewyingo@gmail.com',
+    description='Bitboard Chess implementation written entirely in Python',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
