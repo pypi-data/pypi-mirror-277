ba5b44fc1e7a0e78e3c77465513ed0639c96ad4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 14:04:35 +0000

    Add package alphafold3-pytorch/0.1.20/alphafold3_pytorch-0.1.20-py3-none-any.whl

---
+++ packages/alphafold3-pytorch/alphafold3_pytorch-0.1.20-py3-none-any.whl/alphafold3_pytorch/__init__.py
@@ -0,0 +1,71 @@
+from alphafold3_pytorch.attention import (
+    Attention,
+    Attend,
+    full_pairwise_repr_to_windowed
+)
+
+from alphafold3_pytorch.alphafold3 import (
+    RelativePositionEncoding,
+    SmoothLDDTLoss,
+    WeightedRigidAlign,
+    ExpressCoordinatesInFrame,
+    ComputeAlignmentError,
+    CentreRandomAugmentation,
+    TemplateEmbedder,
+    PreLayerNorm,
+    AdaptiveLayerNorm,
+    ConditionWrapper,
+    OuterProductMean,
+    MSAPairWeightedAveraging,
+    TriangleMultiplication,
+    AttentionPairBias,
+    TriangleAttention,
+    Transition,
+    MSAModule,
+    PairformerStack,
+    DiffusionTransformer,
+    DiffusionModule,
+    ElucidatedAtomDiffusion,
+    InputFeatureEmbedder,
+    ConfidenceHead,
+    DistogramHead,
+    Alphafold3
+)
+
+from alphafold3_pytorch.trainer import (
+    Trainer,
+    DataLoader,
+    AtomInput
+)
+
+__all__ = [
+    Attention,
+    Attend,
+    RelativePositionEncoding,
+    SmoothLDDTLoss,
+    WeightedRigidAlign,
+    ExpressCoordinatesInFrame,
+    ComputeAlignmentError,
+    CentreRandomAugmentation,
+    TemplateEmbedder,
+    PreLayerNorm,
+    AdaptiveLayerNorm,
+    ConditionWrapper,
+    OuterProductMean,
+    MSAPairWeightedAveraging,
+    TriangleMultiplication,
+    AttentionPairBias,
+    TriangleAttention,
+    Transition,
+    MSAModule,
+    PairformerStack,
+    DiffusionTransformer,
+    DiffusionModule,
+    ElucidatedAtomDiffusion,
+    InputFeatureEmbedder,
+    ConfidenceHead,
+    DistogramHead,
+    Alphafold3,
+    AtomInput,
+    Trainer
+]
