3d6cf1a088202f78243ce51440a53b493c05a930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 17:06:07 +0000

    Add package itkwasm-sub-mesh/2.0.0/itkwasm_sub_mesh-2.0.0-py3-none-any.whl

---
+++ packages/itkwasm-sub-mesh/itkwasm_sub_mesh-2.0.0-py3-none-any.whl/itkwasm_sub_mesh/__init__.py
@@ -0,0 +1,8 @@
+# Generated file. To retain edits, remove this comment.
+
+"""itkwasm-sub-mesh: Extract a subset of a mesh given by the cell identifiers."""
+
+from .sub_mesh_async import sub_mesh_async
+from .sub_mesh import sub_mesh
+
+from ._version import __version__
---
+++ packages/itkwasm-sub-mesh/itkwasm_sub_mesh-2.0.0-py3-none-any.whl/itkwasm_sub_mesh/sub_mesh_async.py
@@ -0,0 +1,28 @@
+# Generated file. Do not edit.
+
+import os
+from typing import Dict, Tuple, Optional, List, Any
+
+from itkwasm import (
+    environment_dispatch,
+    Mesh,
+)
+
+async def sub_mesh_async(
+    mesh: Mesh,
+    cell_identifiers: List[int] = [],
+) -> Mesh:
+    """Extract a subset of a mesh given by the cell identifiers.
+
+    :param mesh: Full mesh
+    :type  mesh: Mesh
+
+    :param cell_identifiers: Cell identifiers for output mesh.
+    :type  cell_identifiers: int
+
+    :return: Sub mesh.
+    :rtype:  Mesh
+    """
+    func = environment_dispatch("itkwasm_sub_mesh", "sub_mesh_async")
+    output = await func(mesh, cell_identifiers=cell_identifiers)
+    return output
