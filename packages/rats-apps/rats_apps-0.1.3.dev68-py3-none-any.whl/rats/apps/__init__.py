eef48675eda068b8b575914f59f3620355b077dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 20:06:12 +0000

    Add package rats-apps/0.1.3.dev68/rats_apps-0.1.3.dev68-py3-none-any.whl

---
+++ packages/rats-apps/rats_apps-0.1.3.dev68-py3-none-any.whl/rats/apps/__init__.py
@@ -0,0 +1,61 @@
+"""
+Provides a small set of libraries to help create new applications.
+
+Applications give you the ability to define a development experience to match your project's
+domain.
+"""
+
+from ._annotations import (
+    AnnotatedContainer,
+    autoid,
+    autoid_service,
+    config,
+    container,
+    fallback_config,
+    fallback_group,
+    fallback_service,
+    group,
+    service,
+)
+from ._composite_container import CompositeContainer
+from ._container import (
+    ConfigProvider,
+    Container,
+    DuplicateServiceError,
+    ServiceNotFoundError,
+    ServiceProvider,
+)
+from ._executables import App, AppContainer, Executable
+from ._ids import ConfigId, ServiceId
+from ._namespaces import ProviderNamespaces
+from ._plugin_container import PluginContainers
+from ._plugins import PluginRunner
+from ._scoping import autoscope
+
+__all__ = [
+    "AnnotatedContainer",
+    "App",
+    "AppContainer",
+    "CompositeContainer",
+    "ConfigId",
+    "Container",
+    "DuplicateServiceError",
+    "Executable",
+    "PluginContainers",
+    "ProviderNamespaces",
+    "ConfigProvider",
+    "ServiceProvider",
+    "ServiceId",
+    "ServiceNotFoundError",
+    "autoid_service",
+    "autoscope",
+    "config",
+    "container",
+    "PluginRunner",
+    "fallback_config",
+    "fallback_group",
+    "fallback_service",
+    "group",
+    "autoid",
+    "service",
+]
