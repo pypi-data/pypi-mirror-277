4e7722854adeaf05664ab5bb6445b8c8c3c48306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 20:05:59 +0000

    Add package nvl/0.1.4/nvl-0.1.4.tar.gz

---
+++ packages/nvl/nvl-0.1.4.tar.gz/nvl-0.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nvl',
+    version='0.1.4',  # Updated version number
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'nvl=nvl.cli:main',
+        ],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A CLI tool to initialize directories.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/nvl',  # Replace with your GitHub repo URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'colorama',  # Add your dependencies here
+        'requests',
+        'numpy',
+    ],
+)
