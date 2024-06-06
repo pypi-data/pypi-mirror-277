7166991b3a4313c3b74d034560ff943736679813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 18:04:31 +0000

    Add package inventree-tui/0.1.3/inventree_tui-0.1.3.tar.gz

---
+++ packages/inventree-tui/inventree_tui-0.1.3.tar.gz/inventree_tui-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.md').read()
+
+setup(
+    name="inventree-tui",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=["textual","inventree","pydantic", "python-dotenv"],
+    entry_points={
+        "console_scripts": [
+            "inventree-tui = inventree_tui.app:InventreeApp.run",
+        ],
+    },
+    description='Terminal UI for InvenTree',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
