99df5f4e860f7f0bfde27d6857c17d7e6c0e5d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 14:05:34 +0000

    Add package langroid/0.1.260/langroid-0.1.260.tar.gz

---
+++ packages/langroid/langroid-0.1.260.tar.gz/langroid-0.1.260/langroid/language_models/config.py
@@ -0,0 +1,18 @@
+from pydantic import BaseSettings
+
+
+class PromptFormatterConfig(BaseSettings):
+    type: str = "llama2"
+
+    class Config:
+        env_prefix = "FORMAT_"
+        case_sensitive = False
+
+
+class Llama2FormatterConfig(PromptFormatterConfig):
+    use_bos_eos: bool = False
+
+
+class HFPromptFormatterConfig(PromptFormatterConfig):
+    type: str = "hf"
+    model_name: str
