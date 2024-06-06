4c89716983a4d05cc6efe05edc397413c1b5b4c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 16:05:12 +0000

    Add package deeplexer/0.0.4/deeplexer-0.0.4-py3-none-any.whl

---
+++ packages/deeplexer/deeplexer-0.0.4-py3-none-any.whl/deeplexer/__init__.py
@@ -0,0 +1,7 @@
+from deeplexer.deeplexer import _Translation, _Deeplexer, _BrowserType
+
+Deeplexer = _Deeplexer
+Translation = _Translation
+BrowserType = _BrowserType
+
+__all__ = ['Deeplexer', 'Translation', 'BrowserType']
