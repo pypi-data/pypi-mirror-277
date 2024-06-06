ec854429a6ed48f5bb28fb87da48045c73eb2e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 20:06:03 +0000

    Add package cosmic-counsel/0.0.5/cosmic_counsel-0.0.5.tar.gz

---
+++ packages/cosmic-counsel/cosmic_counsel-0.0.5.tar.gz/cosmic_counsel-0.0.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cosmic-counsel',
+    version='0.0.5',
+    author='Collin Paran, Eric Willis',
+    url='https://www.boozallen.com/',
+    description='Space R&D',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={'cosmic-counsel': ['models/*.sav', 'data/*.json']},
+    install_requires=[
+        'torch',
+        'transformers',
+        'numpy',
+        'pandas',
+        'uvicorn',
+        'bitsandbytes',
+        'accelerate',
+        'sentence-transformers',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    license='Apache 2.0',
+)
