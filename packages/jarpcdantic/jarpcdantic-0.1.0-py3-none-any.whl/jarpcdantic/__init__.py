60c4effa13ed9abe4098a8ba81272395e7824bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-06 18:04:33 +0000

    Add package jarpcdantic/0.1.0/jarpcdantic-0.1.0-py3-none-any.whl

---
+++ packages/jarpcdantic/jarpcdantic-0.1.0-py3-none-any.whl/jarpcdantic/__init__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from .client import AsyncJarpcClient, JarpcClient
+from .dispatcher import JarpcDispatcher
+from .errors import (
+    JarpcError,
+    JarpcExternalServiceUnavailable,
+    JarpcForbidden,
+    JarpcInternalError,
+    JarpcInvalidParams,
+    JarpcInvalidRequest,
+    JarpcMethodNotFound,
+    JarpcParseError,
+    JarpcServerError,
+    JarpcTimeout,
+    JarpcUnauthorized,
+    JarpcUnknownError,
+    JarpcValidationError,
+    raise_exception,
+)
+from .format import JarpcRequest, JarpcResponse
+from .manager import AsyncJarpcManager, JarpcManager
+
+__all__ = (
+    # client
+    "AsyncJarpcClient",
+    "JarpcClient",
+    # dispatcher
+    "JarpcDispatcher",
+    # errors
+    "JarpcError",
+    "JarpcExternalServiceUnavailable",
+    "JarpcForbidden",
+    "JarpcInternalError",
+    "JarpcInvalidParams",
+    "JarpcInvalidRequest",
+    "JarpcMethodNotFound",
+    "JarpcParseError",
+    "JarpcServerError",
+    "JarpcTimeout",
+    "JarpcUnknownError",
+    "JarpcUnauthorized",
+    "JarpcValidationError",
+    "raise_exception",
+    # format
+    "JarpcRequest",
+    "JarpcResponse",
+    # manager
+    "AsyncJarpcManager",
+    "JarpcManager",
+)
+
+__version__ = "0.1.0"
