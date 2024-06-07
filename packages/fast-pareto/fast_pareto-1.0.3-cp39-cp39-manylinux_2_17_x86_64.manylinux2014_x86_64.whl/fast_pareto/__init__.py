b4d368766e0687502285415788ee38547011e7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:26 +0000

    Add package fast-pareto/1.0.3/fast_pareto-1.0.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/fast-pareto/fast_pareto-1.0.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/fast_pareto/__init__.py
@@ -0,0 +1,14 @@
+from fast_pareto.hypervolume import hypervolume
+from fast_pareto.pareto import is_pareto_front
+from fast_pareto.pareto import nondominated_rank
+
+
+__version__ = "1.0.3"
+__copyright__ = "Copyright (C) 2024 Shuhei Watanabe"
+__licence__ = "Apache-2.0 License"
+__author__ = "Shuhei Watanabe"
+__author_email__ = "shuhei.watanabe.utokyo@gmail.com"
+__url__ = "https://github.com/nabenabe0928/fast-pareto"
+
+
+__all__ = ["nondominated_rank", "hypervolume", "is_pareto_front"]
