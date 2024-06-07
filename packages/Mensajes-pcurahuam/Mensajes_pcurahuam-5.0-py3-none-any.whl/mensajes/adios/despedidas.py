31f8199cf8f162ad3e5bf14874aa62b450f4fb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:06:18 +0000

    Add package Mensajes-pcurahuam/5.0/Mensajes_pcurahuam-5.0-py3-none-any.whl

---
+++ packages/Mensajes-pcurahuam/Mensajes_pcurahuam-5.0-py3-none-any.whl/mensajes/adios/despedidas.py
@@ -0,0 +1,6 @@
+def despedir():
+    print("Hola, me despedido desde despedidas.despedir()")
+
+class Despedida:
+    def __init__(self):
+        print("Hola, me despedido desde Despedida.__init__()")
