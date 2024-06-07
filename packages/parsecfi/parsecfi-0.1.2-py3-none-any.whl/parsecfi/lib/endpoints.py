64fa0000fcaa27f43b32afde97d864b5919ded0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:07:42 +0000

    Add package parsecfi/0.1.2/parsecfi-0.1.2-py3-none-any.whl

---
+++ packages/parsecfi/parsecfi-0.1.2-py3-none-any.whl/parsecfi/lib/endpoints.py
@@ -0,0 +1,22 @@
+from parsecfi.lib.endpoints_list import (
+	lending_market, 
+	contract_logs, 
+	token_holders, 
+	token_acc, 
+	trending_contracts,
+	transfers,
+	address,
+	transactions
+)
+
+def load():
+	return {
+		'lending_market': lending_market,
+		'contract_logs': contract_logs,
+		'token_holders': token_holders,
+		'token_acc': token_acc,
+		'trending_contracts': trending_contracts,
+		'transfers': transfers,
+		'address': address,
+		'transactions': transactions
+	}
