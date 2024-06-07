6968b2542851b0d2c57874fca638b103385c3dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 03:05:36 +0000

    Add package viktor-feedback-manager/0.0.1/viktor_feedback_manager-0.0.1-py3-none-any.whl

---
+++ packages/viktor-feedback-manager/viktor_feedback_manager-0.0.1-py3-none-any.whl/viktor_feedback_manager/__init__.py
@@ -0,0 +1,4 @@
+"""Entry point for the package"""
+
+from viktor_feedback_manager.feedback_entity_controller import Feedback
+from viktor_feedback_manager.feedback_manager_entity_controller import FeedbackManager
