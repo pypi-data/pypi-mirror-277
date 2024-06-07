f566b9c43964571523726c10123c397405aae511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:46 +0000

    Add package kubernetes-stubs-elephant-fork/30.1.0/kubernetes_stubs_elephant_fork-30.1.0-py2.py3-none-any.whl

---
+++ packages/kubernetes-stubs-elephant-fork/kubernetes_stubs_elephant_fork-30.1.0-py2.py3-none-any.whl/kubernetes-stubs/client/models/v1alpha2_named_resources_request.pyi
@@ -0,0 +1,14 @@
+import datetime
+import typing
+
+import kubernetes.client
+
+class V1alpha2NamedResourcesRequest:
+    selector: str
+    
+    def __init__(self, *, selector: str) -> None:
+        ...
+    def to_dict(self) -> V1alpha2NamedResourcesRequestDict:
+        ...
+class V1alpha2NamedResourcesRequestDict(typing.TypedDict, total=False):
+    selector: str
