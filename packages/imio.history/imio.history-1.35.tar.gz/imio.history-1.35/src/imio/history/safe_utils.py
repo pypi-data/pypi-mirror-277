c84a43b706e273bdc094853439d9f9f321787dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 16:06:04 +0000

    Add package imio.history/1.35/imio.history-1.35.tar.gz

---
+++ packages/imio.history/imio.history-1.35.tar.gz/imio.history-1.35/src/imio/history/safe_utils.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+from imio.history.utils import getLastAction
+from imio.history.utils import getLastWFAction
+from imio.history.utils import getPreviousEvent
