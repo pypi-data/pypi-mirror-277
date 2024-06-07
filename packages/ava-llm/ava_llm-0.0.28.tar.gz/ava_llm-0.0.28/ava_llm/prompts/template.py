0f845e3fc63ddbdf202a3640981bc0611eb20ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 16:06:03 +0000

    Add package ava-llm/0.0.28/ava_llm-0.0.28.tar.gz

---
+++ packages/ava-llm/ava_llm-0.0.28.tar.gz/ava_llm-0.0.28/ava_llm/prompts/template.py
@@ -0,0 +1,14 @@
+from ava_llm.prompts.base import PromptTemplate
+
+
+class CustomTemplate(PromptTemplate):
+    template = """{text}"""
+
+
+class ConversationTemplate(PromptTemplate):
+    template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+Current conversation:
+{history}
+Human: {input}
+AI:
+"""
