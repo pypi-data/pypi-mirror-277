420be0393be24b18f0e38f3b9cdacb35e0bfdb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:05:18 +0000

    Add package phlop/0.0.22/phlop-0.0.22.tar.gz

---
+++ packages/phlop/phlop-0.0.22.tar.gz/phlop-0.0.22/phlop/run/stats_man.py
@@ -0,0 +1,30 @@
+#
+#
+
+import logging
+import sys
+
+from phlop.app import stats_man as sman
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = sman.cli_args_parser()
+    cli_args = sman.verify_cli_args(parser.parse_args())
+    try:
+        info = dict(exe=cli_args.remaining)
+        statsman = sman.RuntimeStatsManager(cli_args, info).join()
+        if cli_args.summary:
+            sman.print_summary(statsman)
+    except (Exception, SystemExit) as e:
+        logger.exception(e)
+        parser.print_help()
+    except:
+        e = sys.exc_info()[0]
+        print(f"Error: Unknown Error {e}")
+
+
+if __name__ == "__main__":
+    main()
