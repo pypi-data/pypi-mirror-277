1f118fbf6f7321a65cc0734204f988a293fac2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:50 +0000

    Add package edgeiq/2.4.0/edgeiq-2.4.0-cp39-none-macosx_10_9_x86_64.whl

---
+++ packages/edgeiq/edgeiq-2.4.0-cp39-none-macosx_10_9_x86_64.whl/edgeiq/_production_client/constants.pyi
@@ -0,0 +1,10 @@
+from _typeshed import Incomplete
+from edgeiq.app_config import AppConfig as AppConfig
+
+PROJECT_ID: Incomplete
+PROJECT_TAG: Incomplete
+TOPIC_PREFIX: Incomplete
+TO_CLOUD_TOPIC_PREFIX: Incomplete
+DEVICE_TAG: Incomplete
+IMAGE_TYPE: str
+RUN_RABBITMQ_CONNECTION: Incomplete
---
+++ packages/edgeiq/edgeiq-2.4.0-cp39-none-macosx_10_9_x86_64.whl/edgeiq/pyarmor_runtime_006224/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.0 (group), 006224, 2024-06-05T16:06:49.576921
+from .pyarmor_runtime import __pyarmor__
