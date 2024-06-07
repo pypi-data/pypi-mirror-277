849e654ec0c22fed3ccd84733ab800e3d93dc44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 15:05:13 +0000

    Add package mlx/0.15.0/mlx-0.15.0-cp38-cp38-macosx_14_0_arm64.whl

---
+++ packages/mlx/mlx-0.15.0-cp38-cp38-macosx_14_0_arm64.whl/mlx/include/mlx/backend/metal/kernels/defines.h
@@ -0,0 +1,15 @@
+// Copyright © 2023 Apple Inc.
+
+#pragma once
+
+#if defined __METAL__ || defined MLX_METAL_JIT
+#define MTL_CONST constant
+#else
+#define MTL_CONST
+#endif
+
+static MTL_CONST constexpr int MAX_REDUCE_SPECIALIZED_DIMS = 4;
+static MTL_CONST constexpr int REDUCE_N_READS = 16;
+static MTL_CONST constexpr int SOFTMAX_N_READS = 4;
+static MTL_CONST constexpr int RMS_N_READS = 4;
+static MTL_CONST constexpr int RMS_LOOPED_LIMIT = 4096;
