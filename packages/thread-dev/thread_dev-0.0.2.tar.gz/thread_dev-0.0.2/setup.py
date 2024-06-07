7e2330b4466f3d9f64a79211a657733e70f66f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 06:04:52 +0000

    Add package thread-dev/0.0.2/thread_dev-0.0.2.tar.gz

---
+++ packages/thread-dev/thread_dev-0.0.2.tar.gz/thread_dev-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="thread-dev",
+    version="0.0.2",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "jupyter-server>=2.0",
+        "jupyter",
+    ],
+    entry_points={
+        "console_scripts": [
+            "jupyter-thread = thread:launch_instance",
+            "thread = thread:launch_instance",
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
