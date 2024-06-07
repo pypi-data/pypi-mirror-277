dfa6ae82bbe1bca1625363f3489643239643c793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 15:04:48 +0000

    Add package xy-work/1.0.0/xy_work-1.0.0.tar.gz

---
+++ packages/xy-work/xy_work-1.0.0.tar.gz/xy_work-1.0.0/xy_work/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+__author__ = "helios"
+__doc__ = "__init__"
+"""
+  * @File    :   __init__.py
+  * @Time    :   2023/04/24 18:29:47
+  * @Author  :   helios
+  * @Version :   1.0
+  * @Contact :   yuyang.0515@qq.com
+  * @License :   (C)Copyright 2019-2023, Ship of Ocean
+  * @Desc    :   None
+"""
+
+
+__version__ = "0.0.1"
