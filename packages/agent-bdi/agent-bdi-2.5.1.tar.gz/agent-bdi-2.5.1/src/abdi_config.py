4ff4cff2427a6e38e010f83766cdd94f2feb5886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:05:29 +0000

    Add package agent-bdi/2.5.1/agent-bdi-2.5.1.tar.gz

---
+++ packages/agent-bdi/agent-bdi-2.5.1.tar.gz/agent-bdi-2.5.1/src/abdi_config.py
@@ -0,0 +1,30 @@
+import logging
+
+from broker import BrokerType
+
+
+LOGGER_NAME = "ABDI"
+
+
+
+class AbdiConfig:
+    def __init__(self, options=None):
+        self.options = options if options else {}
+
+        self.log_level = logging.DEBUG
+        self.log_dir = "./_log"
+        
+        
+    def get_broker_type(self) -> BrokerType:
+        if broker_type := self.get("broker_type"):
+            return BrokerType(broker_type.lower())
+        else:
+            return BrokerType.Empty
+        
+    
+    def get(self, key:str, default=None):
+        return self.options.get(key, default)
+        
+    
+    def set(self, key:str, value):
+        self.options[key] = value
