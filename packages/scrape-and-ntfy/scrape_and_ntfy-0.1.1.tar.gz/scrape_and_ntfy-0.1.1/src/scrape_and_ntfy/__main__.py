8f05de268db0c218686da13311725e0175ed4f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 16:06:27 +0000

    Add package scrape-and-ntfy/0.1.1/scrape_and_ntfy-0.1.1.tar.gz

---
+++ packages/scrape-and-ntfy/scrape_and_ntfy-0.1.1.tar.gz/scrape_and_ntfy-0.1.1/src/scrape_and_ntfy/__main__.py
@@ -0,0 +1,86 @@
+from scrape_and_ntfy.utils.logging import logger
+from scrape_and_ntfy.utils.cli_args import args
+from scrape_and_ntfy.scraping import scraper, UrlScraper
+from scrape_and_ntfy.scraping import notifier
+import selenium
+import sys
+import toml
+
+
+def main():
+    logger.debug(f"Args: {args}")
+    try: 
+        config = toml.load(args.path_to_toml)
+    except FileNotFoundError:
+        logger.critical(f"File {args.path_to_toml} not found")
+        sys.exit(1)
+    if args.browser == "chrome":
+        options = selenium.webdriver.ChromeOptions()
+        if args.headless_path:
+            # If headless_path is set, use the binary at that path
+            logger.info("Using Chrome headless")
+            options.binary_location = args.headless_path
+            options.add_argument("--headless")
+        else:
+            logger.info("Using Chrome")
+        scraper.driver = selenium.webdriver.Chrome(options=options)
+    elif args.browser == "firefox":
+        options = selenium.webdriver.FirefoxOptions()
+        if args.headless_path:
+            # If headless_path is set, use the binary at that path
+            logger.info("Using Firefox headless")
+            options.binary_location = args.headless_path
+            options.add_argument("--headless")
+        else:
+            logger.info("Using Firefox")
+        scraper.driver = selenium.webdriver.Firefox(options=options)
+    elif args.browser == "edge":
+        options = selenium.webdriver.EdgeOptions()
+        # if args.headless_path:
+        #     # If headless_path is set, use the binary at that path
+        #     logger.info("Using Edge headless")
+        #     options.binary_location = args.headless_path
+        #     options.add_argument("--headless")
+        # else:
+        #     logger.info("Using Edge")
+        logger.info("Using Edge")
+        scraper.driver = selenium.webdriver.Edge(options=options)
+    elif args.browser == "safari":
+        logger.info("Using Safari")
+        scraper.driver = selenium.webdriver.Safari()
+    else:
+        raise ValueError("Invalid browser")
+
+    for s in config["scrapers"]:
+        notifiers = []
+        for n in s["notifiers"]:
+            # Check if the notify_on values are valid (check against Notifier.NotifyOn)
+            notify_on_list = []
+            for notify_on in n["config"]["notify_on"]:
+                if notify_on not in [no.value for no in list(notifier.Notifier.NotifyOn)]:
+                    raise ValueError(f"Invalid notify_on value: {notify_on}")
+                else:
+                    # Get the Enum object from the string value
+                    notify_on_list.append(notifier.Notifier.NotifyOn(notify_on))
+            if n["type"] == "webhook":
+                notifiers.append(notifier.Webhook(url=n["config"]["url"], notify_on=notify_on_list))
+            elif n["type"] == "ntfy":
+                notifiers.append(notifier.Ntfy(url=n["config"]["url"], notify_on=notify_on_list, on_click=n["config"]["on_click"], priority=n["config"]["priority"], tags=n["config"]["tags"]))
+        UrlScraper(
+            url=s["url"],
+            css_selector=s["css_selector"],
+            interval=s["interval"],
+            notifiers=notifiers,
+        )
+    UrlScraper.clean_db()
+    try:
+        while True:
+            UrlScraper.scrape_all_urls()
+    except KeyboardInterrupt:
+        logger.info("Keyboard interrupt detected; exiting")
+        scraper.driver.quit()
+        exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/scrape-and-ntfy/scrape_and_ntfy-0.1.1.tar.gz/scrape_and_ntfy-0.1.1/src/scrape_and_ntfy/utils/cli_args.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+import sys
+from pathlib import Path
+
+from scrape_and_ntfy.utils.logging import logger
+
+import dotenv
+
+
+def set_argparse() -> None:
+    """
+    Set up the argument parser and parse the arguments to args
+    """
+    global args
+
+    if Path(".env").is_file():
+        dotenv.load_dotenv()
+        logger.info("Loaded .env file")
+    else:
+        logger.warning(".env file not found")
+    argparser = argparse.ArgumentParser(
+        description="Scrape a website and notify regarding specific changes"
+    )
+    argparser.add_argument(
+        "--db-url",
+        help="The URL to the database",
+        default=os.getenv("DB_URL") if os.getenv("DB_URL") else "sqlite:///database/db.db",
+    )
+    argparser.add_argument(
+        "--browser",
+        help="The browser to use. Generally, if the browser is based on Chromium, use Chrome and if it's based on Firefox, use Firefox.",
+        default=os.getenv("BROWSER") if os.getenv("BROWSER") else "chrome",
+        choices=["chrome", "firefox", "edge", "safari"],
+    )
+    argparser.add_argument(
+        "--path-to-toml",
+        help="The path to the TOML file",
+        default=os.getenv("PATH_TO_TOML") if os.getenv("PATH_TO_TOML") else "config.toml",
+    )
+    argparser.add_argument(
+        "--headless-path",
+        help="The path to whatever browser binary to use headlessly. Ensure this matches --browser/$BROWSER. This does not work with Safari nor Edge.",
+        default=os.getenv("HEADLESS_PATH") if os.getenv("HEADLESS_PATH") else "",
+        # Not using validate_path_to_file because having "" as the default raises an error
+        type=str,
+    )
+    debug = argparser.add_argument_group("Debugging options")
+    debug.add_argument(
+        "--log-level",
+        help="The log level to use.",
+        default=os.getenv("LOG_LEVEL") if os.getenv("LOG_LEVEL") else "INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    )
+    args = argparser.parse_args()
+
+# def validate_path_to_file(path: str) -> str:
+#     """
+#     Validate that the path to the file exists
+#     """
+#     if Path(path).is_file():
+#         return path
+#     else:
+#         raise argparse.ArgumentTypeError(f"File {path} is not a file (or does not exist)")
+
+
+def check_required_args(required_args: list[str], argparser: argparse.ArgumentParser):
+    """
+    Check if required arguments are set
+    Useful if using enviroment variables with argparse as default and required are mutually exclusive
+    """
+    for arg in required_args:
+        args = argparser.parse_args()
+        if getattr(args, arg) is None:
+            # raise ValueError(f"{arg} is required")
+            logger.critical(f"{arg} is required")
+            sys.exit(1)
+
+
+
+set_argparse()
