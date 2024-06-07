d13f80198c4bbd7279f9d1aaa548dd2ccf8c5753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 03:05:27 +0000

    Add package uprock-sdk/0.1.19/uprock_sdk-0.1.19-py3-none-any.whl

---
+++ packages/uprock-sdk/uprock_sdk-0.1.19-py3-none-any.whl/uprock_sdk/telegram_bots.py
@@ -0,0 +1,26 @@
+import httpx
+from pydantic import BaseModel, RedisDsn
+
+from uprock_sdk import GLOBAL_SETTINGS
+
+CLIENT = httpx.AsyncClient(base_url=GLOBAL_SETTINGS.CORE_API_URL)
+
+
+class TelegramBotRead(BaseModel):
+    id: int
+
+    name: str
+    slug: str
+
+    token: str
+    redis_uri: RedisDsn
+
+    namespace_id: int
+
+
+async def get(id_: int) -> TelegramBotRead:
+    response = await CLIENT.get(f"/v1/internal/telegram_bots/{id_}")
+    response.raise_for_status()
+
+    raw_telegram_bot_data = response.json()
+    return TelegramBotRead(**raw_telegram_bot_data)
