6804e08650d38be6fd6016aa8653d63ffec0e75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:51 +0000

    Add package posting/0.1.0a10/posting-0.1.0a10-py3-none-any.whl

---
+++ packages/posting/posting-0.1.0a10-py3-none-any.whl/posting/widgets/datatable.py
@@ -0,0 +1,28 @@
+from textual.binding import Binding
+from textual.widgets import DataTable
+from textual.widgets.data_table import CellKey, RowKey
+
+
+class PostingDataTable(DataTable[str]):
+    BINDINGS = [
+        Binding("up,k", "cursor_up", "Cursor Up", show=False),
+        Binding("down,j", "cursor_down", "Cursor Down", show=False),
+        Binding("right,l", "cursor_right", "Cursor Right", show=False),
+        Binding("left,h", "cursor_left", "Cursor Left", show=False),
+        Binding("f", "toggle_fixed_columns", "Toggle Fixed Column", show=False),
+    ]
+
+    def action_toggle_fixed_columns(self) -> None:
+        self.fixed_columns = 1 if self.fixed_columns == 0 else 0
+
+    def remove_row(self, row_key: RowKey | str) -> None:
+        rv = super().remove_row(row_key)
+
+        # TODO - fix this inside Textual.
+        if self.row_count > 0:
+            row_zero = list(self._data.keys())[0]
+            columns = set(list(self._data.values())[0].keys())
+            self._update_column_widths(
+                {CellKey(row_zero, column) for column in columns}
+            )
+        return rv
