c241462cdd0462ed337f95a9df88d1c89e7c604f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 15:05:13 +0000

    Add package video-sorter/0.1.1/video_sorter-0.1.1-py3-none-any.whl

---
+++ packages/video-sorter/video_sorter-0.1.1-py3-none-any.whl/vsorter/vsorter_gui.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: nu:ai:ts=4:sw=4
+
+#
+#  Copyright (C) 2024 Joseph Areeda <joseph.areeda@ligo.org>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+""""""
+
+import time
+import traceback
+
+from PyQt6.QtGui import QPalette, QColor, QAction, QIcon
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QHBoxLayout, QStackedLayout, QTabWidget, \
+    QToolBar, QStatusBar, QDialog, QDialogButtonBox
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+)
+
+start_time = time.time()
+
+import argparse
+import logging
+from pathlib import Path
+import re
+import subprocess
+import sys
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = '0.0.0'
+
+__author__ = 'joseph areeda'
+__email__ = 'joseph.areeda@ligo.org'
+__process_name__ = Path(__file__).name
+
+logger = None
+
+
+def parser_add_args(parser):
+    """
+    Set up command parser
+    :param argparse.ArgumentParser parser:
+    :return: None but parser object is updated
+    """
+    parser.add_argument('-v', '--verbose', action='count', default=1,
+                        help='increase verbose output')
+    parser.add_argument('-V', '--version', action='version',
+                        version=__version__)
+    parser.add_argument('-q', '--quiet', default=False, action='store_true',
+                        help='show only fatal errors')
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press me for a dialog!")
+        button.clicked.connect(self.button_clicked)
+        self.setCentralWidget(button)
+
+    def button_clicked(self, s):
+        print("click", s)
+
+        dlg = CustomDialog(self)
+        if dlg.exec():
+            print("Success!")
+        else:
+            print("Cancel!")
+
+
+class CustomDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle("HELLO!")
+
+        QBtn = QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("Something happened, is that OK?")
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+
+
+def main():
+    global logger
+
+    logging.basicConfig()
+    logger = logging.getLogger(__process_name__)
+    logger.setLevel(logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description=__doc__, prog=__process_name__,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser_add_args(parser)
+    args = parser.parse_args()
+    verbosity = 0 if args.quiet else args.verbose
+
+    if verbosity < 1:
+        logger.setLevel(logging.CRITICAL)
+    elif verbosity < 2:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.DEBUG)
+
+    # debugging?
+    logger.debug(f'{__process_name__} version: {__version__} called with arguments:')
+    for k, v in args.__dict__.items():
+        logger.debug('    {} = {}'.format(k, v))
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (ValueError, TypeError, OSError, NameError, ArithmeticError, RuntimeError) as ex:
+        print(ex, file=sys.stderr)
+        traceback.print_exc(file=sys.stderr)
+
+    if logger is None:
+        logging.basicConfig()
+        logger = logging.getLogger(__process_name__)
+        logger.setLevel(logging.DEBUG)
+    # report our run time
+    logger.info(f'Elapsed time: {time.time() - start_time:.1f}s')
