58aa132dd2c65a6febae20ddc639d50663aa7086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:25 +0000

    Add package fractlang/0.1.0/fractlang-0.1.0.tar.gz

---
+++ packages/fractlang/fractlang-0.1.0.tar.gz/fractlang-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fractlang',
+    version='0.1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'fractx=fractlang.fractx:main',
+        ],
+    },
+    install_requires=[
+        # Dependensi lain jika ada
+    ],
+    author='ryznxx',
+    author_email='7ryznxx@gmail.com',
+    description='A custom programming language interpreter for Fractlang',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/username/fractlang',  # Ganti dengan URL repositori Anda
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
