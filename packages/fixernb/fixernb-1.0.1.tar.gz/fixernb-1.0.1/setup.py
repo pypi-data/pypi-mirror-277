423fbc1163887a0d8088e6e0d80ea19d61477737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:05:22 +0000

    Add package fixernb/1.0.1/fixernb-1.0.1.tar.gz

---
+++ packages/fixernb/fixernb-1.0.1.tar.gz/fixernb-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fixernb',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'nbformat',
+    ],
+    entry_points={
+        'console_scripts': [
+            'run-updater=fixernb.fixer:fixer',
+        ],
+    },
+    author='Ashkan Ahmadi',
+    author_email='netblag.dev@gmail.com',
+    description='A tool to update Jupyter notebooks to version 4',
+    url='https://github.com/netblag/jupyter-notebook-fixer',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
