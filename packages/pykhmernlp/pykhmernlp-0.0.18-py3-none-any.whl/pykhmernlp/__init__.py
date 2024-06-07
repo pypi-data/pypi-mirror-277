676b8c1bb70f8022a44907699b3af12e3b4d9b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 16:06:18 +0000

    Add package pykhmernlp/0.0.18/pykhmernlp-0.0.18-py3-none-any.whl

---
+++ packages/pykhmernlp/pykhmernlp-0.0.18-py3-none-any.whl/pykhmernlp/__init__.py
@@ -0,0 +1,18 @@
+import pykhmernlp.address as address
+import pykhmernlp.corpus as corpus
+import pykhmernlp.pronounce as pronounce
+import pykhmernlp.tokenizer as tokenizer
+import pykhmernlp.tha as tha 
+import pykhmernlp.number as number
+
+
+
+__all__ = [
+    "remove_duplicate_spaces",
+    "remove_zero_width_characters"
+]
+
+from pykhmernlp.utils import (
+    remove_duplicate_spaces,
+    remove_zero_width_characters
+)
