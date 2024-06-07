e4fd7bd972357c1da3e91ae23af7a4bfc61781aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 03:05:18 +0000

    Add package huaweicloudsdkeps/3.1.99/huaweicloudsdkeps-3.1.99-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkeps/huaweicloudsdkeps-3.1.99-py2.py3-none-any.whl/huaweicloudsdkeps/v1/region/eps_region.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.region.region import Region
+from huaweicloudsdkcore.region.provider import RegionProviderChain
+
+class EpsRegion:
+    _PROVIDER = RegionProviderChain.get_default_region_provider_chain("EPS")
+
+    CN_NORTH_4 = Region("cn-north-4",
+                        "https://eps.myhuaweicloud.com",
+                        "https://eps.myhuaweicloud.cn")
+    EU_WEST_101 = Region("eu-west-101",
+                        "https://eps.eu-west-101.myhuaweicloud.eu")
+
+    static_fields = {
+        "cn-north-4": CN_NORTH_4,
+        "eu-west-101": EU_WEST_101,
+    }
+
+    @classmethod
+    def value_of(cls, region_id, static_fields=None):
+        if not region_id:
+            raise KeyError("Unexpected empty parameter: region_id")
+
+        fields = static_fields or cls.static_fields
+
+        region = cls._PROVIDER.get_region(region_id)
+        if region:
+            return region
+
+        if region_id in fields:
+            return fields.get(region_id)
+
+        raise KeyError("region_id '%s' is not in the following supported regions of service 'Eps': [%s]" % (
+            region_id, ", ".join(sorted(fields.keys()))))
