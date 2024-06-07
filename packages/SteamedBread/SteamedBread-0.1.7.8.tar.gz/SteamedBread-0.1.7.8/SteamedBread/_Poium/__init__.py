07df07d15e8057c02b5717cda3ab9fb114653bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:07:39 +0000

    Add package SteamedBread/0.1.7.8/SteamedBread-0.1.7.8.tar.gz

---
+++ packages/SteamedBread/SteamedBread-0.1.7.8.tar.gz/SteamedBread-0.1.7.8/SteamedBread/_Poium/__init__.py
@@ -0,0 +1,18 @@
+"""
+@Author: 馒头 (chocolate)
+@Email: neihanshenshou@163.com
+@File: __init__.py
+@Time: 2023/12/9 18:00
+"""
+
+from .config import Browser
+from .javascript import CSSElement
+from .processing import compress_image
+from .processing import processing
+from .selenium import Element
+from .selenium import Elements
+from .webdriver import Page
+
+__author__ = "馒头"
+
+__version__ = "0.1.0"
