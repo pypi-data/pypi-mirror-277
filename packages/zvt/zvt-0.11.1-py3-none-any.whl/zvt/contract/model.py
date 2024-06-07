6798821a5594efb6c0924698fafe8a4b78bd39c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:50 +0000

    Add package zvt/0.11.1/zvt-0.11.1-py3-none-any.whl

---
+++ packages/zvt/zvt-0.11.1-py3-none-any.whl/zvt/contract/model.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class CustomModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True, allow_inf_nan=True)
+
+
+class MixinModel(CustomModel):
+    id: str
+    entity_id: str
+    timestamp: datetime
