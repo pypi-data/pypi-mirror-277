a9d3decc9bd325c7b534fe0e52e02c333648a9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:07:01 +0000

    Add package psqlpy/0.6.1/psqlpy-0.6.1-cp310-none-win32.whl

---
+++ packages/psqlpy/psqlpy-0.6.1-cp310-none-win32.whl/psqlpy/extra_types.py
@@ -0,0 +1,27 @@
+from ._internal.extra_types import (
+    BigInt,
+    Integer,
+    PyCustomType,
+    PyJSON,
+    PyJSONB,
+    PyMacAddr6,
+    PyMacAddr8,
+    PyText,
+    PyUUID,
+    PyVarChar,
+    SmallInt,
+)
+
+__all__ = [
+    "SmallInt",
+    "Integer",
+    "BigInt",
+    "PyUUID",
+    "PyJSONB",
+    "PyJSON",
+    "PyMacAddr6",
+    "PyMacAddr8",
+    "PyVarChar",
+    "PyText",
+    "PyCustomType",
+]
