cc820d757cf051a28a08e4ba9a358fa44c6892e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:07:06 +0000

    Add package MALRULES/0.1/MALRULES-0.1-py3-none-any.whl

---
+++ packages/MALRULES/MALRULES-0.1-py3-none-any.whl/MALRULES/__init__.py
@@ -0,0 +1,4 @@
+# MALRULES/__init__.py
+
+from .MALRULES_analysis import is_file_suspicious, determine_suspicion_level, calculate_suspicion_score_and_families
+from .hash_utils import generate_file_hashes
