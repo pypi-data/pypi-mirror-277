c38ddd97bcc214e6963615b049b52cbed3462bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 15:04:51 +0000

    Add package multi-media-operations/0.0.6/multi_media_operations-0.0.6.tar.gz

---
+++ packages/multi-media-operations/multi_media_operations-0.0.6.tar.gz/multi_media_operations-0.0.6/multi_media_operations/__init__.py
@@ -0,0 +1,13 @@
+"""
+@Time : 2023/8/3 15:02 
+@Author : skyoceanchen
+@TEL: 18916403796
+@项目：multi_media_operations
+@File : __init__.by
+@PRODUCT_NAME :PyCharm
+"""
+import warnings
+
+warnings.filterwarnings('ignore')
+from img_operations import *
+from video_operations import *
