0793f2f50fd1d5485269057da6f54592129d6612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:51 +0000

    Add package meu-cli/0.1/meu-cli-0.1.tar.gz

---
+++ packages/meu-cli/meu-cli-0.1.tar.gz/meu-cli-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='meu-cli',
+    version='0.1',
+    py_modules=['meu_cli'],
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        meu-cli=meu_cli:cli
+    ''',
+)
