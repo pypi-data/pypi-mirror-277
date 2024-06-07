959edb5314e1abc9327d36d54e008ef18a00b723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 06:04:47 +0000

    Add package asyncflows/0.1.8/asyncflows-0.1.8.tar.gz

---
+++ packages/asyncflows/asyncflows-0.1.8.tar.gz/asyncflows-0.1.8/asyncflows/models/config/flow.py
@@ -0,0 +1,119 @@
+from typing import Union
+
+import pydantic
+from pydantic import Field
+
+from asyncflows.models.config.action import (
+    ActionInvocation,
+    build_actions,
+)
+from asyncflows.models.config.common import StrictModel
+from asyncflows.models.config.model import ModelConfig
+from asyncflows.models.config.value_declarations import ValueDeclaration
+from asyncflows.models.primitives import (
+    ContextVarName,
+    ContextVarPath,
+    ExecutableId,
+    HintLiteral,
+)
+from asyncflows.models.config.action import build_hinted_value_declaration
+from asyncflows.models.config.value_declarations import (
+    LinkDeclaration,
+    LambdaDeclaration,
+)
+from asyncflows.utils.config_utils import templatify_model
+
+
+class Loop(StrictModel):
+    for_: ContextVarName = Field(
+        ...,
+        alias="for",
+    )
+    in_: ValueDeclaration = Field(
+        ...,
+        alias="in",
+    )
+    flow: "FlowConfig"
+
+
+def build_model_config(
+    strict: bool = False,
+):
+    # Dynamically build the model config like ActionModel, with the ValueDeclarations
+
+    HintedValueDeclaration = build_hinted_value_declaration(
+        strict=strict, excluded_declaration_types=[LinkDeclaration, LambdaDeclaration]
+    )
+
+    fields = templatify_model(
+        ModelConfig,
+        vars_=None,
+        links=None,
+        add_union=HintedValueDeclaration,  # type: ignore
+        strict=strict,
+    )
+
+    return pydantic.create_model(
+        "ModelConfigDeclaration",
+        __base__=ModelConfig,
+        __module__=__name__,
+        # model_config=ConfigDict(
+        #     arbitrary_types_allowed=True,
+        # ),
+        **fields,  # pyright: ignore[reportGeneralTypeIssues]
+    )
+
+
+ModelConfigDeclaration = build_model_config()
+
+
+class ActionConfig(StrictModel):
+    default_model: ModelConfigDeclaration  # type: ignore
+    action_timeout: float = 360
+    flow: "FlowConfig"
+    default_output: ContextVarPath  # TODO `| ValueDeclaration`
+
+
+Executable = Union[ActionInvocation, Loop]
+FlowConfig = dict[ExecutableId, Executable]
+
+
+def build_hinted_action_config(
+    action_names: list[str] | None = None,
+    vars_: HintLiteral | None = None,
+    links: HintLiteral | None = None,
+    strict: bool = False,
+):
+    HintedValueDeclaration = build_hinted_value_declaration(
+        # vars_=vars_,
+        links=links,
+        strict=strict,
+    )
+
+    ActionInvocationUnion = Union[
+        tuple(
+            build_actions(
+                action_names=action_names,
+                vars_=vars_,
+                links=links,
+                strict=strict,
+            )
+        )  # pyright: ignore
+    ]
+
+    class HintedLoop(Loop):
+        in_: HintedValueDeclaration = Field(  # type: ignore
+            ...,
+            alias="in",
+        )
+        flow: "HintedFlowConfig"  # type: ignore
+
+    class HintedActionConfig(ActionConfig):
+        flow: "HintedFlowConfig"  # type: ignore
+
+    HintedExecutable = Union[ActionInvocationUnion, HintedLoop]
+    HintedFlowConfig = dict[ExecutableId, HintedExecutable]
+
+    HintedActionConfig.model_rebuild()  # TODO is this necessary?
+
+    return HintedActionConfig
---
+++ packages/asyncflows/asyncflows-0.1.8.tar.gz/asyncflows-0.1.8/asyncflows/utils/type_utils.py
@@ -0,0 +1,90 @@
+import typing
+from typing import Any, Literal, Union, Annotated
+
+import pydantic
+from pydantic import Field
+
+from asyncflows.models.primitives import HintType, HintLiteral
+
+
+def _get_recursive_subfields(
+    obj: dict | pydantic.BaseModel | Any, prefix: str = ""
+) -> list[type[str]]:
+    out = []
+    if isinstance(obj, dict):
+        for name, field in obj.items():
+            # out.append(name)
+            out.extend(_get_recursive_subfields(field, prefix=f"{name}."))
+    elif isinstance(obj, type) and issubclass(obj, pydantic.BaseModel):
+        for name, field in obj.model_fields.items():
+            field_annotation = Field(...)
+            if field.description:
+                field_annotation.description = field.description
+            out.append(
+                Annotated[
+                    Literal[f"{prefix}{name}"],  # type: ignore
+                    field_annotation,
+                ]
+            )
+            out.extend(_get_recursive_subfields(field.annotation, prefix=f"{name}."))
+    return out
+
+
+def get_path_literal(
+    vars_: HintType,
+    strict: bool,
+) -> type[str]:
+    union_elements = []
+
+    if not strict:
+        union_elements.append(str)
+
+    # if there are any strings, then that's the name of the var
+    string_vars = [var for var in vars_ if isinstance(var, str)]
+    if string_vars:
+        union_elements.append(Literal[tuple(string_vars)])  # type: ignore
+
+    # if there are any models, then each recursive subfield is a var, like jsonpath
+    model_vars = [var for var in vars_ if isinstance(var, (pydantic.BaseModel, dict))]
+    for model_var in model_vars:
+        subfields = _get_recursive_subfields(model_var)
+        if subfields:
+            union_elements.extend(subfields)
+
+    return Union[tuple(union_elements)]  # type: ignore
+
+
+def get_var_string(
+    hint_literal: HintLiteral | None,
+    strict: bool,
+) -> str:
+    if hint_literal is None:
+        var_str = ""
+    else:
+        # hint_literal is a nested annotated literal union, pull out only the strings
+        strings = []
+        frontier = [hint_literal]
+        while frontier:
+            arg = frontier.pop()
+            if isinstance(arg, str):
+                strings.append(arg)
+            elif hasattr(arg, "__args__"):
+                frontier.extend(arg.__args__)  # type: ignore
+
+        var_str = "".join(strings)
+    if strict:
+        var_str = var_str + "__strict"
+    return var_str
+
+
+def filter_none_from_type(
+    type_: type[Any],
+) -> type[Any]:
+    type_args = typing.get_args(type_)
+    if type(None) not in type_args:
+        raise ValueError(f"Type {type_} does not contain None")
+    new_type_args = tuple(arg for arg in type_args if arg is not type(None))
+    if len(type_args) == 1:
+        return type_args[0]
+    else:
+        return Union[new_type_args]  # type: ignore
