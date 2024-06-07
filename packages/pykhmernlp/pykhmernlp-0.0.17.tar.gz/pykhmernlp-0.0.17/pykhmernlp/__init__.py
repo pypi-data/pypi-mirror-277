719814740054f56353b9b897cad54ab969a94709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 15:05:25 +0000

    Add package pykhmernlp/0.0.17/pykhmernlp-0.0.17.tar.gz

---
+++ packages/pykhmernlp/pykhmernlp-0.0.17.tar.gz/pykhmernlp-0.0.17/pykhmernlp/__init__.py
@@ -0,0 +1,6 @@
+import pykhmernlp.address as address
+import pykhmernlp.corpus as corpus
+import pykhmernlp.pronounce as pronounce
+import pykhmernlp.tokenizer as tokenizer
+import pykhmernlp.tha as tha 
+import pykhmernlp.number as number
