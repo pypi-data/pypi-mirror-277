33413a37409246d597241b9df32db6dd6ee168d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:05:50 +0000

    Add package movel/0.1.2/movel-0.1.2.tar.gz

---
+++ packages/movel/movel-0.1.2.tar.gz/movel-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# with open('README.md') as f:
+#     long_description = f.read()
+
+setup(
+    # author='Kaitlyn Chen',
+    # author_email='kaichen@g.hmc.edu',
+    # description='calculating moment of velocity',
+    # long_description=long_description,
+    # long_description_content_type='text/markdown', 
+    # name='moment-of-velocity',
+    # url='https://github.com/kaitlynchen1/moment-of-velocity',
+    version='0.1.2',
+)
