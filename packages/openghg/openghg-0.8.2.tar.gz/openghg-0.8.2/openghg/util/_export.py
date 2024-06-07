56e5d29c25e23d5230e1a0c94adab7adb1b65621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:07:39 +0000

    Add package openghg/0.8.2/openghg-0.8.2.tar.gz

---
+++ packages/openghg/openghg-0.8.2.tar.gz/openghg-0.8.2/openghg/util/_export.py
@@ -0,0 +1,136 @@
+from __future__ import annotations
+from json import dump, loads
+from pathlib import Path
+from typing import Dict, List, Union, Optional, TYPE_CHECKING
+from addict import Dict as aDict
+
+if TYPE_CHECKING:
+    from openghg.dataobjects import ObsData
+
+__all__ = ["to_dashboard", "to_dashboard_mobile"]
+
+
+def to_dashboard(
+    data: Union[ObsData, List[ObsData]],
+    selected_vars: List,
+    downsample_n: int = 3,
+    filename: Optional[str] = None,
+) -> Union[Dict, None]:
+    """Takes a Dataset produced by OpenGHG and outputs it into a JSON
+    format readable by the OpenGHG dashboard or a related project.
+
+    This also exports a separate file with the locations of the sites
+    for use with map selector component.
+
+    Note - this function does not currently support export of data from multiple
+    inlets.
+
+    Args:
+        data: Dictionary of retrieved data
+        selected_vars: The variables to want to export
+        downsample_n: Take every nth value from the data
+        filename: filename to write output to
+    Returns:
+        None
+    """
+    to_export = aDict()
+
+    if not isinstance(selected_vars, list):
+        selected_vars = [selected_vars]
+
+    selected_vars = [str(c).lower() for c in selected_vars]
+
+    if not isinstance(data, list):
+        data = [data]
+
+    for obs in data:
+        measurement_data = obs.data
+        attributes = measurement_data.attrs
+        metadata = obs.metadata
+
+        df = measurement_data.to_dataframe()
+
+        rename_lower = {c: str(c).lower() for c in df.columns}
+        df = df.rename(columns=rename_lower)
+        # We just want the selected variables
+        to_extract = [c for c in df.columns if c in selected_vars]
+
+        if not to_extract:
+            continue
+
+        df = df[to_extract]
+
+        # Downsample the data
+        if downsample_n > 1:
+            df = df.iloc[::downsample_n]
+
+        network = metadata["network"]
+        instrument = metadata["instrument"]
+
+        try:
+            latitude = attributes["latitude"]
+        except KeyError:
+            latitude = metadata["latitude"]
+
+        try:
+            longitude = attributes["longitude"]
+        except KeyError:
+            longitude = metadata["longitude"]
+
+        # TODO - remove this if we add site location to standard metadata
+        location = {
+            "latitude": latitude,
+            "longitude": longitude,
+        }
+        metadata.update(location)
+
+        json_data = loads(df.to_json())
+
+        species = metadata["species"]
+        site = metadata["site"]
+        inlet = metadata["inlet"]
+
+        to_export[species][network][site][inlet][instrument] = {
+            "data": json_data,
+            "metadata": metadata,
+        }
+
+    if filename is not None:
+        with open(filename, "w") as f:
+            dump(obj=to_export, fp=f)
+        return None
+    else:
+        # TODO - remove this once addict is stubbed
+        export_dict: Dict = to_export.to_dict()
+        return export_dict
+
+
+def to_dashboard_mobile(data: Dict, filename: Union[str, Path, None] = None) -> Union[Dict, None]:
+    """Export the Glasgow LICOR data to JSON for the dashboard
+
+    Args:
+        data: Data dictionary
+        filename: Filename for export of JSON
+    Returns:
+        dict or None: Dictonary if no filename given
+    """
+    to_export = aDict()
+
+    for species, species_data in data.items():
+        spec_data = species_data["data"]
+        metadata = species_data["metadata"]
+
+        latitude = spec_data["latitude"].values.tolist()
+        longitude = spec_data["longitude"].values.tolist()
+        ch4 = spec_data["ch4"].values.tolist()
+
+        to_export[species]["data"] = {"lat": latitude, "lon": longitude, "z": ch4}
+        to_export[species]["metadata"] = metadata
+
+    if filename is not None:
+        with open(filename, "w") as f:
+            dump(to_export, f)
+        return None
+    else:
+        to_return: Dict = to_export.to_dict()
+        return to_return
---
+++ packages/openghg/openghg-0.8.2.tar.gz/openghg-0.8.2/openghg/util/_strings.py
@@ -0,0 +1,134 @@
+from typing import Any, Dict, List, Optional, Set, Tuple, Union, overload
+
+__all__ = ["clean_string", "to_lowercase"]
+
+
+@overload
+def clean_string(to_clean: str) -> str: ...
+
+
+@overload
+def clean_string(to_clean: None) -> None: ...
+
+
+def clean_string(to_clean: Optional[str]) -> Union[str, None]:
+    """Returns a lowercase string with only alphanumeric
+    characters and underscores.
+
+    Args:
+        to_clean: String to clean
+    Returns:
+        str or None: Clean string
+    """
+    import re
+
+    if to_clean is None:
+        return None
+
+    if isinstance(to_clean, bool):
+        return str(to_clean).lower()
+
+    try:
+        # This might be used with numbers
+        if is_number(to_clean):
+            return str(to_clean)
+
+        # Removes all whitespace
+        cleaner = re.sub(r"\s+", "", to_clean, flags=re.UNICODE).lower()
+        # Removes non-alphanumeric characters but keep underscores
+        # cleanest = re.sub(r"\W+", "", cleaner)
+        cleanest = re.sub(r"[^\w-]+", "", cleaner)
+    except TypeError:
+        return to_clean
+
+    return cleanest
+
+
+@overload
+def to_lowercase(d: Dict, skip_keys: Optional[List] = None) -> Dict: ...
+
+
+@overload
+def to_lowercase(d: List, skip_keys: Optional[List] = None) -> List: ...
+
+
+@overload
+def to_lowercase(d: Tuple, skip_keys: Optional[List] = None) -> Tuple: ...
+
+
+@overload
+def to_lowercase(d: Set, skip_keys: Optional[List] = None) -> Set: ...
+
+
+@overload
+def to_lowercase(d: str, skip_keys: Optional[List] = None) -> str: ...
+
+
+def to_lowercase(
+    d: Union[Dict, List, Tuple, Set, str], skip_keys: Optional[List] = None
+) -> Union[Dict, List, Tuple, Set, str]:
+    """Convert an object to lowercase. All keys and values in a dictionary will be converted
+    to lowercase as will all objects in a list, tuple or set. You can optionally pass in a list of keys to
+    skip when lowercasing a dictionary.
+
+    Based on the answer https://stackoverflow.com/a/40789531/1303032
+
+    Args:
+        d: Object to lower case
+        skip_keys: List of keys to skip when lowercasing.
+    Returns:
+        dict: Dictionary of lower case keys and values
+    """
+    if skip_keys is None:
+        skip_keys = []
+
+    if isinstance(d, dict):
+        lowercased = {k.lower(): to_lowercase(v) for k, v in d.items() if k not in skip_keys}
+
+        if skip_keys:
+            missing = {k: v for k, v in d.items() if k not in lowercased}
+            lowercased.update(missing)
+
+        return lowercased
+    elif isinstance(d, (list, set, tuple)):
+        t = type(d)
+        return t(to_lowercase(o) for o in d)
+    elif isinstance(d, str):
+        return d.lower()
+    else:
+        return d
+
+
+def is_number(s: Any) -> bool:
+    """Is it a number?
+
+    https://stackoverflow.com/q/354038
+
+    Args:
+        s: String which may be a number
+    Returns:
+        bool
+    """
+    if isinstance(s, bool):
+        return False
+
+    try:
+        float(s)
+        return True
+    except (ValueError, TypeError):
+        return False
+
+
+def remove_punctuation(s: str) -> str:
+    """Removes punctuation and converts the passed string
+    to lowercase
+
+    Args:
+        s: String to convert
+    Returns:
+        str: Unpunctuated, lowercased string
+    """
+    import re
+
+    s = s.lower()
+    return re.sub(r"[^\w\s]", "", s)
