8aafb5c76de92af44506028aba46e45a309906f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 16:06:10 +0000

    Add package AutoLMS/0.4.3/autolms-0.4.3.tar.gz

---
+++ packages/AutoLMS/autolms-0.4.3.tar.gz/autolms-0.4.3/README.md
@@ -0,0 +1,75 @@
+# AutoLMS
+
+[![PyPI version shields.io](https://img.shields.io/pypi/v/AutoLMS.svg)](https://pypi.python.org/pypi/AutoLMS/)
+[![PyPI license](https://img.shields.io/pypi/l/AutoLMS.svg?color=blue)](https://pypi.python.org/pypi/AutoLMS/)
+[![PyPI download month](https://img.shields.io/pypi/dm/AutoLMS.svg)](https://pypi.python.org/pypi/AutoLMS/)
+[![GitHub stars](https://img.shields.io/github/stars/itsamirhn/AutoLMS.svg?style=social&label=Star&maxAge=2592000)](https://GitHub.com/itsamirhn/AutoLMS/stargazers/)
+
+AutoLMS is a tool for automating LMS and get into class automatically on specified times.
+
+### Supported Universities
+
+- University of Tehran
+- Kharazmi University
+- Shahid Beheshti University
+- Iran University of Science and Technology
+
+if your university is not listed, feel free to contribute or make an issue.
+
+## Install
+
+Use the pip package manager to install AutoLMS:
+
+```sh
+$ pip install autolms
+```
+
+## Requirements
+
+You have to install these manually:
+
+- python3
+- Chrome
+- [ChromeDriver](https://chromedriver.chromium.org/downloads)
+
+### How to download ChromeDriver?
+
+If you are new to ChromeDriver, follow instructions below:
+
+1) Find out your Chrome app version by going to `chrome://version` address with Chrome
+2) Go to this [Address](https://chromedriver.chromium.org/downloads) and Choose your Chrome corresponding version in the
+   list
+    1) For example if you are using Chrome version 98.x.x then choose
+       `ChromeDriver 98.x.x`
+    2) Be careful, if you got error on run, it may because of difference between Chrome and ChromeDriver versions
+3) Select the file compatible with your system (Mac or Linux or Win)
+4) Unzip the downloaded file, **You don't need to open or run it**
+5) Put the `chromedriver` anywhere you want
+6) In the configuration of AutoLMS, **program will try to find your `chromdriver` automatically**, If it doesn't found
+   or the path is wrong, type `chromedriver` path manually or just drag & drop the file into terminal
+
+## Configuration
+
+You should configure AutoLMS with this command for first time usage:
+
+```sh
+$ autolms setup
+```
+
+And anytime you wanted to edit configs (e.g. add new session or course):
+
+```sh
+$ autolms edit
+```
+
+## Run
+
+Finally, run the program for always:
+
+```sh
+$ autolms run
+```
+
+## License
+
+AutoLMS is MIT licensed.
---
+++ packages/AutoLMS/autolms-0.4.3.tar.gz/autolms-0.4.3/src/autolms/main.py
@@ -0,0 +1,88 @@
+import datetime
+import time
+
+import fire
+import schedule
+
+import autolms.config as config
+from autolms.core import LMSDriver
+
+events = []
+
+
+def check(chromedriver, username, password, url, course_id=None):
+    driver = LMSDriver(chromedriver, username, password, url)
+    if course_id:
+        driver.go_to_course_last_event(course_id)
+    else:
+        driver.go_to_last_event()
+
+
+def go(course_id=None, course_name=None):
+    cfg = config.get_config()
+    if not cfg:
+        print("Run `setup` command for config first.")
+        return
+    if not course_name:
+        course_name = course_id
+    chromedriver = cfg["paths"]["chromedriver"]
+    username = cfg["credentials"]["username"]
+    password = cfg["credentials"]["password"]
+    url = cfg["credentials"]["url"]
+    try:
+        print(f"Starting {course_name}...")
+        check(chromedriver, username, password, url, course_id)
+    except Exception as e:
+        print(e)
+    finally:
+        events.append({"status": "finish", "name": course_name, "id": course_id})
+        print("Finish")
+
+
+def setup():
+    if config.setup():
+        print("You can use `run` command now.")
+
+
+def edit():
+    if config.edit():
+        print("You can use `run` command now.")
+
+
+def run(alert=False):
+    cfg = config.get_config()
+    if not cfg:
+        print("Run `setup` command for config first.")
+        return
+    for course in cfg["courses"]:
+        for session in course["sessions"]:
+            session_time = datetime.datetime.strptime(session["time"], "%H:%M") - datetime.timedelta(
+                seconds=int(cfg["options"]["rush"])
+            )
+            rushed_time = session_time.strftime("%H:%M:%S")
+            getattr(schedule.every(), session["day"]).at(rushed_time).do(
+                go, course_id=course["id"], course_name=course["name"]
+            )
+            print("Add job for %s on %s at %s" % (course["name"], session["day"], rushed_time))
+    print("Running schedule...")
+    while True:
+        schedule.run_pending()
+        if alert:
+            while len(events) > 0:
+                yield events.pop(0)
+        time.sleep(1)
+
+
+def main():
+    fire.Fire(
+        {
+            "go": go,
+            "run": run,
+            "setup": setup,
+            "edit": edit,
+        }
+    )
+
+
+if __name__ == "__main__":
+    main()
