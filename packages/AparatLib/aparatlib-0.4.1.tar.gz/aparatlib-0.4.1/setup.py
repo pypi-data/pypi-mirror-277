14bf1ecbfdef83efd48141b87290dba17f04268f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:13 +0000

    Add package AparatLib/0.4.1/aparatlib-0.4.1.tar.gz

---
+++ packages/AparatLib/aparatlib-0.4.1.tar.gz/aparatlib-0.4.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AparatLib',
+    version='0.4.1',
+    packages=find_packages(),
+    author='Abol',
+    author_email='abaqry8686@gmail.com',
+    description='A library for interacting with the Aparat API',
+    long_description=open('C:\\Users\\Bagheri\\Desktop\\projects\\python\\Github projects\\aparat\\README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AbolDev/AparatLib',
+    install_requires=[
+        'requests',
+        'python-magic',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    keywords = [
+        'python', 'library', 'aparat', 'aparat-api', 'aparat-downloader',
+        'aparat-playlist-downloader', 'aparat-uploader', 'aparat-upload', 'aparat-dl',
+        'aparat-news', 'aparat-lib', 'aparat-python',
+    ],
+)
