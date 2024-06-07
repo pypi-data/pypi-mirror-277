4ca7e928d2360f7aa16b8b8b41b85f64ba6d9207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 06:05:03 +0000

    Add package tensortrax/0.25.1/tensortrax-0.25.1-py3-none-any.whl

---
+++ packages/tensortrax/tensortrax-0.25.1-py3-none-any.whl/tensortrax/math/special/__init__.py
@@ -0,0 +1,29 @@
+"""
+tensorTRAX: Math on (Hyper-Dual) Tensors with Trailing Axes.
+"""
+
+from ._special_tensor import (
+    ddot,
+    dev,
+    erf,
+    from_triu_1d,
+    from_triu_2d,
+    sym,
+    tresca,
+    triu_1d,
+    try_stack,
+    von_mises,
+)
+
+__all__ = [
+    "ddot",
+    "dev",
+    "erf",
+    "from_triu_1d",
+    "from_triu_2d",
+    "sym",
+    "tresca",
+    "triu_1d",
+    "try_stack",
+    "von_mises",
+]
