07b5ca0c9c2383f00f85ce07c8b074f663db3df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 06:04:50 +0000

    Add package astro-ssptools/1.0.0/astro_ssptools-1.0.0-py3-none-any.whl

---
+++ packages/astro-ssptools/astro_ssptools-1.0.0-py3-none-any.whl/ssptools/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for SSPTools."""
+
+__author__ = """Eduardo Balbinot"""
+__email__ = "eduardo.balbinot@gmail.com"
+__version__ = "0.2.0"
+
+from .evolve_mf import *
