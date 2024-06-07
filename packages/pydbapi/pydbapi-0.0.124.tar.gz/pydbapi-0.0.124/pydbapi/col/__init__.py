5da1767aa418f6564ff207ba0eed8a305e36c495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 15:04:44 +0000

    Add package pydbapi/0.0.124/pydbapi-0.0.124.tar.gz

---
+++ packages/pydbapi/pydbapi-0.0.124.tar.gz/pydbapi-0.0.124/pydbapi/col/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: longfengpili
+# @Date:   2023-06-02 15:27:41
+# @Last Modified by:   longfengpili
+# @Last Modified time: 2023-07-27 15:33:34
+# @github: https://github.com/longfengpili
+
+
+from .colmodel import ColumnModel, ColumnsModel
+
+__all__ = ['ColumnModel', 'ColumnsModel']
