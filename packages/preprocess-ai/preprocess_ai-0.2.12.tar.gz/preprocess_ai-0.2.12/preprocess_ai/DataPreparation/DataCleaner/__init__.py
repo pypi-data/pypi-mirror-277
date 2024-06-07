1d1a6f8a4b4bbd528ad1ad29ee95134d5313eb8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 06:04:55 +0000

    Add package preprocess-ai/0.2.12/preprocess_ai-0.2.12.tar.gz

---
+++ packages/preprocess-ai/preprocess_ai-0.2.12.tar.gz/preprocess_ai-0.2.12/preprocess_ai/DataPreparation/DataCleaner/__init__.py
@@ -0,0 +1,6 @@
+from .dataCleaner import DataCleaner
+from .duplicationHandler import DuplicationHandler
+from .outlierHandler import OutlierHandler
+from .dataTransformer import DataTransformer
+
+__all__ = ['DataCleaner', 'DuplicationHandler', 'OutlierHandler','DataTransformer']
---
+++ packages/preprocess-ai/preprocess_ai-0.2.12.tar.gz/preprocess_ai-0.2.12/preprocess_ai/DataPreparation/DataTransformation/__init__.py
@@ -0,0 +1,5 @@
+from .categoricalEncoder import CategoricalEncoder
+from .featureEngineer import FeatureEngineer
+from .scaler import Scaler
+
+__all__ = ['CategoricalEncoder', 'FeatureEngineer', 'Scaler']
