23e642930e695dc41a198378d1bf7f56eda7bbcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 03:05:02 +0000

    Add package pybloxlink/1.0.0/pybloxlink-1.0.0.tar.gz

---
+++ packages/pybloxlink/pybloxlink-1.0.0.tar.gz/pybloxlink-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+import re
+
+version = ''
+with open('./bloxlink/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+setup(version=str(version))
