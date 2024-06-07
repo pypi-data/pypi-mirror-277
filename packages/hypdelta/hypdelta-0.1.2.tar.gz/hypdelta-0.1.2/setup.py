788d32b30b688675a2dab4589748530565f42319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 03:05:10 +0000

    Add package hypdelta/0.1.2/hypdelta-0.1.2.tar.gz

---
+++ packages/hypdelta/hypdelta-0.1.2.tar.gz/hypdelta-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="hypdelta",
+    version="0.1.2",
+    author="Tanya Matveeva",
+    author_email="tpmatveeva@hse.ru",
+    description="A Python library for calculating delta hyperbolicity.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tnmtvv/hypdelta",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy>=1.24.4",
+        "numba>=0.57.1",
+        "scipy>=1.4.0",
+    ],
+)
