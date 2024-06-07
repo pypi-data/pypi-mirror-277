04f57c3fc268e37879809f83ff85586b8582d6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 16:06:16 +0000

    Add package alg-library/0.3/alg_library-0.3.tar.gz

---
+++ packages/alg-library/alg_library-0.3.tar.gz/alg_library-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='alg_library',
+      version='0.3',
+      description='alg_library',
+      packages=['alg_library'],
+      author_email='ilapronin0700@gmail.com',
+      zip_safe=False)
