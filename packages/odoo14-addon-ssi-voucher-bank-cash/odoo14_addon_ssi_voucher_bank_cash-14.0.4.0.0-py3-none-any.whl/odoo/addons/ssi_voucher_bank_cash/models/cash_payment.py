98e102de022c4d51e143a57aabd3ae957f4b2704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 03:05:46 +0000

    Add package odoo14-addon-ssi-voucher-bank-cash/14.0.4.0.0/odoo14_addon_ssi_voucher_bank_cash-14.0.4.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-voucher-bank-cash/odoo14_addon_ssi_voucher_bank_cash-14.0.4.0.0-py3-none-any.whl/odoo/addons/ssi_voucher_bank_cash/models/cash_payment.py
@@ -0,0 +1,155 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class CashPayment(models.Model):
+    _name = "account.cash_payment"
+    _inherit = "mixin.bank_voucher"
+    _description = "Cash Payment"
+
+    @api.model
+    def _default_type_id(self):
+        return self.env.ref("ssi_voucher_bank_cash.voucher_type_cash_payment").id
+
+    type_id = fields.Many2one(
+        default=lambda self: self._default_type_id(),
+    )
+    company_currency_id = fields.Many2one(
+        string="Company Currency",
+        comodel_name="res.currency",
+        related="company_id.currency_id",
+        store=True,
+    )
+
+    line_ids = fields.One2many(
+        comodel_name="account.cash_payment_line",
+    )
+    line_summary_ids = fields.One2many(
+        comodel_name="account.cash_payment_line_summary",
+    )
+    line_dr_ids = fields.One2many(
+        comodel_name="account.cash_payment_line",
+    )
+    line_cr_ids = fields.One2many(
+        comodel_name="account.cash_payment_line",
+    )
+
+
+class CashPaymentLine(models.Model):
+    _name = "account.cash_payment_line"
+    _inherit = "mixin.account.voucher.line"
+    _description = "Cash Payment Line"
+
+    voucher_id = fields.Many2one(
+        comodel_name="account.cash_payment",
+    )
+    tax_ids = fields.One2many(
+        comodel_name="account.cash_payment_line_tax",
+    )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_id.currency_id",
+        store=True,
+    )
+    company_currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_id.company_currency_id",
+        store=True,
+    )
+
+
+class CashPaymentLineTax(models.Model):
+    _name = "account.cash_payment_line_tax"
+    _inherit = "mixin.account.voucher.line.tax"
+    _description = "Cash Payment Line Tax"
+
+    voucher_line_id = fields.Many2one(
+        comodel_name="account.cash_payment_line",
+    )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_line_id.currency_id",
+        store=True,
+    )
+    company_currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_line_id.company_currency_id",
+        store=True,
+    )
+
+
+class CashPaymentLineSummary(models.Model):
+    _name = "account.cash_payment_line_summary"
+    _inherit = "mixin.account.voucher.line.summary"
+    _description = "Cash Payment Line Summary"
+    _auto = False
+
+    voucher_id = fields.Many2one(
+        comodel_name="account.cash_payment",
+    )
+
+    def _select(self):
+        select_str = """
+        SELECT
+            row_number() OVER() as id,
+            a.voucher_id AS voucher_id,
+            a.account_id AS account_id,
+            a.partner_id AS partner_id,
+            a.currency_id AS currency_id,
+            SUM(a.amount_before_tax) AS amount_before_tax,
+            SUM(a.amount_tax) AS amount_tax,
+            SUM(a.amount_after_tax) AS amount_after_tax
+        """
+        return select_str
+
+    def _get_from_table(self):
+        return "account_cash_payment_line"
+
+    def _from(self):
+        from_str = """
+        %s AS a
+        """ % (
+            self._get_from_table()
+        )
+        return from_str
+
+    def _where(self):
+        where_str = """
+        WHERE 1 = 1
+        """
+        return where_str
+
+    def _join(self):
+        join_str = """
+        """
+        return join_str
+
+    def _group_by(self):
+        group_str = """
+        GROUP BY a.voucher_id,a.account_id,a.partner_id,a.currency_id
+        """
+        return group_str
+
+    def init(self):
+        # tools.drop_view_if_exists(self._cr, self._table)
+        # pylint: disable=locally-disabled, sql-injection
+        self._cr.execute(
+            """CREATE or REPLACE VIEW %s as (
+            %s
+            FROM %s
+            %s
+            %s
+            %s
+        )"""
+            % (
+                self._table,
+                self._select(),
+                self._from(),
+                self._join(),
+                self._where(),
+                self._group_by(),
+            )
+        )
