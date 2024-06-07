a2f9d54328a996a38062a8586a6d9461ef5bbbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:47 +0000

    Add package edgeiq/2.4.0/edgeiq-2.4.0-cp310-none-macosx_11_0_arm64.whl

---
+++ packages/edgeiq/edgeiq-2.4.0-cp310-none-macosx_11_0_arm64.whl/edgeiq/_production_client/credentials.pyi
@@ -0,0 +1,13 @@
+from _typeshed import Incomplete
+
+def get_shortened_systemId(systemId): ...
+
+CREDENTIAL_ENDPOINT: Incomplete
+ENDPOINT: Incomplete
+CLIENT_ID: Incomplete
+CERTIFICATE_FOLDER: str
+CERTIFICATES: Incomplete
+CREDENTIALS_EXIST: bool
+PATH_TO_CERT: Incomplete
+PATH_TO_KEY: Incomplete
+PATH_TO_ROOT: Incomplete
---
+++ packages/edgeiq/edgeiq-2.4.0-cp310-none-macosx_11_0_arm64.whl/edgeiq/pyarmor_runtime_006224/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.0 (group), 006224, 2024-06-05T16:09:40.471513
+from .pyarmor_runtime import __pyarmor__
