b6710a6626542d26135228da6fdb18d1b92d07d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:05:04 +0000

    Add package opla-ssg/0.1.2/opla_ssg-0.1.2-py3-none-any.whl

---
+++ packages/opla-ssg/opla_ssg-0.1.2-py3-none-any.whl/opla/templating.py
@@ -0,0 +1,34 @@
+"""Provide a function to get the jinja template."""
+
+from pathlib import Path
+
+import jinja2
+
+
+TEMPLATES_PATH = Path(__file__).parent / "themes"
+
+
+def get_template(header: dict) -> jinja2.Template:
+    """
+    Get the appropriate template according to the theme name
+
+    Args:
+        header: the header of the markdown file
+
+    Returns:
+        jinja2.Template: the requested template
+    """
+
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATES_PATH))
+
+    theme = header["theme"]["name"]
+    if theme in ("rawhtml", "water"):
+        template = env.get_template("base/templates/base.j2.html")
+    elif theme == "materialize":
+        template = env.get_template("materialize/templates/index.j2.html")
+        # Add default color to theme
+        color = header["theme"].get("color", "teal")
+        header["theme"]["color"] = color
+    else:
+        raise ValueError(f"Unknown theme name: '{theme}'")
+    return template
