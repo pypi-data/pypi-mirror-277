8deec5478ffed6f61dd1f2bb9004a2b1f75d91a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:07:47 +0000

    Add package UncountablePythonSDK/0.0.28/uncountablepythonsdk-0.0.28.tar.gz

---
+++ packages/UncountablePythonSDK/uncountablepythonsdk-0.0.28.tar.gz/uncountablepythonsdk-0.0.28/uncountable/integration/job.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from uncountable.core.client import Client
+from uncountable.integration.types import JobDefinition
+
+from abc import ABC, abstractmethod
+
+
+@dataclass
+class JobArgumentsBase:
+    job_definition: JobDefinition
+    client: Client
+
+
+@dataclass
+class CronJobArguments(JobArgumentsBase):
+    # can imagine passing additional data such as in the sftp or webhook cases
+    pass
+
+
+JobArguments = CronJobArguments
+
+
+@dataclass
+class JobResult:
+    success: bool
+
+
+class Job(ABC):
+    _unc_job_registered: bool = False
+
+    @abstractmethod
+    def run(self, args: JobArguments) -> JobResult:
+        ...
+
+
+class CronJob(Job):
+
+    @abstractmethod
+    def run(self, args: CronJobArguments) -> JobResult:
+        ...
+
+
+def register_job(cls: Job):
+    cls._unc_job_registered = True
+    return cls
