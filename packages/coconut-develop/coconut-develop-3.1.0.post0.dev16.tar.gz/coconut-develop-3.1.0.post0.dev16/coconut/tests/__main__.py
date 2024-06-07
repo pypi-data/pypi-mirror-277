7fa1912bb37e4668e7051ada01bd14dfa73af3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 15:05:24 +0000

    Add package coconut-develop/3.1.0.post0.dev16/coconut-develop-3.1.0.post0.dev16.tar.gz

---
+++ packages/coconut-develop/coconut-develop-3.1.0.post0.dev16.tar.gz/coconut-develop-3.1.0.post0.dev16/coconut/tests/__main__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------------------------------------------------------------------
+# INFO:
+# -----------------------------------------------------------------------------------------------------------------------
+
+"""
+Author: Evan Hubinger
+License: Apache 2.0
+Description: Compile Coconut test source.
+"""
+
+# -----------------------------------------------------------------------------------------------------------------------
+# IMPORTS:
+# -----------------------------------------------------------------------------------------------------------------------
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+from coconut.root import *  # NOQA
+
+import sys
+
+from coconut.constants import WINDOWS
+from coconut.tests.main_test import comp_all
+
+# -----------------------------------------------------------------------------------------------------------------------
+# MAIN:
+# -----------------------------------------------------------------------------------------------------------------------
+
+
+def main(args=None):
+    """Compile everything with given arguments."""
+    # process args
+    if args is None:
+        args = sys.argv[1:]
+    try:
+        target_ind = args.index("--target")
+    except ValueError:
+        agnostic_target = None
+    else:
+        agnostic_target = args[target_ind + 1]
+        args = args[:target_ind] + args[target_ind + 2:]
+
+    # compile everything
+    print("Compiling Coconut test suite with args %r and agnostic_target=%r." % (args, agnostic_target))
+    comp_all(
+        args,
+        agnostic_target=agnostic_target,
+        expect_retcode=0 if "--mypy" not in args else None,
+        check_errors="--verbose" not in args,
+        ignore_output=WINDOWS and "--mypy" not in args,
+    )
+
+
+if __name__ == "__main__":
+    main()
