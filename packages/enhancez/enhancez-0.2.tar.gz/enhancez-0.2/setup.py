954bd21fffa0ace7b619d825c221b9264a86d772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 14:05:16 +0000

    Add package enhancez/0.2/enhancez-0.2.tar.gz

---
+++ packages/enhancez/enhancez-0.2.tar.gz/enhancez-0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='enhancez',
+    version='0.2',
+    packages=find_packages(),
+    author='Lalith Kumar Shiyam Sundar ',
+    author_email='lalith.shiyamsundar@meduniwien.ac.at',
+    url='',  # Add your package's homepage URL here
+    description='',  # Add a short description of the package here
+    # long_description=open('README.md').read(),
+    # long_description_content_type='text/markdown',
+    install_requires=[
+        'streamlit',
+        'moosez',
+        'falconz',
+        'pumaz',
+        'halo',
+        'altair',
+        'd3blocks',
+        'SimpleITK',
+        'psutil',
+        'streamlit_lottie',
+        'extra_streamlit_components',
+        'streamlit_image_comparison'
+    ],  # List your package's dependencies here
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.10',
+    entry_points={
+        'console_scripts': [
+            'enhancez=enhancez.run_streamlit:run',
+        ],
+    },
+)
