b659e4d1ded9e5545660956b057277fb363cd418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 06:04:39 +0000

    Add package allianceauth-afat/3.0.2/allianceauth_afat-3.0.2-py3-none-any.whl

---
+++ packages/allianceauth-afat/allianceauth_afat-3.0.2-py3-none-any.whl/afat/helper/fatlinks.py
@@ -0,0 +1,44 @@
+"""
+Helper functions for fat links view
+"""
+
+# Standard Library
+from typing import Dict, List, Union
+
+# Alliance Auth
+from allianceauth.authentication.admin import User
+
+# Alliance Auth AFAT
+from afat.models import FatLink
+
+
+def get_esi_fleet_information_by_user(
+    user: User,
+) -> Dict[str, Union[bool, List[Dict[int, FatLink]]]]:
+    """
+    Get ESI fleet information by a given FC (user)
+
+    :param user:
+    :type user:
+    :return:
+    :rtype:
+    """
+
+    has_open_esi_fleets = False
+    open_esi_fleets_list = []
+    open_esi_fleets = (
+        FatLink.objects.select_related_default()
+        .filter(creator=user, is_esilink=True, is_registered_on_esi=True)
+        .order_by("character__character_name")
+    )
+
+    if open_esi_fleets.count() > 0:
+        has_open_esi_fleets = True
+
+        for open_esi_fleet in open_esi_fleets:
+            open_esi_fleets_list.append(open_esi_fleet)
+
+    return {
+        "has_open_esi_fleets": has_open_esi_fleets,
+        "open_esi_fleets_list": open_esi_fleets_list,
+    }
