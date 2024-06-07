3d958ffb5ec6daa3df76c8ac61e50ccf1f3e054e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 15:05:05 +0000

    Add package tfy-mlflow-client/0.0.46/tfy-mlflow-client-0.0.46.tar.gz

---
+++ packages/tfy-mlflow-client/tfy-mlflow-client-0.0.46.tar.gz/tfy-mlflow-client-0.0.46/mlflow/entities/mlfoundry_artifacts/custom_metric.py
@@ -0,0 +1,27 @@
+from mlflow.entities.mlfoundry_artifacts import utils
+from mlflow.entities.mlfoundry_artifacts.enums import (
+    CustomMetricType,
+    CustomMetricValueType,
+)
+from mlflow.pydantic_v1 import BaseModel
+
+
+class CustomMetric(BaseModel):
+    class Config:
+        orm_mode = True
+        validate_assignment = True
+        use_enum_values = True
+        allow_mutation = False
+        smart_union = True
+        extra = "allow"
+
+    name: str
+    value_type: CustomMetricValueType
+    type: CustomMetricType
+
+    @classmethod
+    def from_proto(cls, message):
+        return cls.parse_obj(utils.dict_from_proto(message))
+
+    def to_proto(self):
+        return utils.dict_to_proto(self.dict())
