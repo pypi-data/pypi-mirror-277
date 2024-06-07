4ff4ccdef933afc8e4441abeeb9da699a0c292e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 05:05:35 +0000

    Add package hbmep/0.5.0/hbmep-0.5.0.tar.gz

---
+++ packages/hbmep/hbmep-0.5.0.tar.gz/hbmep-0.5.0/src/hbmep/__init__.py
@@ -0,0 +1,15 @@
+import multiprocessing
+import logging
+
+import jax
+import numpyro
+
+PLATFORM = "cpu"
+numpyro.set_platform(PLATFORM)
+
+cpu_count = multiprocessing.cpu_count() - 2
+numpyro.set_host_device_count(cpu_count)
+numpyro.enable_x64()
+numpyro.enable_validation()
+
+logger = logging.getLogger(__name__)
