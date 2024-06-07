f5926547ef228123cd73cbe5d7fa4885138c8444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:18 +0000

    Add package mtai/0.0.7/mtai-0.0.7-py3-none-any.whl

---
+++ packages/mtai/mtai-0.0.7-py3-none-any.whl/mtai/mt.py
@@ -0,0 +1,16 @@
+"""Entry point defined here."""
+
+from mtai.base import MTAIBase
+from mtai.tags import Tag
+from mtai.prompts import Prompt
+
+
+class MT(MTAIBase):
+    """MT Class used across defined."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize mtai with secret key."""
+        MTAIBase.__init__(self, *args, **kwargs)
+
+        self.tags = Tag
+        self.prompts = Prompt
