115d6bbb034b43a36c436b9b4b8994155b295a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 06:04:53 +0000

    Add package algopy/0.6.0/algopy-0.6.0.tar.gz

---
+++ packages/algopy/algopy-0.6.0.tar.gz/algopy-0.6.0/algopy/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM ALGOPY SETUP.PY
+short_version='0.6.0'
+version='0.6.0'
+release=False
+
+if not release:
+    version += '.devc8d0adcc3979f03f90a82ab377a4ba7461d70fdf'
