e2b67c401d9c99749ca5fea85c3ff66a23d002f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 04:04:20 +0000

    Add package netvulnscan/0.1.1/netvulnscan-0.1.1.tar.gz

---
+++ packages/netvulnscan/netvulnscan-0.1.1.tar.gz/netvulnscan-0.1.1/netvulnscan/__init__.py
@@ -0,0 +1,14 @@
+"""
+
+NetVulnScan package
+
+A package to discover network devices, scan ports and services, and find vulnerabilities
+
+"""
+
+__version__ = '0.1.1'
+__author__ = 'Hrishikesh'
+__email__ = 'rishirane1204@gmail.com'
+
+
+
