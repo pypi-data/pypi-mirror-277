280f2c2b2cb5ff0810b73add376dd2b247cabdac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 15:04:37 +0000

    Add package llm-sql-prompt/0.4.0/llm_sql_prompt-0.4.0.tar.gz

---
+++ packages/llm-sql-prompt/llm_sql_prompt-0.4.0.tar.gz/llm_sql_prompt-0.4.0/llm_sql_prompt/__init__.py
@@ -0,0 +1,29 @@
+import click
+from . import sqlite
+from . import postgres
+
+
+@click.command()
+@click.argument(
+    "database_url",
+)
+@click.argument(
+    "table_name",
+    required=False,
+)
+def main(database_url, table_name):
+    """
+    Generate a prompt for a table in a database for use in chatgpt or other LLMs to help write SQL.
+
+    - DATABASE_URL Could be a file path or a postgres url reference. Does not support postgres+ssh protocols.
+
+    - TABLE_NAME Name of the table to generate a prompt for. If not provided, will generate a prompt for all tables in the database.
+    """
+
+    if "postgresql" in database_url:
+        postgres.describe_database_and_table(database_url, table_name)
+    elif "sqlite" in database_url:
+        sqlite.describe_database_and_table(database_url, table_name)
+    else:
+        print("Unknown database type.")
+        exit(1)
