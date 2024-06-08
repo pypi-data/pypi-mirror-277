7bf601d864784cba66b77ab11057b29a22bdba09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:04:30 +0000

    Add package ansible-doctor/4.1.0/ansible_doctor-4.1.0.tar.gz

---
+++ packages/ansible-doctor/ansible_doctor-4.1.0.tar.gz/ansible_doctor-4.1.0/ansibledoctor/exception.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Doctor exception module."""
+
+
+class DoctorError(Exception):
+    """Define generic exception."""
+
+    def __init__(self, msg, original_exception=""):
+        super().__init__(f"{msg}\n{original_exception}")
+        self.original_exception = original_exception
+
+
+class YAMLError(DoctorError):
+    """Errors while reading a yaml file."""
+
+    pass
+
+
+class ConfigError(DoctorError):
+    """Errors related to config file handling."""
+
+    pass
+
+
+class InputError(DoctorError):
+    """Errors related to config file handling."""
+
+    pass
