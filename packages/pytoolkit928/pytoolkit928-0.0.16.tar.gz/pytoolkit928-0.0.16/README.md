d71cf03a72a852fbf1eb155227ae90dbfadb2bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 04:05:24 +0000

    Add package pytoolkit928/0.0.16/pytoolkit928-0.0.16.tar.gz

---
+++ packages/pytoolkit928/pytoolkit928-0.0.16.tar.gz/pytoolkit928-0.0.16/README.md
@@ -0,0 +1,135 @@
+# PyToolkit
+
+Python General tools
+
+## Utilities
+
+`string_or_list` function allows you to interpret a string and return a list. Provides you the option of adding a delimeter using an OR function to return a possible string that you may be expecting possible commond delimeters. Such as: `",|:|\|, "`.
+
+__Example:__
+
+```bash
+>>> from pytoolkit.utils import string_or_list
+
+>>> test = 'string1,string2 string3|string4'
+>>> string_or_list(test)
+['string1,string2 string3|string4']
+>>> string_or_list(test,delimeters=',| ')
+['string1', 'string2', 'string3|string4']
+>>> string_or_list(test,delimeters=',| |\|')
+['string1', 'string2', 'string3', 'string4']
+```
+
+### Dataclass Base
+
+Used for basic extended functionality for dataclass declerations. Includes the ability to create a dataclass from a ``dictionary`` or from ``**kwargs``. Also, includes a conversion from ``Dataclass`` to a Python ``dictionary``.
+
+__Usage:__
+
+```python
+from typing import Optional
+from dataclasses import dataclass
+
+from pytoolkit.utilities import BaseMonitor, NONETYPE
+
+@dataclass
+class Sample(BaseMonitor):
+    key1: str
+    key2: str
+    key3: int
+    key5: Optional[str] = NONETYPE
+
+# create a sample module
+_dict = {"key1": "value1", "key2": "value2", "key3": 3}
+
+sample1 = Sample.create_from_dict(_dict)
+sample2 = Sample.create_from_kwargs(**_dict)
+
+print(sample1)
+print(sample2)
+print(sample1.to_dict())
+```
+
+__OUTPUT:__
+
+```bash
+>>> print(sample1)
+Sample(key1='value1', key2='value2', key3=3, key5=<object object at 0x10c8e8b70>)
+>>> print(sample2)
+Sample(key1='value1', key2='value2', key3=3, key5=<object object at 0x10c8e8b70>)
+>>> print(sample1.to_dict())
+{'key1': 'value1', 'key2': 'value2', 'key3': 3}
+```
+
+### Maniuplating Dictionaries
+
+__Flatten a Dictionary:__
+
+```python
+import json
+from pytoolkit import utilities
+
+sample_dict = {"key1":"value","key2": "value2", "metadata": {"key1": "meta_value1","key2":"meta_value2"}}
+
+# Convert dictionary into a flat dictionary
+flat_dict = utilities.flatten_dict(sample_dict)
+
+# Convert dictionary back into a nested ditionary
+nest_dict = utilities.nested_dict(flat_dict)
+
+print(f"This is a Flattened Dictionary:\n{json.dumps(flat_dict,indent=1)}")
+print(f"This is a Nested Dictionary:\n{json.dumps(nest_dict,indent=1)}")
+```
+
+__OUTPUT:__
+
+```bash
+This is a Flattened Dictionary:
+{
+ "key1": "value",
+ "key2": "value2",
+ "metadata.key1": "meta_value1",
+ "metadata.key2": "meta_value2"
+}
+
+This is a Nested Dictionary:
+{
+ "key1": "value",
+ "key2": "value2",
+ "metadata": {
+  "key1": "meta_value1",
+  "key2": "meta_value2"
+ }
+}
+```
+
+The above is using the default `'.'` seperator value. There is a mix of commands that can be passed to adjust how the dictionary is expressed. This is useful for expressing data in otherformats that do not allow for nested dictionaries, but need a way to recreate the original formated datastructure.
+
+__Nested Dictionary:__
+
+__TOOD:__ Create a way to extract a CSV or XCEL file and turn it into a proper dictionary based on the type. Integrate with Splunk
+
+__TODO:__ Add splunk HEC fromatter with proper chunck
+
+__TODO:__ KVSTORE configuration tool.
+
+### Utilities
+
+Sanatize dictionary data
+
+```python
+from pytoolkit.utils import sanatize_data
+
+test_dict = {
+    "value1": "one",
+    "value2": "two",
+    "subvalue01": {
+        "password": "welcome123",
+        "username": "testuser",
+    }
+}
+
+sanatized_dict = sanatize_data(data=test_dict)
+print(sanatize_dict)
+# {"value1": "one", "value2": "two", "subvalue01": { "password": "[MASKED]", "username": "testuser"}}
+```
---
+++ packages/pytoolkit928/pytoolkit928-0.0.16.tar.gz/pytoolkit928-0.0.16/src/pytoolkit/files.py
@@ -0,0 +1,194 @@
+# pylint: disable=invalid-name
+"""Files."""
+
+import re
+import json
+from typing import Any, Union
+from pathlib import Path
+import platform
+import tempfile
+
+import yaml
+
+from pytoolkit.static import ENCODING, FILE_UMASK_PERMISSIONS, CONFIG_PATH
+
+
+class BytesDump(json.JSONEncoder):
+    """Resovlve error with byte present in Dict."""
+
+    def default(self, o: Any) -> Union[str, Any]:
+        """Resolve error with byte in Dict."""
+        if isinstance(o, bytes):
+            return o.decode()
+        return json.JSONEncoder.default(self, o)
+
+
+def get_config_section(filename: str, ftype: str, section: str = ""):
+    """
+    Returns a configuration dictionary or a section of a configuration if found.
+
+    :param filename: _description_
+    :type filename: str
+    :param ftype: _description_
+    :type ftype: str
+    :param section: _description_, defaults to ""
+    :type section: str, optional
+    :return: _description_
+    :rtype: _type_
+    """
+    settings = read_yaml(Path(filename))
+    if section:
+        settings = settings.get(section, {})
+    return settings
+
+
+def read_yaml(filename: Path) -> dict[str, Any]:
+    """
+    Read in a YAML configuration file.
+
+    :param filename: Yaml File Full Path
+    :type filename: Path
+    :return: Yaml Configurations
+    :rtype: dict[str,Any]
+    """
+    check_file(filename=str(filename))
+    with open(filename, "r", encoding=ENCODING) as r_yaml:
+        settings: Any = yaml.safe_load(r_yaml)
+    return settings
+
+
+def get_tempdir() -> str:
+    """Returns tempdir"""
+    return tempfile.gettempdir()
+
+
+def get_var_dir(extend_path: Union[str, None] = None, mode: str = "default") -> str:
+    """
+    Get default var directory depending on OS. Extend to application path if path supplied.
+     Allows for user to create a directory strcture where reports or logs can be stored. Using
+     a variable directory or extend a path in a users home directory.
+
+    :param extend_path: Extends the system default location;
+     creates a new app directory, defaults to None
+    :type extend_path: str, optional
+    :return: Log Directory for Logs
+    :rtype: str
+    """
+    directory: dict[str, Path] = {
+        "darwin": Path.joinpath(Path.home() / "Library/Logs"),
+        "linux": Path("/var/log"),
+    }
+    plat: str = platform.system()
+    try:
+        path = (
+            Path(f"{str(directory[plat.lower()])}/{extend_path}")
+            if extend_path
+            else Path(str(directory[plat.lower()]))
+        )
+        mkdir(path=path, mode=mode)
+        return str(path)
+    except KeyError:
+        path = (
+            Path(f"{str(tempfile.gettempdir())}/{extend_path}")
+            if extend_path
+            else Path(tempfile.gettempdir())
+        )
+        mkdir(path=path, mode=mode)
+        return str(path)
+
+
+def set_location(
+    location: str, extend_path: Union[str, None] = None, mode: str = "default"
+) -> str:
+    """Set default logDir or configuration directory based on mode defined."""
+    loc = "var"
+    if bool(re.match(r"(home|homedir)", location)):
+        loc = "home"
+    mode = mode if mode in FILE_UMASK_PERMISSIONS else "default"
+    base_dir = {"home": set_homedir, "var": get_var_dir}
+    return base_dir[loc](extend_path=extend_path, mode=mode)
+
+
+def set_homedir(extend_path: Union[str, None] = None, mode: str = "default") -> str:
+    """
+    Return the users home dir and can extend if using a subdirctory.
+     Use `mode` to restrict permissions.
+
+    :param extend_path: _description_, defaults to None
+    :type extend_path: Union[str,None], optional
+    :param mode: _description_, defaults to "default"
+    :type mode: str, optional
+    :return: _description_
+    :rtype: str
+    """
+    path = Path(Path.home() / extend_path) if extend_path else Path.home()
+    mkdir(path=path, mode=mode)
+    return str(path)
+
+
+def mkdir(path: Path, mode: str = "default") -> None:
+    """Makes Dir based on permissions passed."""
+    # for parent in reversed(path.parents):
+    path.mkdir(mode=FILE_UMASK_PERMISSIONS[mode], parents=True, exist_ok=True)
+
+
+def get_home() -> str:
+    """Return current users `home` direcotry."""
+    return str(Path.home())
+
+
+def with_suffix(logName: str) -> str:
+    """Add suffix to logname."""
+    return str(Path(logName).with_suffix(".log"))
+
+
+def check_file(filename: str) -> None:
+    """
+    Checks if a file exists.
+
+    :param filename: _description_
+    :type filename: str
+    :raises ValueError: _description_
+    """
+    if not Path(filename).is_file():
+        raise ValueError(f"Not a file {filename}")
+
+
+def get_config_location(
+    config_location: list[str],
+    app_name: Union[str, None] = None,
+    file_format: str = "yml",
+) -> str:
+    """
+    Retrieve configuraiton lcoation if one exists in the paths to search.
+
+        Ex:
+            config_location = [
+                str(Path.joinpath(Path.home() / ".config/application.yaml")),
+                str(Path.joinpath(Path.home() / ".config/application.yml")),
+                str(Path("/etc/appname/appconfig.yaml")),
+                str(Path("/Users/guest/Library/logs/appname/appconfig.yml"))
+            ]
+        OR:
+            config_location = [
+                str(Path.joinpath(Path.home() / ".config/application.yaml")),
+                str(Path.joinpath(Path.home() / ".config/application.yml")),
+                str(Path("/etc/appname/appconfig.yaml")),
+                str(Path("/Users/guest/Library/logs/appname/appconfig.yml"))
+            ]
+    :param config_location: _description_
+    :type config_location: list[str]
+    :return: _description_
+    :rtype: str
+    """
+    for location in config_location:
+        # TODO: Allow for diff types as currentlu just allowing yaml.
+        if Path.is_file(
+            Path(
+                CONFIG_PATH.format(location, app_name, file_format)
+                if app_name
+                else location
+            )
+        ):
+            return location
+    return ""
