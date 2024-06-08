dc2b6de843db6f25cd55c5c4716b939c1a719ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:04:56 +0000

    Add package deplyai/0.0.12/deplyai-0.0.12.tar.gz

---
+++ packages/deplyai/deplyai-0.0.12.tar.gz/deplyai-0.0.12/deplyai/_schema/cli.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field, RootModel, ValidationError, field_validator
+from typing import Any, Dict, List, Optional, Set
+
+class ProfileCredentials(BaseModel):
+    tenant: int
+    token: str
