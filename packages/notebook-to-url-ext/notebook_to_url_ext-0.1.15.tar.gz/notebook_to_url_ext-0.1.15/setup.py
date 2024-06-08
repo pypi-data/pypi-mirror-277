fce070c89de3ea947793683394ba58df9a68d483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:05:11 +0000

    Add package notebook-to-url-ext/0.1.15/notebook_to_url_ext-0.1.15.tar.gz

---
+++ packages/notebook-to-url-ext/notebook_to_url_ext-0.1.15.tar.gz/notebook_to_url_ext-0.1.15/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='notebook_to_url_ext',
+    version='0.1.15',  # Increment the version number
+    packages=find_packages(),
+    include_package_data=True,
+    data_files=[
+        ('share/jupyter/labextensions/notebook_to_url_ext', [
+            'notebook-to-url-lib/index.js',
+            'notebook-to-url-lib/remoteEntry.js',
+            'package.json',
+            'install.json'
+        ])
+    ],
+    zip_safe=False,
+    install_requires=[
+        'jupyterlite',
+        'jupyterlab'
+    ],
+    entry_points={},
+)
