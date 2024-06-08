5e2232d97560f6bf2de6aac6046faa52855ec043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:05:40 +0000

    Add package refry/0.1.0/refry-0.1.0-py3-none-any.whl

---
+++ packages/refry/refry-0.1.0-py3-none-any.whl/refry/main.py
@@ -0,0 +1,26 @@
+import functools
+import time
+from typing import Any, Callable
+
+
+def retry(
+    rate_limit_exception: type[Exception]= Exception, backoff_increment: int = 5, retries: int = 5
+) -> Callable:
+    """
+    Decorator to retry a function if it raises a custom exception.
+    """
+
+    def _outer_wrapper(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def _wrapper(*args: list[Any], **kwargs: list[Any]) -> Any:
+            current_backoff = backoff_increment
+            for attempt in range(retries):
+                try:
+                    return func(*args, **kwargs)
+                except rate_limit_exception:
+                    print(f"Attempt {attempt + 1} failed. Retrying in {current_backoff} seconds.")
+                    time.sleep(current_backoff)
+                    current_backoff += backoff_increment
+
+        return _wrapper
+    return _outer_wrapper
