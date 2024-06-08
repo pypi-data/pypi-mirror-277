161160eaf883b447ab8cfda906bef3a7e8e69a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:04:58 +0000

    Add package packageclubs/0.0.4/packageclubs-0.0.4.tar.gz

---
+++ packages/packageclubs/packageclubs-0.0.4.tar.gz/packageclubs-0.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from typing import List
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.4"
+REPO_NAME = "mongodbconnectorpkg-main"
+PKG_NAME= "packageclubs"
+AUTHOR_USER_NAME = "Ritesh799"
+AUTHOR_EMAIL = "work.ritesh2003@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
+
+
+
