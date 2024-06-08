2657b2002cea61425c1901c48da00484171e430a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:04:45 +0000

    Add package pycon-demo-library-python/0.1.1/pycon_demo_library_python-0.1.1.tar.gz

---
+++ packages/pycon-demo-library-python/pycon_demo_library_python-0.1.1.tar.gz/pycon_demo_library_python-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pycon-demo-library-python',
+    description='Util functions for PyCon demo library',
+    version='0.1.1',
+    url='https://andresrestrepo.github.io/portfolio_team/',
+    author='AFC',
+    author_email='andresfelipe25@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['json-logging>=1.3.0']
+)
