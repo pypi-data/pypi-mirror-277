99e05fd9c6ae72294a3766b627b2764f4158228b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 04:05:24 +0000

    Add package isfhyfp/1.1.0/isfhyfp-1.1.0-py3-none-any.whl

---
+++ packages/isfhyfp/isfhyfp-1.1.0-py3-none-any.whl/isfhyfp/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author  : isFhy
+# @Email   : fhy.sdwh@gmail.com
+# @File    : __init__.py
+# @Time    : 2024/6/7 15:23
+# **********************************************
+
+
