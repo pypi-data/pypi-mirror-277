a14e8ba8a705cf16b81b6d34cb4186d42511ca0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 04:05:32 +0000

    Add package napari-moltrack/0.0.12/napari_moltrack-0.0.12-py3-none-any.whl

---
+++ packages/napari-moltrack/napari_moltrack-0.0.12-py3-none-any.whl/moltrack/funcs/import_utils.py
@@ -0,0 +1,367 @@
+import traceback
+import numpy as np
+import os
+from PIL import Image
+from qtpy.QtWidgets import QFileDialog
+from moltrack.funcs.compute_utils import Worker
+import time
+import multiprocessing
+from multiprocessing import shared_memory, Manager
+from functools import partial
+import tifffile
+import concurrent.futures
+from astropy.io import fits
+
+
+def crop_frame(image, crop_mode):
+
+    try:
+
+        if "left" in crop_mode.lower():
+            crop = image[:, :image.shape[1]//2]
+        elif "right" in crop_mode.lower():
+            crop = image[:, image.shape[1]//2:]
+        elif "brightest" in crop_mode.lower():
+            left = image[:, :image.shape[1]//2]
+            right = image[:, image.shape[1]//2:]
+            crop = left if np.mean(left) > np.mean(right) else right
+        else:
+            crop = image
+
+    except:
+        print(traceback.format_exc())
+        crop = image
+
+    return crop
+
+def import_image_data(dat, progress_dict={}, index=0):
+
+    try:
+
+        path = dat["path"]
+        crop_mode = dat["import_crop_mode"]
+        import_limit = dat["import_limit"]
+        frame_averaging = dat["frame_averaging"]
+
+        base, ext = os.path.splitext(path)
+
+        images = []
+
+        if ext.lower() == ".tif":
+
+            with Image.open(path) as image:
+
+                n_frames = image.n_frames
+
+                if import_limit != "None":
+                    if import_limit.isdigit():
+                        import_limit = int(import_limit)
+                        if n_frames > import_limit:
+                            n_frames = import_limit
+
+                for frame_index in range(n_frames):
+
+                    image.seek(frame_index)
+                    img_frame = np.array(image)
+
+                    img_frame = crop_frame(img_frame, crop_mode)
+
+                    images.append(img_frame)
+
+                    progress = int(((frame_index + 1) / n_frames)*100)
+                    progress_dict[index] = progress
+
+        elif ext.lower() == ".fits":
+
+            with fits.open(path) as hdul:
+
+                n_frames = hdul[0].data.shape[0]
+
+                if import_limit != "None":
+                    if import_limit.isdigit():
+                        import_limit = int(import_limit)
+                        if n_frames > import_limit:
+                            n_frames = import_limit
+
+                for frame_index in range(n_frames):
+
+                    img_frame = hdul[0].data[frame_index]
+
+                    img_frame = crop_frame(img_frame, crop_mode)
+
+                    images.append(img_frame)
+
+                    progress = int(((frame_index + 1) / n_frames)*100)
+                    progress_dict[index] = progress
+
+        if len(images) > 0:
+            images = np.stack(images, axis=0)
+
+            if frame_averaging == True:
+                images = np.mean(images, axis=0)
+                images = np.expand_dims(images, axis=0)
+
+            dat["data"] = images
+
+    except:
+        print(traceback.format_exc())
+        pass
+
+    return dat
+
+
+
+class _import_utils:
+
+
+    def get_image_info(self, path):
+
+        if self.verbose:
+            print(f"Getting image info for {path}")
+
+        base, ext = os.path.splitext(path)
+
+        if ext.lower() == ".tif":
+
+            image_size = os.path.getsize(path)  # Get file size directly
+
+            with tifffile.TiffFile(path) as tif:
+                n_frames = len(tif.pages)  # Number of pages (frames)
+                page_shape = tif.pages[0].shape  # Dimensions of the first page
+                dtype = tif.pages[0].dtype  # Data type of the first page
+
+            image_shape = (n_frames, page_shape[0], page_shape[1])
+
+        elif ext.lower() == ".fits":
+
+            image_size = os.path.getsize(path)
+
+            with fits.open(path, mode='readonly', ignore_missing_end=True) as hdul:
+
+                header = hdul[0].header
+
+                # Extract shape information from the header
+                if header['NAXIS'] == 3:
+                    image_shape = (header['NAXIS3'], header['NAXIS2'], header['NAXIS1'])
+                else:
+                    image_shape = (header['NAXIS2'], header['NAXIS1'])
+
+                n_frames = image_shape[0] if len(image_shape) == 3 else 1
+                page_shape = image_shape[1:] if len(image_shape) == 3 else image_shape
+
+                # Determine the data type from BITPIX
+                bitpix_to_dtype = {8: np.dtype('uint8'),
+                                   16: np.dtype('uint16'),
+                                   32: np.dtype('uint32'),
+                                   -32: np.dtype('float32'),
+                                   -64: np.dtype('float64'),
+                                   }
+
+                dtype = bitpix_to_dtype[header['BITPIX']]
+
+        return n_frames, image_shape, dtype, image_size
+
+    def format_import_path(self, path):
+
+        try:
+
+            path = os.path.normpath(path)
+
+            if os.name == "nt":
+                if path.startswith("\\\\"):
+                    path = '\\\\?\\UNC\\' + path[2:]
+
+                    if "%" in str(path):
+                        path = path.replace("%", "%%")
+
+                if path.startswith("UNC"):
+                    path = '\\\\?\\' + path
+
+                    if "%" in str(path):
+                        path = path.replace("%", "%%")
+
+        except:
+            print(traceback.format_exc())
+            pass
+
+        return path
+
+
+    def populate_import_jobs(self, progress_callback=None, paths=[]):
+
+        import_jobs = []
+
+        try:
+
+            import_crop_mode = self.gui.import_crop_mode.currentText()
+            import_limit = self.gui.import_limit.currentText()
+            frame_averaging = self.gui.frame_averaging.isChecked()
+
+            for path_index, path in enumerate(paths):
+
+                path = self.format_import_path(path)
+                dataset_name = os.path.basename(path)
+
+                image_dict = {"path": path,
+                              "dataset_name": dataset_name,
+                              "import_limit": import_limit,
+                              "import_crop_mode": import_crop_mode,
+                              "frame_averaging": frame_averaging
+                              }
+
+                import_jobs.append(image_dict)
+
+        except:
+            print(traceback.format_exc())
+
+        return import_jobs
+
+
+    def process_compute_jobs(self, compute_jobs, progress_callback=None):
+
+        results = []
+
+        if self.verbose:
+            print(f"Processing {len(compute_jobs)} compute jobs.")
+
+        cpu_count = int(multiprocessing.cpu_count() * 0.75)
+        timeout_duration = 10  # Timeout in seconds
+
+        with Manager() as manager:
+            progress_dict = manager.dict()
+
+            with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count) as executor:
+
+                # Submit all jobs and store the future objects
+                futures = [executor.submit(import_image_data, job, progress_dict, i) for i, job in enumerate(compute_jobs)]
+
+                while any(not future.done() for future in futures):
+                    # Calculate and emit progress
+                    total_progress = sum(progress_dict.values())
+                    overall_progress = int((total_progress / len(compute_jobs)))
+                    if progress_callback is not None:
+                        progress_callback.emit(overall_progress)
+                    time.sleep(0.1)  # Update frequency
+
+                # Wait for all futures to complete
+                concurrent.futures.wait(futures)
+
+                # Retrieve and process results
+                results = [future.result() for future in futures if future.done()]
+
+        if self.verbose:
+            print("Finished processing compute jobs.")
+
+        return results
+
+    def populate_import_dataset_dict(self, import_list):
+
+        try:
+
+            concat_images = self.gui.import_concatenate.isChecked()
+
+            if self.verbose:
+                print("Populating dataset dict")
+
+            import_dict = {}
+
+            for import_data in import_list:
+
+                if type(import_data) == dict:
+
+                    if "data" in import_data.keys():
+
+                        dataset_name = import_data["dataset_name"]
+                        import_dict[dataset_name] = import_data
+
+            if concat_images == True:
+
+                dataset_list = list(import_dict.keys())
+
+                image_list = []
+                path_list = []
+
+                for dataset_name in dataset_list:
+
+                    dataset_image = import_dict[dataset_name].pop("data")
+                    dataset_path = import_dict[dataset_name].pop("path")
+
+                    dataset_path = [dataset_path]*dataset_image.shape[0]
+
+                    image_list.append(dataset_image)
+                    path_list.extend(dataset_path)
+
+                image_list = np.concatenate(image_list, axis=0)
+
+                if dataset_list[0] not in self.dataset_dict.keys():
+                    self.dataset_dict[dataset_list[0]] = {"data": image_list, "path": path_list}
+
+            else:
+                dataset_list = list(import_dict.keys())
+
+                for dataset_name in dataset_list:
+                    dataset_dict = import_dict.pop(dataset_name)
+                    self.dataset_dict[dataset_name] = dataset_dict
+
+        except:
+            print(traceback.format_exc())
+            pass
+
+    def import_data(self, progress_callback=None, paths=[], import_mode="data"):
+
+        import_jobs = self.populate_import_jobs(paths=paths)
+
+        results = self.process_compute_jobs(import_jobs,
+            progress_callback=progress_callback)
+
+        if import_mode.lower() == "data":
+            self.populate_import_dataset_dict(results)
+        else:
+
+            if len(results) > 0:
+                if type(results[0]) == dict:
+                    if "data" in results[0].keys():
+                        self.segmentation_image = results[0]["data"]
+
+    def import_data_finished(self):
+
+        self.populate_dataset_selectors()
+        self.update_ui()
+        self.update_active_image()
+        self.draw_segmentation_image()
+
+    def init_import_data(self):
+
+        try:
+
+            import_mode = self.gui.import_mode.currentText()
+            desktop = os.path.expanduser("~/Desktop")
+
+            if import_mode.lower() == "data":
+
+                paths = QFileDialog.getOpenFileNames(self, 'Open file',
+                    desktop, "Image files (*.tif *.fits)")[0]
+
+                paths = [path for path in paths if path != ""]
+
+            else:
+                path = QFileDialog.getOpenFileName(self, 'Open file',
+                    desktop, "Image files (*.tif *.fits)")[0]
+                paths = [path]
+
+            if paths != []:
+
+                self.update_ui(init=True)
+
+                self.worker = Worker(self.import_data, paths=paths, import_mode=import_mode)
+                self.worker.signals.progress.connect(partial(self.moltrack_progress,
+                    progress_bar=self.gui.import_progressbar))
+                self.worker.signals.finished.connect(self.import_data_finished)
+                self.worker.signals.error.connect(self.update_ui)
+                self.threadpool.start(self.worker)
+
+
+        except:
+            self.update_ui()
+            print(traceback.format_exc())
+            pass
---
+++ packages/napari-moltrack/napari_moltrack-0.0.12-py3-none-any.whl/moltrack/funcs/loc_filter_utils.py
@@ -0,0 +1,271 @@
+import traceback
+import numpy as np
+import pandas as pd
+from shapely.geometry import Polygon, Point, MultiPolygon, MultiPoint
+from shapely.strtree import STRtree
+
+class _loc_filter_utils:
+
+
+    def remove_seglocs(self, viewer=None):
+
+        try:
+
+            layer_names = [layer.name for layer in self.viewer.layers]
+
+            segmentations = self.gui.remove_seglocs_segmentation.currentText()
+            dataset = self.gui.remove_seglocs_dataset.currentText()
+            loc_datasets = list(self.localisation_dict.keys())
+
+            if segmentations not in layer_names:
+                print(f"Segmentation {segmentations} not found in viewer.")
+                return
+            else:
+                polygons = self.viewer.layers[segmentations].data.copy()
+
+                if len(polygons) == 0:
+                    print(f"No polygons found in segmentation {segmentations}.")
+                    return
+
+            if dataset not in loc_datasets:
+                print(f"Dataset {dataset} not found in localisation dict.")
+                return
+
+            loc_dict = self.localisation_dict[dataset]
+
+            locs = loc_dict["localisations"].copy()
+            n_locs = len(locs)
+
+            if n_locs == 0:
+                print(f"No localisations found in dataset {dataset}.")
+                return
+
+            filtered_locs = []
+
+            polygons = [Polygon(polygon) for polygon in polygons]
+            coords = np.stack([locs["x"], locs["y"]], axis=1)
+            points = [Point(coord) for coord in coords]
+
+            spatial_index = STRtree(points)
+
+            for polygon_index, polygon in enumerate(polygons):
+
+                possible_points = spatial_index.query(polygon)
+
+                polygon_point_indices = []
+
+                for point_index in possible_points:
+
+                    point = points[point_index]
+
+                    if polygon.contains(point):
+
+                        polygon_point_indices.append(point_index)
+
+                if len(polygon_point_indices) > 0:
+
+                    polygon_locs = locs[polygon_point_indices]
+
+                    seg_name = segmentations[:-1].lower() + "_index"
+
+                    polygon_locs = pd.DataFrame(polygon_locs)
+
+                    if "cell_index" in polygon_locs.columns:
+                        polygon_locs = polygon_locs.drop(columns=["cell_index"])
+                    if "segmentation_index" in polygon_locs.columns:
+                        polygon_locs = polygon_locs.drop(columns=["segmentation_index"])
+
+                    polygon_locs[seg_name] = polygon_index
+                    polygon_locs = polygon_locs.to_records(index=False)
+
+                    filtered_locs.append(polygon_locs)
+
+            if len(filtered_locs) > 0:
+                filtered_locs = np.hstack(filtered_locs).view(np.recarray).copy()
+
+                loc_dict["localisations"] = filtered_locs
+                self.localisation_dict[dataset] = loc_dict
+
+                n_filtered = len(filtered_locs)
+                n_removed = n_locs - n_filtered
+
+                print(f"Removed {n_removed} localisations.")
+
+                self.draw_localisations()
+
+                self.update_filter_criterion()
+                self.update_criterion_ranges()
+
+        except:
+            print(traceback.format_exc())
+
+        pass
+
+
+    def pixseq_filter_localisations(self, viewer=None):
+
+        try:
+
+            dataset = self.gui.picasso_filter_dataset.currentText()
+            criterion = self.gui.filter_criterion.currentText()
+            min_value = self.gui.filter_min.value()
+            max_value = self.gui.filter_max.value()
+
+            if dataset in self.localisation_dict.keys():
+
+                loc_dict = self.localisation_dict[dataset]
+
+                if "localisations" in loc_dict.keys():
+
+                    locs = loc_dict["localisations"].copy()
+
+                    if len(locs) > 0:
+
+                        columns = list(locs.dtype.names)
+
+                        if criterion in columns:
+
+                            self.gui.filter_localisations.setEnabled(False)
+
+                            n_locs = len(locs)
+
+                            locs = locs[locs[criterion] > min_value]
+                            locs = locs[locs[criterion] < max_value]
+
+                            n_filtered = len(locs)
+
+                            if n_filtered < n_locs:
+
+                                n_removed = n_locs - n_filtered
+
+                                loc_dict["localisations"] = locs
+
+                                self.localisation_dict[dataset] = loc_dict
+                                self.draw_localisations(update_vis=True)
+
+            self.update_criterion_ranges()
+            print(f"Filtered {n_removed} localisations.")
+
+            self.gui.filter_localisations.setEnabled(True)
+
+        except:
+            self.gui.filter_localisations.setEnabled(True)
+            print(traceback.format_exc())
+
+
+    def update_filter_dataset(self, viewer=None):
+
+        if self.gui.picasso_filter_type.currentText() == "Localisations":
+            self.gui.picasso_filter_dataset.setEnabled(True)
+            self.gui.picasso_filter_dataset.show()
+            self.gui.picasso_filter_dataset_label.show()
+        else:
+            self.gui.picasso_filter_dataset.setEnabled(False)
+            self.gui.picasso_filter_dataset.hide()
+            self.gui.picasso_filter_dataset_label.hide()
+
+        self.update_filter_criterion()
+        self.update_criterion_ranges()
+
+    def update_filter_criterion(self, viewer=None):
+
+
+        try:
+
+            columns = []
+
+            dataset = self.gui.picasso_filter_dataset.currentText()
+            selector = self.gui.filter_criterion
+
+            if dataset in self.localisation_dict.keys():
+
+                loc_dict = self.localisation_dict[dataset]
+
+                if "localisations" in loc_dict.keys():
+
+                    locs = loc_dict["localisations"].copy()
+
+                    if len(locs) > 0:
+
+                        columns = list(locs.dtype.names)
+
+                        columns = [col for col in columns if col not in ["dataset","channel"]]
+
+            selector.clear()
+
+            if len(columns) > 0:
+                selector.addItems(columns)
+
+        except:
+            print(traceback.format_exc())
+
+
+    def update_criterion_ranges(self, viewer=None, plot=True):
+
+        try:
+
+            self.filter_graph_canvas.clear()
+
+            dataset = self.gui.picasso_filter_dataset.currentText()
+            criterion = self.gui.filter_criterion.currentText()
+
+            if dataset in self.localisation_dict.keys():
+
+                loc_dict = self.localisation_dict[dataset]
+
+                if "localisations" in loc_dict.keys():
+
+                    locs = loc_dict["localisations"].copy()
+
+                    if len(locs) > 0:
+
+                        columns = list(locs.dtype.names)
+
+                        if criterion in columns:
+
+                            values = locs[criterion]
+
+                            if values.dtype in [np.float32, np.float64, np.int32, np.int64]:
+
+                                if plot:
+                                    self.plot_filter_graph(criterion, values)
+
+                                min_value = np.min(values)
+                                max_value = np.max(values)
+
+                                self.gui.filter_min.setMinimum(min_value)
+                                self.gui.filter_min.setMaximum(max_value)
+
+                                self.gui.filter_max.setMinimum(min_value)
+                                self.gui.filter_max.setMaximum(max_value)
+
+                                self.gui.filter_min.setValue(min_value)
+                                self.gui.filter_max.setValue(max_value)
+
+        except:
+            print(traceback.format_exc())
+
+    def plot_filter_graph(self, criterion = "", values = None):
+
+        try:
+            self.filter_graph_canvas.clear()
+
+            if values is not None:
+
+                values = values[~np.isnan(values)]
+
+                if len(values) > 0:
+                    ax = self.filter_graph_canvas.addPlot()
+
+                    # Create histogram
+                    y, x = np.histogram(values, bins=100)
+
+                    ax.plot(x, y, stepMode=True, fillLevel=0, brush=(0, 0, 255, 75))
+                    ax.setLabel('bottom', f"{criterion} values")
+                    ax.setLabel('left', 'Frequency')
+
+        except:
+            print(traceback.format_exc())
+
+
+
