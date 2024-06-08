29cab4e58953328c418313a12687d9c339d312dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 04:05:33 +0000

    Add package lootbox/0.0.0/lootbox-0.0.0.tar.gz

---
+++ packages/lootbox/lootbox-0.0.0.tar.gz/lootbox-0.0.0/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+setup(
+    name='lootbox',
+    version='0.0.0',
+    url='',
+    author='',
+    author_email='',
+    description='',
+    long_description='lootbox ',
+    long_description_content_type='text/markdown'
+)
