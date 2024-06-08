b21718cf26d2aacd6a928b098bee6a69f8a27dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:05:04 +0000

    Add package gitoptim/0.1.0.dev28/gitoptim-0.1.0.dev28.tar.gz

---
+++ packages/gitoptim/gitoptim-0.1.0.dev28.tar.gz/gitoptim-0.1.0.dev28/gitoptim/schemas/workflows.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class AnalyseLogsSchema(BaseModel):
+    logs: str
+    pipeline_id: int
+    job_id: int
+    commit_ref: str
+    event_type: str = "gitlab-job-logs"
