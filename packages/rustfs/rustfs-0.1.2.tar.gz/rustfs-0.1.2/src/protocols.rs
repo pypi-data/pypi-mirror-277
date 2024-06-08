63f1bfce5064a183a20bc57bbf8e2c1ebeafe2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:04:52 +0000

    Add package rustfs/0.1.2/rustfs-0.1.2.tar.gz

---
+++ packages/rustfs/rustfs-0.1.2.tar.gz/rustfs-0.1.2/src/protocols.rs
@@ -0,0 +1,54 @@
+use anyhow::Result;
+use object_store::multipart::MultiPartStore;
+use object_store::{ObjectMeta, ObjectStore};
+
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::io::Error;
+
+pub trait MultiPartObjectStore: ObjectStore + MultiPartStore {}
+
+impl<T: ObjectStore + MultiPartStore> MultiPartObjectStore for T {}
+
+pub type DynMultiPartObjectStore = dyn MultiPartObjectStore;
+
+pub enum Info {
+    Name(String),
+    Size(usize),
+    Type(String),
+}
+
+pub enum ListInfo {
+    Info(ObjectMeta),
+    Location(String),
+}
+
+pub type Details = HashMap<String, Info>;
+
+pub trait AbstractFileHandle {
+    fn close(&mut self) -> Result<(), Error>;
+    fn flush(&mut self) -> Result<(), Error>;
+    fn readable(&self) -> bool;
+    fn isatty(&self) -> bool;
+    fn seekable(&self) -> bool;
+    fn writable(&self) -> bool;
+    fn seek(&mut self, offset: usize, whence: u8) -> Result<usize, Error>;
+    fn tell(&self) -> usize;
+    fn readline(&mut self) -> Result<i64, Error>;
+    fn readlines(&mut self) -> Result<i64, Error>;
+    fn fileno(&mut self) -> Result<i64, Error>;
+    fn truncate(&mut self) -> Result<i64, Error>;
+    fn write(&mut self, data: &[u8]) -> Result<i64, Error>;
+    fn read(&mut self, n: i64) -> Result<Bytes, Error>;
+}
+
+pub trait Fsspec {
+    /// Return dict with keys: name (full path in the FS), size (in bytes), type (file,
+    /// directory, or something else) and other FS-specific keys
+    async fn info(&self, path: &str) -> Result<Details>;
+    async fn get(&self, rpath: &str, lpath: &str, recursive: bool) -> Result<()>;
+    async fn put(&self, lpath: &str, rpath: &str, recursive: bool) -> Result<()>;
+    async fn ls(&self, path: &str, detail: bool) -> Result<Vec<ListInfo>>;
+    async fn is_dir(&self, path: &str) -> Result<bool>;
+    fn open(&self, path: &str, mode: &str) -> Result<impl AbstractFileHandle>;
+}
