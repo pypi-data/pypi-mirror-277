e21f420c6bb68eca44daa7ca4f21a6dbd42b45ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:05:07 +0000

    Add package matplobblib/0.1.9/matplobblib-0.1.9-py3-none-any.whl

---
+++ packages/matplobblib/matplobblib-0.1.9-py3-none-any.whl/matplobblib/__init__.py
@@ -0,0 +1,11 @@
+import importlib as _importlib
+
+submodules = [
+    'TVIMS'
+]
+def __dir__():
+    return submodules
+
+def __getattr__(name):
+    if name in submodules:
+        return _importlib.import_module(f'matplobblib.{name}')
