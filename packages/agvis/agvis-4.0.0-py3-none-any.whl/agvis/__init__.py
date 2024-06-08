a26826b18b5d8f2e37aecc9b958c32a9c9c69a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:04:49 +0000

    Add package agvis/4.0.0/agvis-4.0.0-py3-none-any.whl

---
+++ packages/agvis/agvis-4.0.0-py3-none-any.whl/agvis/__init__.py
@@ -0,0 +1,9 @@
+from . import _version
+__version__ = _version.get_versions()['version']
+
+from agvis.main import config_logger  # NOQA
+from agvis.web import AgvisWeb
+
+__author__ = 'Nicholas West, Nicholas Parsly, Zack Malkmus, and Jinning Wang'
+
+__all__ = ['main', 'cli', '__version__']
