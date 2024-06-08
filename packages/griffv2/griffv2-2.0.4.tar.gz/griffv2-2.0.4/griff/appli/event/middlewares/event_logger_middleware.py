ad481078788abf308e0ae15926799b3d7ad1141c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 04:05:24 +0000

    Add package griffv2/2.0.4/griffv2-2.0.4.tar.gz

---
+++ packages/griffv2/griffv2-2.0.4.tar.gz/griffv2-2.0.4/griff/appli/event/middlewares/event_logger_middleware.py
@@ -0,0 +1,17 @@
+from loguru import logger
+
+from griff.appli.event.event import Event
+from griff.appli.event.event_handler import EventResponse
+from griff.appli.event.event_middleware import EventMiddleware
+from griff.appli.message.message_middleware import MessageContext
+
+
+class EventLoggerMiddleware(EventMiddleware):
+    async def dispatch(
+        self, message: Event, context: MessageContext | None = None
+    ) -> EventResponse:
+        logger.info(f"dispatch event: {message.short_classname()}")
+        logger.debug(message.model_dump())
+        response = await self._next.dispatch(message, context)
+        logger.debug(response.model_dump())
+        return response
