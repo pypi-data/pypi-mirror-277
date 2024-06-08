2f5aee47b4d490eff2ed9a4d1dd6feb6d19da9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 04:05:14 +0000

    Add package fancykimai/0.1.7/fancykimai-0.1.7.tar.gz

---
+++ packages/fancykimai/fancykimai-0.1.7.tar.gz/fancykimai-0.1.7/fancykimai/commands/config.py
@@ -0,0 +1,51 @@
+import click
+import os
+from fancykimai.functions.config import get_config, set_config, unset_config
+from fancykimai.classes.click_groups import AliasedGroup
+
+@click.group(name="config", cls=AliasedGroup)
+def config_group():
+    """
+    Configuration commands
+    """
+    pass
+
+@config_group.command()
+def show():
+    """
+    Show the configuration file
+    """
+    config_file = os.path.expanduser("~/.config/fancykimai/config.json")
+    if os.path.exists(config_file):
+        with open(config_file, "r") as f:
+            config_data = f.read()
+            click.echo(config_data)
+    else:
+        click.echo("Configuration file not found.")
+
+@config_group.command()
+@click.argument("key")
+@click.argument("value")
+def set(key, value):
+    """
+    Set a configuration value
+    """
+    set_config(key, value)
+    click.echo(f"Set {key} to {value}")
+
+@config_group.command()
+@click.argument("key")
+def get(key):
+    """
+    Get a configuration value
+    """
+    click.echo(get_config(key))
+
+@config_group.command()
+@click.argument("key")
+def unset(key):
+    """
+    Unset a configuration value
+    """
+    unset_config(key)
+    click.echo(f"Unset {key}")
