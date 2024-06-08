f052ad6a2f10e30ecd9746c41225fdf17099e4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 06:04:32 +0000

    Add package systemID/25.0.1/systemID-25.0.1-py3-none-any.whl

---
+++ packages/systemID/systemID-25.0.1-py3-none-any.whl/systemID/SystemIDAlgorithms/GetObserverMarkovParameters.py
@@ -0,0 +1,50 @@
+"""
+Author: Damien GUEHO
+Copyright: Copyright (C) 2023 Damien GUEHO
+License: Public Domain
+Version: 24
+Date: April 2022
+Python: 3.7.7
+"""
+
+
+import numpy as np
+from scipy.linalg import fractional_matrix_power as matpow
+
+
+def getObserverMarkovParameters(A, B, C, D, G, number_steps):
+    """
+    Purpose:
+
+
+    Parameters:
+        -
+
+    Returns:
+        -
+
+    Imports:
+        -
+
+    Description:
+
+
+    See Also:
+        -
+    """
+
+    # Dimensions
+    output_dimension, input_dimension = D(0).shape
+    state_dimension, _ = A(0).shape
+
+    # Initialisation
+    observer_markov_parameters = [D(0)]
+    B_bar = np.zeros([state_dimension, output_dimension + input_dimension])
+    B_bar[:, 0:input_dimension] = B(0) + np.matmul(G(0), D(0))
+    B_bar[:, input_dimension:output_dimension + input_dimension] = -G(0)
+    A_bar = A(0) + np.matmul(G(0), C(0))
+
+    for i in range(number_steps - 1):
+        observer_markov_parameters.append(np.matmul(C(0), np.matmul(matpow(A_bar, i), B_bar)))
+
+    return observer_markov_parameters
---
+++ packages/systemID/systemID-25.0.1-py3-none-any.whl/systemID/SystemIDAlgorithms/TimeVaryingObserverKalmanIdentificationAlgorithmWithObserver.py
@@ -0,0 +1,101 @@
+"""
+Author: Damien GUEHO
+Copyright: Copyright (C) 2023 Damien GUEHO
+License: Public Domain
+Version: 24
+Date: April 2022
+Python: 3.7.7
+"""
+
+
+
+import numpy as np
+import scipy.linalg as LA
+
+from systemID.SystemIDAlgorithms.GetTVMarkovParametersFromTVObserverMarkovParameters import getTVMarkovParametersFromTVObserverMarkovParameters
+from systemID.SystemIDAlgorithms.GetTVObserverGainMarkovParametersFromTVObserverMarkovParameters import getTVObserverGainMarkovParametersFromTVObserverMarkovParameters
+
+
+def timeVaryingObserverKalmanIdentificationAlgorithmWithObserver(input_signals, output_signals, **kwargs):
+    """
+    Purpose:
+
+
+    Parameters:
+        -
+
+    Returns:
+        -
+
+    Imports:
+        -
+
+    Description:
+
+
+    See Also:
+        -
+    """
+
+    # Dimensions
+    input_dimension = input_signals[0].dimension
+    output_dimension = output_signals[0].dimension
+    number_steps = output_signals[0].number_steps
+    number_forced_experiments = len(output_signals)
+
+    # Observer order
+    observer_order = kwargs.get("observer_order", number_steps)
+
+
+    # Time Varying hki_observer1, hki_observer2 and D matrices
+    hki_observer1 = np.zeros([(number_steps - 1) * output_dimension, observer_order * input_dimension])
+    hki_observer2 = np.zeros([(number_steps - 1) * output_dimension, observer_order * output_dimension])
+    D = np.zeros([output_dimension, input_dimension, number_steps])
+
+
+    # TVOKID
+    for k in range(number_steps):
+
+        # Initialize matrices y and V
+        if k == 0:
+            number_rows_V = input_dimension
+        else:
+            number_rows_V = input_dimension + min(observer_order, k) * (input_dimension + output_dimension)
+        number_columns_V = number_forced_experiments
+
+        V = np.zeros([number_rows_V, number_columns_V])
+        y = np.zeros([output_dimension, number_columns_V])
+
+        # Populate matrices y and V
+        for j in range(number_columns_V):
+            y[:, j] = output_signals[j].data[:, k]
+            V[0:input_dimension, j] = input_signals[j].data[:, k]
+            for i in range(min(observer_order, k)):
+                V[input_dimension + i * (input_dimension + output_dimension):input_dimension + (i + 1) * (input_dimension + output_dimension), j] = np.concatenate((input_signals[j].data[:, k - i - 1], output_signals[j].data[:, k - i - 1]))
+
+        # Least-Squares solution for Observer Markov Parameters
+        Mk = np.matmul(y, LA.pinv(V))
+        # print('Error TVOKID', LA.norm(y - np.matmul(Mk, V)))
+
+        # Extract Dk
+        D[:, :, k] = Mk[:, 0:input_dimension]
+
+        # Extract Observer Markov Parameters
+        for j in range(min(observer_order, k)):
+            h_observer = Mk[:, input_dimension + j * (input_dimension + output_dimension):input_dimension + (j + 1) * (input_dimension + output_dimension)]
+            h1 = h_observer[:, 0:input_dimension]
+            h2 = - h_observer[:, input_dimension:input_dimension + output_dimension]
+            hki_observer1[(k - 1) * output_dimension:k * output_dimension, j * input_dimension:(j + 1) * input_dimension] = h1
+            hki_observer2[(k - 1) * output_dimension:k * output_dimension, j * output_dimension:(j + 1) * output_dimension] = h2
+
+    # Get TV Markov Parameters from TV Observer Markov Parameters
+    hki, h2, r = getTVMarkovParametersFromTVObserverMarkovParameters(D, hki_observer1, hki_observer2, observer_order)
+
+    # Get TV Observer Gain Markov Parameters from TV Observer Markov Parameters
+    hkio = getTVObserverGainMarkovParametersFromTVObserverMarkovParameters(hki_observer2, observer_order)
+
+    return D, hki, hkio, hki_observer1, hki_observer2
+
+
+
+
