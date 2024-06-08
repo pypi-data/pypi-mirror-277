9ef8854fd5aab228bdd8235b292cad6eaa425a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:06:57 +0000

    Add package py-ce-forms-api/0.0.8/py_ce_forms_api-0.0.8-py3-none-any.whl

---
+++ packages/py-ce-forms-api/py_ce_forms_api-0.0.8-py3-none-any.whl/py_ce_forms_api/__init__.py
@@ -0,0 +1,9 @@
+from .api import APIClient
+from .form import Form, FormBlock
+from .query import FormsQuery, FormMutate, FormsRes
+from .accounts import Accounts
+from .client import CeFormsClient
+from .assets import Assets
+from .processing import Processing, Task, TaskPool
+
+__title__ = 'py_ce_forms_api'
