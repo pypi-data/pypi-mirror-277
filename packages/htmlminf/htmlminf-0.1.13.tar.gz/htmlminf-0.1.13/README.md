24eb135b71a69b11852d8c67c51b0db2df17d2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 06:04:33 +0000

    Add package htmlminf/0.1.13/htmlminf-0.1.13.tar.gz

---
+++ packages/htmlminf/htmlminf-0.1.13.tar.gz/htmlminf-0.1.13/README.md
@@ -0,0 +1,9 @@
+**NOTE: This is a fork of htmlmin**
+
+A configurable HTML Minifier with safety features.
+
+Documentation: https://htmlmin.readthedocs.io/en/latest/
+
+<a href='https://github.com/mrf345/htmlminf/actions/workflows/ci.yml'>
+  <img src='https://github.com/mrf345/htmlminf/actions/workflows/ci.yml/badge.svg'>
+</a>
---
+++ packages/htmlminf/htmlminf-0.1.13.tar.gz/htmlminf-0.1.13/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+from htmlmin import __version__
+
+here = os.path.dirname(__file__)
+
+README = open(os.path.join(here, 'README.md')).read()
+LICENSE = open(os.path.join(here, 'LICENSE')).read()
+
+setup(
+    name='htmlminf',
+    version=__version__,
+    license='BSD',
+    description='An HTML Minifier',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://htmlmin.readthedocs.io/en/latest/',
+    download_url='https://github.com/mrf345/htmlminf',
+    author='Mohamed Feddad',
+    author_email='mrf345@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    test_suite='htmlmin.tests.tests.suite',
+    install_requires=[],
+    tests_require=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+    entry_points={
+        'console_scripts': [
+            'htmlmin = htmlmin.command:main',
+        ],
+    },
+)
