3b9cf06ec7f55b3a7f203b00f499af7cbc8a5b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 06:04:30 +0000

    Add package luxos/0.1.0b35/luxos-0.1.0b35-py3-none-any.whl

---
+++ packages/luxos/luxos-0.1.0b35-py3-none-any.whl/luxos/exceptions.py
@@ -0,0 +1,34 @@
+import asyncio
+
+
+class LuxosBaseException(Exception):
+    pass
+
+
+class MinerConnectionError(LuxosBaseException):
+    def __init__(self, host: str, port: int, *args, **kwargs):
+        super().__init__(host, port, *args, **kwargs)
+        self.address = (host, port)
+
+    def __str__(self):
+        msg = "unknown reason"
+        if getattr(self, "__cause__"):
+            msg = repr(self.__cause__)
+        elif self.args[2:]:
+            msg = str(self.args[2])
+        return (
+            f"<{self.address[0]}:{self.address[1]}>: {self.__class__.__name__}, "
+            f"{msg}"
+        )
+
+
+class MinerCommandTimeoutError(MinerConnectionError, asyncio.TimeoutError):
+    pass
+
+
+class MinerCommandSessionAlreadyActive(MinerConnectionError):
+    pass
+
+
+class MinerCommandMalformedMessageError(MinerConnectionError):
+    pass
