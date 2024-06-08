d980d0eba96814a7a3614d6934a8de9c990085a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:04:40 +0000

    Add package dcnum/0.23.1/dcnum-0.23.1.tar.gz

---
+++ packages/dcnum/dcnum-0.23.1.tar.gz/dcnum-0.23.1/src/dcnum/segm/segm_torch/__init__.py
@@ -0,0 +1,19 @@
+import importlib
+
+try:
+    torch = importlib.import_module("torch")
+    req_maj = 2
+    req_min = 3
+    ver_tuple = torch.__version__.split(".")
+    act_maj = int(ver_tuple[0])
+    act_min = int(ver_tuple[1])
+    if act_maj < req_maj or (act_maj == req_maj and act_min < req_min):
+        raise ValueError(f"Your PyTorch version {act_maj}.{act_min} is not "
+                         f"supported, please update to at least "
+                         f"{req_maj}.{req_min}")
+except ImportError:
+    pass
+else:
+    from .segm_torch_mpo import SegmentTorchMPO  # noqa: F401
+    if torch.cuda.is_available():
+        from .segm_torch_sto import SegmentTorchSTO  # noqa: F401
