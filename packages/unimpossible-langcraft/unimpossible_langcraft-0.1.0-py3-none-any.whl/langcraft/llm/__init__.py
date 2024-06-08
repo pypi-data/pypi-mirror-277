a874e53214eb27c02b7120449bcca35a315195cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:05:24 +0000

    Add package unimpossible-langcraft/0.1.0/unimpossible_langcraft-0.1.0-py3-none-any.whl

---
+++ packages/unimpossible-langcraft/unimpossible_langcraft-0.1.0-py3-none-any.whl/langcraft/llm/__init__.py
@@ -0,0 +1,18 @@
+from langcraft.llm.llm_models import LLMs
+from langcraft.llm.llm_action import (
+    CompletionBrief,
+    CompletionResult,
+    Image,
+    Message,
+    MessageRole,
+    UserConversationTurn,
+    AssistantConversationTurn,
+    CompletionAction,
+    ActionDescriptor,
+    LLMAction,
+    Actions
+)
+from langcraft.llm.claude import ClaudeCompletionAction
+from langcraft.llm.gpt import GPTCompletionAction
+from langcraft.llm.gemini import GeminiCompletionAction
+
