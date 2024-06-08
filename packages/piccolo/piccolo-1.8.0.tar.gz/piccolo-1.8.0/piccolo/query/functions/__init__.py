e0ef08a6b9b7f4219a67599ca0d45e0d8a2c640f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 06:04:23 +0000

    Add package piccolo/1.8.0/piccolo-1.8.0.tar.gz

---
+++ packages/piccolo/piccolo-1.8.0.tar.gz/piccolo-1.8.0/piccolo/query/functions/__init__.py
@@ -0,0 +1,18 @@
+from .aggregate import Avg, Count, Max, Min, Sum
+from .string import Length, Lower, Ltrim, Reverse, Rtrim, Upper
+from .type_conversion import Cast
+
+__all__ = (
+    "Avg",
+    "Cast",
+    "Count",
+    "Length",
+    "Lower",
+    "Ltrim",
+    "Max",
+    "Min",
+    "Reverse",
+    "Rtrim",
+    "Sum",
+    "Upper",
+)
