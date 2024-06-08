a0ed74e47660c2c4fb770e494c7e90bfd8c2e773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:04:54 +0000

    Add package person-linkage-case-study/0.1.0/person_linkage_case_study-0.1.0-py3-none-any.whl

---
+++ packages/person-linkage-case-study/person_linkage_case_study-0.1.0-py3-none-any.whl/person_linkage_case_study_utils/__about__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "person_linkage_case_study_utils"
+__summary__ = "Utilities for the UW person linkage case study."
+__uri__ = "https://github.com/ihmeuw/person_linkage_case_study"
+
+__version__ = "0.0.1"
+
+__author__ = "IHME Simulation Science Team"
+__email__ = "zmbc@uw.edu"
+
+__license__ = "BSD-3-Clause"
+__copyright__ = f"Copyright 2022 {__author__}"
