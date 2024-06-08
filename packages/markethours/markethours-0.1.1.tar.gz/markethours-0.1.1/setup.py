7f9d9f82e90cfa5c1df1155680ceb9bd28986635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 06:04:32 +0000

    Add package markethours/0.1.1/markethours-0.1.1.tar.gz

---
+++ packages/markethours/markethours-0.1.1.tar.gz/markethours-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('README.rst', 'r') as f:
+  long_description = f.read()
+
+setuptools.setup(
+  name = 'markethours',
+  version = '0.1.1',
+  author = 'Mason Krause',
+  description = 'A python library for referencing and localizing US stock trading hours',
+  long_description = long_description,
+  packages = setuptools.find_packages(),
+  include_package_data = True,
+  python_requires = '>=3.7',
+  install_requires = [
+    'requests',
+    'pytz'])
