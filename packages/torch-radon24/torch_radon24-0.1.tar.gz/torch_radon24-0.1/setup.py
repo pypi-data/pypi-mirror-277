a59013ae86156912d2469a4697a42dab48980cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 04:05:19 +0000

    Add package torch-radon24/0.1/torch_radon24-0.1.tar.gz

---
+++ packages/torch-radon24/torch_radon24-0.1.tar.gz/torch_radon24-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name="torch_radon24",
+    version="0.1",
+    description="Radon Transformation for Pytorch 2.0 package",
+    author="Minh Nhat Trinh",
+    license="GNU GENERAL PUBLIC LICENSE",
+    packages=["torch_radon24"],
+    install_requires=[
+        "torch>=2.0.0",
+        "numpy>=1.21.0",
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
