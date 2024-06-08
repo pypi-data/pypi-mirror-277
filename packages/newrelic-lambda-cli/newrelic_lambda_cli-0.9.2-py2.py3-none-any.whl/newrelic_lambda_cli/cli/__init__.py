468b9ed467acb0ae54596d1653d94745b087f791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:07:02 +0000

    Add package newrelic-lambda-cli/0.9.2/newrelic_lambda_cli-0.9.2-py2.py3-none-any.whl

---
+++ packages/newrelic-lambda-cli/newrelic_lambda_cli-0.9.2-py2.py3-none-any.whl/newrelic_lambda_cli/cli/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import click
+
+from newrelic_lambda_cli.cli import functions, integrations, layers, subscriptions
+
+
+@click.group()
+@click.version_option()
+@click.option("--verbose", "-v", help="Increase verbosity", is_flag=True)
+@click.pass_context
+def cli(ctx, verbose):
+    ctx.ensure_object(dict)
+    ctx.obj["VERBOSE"] = verbose
+
+
+def register_groups(group):
+    functions.register(group)
+    integrations.register(group)
+    layers.register(group)
+    subscriptions.register(group)
+
+
+def main():
+    register_groups(cli)
+    cli()
