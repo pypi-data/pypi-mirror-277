8af1121d48b7936a6388acce0fc5dc4642289407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:05:10 +0000

    Add package csvtorasa/0.1.1/csvtorasa-0.1.1.tar.gz

---
+++ packages/csvtorasa/csvtorasa-0.1.1.tar.gz/csvtorasa-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="csvtorasa",
+    version="0.1.1",
+    author='Lohith Arcot',
+    author_email='lohith.arcot@gmail.com',
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pandas>=2.2.2",
+    ],
+    entry_points={
+        # "console_scripts":[
+        #     "csvtorasa = csvtorasa:create_rasa_files"
+        # ]
+    },
+)
