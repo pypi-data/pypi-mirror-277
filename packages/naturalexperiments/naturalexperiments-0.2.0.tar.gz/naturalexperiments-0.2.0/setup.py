ddf4f8d78f880e5b96046f45be3c0c28dd56ff11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 05:04:50 +0000

    Add package naturalexperiments/0.2.0/naturalexperiments-0.2.0.tar.gz

---
+++ packages/naturalexperiments/naturalexperiments-0.2.0.tar.gz/naturalexperiments-0.2.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Load required packages from requirements.txt
+
+try:
+    with open('requirements.txt') as f:
+        required = f.read().splitlines()
+except FileNotFoundError:
+    required = []
+
+setuptools.setup(
+    name="naturalexperiments",
+    version="0.2.0",
+    author="R. Teal Witter",
+    author_email="rtealwitter@gmail.com",
+    description="Estimators and datasets for treatment effect estimation in natural experiments.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rtealwitter/naturalexperiments",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=required,
+)
