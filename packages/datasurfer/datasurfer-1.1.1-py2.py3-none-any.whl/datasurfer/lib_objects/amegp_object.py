49870977a6b2804ada5ed075b7216127fd1e5b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:06:19 +0000

    Add package datasurfer/1.1.1/datasurfer-1.1.1-py2.py3-none-any.whl

---
+++ packages/datasurfer/datasurfer-1.1.1-py2.py3-none-any.whl/datasurfer/lib_objects/amegp_object.py
@@ -0,0 +1,156 @@
+#%% Import Libraries
+
+import re
+import pandas as pd
+from pathlib import Path
+from datasurfer.datainterface import DataInterface
+
+
+#%% AMEGP_OJBECT
+class AMEGPObject(DataInterface):
+    """
+    Represents an object for handling AMEGP data.
+
+    Args:
+        path (str): The path to the data file.
+        config (dict): Configuration settings for the object.
+        name (str): The name of the object.
+        comment (str): Additional comment for the object.
+
+    Attributes:
+        path (str): The path to the data file.
+        config (dict): Configuration settings for the object.
+        name (str): The name of the object.
+        comment (str): Additional comment for the object.
+        df (pd.DataFrame): The data stored in a pandas DataFrame.
+
+    Methods:
+        __init__(self, path=None, config=None, name=None, comment=None):
+            Initializes a new instance of the AMEGPObject class.
+        __setitem__(self, name, value):
+            Sets the value of a specific item in the object.
+        get_df(self):
+            Retrieves the data as a transposed DataFrame.
+        set_value(self, name, value):
+            Sets the value of a specific item in the DataFrame.
+        save(self, name=None):
+            Saves the object's data to a file.
+
+    """
+    exts = ['.amegp']
+    
+    def __init__(self, path=None, name=None, comment=None):
+        """
+        Initializes a new instance of the AMEGPObject class.
+
+        Args:
+            path (str): The path to the data file.
+            config (dict): Configuration settings for the object.
+            name (str): The name of the object.
+            comment (str): Additional comment for the object.
+
+        """
+        super().__init__(path, name=name, comment=comment)     
+        
+    @property
+    def stem(self):
+        
+        r = re.compile(r'(.+)(\.amegp.*)')
+        return r.match(self.path.name).group(1)
+    
+    @property
+    def ext(self):
+        
+        r = re.compile(r'(.+)(\.amegp.*)')
+        return r.match(self.path.name).group(2)      
+    
+    @property
+    def ext_idx(self):
+        r = re.compile(r'(.+)\.amegp[.]{0,1}(.*)')     
+        return r.match(self.path.name).group(2) 
+         
+    @property
+    def name(self):
+        if self._name is None:
+            
+            assert self.path is not None, 'Expect a name for data object.'
+            return self.stem+self.ext_idx
+        else:
+            return self._name
+    @name.setter
+    def name(self, value):  
+        self._name = value
+
+    
+    def __setitem__(self, name, value):
+        """
+        Sets the value of a specific item in the object.
+
+        Args:
+            name (str): The name of the item.
+            value: The value to be set.
+
+        """
+        self.set_value(name, value)
+        
+    def get_df(self):
+        """
+        Retrieves the data as a transposed DataFrame.
+
+        Returns:
+            pd.DataFrame: The data stored in a pandas DataFrame.
+
+        """
+        df = pd.read_xml(self.path)
+        df.set_index('VARNAME', inplace=True)
+        return df.transpose()
+    
+    def set_value(self, name, value):
+        """
+        Sets the value of a specific item in the DataFrame.
+
+        Args:
+            name (str): The name of the item.
+            value: The value to be set.
+
+        Returns:
+            self: The updated AMEGPObject instance.
+
+        """
+        self.df.at['VALUE', name] = value
+        return self
+    
+    def save(self, name=None):
+        """
+        Saves the object's data to a file.
+
+        Args:
+            name (str, optional): The name of the file to save the data to. If not provided, the original file will be overwritten.
+
+        Returns:
+            self: The AMEGPObject instance.
+
+        """
+        name = name or self.path
+        
+        with open(self.path, 'r') as fobj:
+            lines = fobj.readlines()
+        
+        newlines = []
+        
+        for l in lines:
+            if re.match(r'^<VARNAME>.+</VARNAME>$', l.strip()):
+                varname, = re.findall(r'^<VARNAME>(.+)</VARNAME>$', l.strip())
+                item = self.df[varname]
+                
+            if re.match(r'^<VALUE>.+</VALUE>$', l.strip()):  
+                str_old, = re.findall(r'^<VALUE>.+</VALUE>$', l.strip())
+                str_new = '<VALUE>{}</VALUE>'.format(item['VALUE'])
+                l = l.replace(str_old, str_new, 1)
+                    
+            newlines.append(l)
+            
+        with open(name, 'w') as fobj:
+            fobj.writelines(newlines)
+            
+        return self
