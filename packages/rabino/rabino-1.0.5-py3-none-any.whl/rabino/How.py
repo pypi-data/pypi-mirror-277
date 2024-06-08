06454806df0dc891bd80fbdbd09d1db67c25ac53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 04:05:30 +0000

    Add package rabino/1.0.5/rabino-1.0.5-py3-none-any.whl

---
+++ packages/rabino/rabino-1.0.5-py3-none-any.whl/rabino/How.py
@@ -0,0 +1,26 @@
+#Copyright (C) 2022 X CODER
+from time import sleep
+
+class color:
+      red = '\033[91m'
+      green = '\033[92m'
+      blue = '\033[94m'
+      yellow = '\033[93m'
+      magenta = '\033[95m'
+      cyan = '\033[96m'
+      white = '\033[97m'
+      bold = '\033[1m'
+      underline = '\033[4m'
+      black='\033[30m'
+
+class chup:
+      x_coder = f"""{color.bold}
+{color.white} [⏦] - {color.cyan} 𝐑𝐚𝐁𝐢𝐍𝐎 {color.yellow} 𝐕𝐄𝐑𝐒𝐈𝐎𝐍  {color.red}1.0.4   
+
+{color.white} [⏦] - {color.cyan} 𝐑𝐚𝐛𝐢𝐧𝐨 {color.yellow}𝐂𝐎𝐏𝐘𝐑𝐈𝐆𝐇𝐓 (𝐂) {color.red} 𝟐𝟎𝟐4 {color.green}𝐌𝐀𝐌𝐀𝐃 𝐂𝐎𝐃𝐄𝐑   
+
+{color.white} [⏦] - {color.cyan} 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑  {color.yellow} 𝐑𝐔𝐁𝐈𝐊𝐀 {color.red} 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 : {color.green} @python_java_source
+
+{color.white}╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾
+"""
+      print(x_coder)
