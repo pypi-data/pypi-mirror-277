a2f78f7c5a9ae44ed295073cc36af620de17e68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 06:04:47 +0000

    Add package metalgpu/0.5.9/metalgpu-0.5.9-py3-none-any.whl

---
+++ packages/metalgpu/metalgpu-0.5.9-py3-none-any.whl/metalgpu/test.py
@@ -0,0 +1,40 @@
+from interface import Interface
+
+instance = Interface()  # Initialise the metal instance
+shader_string = """
+#include <metal_stdlib>
+
+using namespace metal;
+
+kernel void adder(device int *arr1 [[buffer(0)]],
+        device int *arr2 [[buffer(1)]],
+        device int *arr3 [[buffer(2)]],
+        uint id [[thread_position_in_grid]]) {
+    arr3[id] = arr2[id] + arr1[id];
+}
+"""
+# Note: For clearer code, use instance.load_shader(shaderPath) to load a metal file
+
+instance.load_shader_from_string(shader_string)
+instance.set_function("adder")
+
+buffer_size = 10  # Number of items in the buffer
+buffer_type = "int"
+
+initial_array = [i for i in range(buffer_size)]
+
+buffer1 = instance.array_to_buffer(initial_array)
+buffer2 = instance.array_to_buffer(initial_array)
+
+print(buffer1.contents)
+print(buffer2.contents)
+
+buffer3 = buffer1 * buffer2
+
+print(buffer3.contents)
+
+buffer4 = instance.create_buffer(buffer_size, buffer_type)
+
+instance.run_function(buffer_size, [buffer1, buffer2, buffer4])
+
+print(buffer4.contents)
