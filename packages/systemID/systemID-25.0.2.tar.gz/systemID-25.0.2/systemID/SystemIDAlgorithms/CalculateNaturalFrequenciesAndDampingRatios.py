f8f04eb5ee72b399b2aefdf5749d25f7e5fd3e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 06:04:40 +0000

    Add package systemID/25.0.2/systemID-25.0.2.tar.gz

---
+++ packages/systemID/systemID-25.0.2.tar.gz/systemID-25.0.2/systemID/SystemIDAlgorithms/CalculateNaturalFrequenciesAndDampingRatios.py
@@ -0,0 +1,48 @@
+"""
+Author: Damien GUEHO
+Copyright: Copyright (C) 2023 Damien GUEHO
+License: Public Domain
+Version: 24
+Date: April 2022
+Python: 3.7.7
+"""
+
+
+import numpy as np
+import scipy.linalg as LA
+
+
+def calculateNaturalFrequenciesAndDampingRatios(systems):
+    """
+    Purpose:
+        Compute the natural frequencies and associated damping ratios from the system matrix A of a list of systems.
+
+
+    Parameters:
+        - **systems** (``list``): list of systems.
+
+    Returns:
+        - **natural_frequencies** (``list``): list of natural frequencies.
+        - **damping_ratios** (``list``): list of damping ratios.
+
+    Imports:
+        - ``import numpy as np``
+        - ``import scipy.linalg as LA``
+
+    Description:
+
+
+    See Also:
+        -
+    """
+
+    natural_frequencies = []
+    damping_ratios = []
+
+    for system in systems:
+        dt = system.dt
+        all_natural_frequencies = np.imag(np.diag(LA.logm(np.diag(LA.eig(system.A(0))[0])) / dt))
+        natural_frequencies.append(all_natural_frequencies[all_natural_frequencies >= 0])
+        damping_ratios.append(np.real(np.flip(np.diag(LA.logm(np.diag(LA.eig(system.A(0))[0])) / dt)))[::2])
+
+    return natural_frequencies, damping_ratios
---
+++ packages/systemID/systemID-25.0.2.tar.gz/systemID-25.0.2/systemID/SystemIDAlgorithms/GetMarkovParameters.py
@@ -0,0 +1,56 @@
+"""
+Author: Damien GUEHO
+Copyright: Copyright (C) 2023 Damien GUEHO
+License: Public Domain
+Version: 24
+Date: April 2022
+Python: 3.7.7
+"""
+
+
+import numpy as np
+
+
+def getMarkovParameters(A, B, C, D, number_steps):
+    """
+        Purpose:
+            This program computes a list comprised of the first :math:`p` time-invariant Markov parameters.
+
+        Parameters:
+            - **A** (``fun``): the system matrix.
+            - **B** (``fun``): the input influence matrix.
+            - **C** (``fun``): the output influence matrix.
+            - **D** (``fun``): the direct transmission matrix.
+            - **number_steps** (``int``): the number :math:`p` of time-invariant Markov parameters to compute.
+
+        Returns:
+            - **markov_parameters** (``list``): a list of the first :math:`p` time-invariant Markov parameters.
+
+        Imports:
+            - ``import numpy as np``
+
+        Description:
+            The list of the first :math:`p` time-invariant initial condition Markov parameters is
+
+            .. math::
+
+                \\left[D, CB, CAB, CA^2B, \\cdots, CA^{p-2}B\\right].
+
+
+        See Also:
+            - :py:mod:`~SystemIDAlgorithms.GetInitialConditionResponseMarkovParameters.getInitialConditionResponseMarkovParameters`
+    """
+
+    A_mat = A(0)
+    B_mat = B(0)
+    C_mat = C(0)
+    D_mat = D(0)
+
+    markov_parameters = [D_mat]
+    temp = C_mat
+
+    for i in range(1, number_steps):
+        markov_parameters.append(np.matmul(temp, B_mat))
+        temp = np.matmul(temp, A_mat)
+
+    return markov_parameters
