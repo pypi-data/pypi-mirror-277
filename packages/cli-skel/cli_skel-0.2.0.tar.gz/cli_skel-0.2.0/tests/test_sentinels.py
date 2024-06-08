ee471e8125b06bfa4d09c1eac06b0d03b06af16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 04:05:14 +0000

    Add package cli-skel/0.2.0/cli_skel-0.2.0.tar.gz

---
+++ packages/cli-skel/cli_skel-0.2.0.tar.gz/cli_skel-0.2.0/tests/test_sentinels.py
@@ -0,0 +1,31 @@
+import unittest
+from copy import copy, deepcopy
+
+from cli_skel.utils.sentinels import MissingType
+
+
+class TestSentinels(unittest.TestCase):
+    def _test_missing_obj(self, missing: MissingType):
+        self.assertTrue(missing == MissingType(missing.name))
+        self.assertTrue(missing is MissingType(missing.name))
+        self.assertTrue(missing == copy(MissingType(missing.name)))
+        self.assertTrue(missing is copy(MissingType(missing.name)))
+        self.assertTrue(missing == deepcopy(MissingType(missing.name)))
+        self.assertTrue(missing is deepcopy(MissingType(missing.name)))
+
+    def test_missing_basics(self):
+        self._test_missing_obj(MissingType())
+
+    def test_missing_named(self):
+        missing1 = MissingType('hello')
+        self._test_missing_obj(missing1)
+
+        missing2 = MissingType('world')
+        self._test_missing_obj(missing2)
+
+        self.assertTrue(missing1 is not missing2)
+        self.assertTrue(missing1 != missing2)
+        self.assertTrue(missing1 is not copy(missing2))
+        self.assertTrue(missing1 != copy(missing2))
+        self.assertTrue(missing1 is not deepcopy(missing2))
+        self.assertTrue(missing1 != deepcopy(missing2))
