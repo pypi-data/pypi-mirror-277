6ee1ecc5606d743a90fff11b9e997e12d951d782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:06:18 +0000

    Add package cornsnake/0.0.64/cornsnake-0.0.64-py3-none-any.whl

---
+++ packages/cornsnake/cornsnake-0.0.64-py3-none-any.whl/cornsnake/zip_dir.py
@@ -0,0 +1,23 @@
+"""
+Function for creating a zip archive from a directory. The `create_zip` function creates a zip archive of a specified directory.
+
+[Documentation](http://docs.mrseanryan.cornsnake.s3-website-eu-west-1.amazonaws.com/cornsnake/zip_dir.html)
+"""
+
+import shutil
+
+
+def create_zip(source_dir, output_zip_file):
+    """
+    Function to create a zip archive from a directory.
+
+    Args:
+    source_dir (str): The path to the source directory to be zipped.
+    output_zip_file (str): The name of the output zip file.
+
+    Returns:
+    None
+    """
+    if output_zip_file.endswith(".zip"):
+        output_zip_file = output_zip_file[:-4]
+    shutil.make_archive(output_zip_file, "zip", source_dir)
