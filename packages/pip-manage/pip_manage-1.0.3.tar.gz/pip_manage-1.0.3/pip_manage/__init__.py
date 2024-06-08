399eb704914d912a8ce488aac6623715c6b55392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 06:04:26 +0000

    Add package pip-manage/1.0.3/pip_manage-1.0.3.tar.gz

---
+++ packages/pip-manage/pip_manage-1.0.3.tar.gz/pip_manage-1.0.3/pip_manage/__init__.py
@@ -0,0 +1,8 @@
+"""pip-manage lets you smoothly manage your installed packages."""
+
+from __future__ import annotations
+
+__version__: Final[str] = "1.0.3"
+__title__: Final[str] = "pip-manage"
+
+from typing import Final
