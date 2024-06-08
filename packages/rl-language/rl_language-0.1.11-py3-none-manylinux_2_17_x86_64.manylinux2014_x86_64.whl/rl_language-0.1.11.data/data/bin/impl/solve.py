a88c34094f5c436ef21642b8485f138970af2217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:06:18 +0000

    Add package rl-language/0.1.11/rl_language-0.1.11-py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/rl-language/rl_language-0.1.11-py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/rl_language-0.1.11.data/data/bin/impl/solve.py
@@ -0,0 +1,41 @@
+#
+# This file is part of the RLC project.
+#
+# RLC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 2 as published by the Free Software Foundation.
+#
+# RLC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with RLC. If not, see <https://www.gnu.org/licenses/>.
+#
+import argparse
+import random
+from loader import Simulation, compile
+from shutil import which
+from ml.raylib.environment import RLCEnvironment
+from command_line import load_simulation_from_args, make_rlc_argparse
+
+
+def main():
+    parser = make_rlc_argparse("solve", description="runs a action of the simulation")
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=str,
+        nargs="?",
+        help="path where to write the output",
+        default="",
+    )
+
+    args = parser.parse_args()
+    with load_simulation_from_args(args) as sim:
+        env = RLCEnvironment(wrapper_path=sim.wrapper_path, output=args.output)
+        while True:
+            obs, reward, done, truncated, info = env.step(
+                [random.choice(env.legal_actions_indicies()) for i in range(env.num_agents)]
+            )
+            if done["__all__"] or truncated["__all__"]:
+                break
+
+
+if __name__ == "__main__":
+    main()
