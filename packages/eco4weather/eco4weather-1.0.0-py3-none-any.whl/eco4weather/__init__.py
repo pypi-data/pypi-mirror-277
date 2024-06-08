94c573f7e0122f8f204be47deb7a3a498da90254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 03:05:04 +0000

    Add package eco4weather/1.0.0/eco4weather-1.0.0-py3-none-any.whl

---
+++ packages/eco4weather/eco4weather-1.0.0-py3-none-any.whl/eco4weather/__init__.py
@@ -0,0 +1,12 @@
+__author__ = "Olivier Pannekoucke"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Olivier Pannekoucke"]
+__license__ = "CeCILL-B"
+__version__ = "1.0"
+__maintainer__ = "Olivier Pannekoucke"
+__email__ = "olivier.pannekoucke@meteo.fr"
+__status__ = "Dev."
+
+
+from .socialplanner import SocialPlanner
+from .hoel import *
